69cad4fda6c4580ae1ade72a8267faad1a1bdf74
Kika Zahonova <kika.zahonova@gmail.com>
2018-01-25 19:28:43 +0100

    bla

--- bash_scripts/orthofinder.sh
+++ bash_scripts/orthofinder.sh
@@ -5,5 +5,5 @@ new_prot=''
 prev_blast='/home/anzhelika/orthofinder_dataset_for_Anna/orthofinder/Results_Jan03/WorkingDirectory/'
 # blast_method=diamond
 
-$of -f $new_prot -b $prev_blast -t 32
+/home/nenarokova/tools/OrthoFinder-2.1.2/orthofinder -f $new_prot -b $prev_blast -t 32
 # -S $blast_method
