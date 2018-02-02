410e60e4f615aa682c362f5c5e0f70239b356174
Kika Zahonova <kika.zahonova@gmail.com>
2018-02-02 14:29:26 +0100

    bla

--- bash_scripts/orthofinder.sh
+++ bash_scripts/orthofinder.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # of='/home/serafim/tools/OrthoFinder-2.1.2_source/orthofinder/orthofinder.py'
-new_prot='/media/4TB1/blastocrithidia/predicted_proteins/'
+# new_prot='/media/4TB1/blastocrithidia/predicted_proteins/'
 prev_blast='/media/4TB1/blastocrithidia/orthofinder/Results_Jan03/WorkingDirectory/'
 # blast_method=diamond
 
-orthofinder -f $new_prot -b $prev_blast -t 32
-# -S $blast_method
+orthofinder -b $prev_blast -t 32
+# -S $blast_method -f $new_prot
