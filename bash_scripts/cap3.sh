a5527522151917ea92a1d66e371f378c43979ae8
Kika Zahonova <kika.zahonova@gmail.com>
2018-02-03 09:27:49 +0100

    bla

--- bash_scripts/cap3.sh
+++ bash_scripts/cap3.sh
@@ -4,4 +4,4 @@ cap3='/home/kika/tools/CAP3/cap3'
 infile='/home/kika/diplonema/genome_assembly/1604_cap3/1604_DNA_scaffolds_filtered.fasta'
 outfile='/home/kika/diplonema/genome_assembly/1604_cap3/1604_DNA_scaffolds_filtered_cap3.out'
 
-cap3 $infile -p 99 -o 150  > $outfile
+cap3 $infile -p 99 -o 150 > $outfile
