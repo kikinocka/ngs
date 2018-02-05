44e64b7de4e1e2488de29e7b7b3a2d470ec0ff88
Kika Zahonova <kika.zahonova@gmail.com>
2018-02-05 14:22:44 +0100

    bla

--- bash_scripts/hmmsearch.sh
+++ bash_scripts/hmmsearch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sdir='/home/kika/MEGAsync/blasto_project/genes/repair/NHEJ/ku_pfam/'
-hmm_prof=$sdir'ku_profile.hmm'
+sdir='/home/kika/MEGAsync/blasto_project/genes/repair/NHEJ/ku80_hmmer/'
+hmm_prof=$sdir'ku80_profile.hmm'
 db='/home/kika/MEGAsync/blasto_project/genome_assembly/jaculum_scaffolds_transc_translated.fasta'
-output=$sdir'ku_search_jac.out'
+output=$sdir'ku80_search_jac.out'
 threads=4
 
 hmmsearch -o $output --cpu $threads $hmm_prof $db
