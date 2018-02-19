6a7699e76c6a7b583f17d959a018bd337408ce51
KikaZah <kika.zahonova@gmail.com>
2018-02-19 14:16:29 +0100

    bla

--- py_scripts/run_pasta.py
+++ py_scripts/run_pasta.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import os
 
-os.chdir('/media/4TB1/blastocrithidia/orthofinder/sc_ogs/')
+os.chdir('/media/4TB1/blastocrithidia/orthofinder/sc_ogs/stops_replaced/')
 files = os.listdir()
 
 for file in files:
-	# if '.fa' in file:
-	if '.aln' not in file:
+	if '.fa' in file:
+	# if '.aln' not in file:
 		print(file)
-		job_desc = file.split('.fa')[0]
+		job_desc = file.split('_replaced.fa')[0]
 		d = 'protein'
 		os.system('run_pasta.py -i {} -d {} -j {}'.format(file, d, job_desc))
