ae5b027e85e159c3d76e50ca3dc23819fb9a8ba7
KikaZah <kika.zahonova@gmail.com>
2018-02-19 14:10:12 +0100

    bla

--- py_scripts/replace.py
+++ py_scripts/replace.py
@@ -2,10 +2,22 @@
 import os
 from Bio import SeqIO
 
-fasta = SeqIO.parse('/home/kika/MEGAsync/blasto_project/reference_tryps_proteoms/TriTrypDB-35_TbruceiTREU927_AnnotatedProteins_2.fasta', 'fasta')
-out = open('/home/kika/MEGAsync/blasto_project/reference_tryps_proteoms/TriTrypDB-35_TbruceiTREU927_AnnotatedProteins_3.fasta', 'w')
+os.chdir('/home/kika/MEGAsync/blasto_project/orthofinder/sc_ogs/')
+files = os.listdir()
 
-for contig in fasta:
-	contig.seq = str(contig.seq).replace('-', '')
-	out.write('>{}\n{}\n'.format(contig.description, contig.seq))
-out.close
+for file in files:
+	name = file.split('.fa')[0]
+	proteins = SeqIO.parse(file, 'fasta')
+	print(name)
+	replaced = open('{}_replaced.fa'.format(name), 'w')
+	notes = open('{}_notes.txt'.format(name), 'w')
+	for protein in proteins:
+		position = protein.seq.find('*')
+		if position == -1:
+			replaced.write('>{}\n{}\n'.format(protein.description, protein.seq))
+		elif position == len(protein.seq) - 1:
+			replaced.write('>{}\n{}\n'.format(protein.description, protein.seq[:-1]))
+			notes.write('{}\tend\n'.format(protein.name))
+		else:
+			replaced.write('>{}\n{}\n'.format(protein.description, str(protein.seq).replace('*', 'X')))
+			notes.write('{}\t{}\n'.format(protein.name, position+1))
