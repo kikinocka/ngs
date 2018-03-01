a05d7797d64a4a9f1af47e4609f1ef1361f6364f
KikaZah <kika.zahonova@gmail.com>
2018-03-01 11:25:20 +0100

    bla

--- py_scripts/replace.py
+++ py_scripts/replace.py
@@ -7,21 +7,22 @@ os.chdir('/media/4TB1/blastocrithidia/orthofinder/other_ogs/')
 files = os.listdir()
 
 for file in files:
-	name = file.split('.fa')[0]
-	proteins = SeqIO.parse(file, 'fasta')
-	print(name)
-	replaced = open('{}_replaced.fa'.format(name), 'w')
-	notes = open('{}_notes.txt'.format(name), 'w')
-	for protein in proteins:
-		position = protein.seq.find('*')
-		if position == -1:
-			replaced.write('>{}\n{}\n'.format(protein.description, protein.seq))
-		elif position == len(protein.seq) - 1:
-			replaced.write('>{}\n{}\n'.format(protein.description, protein.seq[:-1]))
-			notes.write('{}\tend\n'.format(protein.name))
-		else:
-			replaced.write('>{}\n{}\n'.format(protein.description, str(protein.seq).replace('*', 'X')))
-			notes.write('{}\t{}\n'.format(protein.name, position+1))
+	if file.endswith('.fa'):
+		name = file.split('.fa')[0]
+		proteins = SeqIO.parse(file, 'fasta')
+		print(name)
+		replaced = open('replaced/{}_replaced.fa'.format(name), 'w')
+		notes = open('replaced/{}_notes.txt'.format(name), 'w')
+		for protein in proteins:
+			position = protein.seq.find('*')
+			if position == -1:
+				replaced.write('>{}\n{}\n'.format(protein.description, protein.seq))
+			elif position == len(protein.seq) - 1:
+				replaced.write('>{}\n{}\n'.format(protein.description, protein.seq[:-1]))
+				notes.write('{}\tend\n'.format(protein.name))
+			else:
+				replaced.write('>{}\n{}\n'.format(protein.description, str(protein.seq).replace('*', 'X')))
+				notes.write('{}\t{}\n'.format(protein.name, position+1))
 
 # for file in files:
 # 	if 'fa' in file:
