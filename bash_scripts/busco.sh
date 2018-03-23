2077f616351d9e2abdf05e1c63acb26b4403e4b9
KikaZah <kika.zahonova@gmail.com>
2018-03-23 19:08:35 +0100

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/tomas/GIT/busco
 
-python BUSCO.py -m tran -i /home/kika/work_dir/CA_Trinity.fasta -o /home/kika/work_dir/BUSCO_CA_trinity --species chlamydomonas_reinhardtii -l /home/tomas/GIT/busco/eukaryota_odb9 -c 16 --long -f
+python BUSCO.py -m tran -i /home/kika/work_dir/CA_Trinity.fasta -o BUSCO_CA_trinity --species chlamydomonas_reinhardtii -l /home/tomas/GIT/busco/eukaryota_odb9 -c 16 --long -f
