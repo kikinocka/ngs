5d8b7073a1fa5993030104491f38ccb73330ee90
KikaZah <kika.zahonova@gmail.com>
2018-03-23 19:25:06 +0100

    bla

--- bash_scripts/busco_plot.sh
+++ bash_scripts/busco_plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/tomas/GIT/busco
 
-python BUSCO_plot.py -wd /home/tomas/GIT/busco/run_BUSCO_CA_trinity/ --no_r
+python BUSCO_plot.py -wd /home/tomas/GIT/busco/run_BUSCO_CA_trinity/
