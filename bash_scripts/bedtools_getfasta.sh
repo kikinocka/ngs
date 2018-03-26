ff38d2faf609ed13da6556c4b417065c208b407d
KikaZah <kika.zahonova@gmail.com>
2018-03-26 14:21:44 +0200

    bla

--- bash_scripts/bedtools_getfasta.sh
+++ bash_scripts/bedtools_getfasta.sh
@@ -4,5 +4,4 @@ genome='/media/4TB1/blastocrithidia/genome_assembly/p57_scaffolds.fa'
 gff='/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/p57_ins_renamed.gff'
 out='/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/p57_ins_nt.txt'
 
-bedtools getfasta -s -name -fi $genome -bed $gff 
-#-fo $out
+bedtools getfasta -s -name -fi $genome -bed $gff -fo $out
