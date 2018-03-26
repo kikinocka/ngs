1eaf3a380d1cdc52932677ebeb21e334d48191a0
KikaZah <kika.zahonova@gmail.com>
2018-03-26 16:26:33 +0200

    bla

--- bash_scripts/bedtools_getfasta.sh
+++ bash_scripts/bedtools_getfasta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-genome='/media/4TB1/blastocrithidia/transcriptome_assembly/trinity_denovo/triat_trinity.fasta'
-gff='/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/triat_only_ins_renamed.gff'
-out='/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/triat_ins_nt.txt'
+genome='/media/4TB1/blastocrithidia/transcriptome_assembly/trinity_denovo/blobtools/lhes1/bexlh1_strict.fa'
+gff='/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/bexlh_only_ins_renamed.gff'
+out='/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/bexlh_ins_nt.txt'
 
 bedtools getfasta -s -name -fi $genome -bed $gff -fo $out
