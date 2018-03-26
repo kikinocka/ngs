ec0b584dbede32ce82e94de4dd9dbe7bb6ad80c2
KikaZah <kika.zahonova@gmail.com>
2018-03-26 14:35:19 +0200

    bla

--- py_scripts/keep_intron_gff.py
+++ py_scripts/keep_intron_gff.py
@@ -4,7 +4,10 @@ import os
 os.chdir('/home/kika/MEGAsync/blasto_project/orthofinder/sg_ogs/jac_renamed/')
 gff = open('p57_insertions.gff')
 
-with open('p57_ins_only.gff', 'w') as out:
+with open('p57_only_ins_renamed.gff', 'w') as out:
 	for line in gff:
 		if line.split('\t')[2] == 'intron':
-			out.write(line)
+			og = line.split('\t')[8].split('=')[1].split(';')[0]
+			ins_name = line.split('\t')[8].split('ID=')[1][:-1]
+			new_line = line.replace(line.split('\t')[2], '{} {}'.format(og, ins_name))
+			out.write(new_line)
