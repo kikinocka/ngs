3fd6bfd4820bd990d8d97cc7ec7e2bd63c9526a0
KikaZah <kika.zahonova@gmail.com>
2018-03-27 18:10:37 +0200

    bla

---
+++ py_scripts/number_of_all_seqs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+from Bio import SeqIO
+
+os.chdir('/media/4TB1/blastocrithidia/orthofinder/other_ogs/stops_replaced/')
+files = os.listdir()
+moved = open('one_prot_files.txt', 'w')
+
+count = 0
+for file in files:
+	if file.endswith('replaced2.fa'):
+		for seq in SeqIO.parse(file, 'fasta'):
+			count += 1
+		print('{}\t{}'.format(file, count))
+		if count == 1:
+			moved.write(file + '\n')
+			os.rename('{}'.format(file), 'one_prot_only/{}'.format(file))
+		else:
+			pass
+		count = 0
