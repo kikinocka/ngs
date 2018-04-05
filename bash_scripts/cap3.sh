ba25bcef9085c55112d073877e2b21f9b7f87303
KikaZah <kika.zahonova@gmail.com>
2018-04-05 17:14:36 +0200

    bla

--- bash_scripts/cap3.sh
+++ bash_scripts/cap3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cap3='/home/kika/programs/CAP3/cap3'
+cap3='/home/kika/tools/CAP3/cap3'
 infile='/home/kika/diplonema/genome_assembly/1621/spades/scaffolds.fasta'
 outfile='/home/kika/diplonema/genome_assembly/1621/cap3/1621_cap3.out'
 
