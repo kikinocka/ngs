665a9a906a07360f3ccdc1d5a658e59ffa788b09
KikaZah <kika.zahonova@gmail.com>
2018-04-10 12:59:49 +0200

    bla

--- bash_scripts/tadpole.sh
+++ bash_scripts/tadpole.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-fwd_reads='/home/kika/diplonema/reads/genome/trimmed/YPF1604_trimmed_1.fq.gz'
-rev_reads='/home/kika/diplonema/reads/genome/trimmed/YPF1604_trimmed_2.fq.gz'
-reads='/home/kika/diplonema/genome_assembly/1604/tadpole/1604_extended_reads.fa'
-assembly='/home/kika/diplonema/genome_assembly/1604/tadpole/1604_tadpole.fa'
+fwd_reads='/home/kika/diplonema/reads/genome/trimmed/YPF1608_trimmed_1.fq.gz'
+rev_reads='/home/kika/diplonema/reads/genome/trimmed/YPF1608_trimmed_2.fq.gz'
+reads='/home/kika/diplonema/genome_assembly/1608/tadpole/1608_extended_reads.fa'
+assembly='/home/kika/diplonema/genome_assembly/1608/tadpole/1608_extended_reads.fa'
 
-/home/kika/tools/bbmap/tadpole.sh in=$reads out=$assembly k=62 threads=32
-# in=$fwd_reads in2=$rev_reads mode=extend
+/home/kika/tools/bbmap/tadpole.sh in=$fwd_reads in2=$rev_reads mode=extend out=$assembly k=62 threads=32
+# in=$reads
