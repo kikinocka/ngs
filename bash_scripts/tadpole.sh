edfea9bf1d71926833e4f7398be82b819f0f89be
KikaZah <kika.zahonova@gmail.com>
2018-04-10 12:41:02 +0200

    bla

--- bash_scripts/tadpole.sh
+++ bash_scripts/tadpole.sh
@@ -2,8 +2,8 @@
 
 fwd_reads='/home/kika/diplonema/reads/genome/trimmed/YPF1604_trimmed_1.fq.gz'
 rev_reads='/home/kika/diplonema/reads/genome/trimmed/YPF1604_trimmed_2.fq.gz'
-# reads='/home/kika/diplonema/genome_assembly/1618/tadpole/1618_extended_reads.fa'
-assembly='/home/kika/diplonema/genome_assembly/1604/tadpole/1604_extended_reads.fa'
+reads='/home/kika/diplonema/genome_assembly/1604/tadpole/1604_extended_reads.fa'
+assembly='/home/kika/diplonema/genome_assembly/1604/tadpole/1604_tadpole.fa'
 
-/home/kika/tools/bbmap/tadpole.sh in=$fwd_reads in2=$rev_reads mode=extend out=$assembly k=62 threads=32
-# 
+/home/kika/tools/bbmap/tadpole.sh in=reads out=$assembly k=62 threads=32
+# in=$fwd_reads in2=$rev_reads mode=extend
