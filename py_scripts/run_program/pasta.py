bae44b19e930244c6edbb423cbceda753a32812a
KikaZah <kika.zahonova@gmail.com>
2018-04-19 11:10:59 +0200

    bla

--- py_scripts/run_program/pasta.py
+++ py_scripts/run_program/pasta.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import os
 
-os.chdir('/home/kika/MEGAsync/blasto_project/orthofinder/other_ogs/')
+os.chdir('/media/4TB1/blastocrithidia/api_NOG/apiNOG_raw_algs_single/')
 files = sorted(os.listdir())
 
 for file in files:
-	if file.endswith('_replaced2.fa'):
+	if file.endswith('.fa'):
 	# if '.aln' not in file:
 		print(file)
-		job_desc = file.split('_')[0]
+		job_desc = file.split('.')[1]
 		d = 'protein'
 		os.system('run_pasta.py -i {} -d {} -j {}'.format(file, d, job_desc))
