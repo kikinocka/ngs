1e3c11ecb4d587aa8c9495ee3fd0073e001d3523
KikaZah <kika.zahonova@gmail.com>
2018-05-09 19:52:14 +0200

    bla

--- py_scripts/ins_length.py
+++ py_scripts/ins_length.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
+#check spliting sequence name - lines 15, 16, 18
 import os
 from Bio import SeqIO
 from collections import defaultdict
 
 
-os.chdir('/media/4TB1/blastocrithidia/api_NOG/apiNOG_raw_algs_single/alignments/')
-insertions = SeqIO.parse('api_ins.txt', 'fasta')
-len_results = open('api_ins_len.txt', 'w')
+os.chdir('/media/4TB1/blastocrithidia/orthofinder/sg_ogs/alignments/jac_renamed/')
+insertions = SeqIO.parse('tryp_ins.txt', 'fasta')
+len_results = open('tryp_ins_len.txt', 'w')
 
 len_dict = {}
 for ins in insertions:
 	print(ins.description)
-	if ins.description.split('__')[1].split(' ')[0] in len_dict.keys():
-		len_dict[ins.description.split('__')[1].split(' ')[0]] += int(ins.description.split(' ')[1].split('_')[1])
+	if ins.description.split('_')[0].split(' ')[0] in len_dict.keys():
+		len_dict[ins.description.split('_')[0].split(' ')[0]] += int(ins.description.split(' ')[1].split('_')[1])
 	else:
-		len_dict[ins.description.split('__')[1].split(' ')[0]] = int(ins.description.split(' ')[1].split('_')[1])
+		len_dict[ins.description.split('_')[0].split(' ')[0]] = int(ins.description.split(' ')[1].split('_')[1])
 
 for key, value in len_dict.items():
 	len_results.write('{}\t{}\n'.format(key, value))
