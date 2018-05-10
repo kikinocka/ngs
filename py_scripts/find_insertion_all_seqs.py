2cf2099acba55b750c2dc74f71663846c4466dcf
KikaZah <kika.zahonova@gmail.com>
2018-05-10 10:20:59 +0200

    bla

--- py_scripts/find_insertion_all_seqs.py
+++ py_scripts/find_insertion_all_seqs.py
@@ -2,36 +2,31 @@
 #check spliting sequence name - lines 45, 105, 111
 import os
 from Bio import AlignIO
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
 
-os.chdir('/home/kika/blasto_project/apicomplexans/')
+os.chdir('/media/4TB1/blastocrithidia/api_NOG/apiNOG_raw_algs_single/alignments/')
 files = os.listdir()
-# ins_results = open('api_ins2.txt', 'w')
-# del_results = open('api_del2.txt', 'w')
-# len_results = open('api_prot_len2.txt', 'w')
-# errors = open('api_errors2.txt', 'w')
+ins_results = open('api_ins.txt', 'w')
+del_results = open('api_del.txt', 'w')
+len_results = open('api_prot_len.txt', 'w')
+errors = open('api_errors.txt', 'w')
 
 def find_insertion(aln_file):
 	aln = AlignIO.read(aln_file, 'fasta')
 	result_list = []
-	ins_aln_positions = defaultdict(dict)
+	ins_aln_positions = defaultdict(OrderedDict)
 	for i in range(aln.get_alignment_length()):
-		print(i)
 		column = aln[:, i]
-		print(column)
 		if '-' in column:
 			column_list = [i, column]
-			print(column_list)
 			result_list.append(column_list)
-	print(result_list)
 	for number in range(len(aln)):
-		print(number)
 		for column in result_list:
 			if column[1][number] != '-':
-				print(column)
-	# 			ins_aln_positions[number][column[0]] = 1
-	# 		else:
-	# 			ins_aln_positions[number][column[0]] = 0
+				ins_aln_positions[number][column[0]] = 1
+			else:
+				ins_aln_positions[number][column[0]] = 0
+	return ins_aln_positions
 	# print(ins_aln_positions)
 	# [0 : [position in aln : 0/1], 1 : [position in aln : 0/1], ..., n : [position in aln : 0/1]]
 	# 0/1 = absence/presence of insertion in that position of the alignment
@@ -67,9 +62,9 @@ def get_peptides(ins_aln_positions, aln_file):
 				stop = int(pos_list[position])
 				ins_seq = ungapped_seq[start:stop+1]
 				aln_stop = int(aln_pos_list[position])
-				# if aln_stop + 1 == aln.get_alignment_length():
-				# 	pass
-				if aln_stop == aln_start:
+				if aln_stop + 1 == aln.get_alignment_length():
+					pass
+				elif aln_stop == aln_start:
 					result = (ins_seq, aln_start+1)
 					del_dict[seq_name].append(result)
 				else:
@@ -79,9 +74,9 @@ def get_peptides(ins_aln_positions, aln_file):
 				stop = int(pos_list[position])
 				ins_seq = ungapped_seq[start:stop+1]
 				aln_stop = int(aln_pos_list[position])
-				# if aln_stop + 1 == aln.get_alignment_length():
-				# 	pass
-				if aln_stop == aln_start:
+				if aln_stop + 1 == aln.get_alignment_length():
+					pass
+				elif aln_stop == aln_start:
 					result = (ins_seq, aln_start+1)
 					del_dict[seq_name].append(result)
 				else:
@@ -106,33 +101,32 @@ for file in files:
 			del_dict = get_peptides(ins_aln_positions, file)[1]
 			len_dict = get_peptides(ins_aln_positions, file)[2]
 			all_len.update(len_dict)
-		except:
-			pass
-# 			for key, value in result_dict.items():
-# 				sp_name = key.split('__')[1]
-# 				for i in value[1:]:
-# 					ins_results.write('>{}__{} length_{} pos_{}-{}\n{}\n'.format(file_name, sp_name, len(i[0]),
-# 						i[1], i[2], i[0]))
+			
+			for key, value in result_dict.items():
+				sp_name = key.split('__')[1]
+				for i in value[1:]:
+					ins_results.write('>{}__{} length_{} pos_{}-{}\n{}\n'.format(file_name, sp_name, len(i[0]),
+						i[1], i[2], i[0]))
 		
-# 			for key, value in del_dict.items():
-# 				sp_name = key.split('__')[1]
-# 				for i in value[1:]:
-# 					del_results.write('>{}__{} {}\n{}\n'.format(file_name, sp_name, i[1], i[0]))
-# 		except ValueError:
-# 			errors.write(file + '\n')
+			for key, value in del_dict.items():
+				sp_name = key.split('__')[1]
+				for i in value[1:]:
+					del_results.write('>{}__{} {}\n{}\n'.format(file_name, sp_name, i[1], i[0]))
+		except ValueError:
+			errors.write(file + '\n')
 
-# result_len = {}
-# for key, value in all_len.items():
-# 	for sp in value:
-# 		if sp[0] in result_len.keys():
-# 			result_len[sp[0]] += sp[1]
-# 		else:
-# 			result_len[sp[0]] = sp[1]
+result_len = {}
+for key, value in all_len.items():
+	for sp in value:
+		if sp[0] in result_len.keys():
+			result_len[sp[0]] += sp[1]
+		else:
+			result_len[sp[0]] = sp[1]
 
-# for key, value in result_len.items():
-# 	len_results.write('{}\t{}\n'.format(key, value))
+for key, value in result_len.items():
+	len_results.write('{}\t{}\n'.format(key, value))
 
-# ins_results.close()
-# del_results.close()
-# len_results.close()
-# errors.close()
+ins_results.close()
+del_results.close()
+len_results.close()
+errors.close()
