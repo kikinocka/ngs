12797eedbac4a01aa01bcbdfdeb479cbedc9df4c
KikaZah <kika.zahonova@gmail.com>
2018-05-15 20:39:25 +0200

    bla

--- bash_scripts/fastqc.sh
+++ bash_scripts/fastqc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-read_dir='/media/4TB1/blastocrithidia/reads/transcriptome/'
-out_dir='/media/4TB1/blastocrithidia/reads/transcriptome/raw/fastqc/'
+read_dir='/media/4TB1/blastocrithidia/reads/transcriptome/trimmed/'
+out_dir='/media/4TB1/blastocrithidia/reads/transcriptome/trimmed/fastqc/'
 
-/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'raw/p57_3-end_enriched_1.fastq.gz'
-/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'raw/p57_3-end_enriched_2.fastq.gz'
+/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'p57_3-end_enriched_trimmed_1.fq.gz'
+/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'p57_3-end_enriched_trimmed_2.fq.gz'
