0d4e771bf0415b9fb76d128dc742d2676d16a4c9
KikaZah <kika.zahonova@gmail.com>
2018-05-16 16:11:36 +0200

    bla

--- bash_scripts/slap_mapper.sh
+++ bash_scripts/slap_mapper.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
-slap_mapper="/home/nenarokova/tools/SLaPMapper/SLaPMapper.pl"
-genome="/media/4TB1/blastocrithidia/genome_assembly/p57_scaffolds.fa"
-reads1="/media/4TB1/blastocrithidia/reads/transcriptome/raw/Blastoc_spP57_1.fastq"
-reads2="/media/4TB1/blastocrithidia/reads/transcriptome/raw/Blastoc_spP57_2.fastq"
-gff="/media/4TB1/blastocrithidia/slap_mapping/empty_annotation.gff"
-work_dir="/media/4TB1/blastocrithidia/slap_mapping"
+slap_mapper='/home/nenarokova/tools/SLaPMapper/SLaPMapper.pl'
+genome='/media/4TB1/blastocrithidia/genome_assembly/p57_scaffolds.fa'
+reads1='/media/4TB1/blastocrithidia/reads/transcriptome/raw/p57_all_1.fastq.gz'
+reads2='/media/4TB1/blastocrithidia/reads/transcriptome/raw/p57_all_2.fastq.gz'
+gff='/media/4TB1/blastocrithidia/mapping/slap_mapping_both_libraries/empty_annotation.gff'
+work_dir='/media/4TB1/blastocrithidia/mapping/slap_mapping_both_libraries/'
 
-SL="AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG"
-min_length="6"
+SL='AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG'
+min_length='6'
 
 cd $work_dir
 ln -s $reads1 reads1.fastq
