ff5eb067692979299ff174baa8f8e04767c6091a
KikaZah <kika.zahonova@gmail.com>
2018-05-17 11:13:35 +0200

    bla

--- bash_scripts/samtools_merge.sh
+++ bash_scripts/samtools_merge.sh
@@ -4,4 +4,4 @@ in1='/media/4TB1/blastocrithidia/mapping/p57_RNA_to_DNA/p57_bw2_sorted.bam'
 in2='/media/4TB1/blastocrithidia/mapping/p57_polyA_RNA_to_DNA/p57_polyA_bw2_sorted.bam'
 out='/media/4TB1/blastocrithidia/mapping/p57_both_RNA_to_DNA/p57_both_libraries.bam'
 
-samtools merge $out $in1 $in2
+samtools merge -@ 32 $out $in1 $in2
