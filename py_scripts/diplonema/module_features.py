8213703b96877a6b8d2ed3c27dadbcd1fd1644c4
KikaZah <kika.zahonova@gmail.com>
2018-06-07 16:52:34 +0200

    bla

--- py_scripts/diplonema/module_features.py
+++ py_scripts/diplonema/module_features.py
@@ -3,41 +3,58 @@ import os
 from collections import OrderedDict
 
 os.chdir('/home/kika/MEGAsync/diplonema_mt/comparison/')
-table = open('1604_multi_modules.tsv')
-# out = open('1601st.tsv', 'w')
+table = open('1618_multi_modules.tsv')
+out = open('1618_multi_modules_stat.tsv', 'w')
+
+def in_range(start, number, end):
+	return start <= number <= end
 
 contigs = OrderedDict(list())
 for line in table:
-	name = line.split('\t')[0]
-	module = line.split('\t')[1]
-	mmin = line.split('\t')[2]
-	mmax = line.split('\t')[3]
-	strand = line.split('\t')[5][:-1]
-	if name not in contigs:
-		contigs[name] = [(module, mmin, mmax, strand)]
-	else:
-		contigs[name].append((module, mmin, mmax, strand))
+	try:
+		name = line.split('\t')[0]
+		module = line.split('\t')[1]
+		mmin = int(line.split('\t')[2])
+		mmax = int(line.split('\t')[3])
+		strand = line.split('\t')[5][:-1]
+		if name not in contigs:
+			contigs[name] = [(module, mmin, mmax, strand)]
+		else:
+			contigs[name].append((module, mmin, mmax, strand))
+	except:
+		pass
 
 for key, value in contigs.items():
 	if len(value) > 1:
 		for i in range(len(value)):
-			while i < len(value) and i+1 != len(value):
-				if value[i][2] > value[i+1][1]:
-					if value[i][2] > value[i+1][2]:
-						# print(key, value[i], value[i+1])
-						# print('embedded')
-						if value[i][3] == value[i+1][3]:
-							print('{}\t{} [{}]\tsame\n'.format(key, value[i][0], value[i+1][0]))
+			for j in range(i + 1, len(value)):
+				if in_range(value[i][1], value[j][1], value[i][2]):
+				#start J inside I
+					if in_range(value[i][1], value[j][2], value[i][2]):
+					#end J inside I => embedded J in I
+						if value[i][3] == value[j][3]:
+							out.write('{}\t{} [{}]\tsame\tembedded\n'.format(key, value[i][0], value[j][0]))
 						else:
-							print('{}\t{} [{}]\topposite\n'.format(key, value[i][0], value[i+1][0]))
+							out.write('{}\t{} [{}]\topposite\tembedded\n'.format(key, value[i][0], value[j][0]))
 					else:
-						# print(key, value[i], value[i+1])
-						# print('overlapping')
-						length = int(value[i][2]) - int(value[i+1][1]) + 1
-						if value[i][3] == value[i+1][3]:
-							print('{}\t{} + {} ({})\tsame\n'.format(key, value[i][0], value[i+1][0], length))
+					#end J outside I => overlapping
+						length = int(value[i][2]) - int(value[j][1]) + 1
+						if value[i][3] == value[j][3]:
+							out.write('{}\t{} + {} ({})\tsame\toverlapping\n'.format(key, value[i][0], value[j][0], length))
 						else:
-							print('{}\t{} + {} ({})\topposite\n'.format(key, value[i][0], value[i+1][0], length))
+							out.write('{}\t{} + {} ({})\topposite\toverlapping\n'.format(key, value[i][0], value[j][0], length))
+				elif in_range(value[i][1], value[j][2], value[i][2]):
+				#end J inside I => overlapping
+					length = int(value[j][2]) - int(value[i][1]) + 1
+					if value[i][3] == value[j][3]:
+						out.write('{}\t{} + {} ({})\tsame\toverlapping\n'.format(key, value[j][0], value[i][0], length))
+					else:
+						out.write('{}\t{} + {} ({})\topposite\toverlapping\n'.format(key, value[j][0], value[i][0], length))
+				elif in_range(value[j][1], value[i][1], value[j][2]) and in_range(value[j][1], value[i][2], value[j][2]):
+				#embedded I in J
+					if value[j][3] == value[i][3]:
+						out.write('{}\t{} [{}]\tsame\tembedded\n'.format(key, value[j][0], value[i][0]))
+					else:
+						out.write('{}\t{} [{}]\topposite\tembedded\n'.format(key, value[j][0], value[i][0]))
 
-				i += 1
-# out.close()
+out.close()
