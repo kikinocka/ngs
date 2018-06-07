e7d6379feb4c1f95b139494a86ecde78e51335a8
KikaZah <kika.zahonova@gmail.com>
2018-06-07 10:17:10 +0200

    bla

--- py_scripts/diplonema/module_features.py
+++ py_scripts/diplonema/module_features.py
@@ -27,44 +27,4 @@ for line in table:
 
 print(all_contigs)
 
-# for contig in all_contigs:
-# 	print(contig.name, contig.mod_coordinates)
-
-# contigs = OrderedDict(list())
-# for line in table:
-# 	name = line.split('\t')[0]
-# 	module = line.split('\t')[1]
-# 	mmin = line.split('\t')[2]
-# 	mmax = line.split('\t')[3]
-# 	strand = line.split('\t')[5][:-1]
-# 	if name not in contigs:
-# 		contigs[name] = [(module, mmin, mmax, strand)]
-# 	else:
-# 		contigs[name].append((module, mmin, mmax, strand))
-
-# for key, value in contigs.items():
-# 	# if len(value) == 2:
-# 	# 	print(key, value)
-# 	# 	print('2')
-# 	# 	# if value[0][2] > value[1][1]:
-# 	# 	# 	if value[0][2] > value[1][2]:
-# 	# 	# 		if value[0][3] == value[1][3]:
-# 	# 	# 			out.write('{}\t{} [{}]\tsame\n'.format(key, value[0][0], value[1][0]))
-# 	# 	# 		else:
-# 	# 	# 			out.write('{}\t{} [{}]\topposite\n'.format(key, value[0][0], value[1][0]))
-# 	# 	# 	else:
-# 	# 	# 		length = int(value[0][2]) - int(value[1][1]) + 1
-# 	# 	# 		if value[0][3] == value[1][3]:
-# 	# 	# 			out.write('{}\t{} + {} ({})\tsame\n'.format(key, value[0][0], value[1][0], length))
-# 	# 	# 		else:
-# 	# 	# 			out.write('{}\t{} + {} ({})\topposite\n'.format(key, value[0][0], value[1][0], length))
-# 	# else:
-# 	for i in range(len(value)):
-# 		# print(key, value, i)
-# 		while i < len(value):
-# 			print(key, value)
-# 			if i+1 == len(value):
-# 				pass
-# 			i += 1
-
 out.close()
