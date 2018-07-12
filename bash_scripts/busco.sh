0fc2629db68004229bbe6b81a3a8a43f8fae2399
KikaZah <kika.zahonova@gmail.com>
2018-07-12 18:45:51 +0200

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -3,10 +3,10 @@
 cd /home/tomas/GIT/busco/
 
 input='/home/kika/work_dir/cre_proteins.fasta'
-out='BUSCO_cre_protists'
+out='BUSCO_cre_euk'
 mode=prot
 species=chlamydomonas_reinhardtii
-lineage='/home/tomas/GIT/busco/protists_ensembl/'
+lineage='/home/tomas/GIT/busco/eukaryota_odb9/'
 threads=16
 
 python BUSCO.py -i $input -m $mode -o $out -sp $species -l $lineage -c $threads --long -f
