4cb6eebcdfb444d7bef7b9637393e569f7eb0a0d
KikaZah <kika.zahonova@gmail.com>
2018-07-12 18:41:08 +0200

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #run on DIPLONEMA server
+cd /home/tomas/GIT/busco/
 
 input='/home/kika/work_dir/cre_proteins.fasta'
-out='/home/kika/work_dir/BUSCO_cre_protists/'
+out='BUSCO_cre_protists'
 mode=prot
 species=chlamydomonas_reinhardtii
 lineage='/home/tomas/GIT/busco/protists_ensembl/'
 threads=16
 
-python /home/tomas/GIT/busco/BUSCO.py -i $input -m $mode -o $out -sp $species -l $lineage -c $threads --long -f
+python BUSCO.py -i $input -m $mode -o $out -sp $species -l $lineage -c $threads --long -f
