676d213c97807ecb07f6943da5354c51ee3eaa6e
KikaZah <kika.zahonova@gmail.com>
2018-07-16 10:19:50 +0200

    bla

--- bash_scripts/segemehl_mapping.sh
+++ bash_scripts/segemehl_mapping.sh
@@ -27,7 +27,7 @@ split_file=$base_name'_split.bed'
 $tools'segemehl_diplonema.x' -d $database -i $index -q $fwd -p $rv -o $samfile -t 32 -s -S -F 6 -u $unmapped 2> $report
 samtools view -bS $samfile | samtools sort -o $bamfile -t 32
 samtools index $bamfile
-rm -f $samfile
+# rm -f $samfile
 gzip -f $unmapped
 samtools view -h $bamfile | gzip -c > $gzip_sam
 ./testrealign.x -d $database -q $gzip_sam -n -T $trans_file -U $split_file
