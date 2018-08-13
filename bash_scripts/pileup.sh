060fd44c3cafc3971a5ca172c1c782145d06d30b
KikaZah <kika.zahonova@gmail.com>
2018-08-13 21:13:34 +0200

    bla

--- bash_scripts/pileup.sh
+++ bash_scripts/pileup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-input='/media/4TB1/diplonema/mapping/RNA_to_transcriptomes/1604/1604_RNA_full_bw2.sam'
-output='/media/4TB1/diplonema/mapping/RNA_to_transcriptomes/1604/1604_RNA_full_bw2_stat.tsv'
-transcriptome='/media/4TB1/diplonema/transcriptomes/1604_Trinity.fasta'
+input='/media/4TB1/diplonema/mapping/RNA_to_transcriptomes/1608/1608_RNA_full_bw2.sam'
+output='/media/4TB1/diplonema/mapping/RNA_to_transcriptomes/1608/1608_RNA_full_bw2_stat.tsv'
+transcriptome='/media/4TB1/diplonema/transcriptomes/1608_Trinity.fasta'
 
 /home/kika/tools/bbmap/pileup.sh in=$input out=$output ref=$transcriptome
