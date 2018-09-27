442a1a002c88ecb1752ed9c64b3e98b8aef0d1d2
KikaZah <kika.zahonova@gmail.com>
2018-09-27 10:57:20 +0200

    bla

--- bash_scripts/fastqc_pbs.sh
+++ bash_scripts/fastqc_pbs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #PBS -N FastQC
-#PBS -l select=1:ncpus=4:mem=4gb:scratch_local=10gb
-#PBS -l walltime=1:00:00
+#PBS -l select=1:ncpus=4:mem=4gb:scratch_local=20gb
+#PBS -l walltime=0:20:00
 
 fastqc='/auto/brno2/home/kika/tools/miniconda3/pkgs/fastqc-0.11.7-5/bin/fastqc'
-read_dir='/media/4TB1/blastocrithidia/reads/transcriptome/trimmed/'
-out_dir='/media/4TB1/blastocrithidia/reads/transcriptome/trimmed/fastqc/'
+read_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/'
+out_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/fastqc/'
 
-fastqc -o $out_dir $read_dir'p57_3-end_enriched_trimmed_1.fq.gz'
-fastqc -o $out_dir $read_dir'p57_3-end_enriched_trimmed_2.fq.gz'
+fastqc -o $out_dir $read_dir'p1_r1.fastq.gz'
+fastqc -o $out_dir $read_dir'p1_r2.fastq.gz'
