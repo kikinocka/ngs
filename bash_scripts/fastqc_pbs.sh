ef5a7920172d3837804295dd93f071b2094a64a8
KikaZah <kika.zahonova@gmail.com>
2018-09-28 15:34:17 +0200

    bla

--- bash_scripts/fastqc_pbs.sh
+++ bash_scripts/fastqc_pbs.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 #PBS -N FastQC
-#PBS -l select=1:ncpus=4:mem=4gb:scratch_local=20gb
+#PBS -l select=1:ncpus=4:mem=40gb:scratch_local=20gb
 #PBS -l walltime=0:20:00
+#PBS -m ae
+#PBS -j oe
+# hashes explained: 
+#-N job name, -q queue, -l select resources, -l walltime, -m ae, -j oe mail will be send at the end of the job
+
+# get name of the machine where the job is run
+cat $PBS_NODEFILE
 
 fastqc='/auto/brno2/home/kika/tools/miniconda3/pkgs/fastqc-0.11.7-5/bin/fastqc'
 read_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/'
-out_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/fastqc/'
+out_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/fastqc/' #must be present beforewards
 
-fastqc -o $out_dir $read_dir'p1_r1.fastq.gz'
-fastqc -o $out_dir $read_dir'p1_r2.fastq.gz'
+#just in case scratch is not created, but never happened to me:
+if [ ! -d "$SCRATCHDIR" ] ; then echo "Scratch directory is not created!" 1>&2; exit 1; fi
+echo $SCRATCHDIR
+trap 'clean_scratch' TERM EXIT
+
+#load modules, may not be necessary
+#https://wiki.metacentrum.cz/wiki/FastQC
+module add fastQC-0.11.5
+
+#copy data to scratch
+cd $read_dir
+cp p1_r1.fastq.gz p1_r2.fastq.gz $SCRATCHDIR
+
+#chdir to scratch and perform operations
+cd $SCRATCHDIR
+fastqc -t 4 -o $out_dir 'p1_r1.fastq.gz'
+fastqc -t 4 -o $out_dir 'p1_r2.fastq.gz'
