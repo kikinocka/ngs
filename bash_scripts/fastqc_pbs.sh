24200c5198c77c12ea775a080813620706424ba2
KikaZah <kika.zahonova@gmail.com>
2018-10-01 16:29:33 +0200

    bla

--- bash_scripts/fastqc_pbs.sh
+++ bash_scripts/fastqc_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N FastQC
 #PBS -l select=1:ncpus=1:mem=50gb:scratch_local=100gb
-#PBS -l walltime=5:0:00
+#PBS -l walltime=5:00:00
 #PBS -m ae
 #PBS -j oe
 #hashes explained: 
--- bash_scripts/spades_pbs.sh
+++ bash_scripts/spades_pbs.sh
@@ -8,24 +8,23 @@ cat $PBS_NODEFILE
 
 #add modules
 module add spades-3.11.1
-# spades='/auto/brno2/home/kika/tools/SPAdes-3.11.1-Linux/bin/spades.py'
 
 read_dir='/storage/brno3-cerit/home/kika/pelomyxa/reads/genome/deep_hiseq/'
-fw='p1_trimmed_1.fq.gz'
-rv='p2_trimmed_1.fq.gz'
-
-outdir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/deep_hiseq/'
-report='spades_report.txt'
+outdir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/deep_hiseq/p1/'
 
 #copy reads to scratch
 cd $read_dir
 cp p1_trimmed_1.fq.gz p1_trimmed_2.fq.gz $SCRATCHDIR
 
+fw='p1_trimmed_1.fq.gz'
+rv='p1_trimmed_2.fq.gz'
+report='p1_spades_report.txt'
+
 #compute on scratch
 cd $SCRATCHDIR
 spades.py --pe1-1 $fw --pe1-2 $rv --careful -t 16 -m 250 -o out 2> $report
 
-#copy results to your directory
+#copy results to your folder
 cd out
 cp -R * $outdir
-cp ../report $outdir
+cp ../$report $outdir
