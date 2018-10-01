c73be3eda70da3440e42d47a29e760b937dddff9
KikaZah <kika.zahonova@gmail.com>
2018-10-01 10:04:51 +0200

    bla

--- bash_scripts/bbduk_pbs.sh
+++ bash_scripts/bbduk_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N BBduk
-#PBS -l select=1:ncpus=30:mem=50gb:scratch_local=50gb
+#PBS -l select=1:ncpus=32:mem=50gb:scratch_local=50gb
 #PBS -l walltime=0:20:00
 #PBS -m ae
 #PBS -j oe
@@ -27,6 +27,6 @@ cp $adapt $SCRATCHDIR
 cp $fw $rv $SCRATCHDIR
 
 cd $SCRATCHDIR
-bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=30 2> $report
+bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 2> $report
 
 cp $trimmed_fw $trimmed_rv $report $trimdir
