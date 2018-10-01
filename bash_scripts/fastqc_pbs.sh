8566efe6cfb1b30f80c8c76bdfad461365744314
KikaZah <kika.zahonova@gmail.com>
2018-10-01 19:36:57 +0200

    bla

--- bash_scripts/fastqc_pbs.sh
+++ bash_scripts/fastqc_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N FastQC
 #PBS -l select=1:ncpus=1:mem=50gb:scratch_local=100gb
-#PBS -l walltime=5:00:00
+#PBS -l walltime=1:00:00
 #PBS -m ae
 #PBS -j oe
 #hashes explained: 
@@ -11,8 +11,8 @@
 cat $PBS_NODEFILE
 
 # fastqc='/auto/brno2/home/kika/tools/miniconda3/pkgs/fastqc-0.11.7-5/bin/fastqc'
-read_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/'
-out_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/fastqc/' #must be present beforewards
+read_dir='/storage/brno3-cerit/home/kika/pelomyxa/reads/genome/deep_miseq/'
+out_dir='/storage/brno3-cerit/home/kika/pelomyxa/reads/genome/deep_miseq/fastqc/'
 
 # #just in case scratch is not created, but never happened to me:
 # if [ ! -d "$SCRATCHDIR" ] ; then echo "Scratch directory is not created!" 1>&2; exit 1; fi
@@ -25,17 +25,12 @@ module add fastQC-0.11.5
 
 #copy data to scratch
 cd $read_dir
-cp p1_trimmed_1.fq.gz p1_trimmed_2.fq.gz p2_trimmed_1.fq.gz p2_trimmed_2.fq.gz p3_trimmed_1.fq.gz p3_trimmed_2.fq.gz p4_trimmed_1.fq.gz p4_trimmed_2.fq.gz p5_trimmed_1.fq.gz p5_trimmed_2.fq.gz $SCRATCHDIR
+cp pelo2_s1_l001_r1_002.fastq.gz pelo2_s1_l001_r2_002.fastq.gz pelo5_s1_l001_r1_002.fastq.gz pelo5_s1_l001_r2_002.fastq.gz $SCRATCHDIR
+
 
 #chdir to scratch and perform operations
 cd $SCRATCHDIR
-fastqc -o $out_dir 'p1_trimmed_1.fq.gz'
-fastqc -o $out_dir 'p1_trimmed_2.fq.gz'
-fastqc -o $out_dir 'p2_trimmed_1.fq.gz'
-fastqc -o $out_dir 'p2_trimmed_2.fq.gz'
-fastqc -o $out_dir 'p3_trimmed_1.fq.gz'
-fastqc -o $out_dir 'p3_trimmed_2.fq.gz'
-fastqc -o $out_dir 'p4_trimmed_1.fq.gz'
-fastqc -o $out_dir 'p4_trimmed_2.fq.gz'
-fastqc -o $out_dir 'p5_trimmed_1.fq.gz'
-fastqc -o $out_dir 'p5_trimmed_2.fq.gz'
+fastqc -o $out_dir 'pelo2_s1_l001_r1_002.fastq.gz'
+fastqc -o $out_dir 'pelo2_s1_l001_r2_002.fastq.gz'
+fastqc -o $out_dir 'pelo5_s1_l001_r1_002.fastq.gz'
+fastqc -o $out_dir 'pelo5_s1_l001_r2_002.fastq.gz'
