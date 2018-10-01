aaac805d457d585aeaf895c40bc7bdc4cf30d752
KikaZah <kika.zahonova@gmail.com>
2018-10-01 09:52:13 +0200

    bla

--- bash_scripts/bbduk_pbs.sh
+++ bash_scripts/bbduk_pbs.sh
@@ -6,10 +6,7 @@
 #PBS -j oe
 cat $PBS_NODEFILE
 
-# inicializace konkretni verze JAVA
-module add jdk-7
-
-bbduk='/auto/brno2/home/kika/tools/bbmap/bbduk.sh'
+# bbduk='/auto/brno2/home/kika/tools/bbmap/bbduk.sh'
 adapt='/auto/brno2/home/kika/tools/bbmap/resources/adapters.fa'
 
 read_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/'
@@ -22,12 +19,14 @@ trimmed_fw=$name'_trimmed_1.fq.gz'
 trimmed_rv=$name'_trimmed_2.fq.gz'
 report=$name"_report.txt"
 
+module add bbmap-36.92 
+
 #copy data to scratch
 cd $read_dir
 cp $adapt $SCRATCHDIR
 cp $fw $rv $SCRATCHDIR
 
 cd $SCRATCHDIR
-$bbduk overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=30 2> $report
+bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=30 2> $report
 
 cp $trimmed_fw $trimmed_rv $report $trimdir
