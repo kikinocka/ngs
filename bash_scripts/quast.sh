87a6baa288a814b71ddb94010eb9c1e1ad51415e
KikaZah <kika.zahonova@gmail.com>
2018-10-02 13:48:20 +0200

    bla

--- bash_scripts/quast.sh
+++ bash_scripts/quast.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-f='/home/kika/MEGAsync/diplonema_mt/1601/genome_assembly/1601_spades_guided_trusted.fasta'
-output='/home/kika/MEGAsync/diplonema_mt/1601/genome_assembly/quast/spades_guided_trusted/'
+f='/home/kika/MEGAsync/pelomyxa/genome_assembly/p1_scaffolds.fasta'
+output='/home/kika/MEGAsync/pelomyxa/genome_assembly/'
 
 /usr/bin/python3.5 /home/kika/programs/quast-4.4/quast.py $f -o $output -t 4
