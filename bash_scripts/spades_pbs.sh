343573272e202d96c6da6ba10d3e828c9f59bccf
KikaZah <kika.zahonova@gmail.com>
2018-10-02 12:58:39 +0200

    bla

--- bash_scripts/spades_pbs.sh
+++ bash_scripts/spades_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N SPAdes
 #PBS -l select=1:ncpus=20:ompthreads=20:mem=250gb:scratch_local=50gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=20:00:00
 #PBS -m ae
 #PBS -j oe
 cat $PBS_NODEFILE
