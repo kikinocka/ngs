6c92f9878ea3cdf409443177b8384c4e8a758f49
KikaZah <kika.zahonova@gmail.com>
2018-10-08 09:03:02 +0200

    bla

--- bash_scripts/quast.sh
+++ bash_scripts/quast.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-f='/home/kika/ownCloud/pelomyxa/genome_assembly/p1_scaffolds_k127.fasta'
-output='/home/kika/ownCloud/pelomyxa/genome_assembly/quast/p1_k127/'
+f='/home/kika/ownCloud/pelomyxa/genome_assembly/p2_scaffolds_k127.fasta'
+output='/home/kika/ownCloud/pelomyxa/genome_assembly/quast/p2_k127/'
 
 /usr/bin/python3.5 /home/kika/programs/quast-4.4/quast.py $f -o $output -t 4
--- bash_scripts/spades_pbs.sh
+++ bash_scripts/spades_pbs.sh
@@ -9,16 +9,16 @@ cat $PBS_NODEFILE
 #add modules
 module add spades-3.11.1
 
-read_dir='/storage/brno3-cerit/home/kika/pelomyxa/reads/genome/deep_hiseq/'
-outdir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/deep_hiseq/p3/'
+read_dir='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/genome/deep_hiseq/'
+outdir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/deep_hiseq/p4/'
 
 #copy reads to scratch
 cd $read_dir
-cp p3_trimmed_1.fq.gz p3_trimmed_2.fq.gz $SCRATCHDIR
+cp p4_trimmed_1.fq.gz p4_trimmed_2.fq.gz $SCRATCHDIR
 
-fw='p3_trimmed_1.fq.gz'
-rv='p3_trimmed_2.fq.gz'
-report='p3_spades_report.txt'
+fw='p4_trimmed_1.fq.gz'
+rv='p4_trimmed_2.fq.gz'
+report='p4_spades_report.txt'
 
 #compute on scratch
 cd $SCRATCHDIR
