0b6803e0a70e81af60e57b7bfd24f20ace6f6477
KikaZah <kika.zahonova@gmail.com>
2018-10-11 12:59:06 +0200

    bla

--- bash_scripts/bbduk_pbs.sh
+++ bash_scripts/bbduk_pbs.sh
@@ -8,12 +8,12 @@ cat $PBS_NODEFILE
 
 adapt='/auto/brno2/home/kika/tools/bbmap/resources/adapters.fa'
 
-read_dir='/storage/brno3-cerit/home/kika/pelomyxa/reads/genome/preliminary_seq/'
-fw='pelo5_s3_l001_r1_001.fastq.gz'
-rv='pelo5_s3_l001_r2_001.fastq.gz'
+read_dir='/storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome/'
+fw='pelo1_r1.fastq.gz'
+rv='pelo1_r2.fastq.gz'
 
-trimdir='/storage/brno3-cerit/home/kika/pelomyxa/reads/genome/preliminary_seq/'
-name='pelo5_001'
+trimdir='/storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome/'
+name='pelo1'
 trimmed_fw=$name'_trimmed_1.fq.gz'
 trimmed_rv=$name'_trimmed_2.fq.gz'
 report=$name"_report.txt"
