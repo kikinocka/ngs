1e4a8a4e6f5fa9b201f20dd9565d3feee19e39d1
KikaZah <kika.zahonova@gmail.com>
2018-10-23 13:48:39 +0200

    bla

--- bash_scripts/curl.sh
+++ bash_scripts/curl.sh
@@ -1,5 +1,5 @@
 #login 
-curl -d "u=kika&p=06trd02lo14" --cookie-jar cjar http://mokosz.natur.biocev.org/doku.php?do=login
+curl -d "u=user&p=password" --cookie-jar cjar http://mokosz.natur.biocev.org/doku.php?do=login
 
 #downloading
 curl --cookie cjar --cookie-jar cjar -o pelo6_r2.fastq.gz http://mokosz.natur.biocev.org/lib/exe/fetch.php?media=pelomyxa:pelo6_s5_l001_r2_001_tr.fastq.gz
