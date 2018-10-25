79375fcbd24353bee27656636025eebdd2f12ee7
KikaZah <kika.zahonova@gmail.com>
2018-10-25 21:26:11 +0200

    bla

--- py_scripts/diplonema/module_len_graph.py
+++ py_scripts/diplonema/module_len_graph.py
@@ -2,6 +2,7 @@
 import os
 import csv
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 os.chdir('/home/kika/MEGAsync/diplonema_mt/')
@@ -11,10 +12,20 @@ ypf1608 = csv.reader(open('1608/1608_module_sizes_mod.tsv'), delimiter='\t', ski
 ypf1618 = csv.reader(open('1618/1618_module_sizes_mod.tsv'), delimiter='\t', skipinitialspace=True)
 ypf1610 = csv.reader(open('1610/1610_module_sizes_mod.tsv'), delimiter='\t', skipinitialspace=True)
 
+def get_cmap(n, name='PiYG'): #hsv for very divergent data?
+	'''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+	RGB color; the keyword argument name must be a standard mpl colormap name.'''
+	colormap = plt.cm.get_cmap(name, n)
+	rgbcolors = []
+	for i in range(colormap.N):
+		rgb = colormap(i)[:3] # will return rgba, we take only first 3 so we get rgb
+		rgbcolors.append(matplotlib.colors.rgb2hex(rgb))
+	return rgbcolors
+
 def csv_parser(file):
 	module_list = []
 	for row in file:
-		module_list.append(row[1])
+		module_list.append(int(row[1]))
 	return module_list
 
 llan = csv_parser(ypf1601)
@@ -23,20 +34,25 @@ rhum = csv_parser(ypf1608)
 sspe = csv_parser(ypf1618)
 ypf10 = csv_parser(ypf1610)
 
-
-y = [djap, rhum, llan, sspe, ypf10] #dosad hodnoty velkosti modulov
-x = [1,2,3,4,5] #zoznam modulov, musia byt cisla myslim
+# module sizes in each species
+y = [djap, rhum, llan, sspe, ypf10]
+#list of species
+x = [1,2,3,4,5]
 
 data = []
 for i in y:
     data.append(np.array(i))
 fig = plt.figure()
-plt.boxplot(data, notch=True)
+bplot = plt.boxplot(data, notch=True, patch_artist=True)
+
+colors = get_cmap(len(x)) #alebo definuj: ['pink', 'lightblue', 'lightgreen']
+for patch, color in zip(bplot['boxes'], colors):
+    patch.set_facecolor(color)
 
 for xe, ye in zip(x, y):
-    plt.plot([xe] * len(ye), ye, 'o', mfc='none', c='black')
+    plt.plot([xe] * len(ye), ye, 'o', mfc='none', c='black', zorder=10) #zorder aby boli hore
     
 plt.xticks([1, 2, 3, 4, 5])
-plt.axes().set_xticklabels(['D.jap', 'R.hum', 'L.lan', 'S.spe', 'YPF'])
+plt.axes().set_xticklabels(['D. japonicum', 'R. humris', 'L. lanifica', 'S. specki', 'YPF1610'])
 
 plt.show()
