527100d3e12065ae8be915f10265658fdaf17d3a
KikaZah <kika.zahonova@gmail.com>
2018-10-29 10:59:47 +0100

    bla

--- bash_scripts/quast.sh
+++ bash_scripts/quast.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-f='/home/kika/ownCloud/pelomyxa/transcriptome_assembly/pelo2_trinity.fasta'
-output='/home/kika/ownCloud/pelomyxa/transcriptome_assembly/quast/pelo2/'
+f='/home/kika/ownCloud/pelomyxa/transcriptome_assembly/pelo5_trinity.fasta'
+output='/home/kika/ownCloud/pelomyxa/transcriptome_assembly/quast/pelo5/'
 
 /usr/bin/python3.5 /home/kika/programs/quast-4.4/quast.py $f -o $output -t 4
--- bash_scripts/trinity_pbs.sh
+++ bash_scripts/trinity_pbs.sh
@@ -15,15 +15,15 @@ out_dir='/storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/'
 
 #copy reads to scratch
 cd $read_dir
-cp pelo5_trimmed_1.fq.gz pelo5_trimmed_2.fq.gz $SCRATCHDIR
+cp pelo6_trimmed_1.fq.gz pelo6_trimmed_2.fq.gz $SCRATCHDIR
 
-fw='pelo5_trimmed_1.fq.gz'
-rv='pelo5_trimmed_2.fq.gz'
-report='pelo5_report.txt'
+fw='pelo6_trimmed_1.fq.gz'
+rv='pelo6_trimmed_2.fq.gz'
+report='pelo6_report.txt'
 
 #compute on scratch
 cd $SCRATCHDIR
-Trinity --seqType fq --left $fw --right $rv --output pelo5_trinity --max_memory 50G --CPU 15 2> $report
+Trinity --seqType fq --left $fw --right $rv --output pelo6_trinity --max_memory 50G --CPU 15 2> $report
 
-cp $report pelo5_trinity/
-cp -r pelo5_trinity/ $outdir
+cp $report pelo6_trinity/
+cp -r pelo6_trinity/ $outdir
