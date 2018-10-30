58dac9e6d67b2284c8c045d17150b069d513da4f
KikaZah <kika.zahonova@gmail.com>
2018-10-30 16:34:29 +0100

    bla

--- bash_scripts/pileup.sh
+++ bash_scripts/pileup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-input='/media/4TB1/diplonema/mapping/RNA_to_transcriptomes/1601/1601_RNA_full_bw2.sam'
-output='/media/4TB1/diplonema/mapping/RNA_to_transcriptomes/1601/1601_RNA_full_bw2_stat.tsv'
-transcriptome='/media/4TB1/diplonema/transcriptomes/1601_Trinity.fasta'
+input='/media/4TB1/diplonema/mapping/DNA_to_contigs/1608/1608_contigs_DNA_bw2.sam'
+output='/media/4TB1/diplonema/mapping/DNA_to_contigs/1608/1608_contigs_DNA_bw2_stat.tsv'
+transcriptome='/media/4TB1/diplonema/mapping/DNA_to_contigs/1608/1608_final_chromosomes.fasta'
 
 /home/kika/tools/bbmap/pileup.sh in=$input out=$output ref=$transcriptome
