bbd596caa2d81f88b783703f4695b5e73ecda3e2
KikaZah <kika.zahonova@gmail.com>
2018-10-30 16:38:24 +0100

    bla

--- bash_scripts/bowtie2_trypka.sh
+++ bash_scripts/bowtie2_trypka.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 bw2_dir='/home/kika/miniconda3/pkgs/bowtie2-2.3.4.2-py36h2d50403_0/bin/'
-base_name='/media/4TB1/diplonema/mapping/DNA_to_contigs/1608/1608_cassettes-arrays_bw2'
-ref='/media/4TB1/diplonema/mapping/DNA_to_contigs/1608/1608_cassettes-arrays.fasta'
+base_name='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/1618_cassettes-arrays_bw2'
+ref='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/1618_cassettes-arrays.fasta'
 $bw2_dir'bowtie2-build' --threads 32 $ref $base_name
 
 read_dir='/media/4TB1/diplonema/reads/genome/merged/'
-p1_1=$read_dir'YPF1608_merged_deduplicated.fq'
+p1_1=$read_dir'YPF1618_merged_deduplicated.fq'
 # p1_2=$read_dir'1621_trimmed_2.fq.gz'
 
 alignment=$base_name'.sam'
