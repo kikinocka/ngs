baa20168e321979f16a90d70d82b8040674d46c2
KikaZah <kika.zahonova@gmail.com>
2018-11-01 13:43:46 +0100

    bla

--- py_scripts/run_program/pasta.py
+++ py_scripts/run_program/pasta.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 
-os.chdir('/home/kika/ownCloud/blastocrithidia/genes/glycolysis/PASTA/')
+os.chdir('/media/4TB1/blastocrithidia/seqfire/dataset_sgOGs/')
 files = sorted(os.listdir())
 
 for file in files:
