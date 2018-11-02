406c4c190bef20aaad2fad6140914adf423550f4
KikaZah <kika.zahonova@gmail.com>
2018-11-02 14:49:08 +0100

    bla

--- py_scripts/blast/blastdbcmd_short.py
+++ py_scripts/blast/blastdbcmd_short.py
@@ -2,11 +2,11 @@
 import subprocess
 
 file = '/home/kika/MEGAsync/Euglena_longa/2013_Sekvenovanie/SUF_system/SufS1_assembly/in'
-one = 'TRINITY_DN97191_c0_g1_i1'
+one = 'TRINITY_DN17973_c0_g1_i1'
 positions = '43693-47247'
 strand = 'plus'
-db = '/home/kika/programs/blast-2.5.0+/bin/bexlh2_strict.fa'
-out = '/home/kika/ownCloud/blastocrithidia/ssu_tree/bexlh2_rRNA_contig.fa'
+db = '/home/kika/programs/blast-2.5.0+/bin/lhes1_PRJNA238835_trinity.fasta'
+out = '/home/kika/ownCloud/blastocrithidia/ssu_tree/lehs1_rRNA_contig.fa'
 
 # subprocess.call('blastdbcmd -entry_batch {} -db {} -out {}'.format(file, db, out), shell=True)
 subprocess.call('blastdbcmd -entry {} -db {} -out {} -strand={}'.format(one, db, out, strand), 
--- py_scripts/run_program/seqfire.py
+++ py_scripts/run_program/seqfire.py
@@ -2,12 +2,12 @@
 import os
 import subprocess
 
-seqfire='/home/kika/programs/seqFIRE/seqfire.py'
+seqfire='/home/kika/tools/seqfire.py'
 
-os.chdir('/home/kika/ownCloud/blastocrithidia/orthofinder/sg_ogs/jac_renamed/seqfire/')
+os.chdir('/media/4TB1/blastocrithidia/seqfire/alignments_sgOGs/')
 files = sorted(os.listdir())
 
 for file in files:
 	if file.endswith('.aln'):
-		subprocess.call('python2 {} -i {} -a 1 -b 1 -p False'.format(seqfire, file), shell=True)
 		print(file)
+		subprocess.call('python2 {} -i {} -a 1 -b 1 -p True'.format(seqfire, file), shell=True)
