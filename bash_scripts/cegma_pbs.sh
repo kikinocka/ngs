18943975bb14ca18db1ddf298fde18ff0e61943c
KikaZah <kika.zahonova@gmail.com>
2018-11-05 17:25:49 +0100

    bla

---
+++ bash_scripts/cegma_pbs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -N CEGMA
+#PBS -l select=1:ncpus=15:mem=100gb:scratch_local=100gb
+#PBS -l walltime=00:10:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add cegma-2.5
+module add blast+-2.2.29
+
+folder='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/transcriptome_assembly/pelo_all_trinity/'
+
+#copy file to scratch
+cd $folder
+cp Trinity.fasta $SCRATCHDIR
+
+transcriptome='Trinity.fasta'
+
+cd $SCRATCHDIR
+cegma -o pelo_cegma -T $PBS_NUM_PPN <-g $transcriptome
+
+cp pelo_cegma $folder
