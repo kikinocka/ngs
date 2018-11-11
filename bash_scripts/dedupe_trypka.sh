d7c4029c5d75ddcf0616efc199fcebd5b075c0bd
KikaZah <kika.zahonova@gmail.com>
2018-11-11 18:56:01 +0100

    bla

--- bash_scripts/dedupe_trypka.sh
+++ bash_scripts/dedupe_trypka.sh
@@ -2,8 +2,8 @@
 
 read_dir='/media/4TB1/diplonema/reads/genome/used/'
 name='1608_'
-merged=$read_dir$name'merged.fq'
-deduplicated=$read_dir$name'merged_deduplicated.fq'
-report=$read_dir$name'report_dedupe.txt'
+merged=$read_dir$name'unmerged.fq'
+deduplicated=$read_dir$name'unmerged_deduplicated.fq'
+report=$read_dir$name'report_dedupe_unmerged.txt'
 
 /home/kika/tools/bbmap/dedupe.sh in=$merged out=$deduplicated k=31 ac=f 2> $report
