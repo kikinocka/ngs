54911c30c03c78bc6d9e4e24026f944ef98c26c4
KikaZah <kika.zahonova@gmail.com>
2018-11-11 19:29:23 +0100

    bla

--- bash_scripts/pileup.sh
+++ bash_scripts/pileup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 file_dir='/media/4TB1/diplonema/mapping/DNA_to_contigs/1608/'
-reference=$file_dir'1608_cassettes-arrays.fasta'
-input=$file_dir'1608_cassettes-arrays_bw2.sam'
-output=$file_dir'1608_cassettes-arrays_bw2_stat.tsv'
+reference=$file_dir'1608_artifical_contig.fasta'
+input=$file_dir'1608_artifical_contig_bw2.sam'
+output=$file_dir'1608_artifical_contig_bw2_stat.tsv'
 
 /home/kika/tools/bbmap/pileup.sh in=$input out=$output ref=$reference
