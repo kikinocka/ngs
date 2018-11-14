a76182be64431234342b3f98cc30e755d8d4a68d
KikaZah <kika.zahonova@gmail.com>
2018-11-14 16:59:17 +0100

    bla

--- py_scripts/run_program/seqfire.py
+++ py_scripts/run_program/seqfire.py
@@ -4,8 +4,8 @@ import subprocess
 
 seqfire='/home/kika/tools/seqfire.py'
 
-os.chdir('/media/4TB1/blastocrithidia/seqfire/alignments_sgOGs/')
-files = sorted(os.listdir('/media/4TB1/blastocrithidia/seqfire/alignments_sgOGs/'))
+os.chdir('/media/4TB1/blastocrithidia/seqfire/alignments_sgOGs/not_analyzed/')
+files = sorted(os.listdir('/media/4TB1/blastocrithidia/seqfire/alignments_sgOGs/not_analyzed/'))
 
 for file in files:
 	if file.endswith('.aln'):
