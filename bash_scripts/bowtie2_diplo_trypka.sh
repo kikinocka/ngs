79ad5a28787677c3cde642afaf58029c33661b43
KikaZah <kika.zahonova@gmail.com>
2018-11-20 09:06:05 +0100

    bla

--- bash_scripts/bowtie2_diplo_trypka.sh
+++ bash_scripts/bowtie2_diplo_trypka.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 bw2_dir='/home/kika/miniconda3/pkgs/bowtie2-2.3.4.2-py36h2d50403_0/bin/'
-base_name='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/NODE_26281_bw2'
-ref='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/NODE_26281_length_616_cov_381.212679.fasta'
+base_name='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/NODE_41415_bw2'
+ref='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/NODE_41415_length_485_cov_989.650838.fasta'
 
-# $bw2_dir'bowtie2-build' --threads 32 $ref $base_name
+$bw2_dir'bowtie2-build' --threads 32 $ref $base_name
 
 read_dir='/media/4TB1/diplonema/reads/genome/used/1618/'
-p1_1=$read_dir'NODE_26281_reads.fastq'
+p1_1=$read_dir'NODE_41415_reads.fastq'
 # p1_2=$read_dir'1618_unmerged_deduplicated.fq'
 
 alignment=$base_name'.sam'
@@ -15,8 +15,8 @@ report=$base_name'_report.txt'
 unmapped_unpaired=$base_name'_unmapped_unpaired.fq'
 unmapped_paired=$base_name'_unmapped_paired.fq'
 
-# $bw2_dir'bowtie2' --very-fast-local -p 32 -I 0 -X 5 \
-# -x $base_name -U $p1_1 --un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $alignment 2> $report
+$bw2_dir'bowtie2' --very-fast-local -p 32 -I 0 -X 5 \
+-x $base_name -U $p1_1 --un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $alignment 2> $report
 # ,$p1_2
 
 samfile=$alignment
