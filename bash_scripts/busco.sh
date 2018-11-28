6ba9b59f59ffc98591cd314c6a57f04c332ed579
KikaZah <kika.zahonova@gmail.com>
2018-11-28 16:16:29 +0100

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #run on DIPLONEMA server
 cd /home/tomas/GIT/busco/
+export AUGUSTUS_CONFIG_PATH=/home/kika/bin/augustus-3.2.3/config/
 
 input='/home/kika/work_dir/pelo_spades.fasta'
 out='BUSCO_pelo_genome'
