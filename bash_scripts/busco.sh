81d9e8ff9934698e756e0f81452c6a87a59cbed5
KikaZah <kika.zahonova@gmail.com>
2018-11-29 13:04:29 +0100

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -6,7 +6,7 @@ export AUGUSTUS_CONFIG_PATH=/home/kika/bin/augustus-3.2.3/config/
 input='/home/kika/work_dir/pelo_spades.fasta'
 out='BUSCO_pelo_genome_crei'
 mode=geno
-species=chlamydomonas
+species=brugia
 lineage='/home/tomas/GIT/busco/eukaryota_odb9/'
 threads=16
 
