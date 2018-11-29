3c163e605e83cc12a099641edf65f42a445f7f6b
KikaZah <kika.zahonova@gmail.com>
2018-11-29 13:12:01 +0100

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -4,9 +4,9 @@ cd /home/tomas/GIT/busco/
 export AUGUSTUS_CONFIG_PATH=/home/kika/bin/augustus-3.2.3/config/
 
 input='/home/kika/work_dir/pelo_spades.fasta'
-out='BUSCO_pelo_genome_bmal'
+out='BUSCO_pelo_genome_tgon'
 mode=geno
-species=brugia
+species=toxoplasma
 lineage='/home/tomas/GIT/busco/eukaryota_odb9/'
 threads=16
 
