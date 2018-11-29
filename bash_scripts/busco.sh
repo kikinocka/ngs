a4d01b895dac151e6502149e05c8b171eae461c8
KikaZah <kika.zahonova@gmail.com>
2018-11-29 13:02:51 +0100

    bla

--- bash_scripts/busco.sh
+++ bash_scripts/busco.sh
@@ -4,11 +4,10 @@ cd /home/tomas/GIT/busco/
 export AUGUSTUS_CONFIG_PATH=/home/kika/bin/augustus-3.2.3/config/
 
 input='/home/kika/work_dir/pelo_spades.fasta'
-out='BUSCO_pelo_genome'
+out='BUSCO_pelo_genome_crei'
 mode=geno
-species=chlamydomonas_reinhardtii
+species=chlamydomonas
 lineage='/home/tomas/GIT/busco/eukaryota_odb9/'
 threads=16
 
-python BUSCO.py -i $input -m $mode -o $out -l $lineage -c $threads --long -f
-#-sp $species 
+python BUSCO.py -i $input -m $mode -o $out -l $lineage -sp $species -c $threads --long -f
