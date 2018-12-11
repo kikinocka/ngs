bb301b500bfcc354c0624796887f8bdaf42e2220
KikaZah <kika.zahonova@gmail.com>
2018-12-11 16:50:47 +0100

    bla

--- bash_scripts/bowtie2_diplo_trypka.sh
+++ bash_scripts/bowtie2_diplo_trypka.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 bw2_dir='/home/kika/miniconda3/pkgs/bowtie2-2.3.4.2-py36h2d50403_0/bin/'
-base_name='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/1618_D02-2_bw2'
-ref='/media/4TB1/diplonema/mapping/DNA_to_contigs/1618/1618_D02-2.fa'
+base_name='/media/4TB1/diplonema/mapping/DNA_to_contigs/1601/1601_arrays_bw2'
+ref='/media/4TB1/diplonema/mapping/DNA_to_contigs/1601/1601_arrays.fasta'
 
 $bw2_dir'bowtie2-build' --threads 32 $ref $base_name
 
-read_dir='/media/4TB1/diplonema/reads/genome/used/1618/'
-p1_1=$read_dir'1618_D02_2_reads.fastq'
-# p1_2=$read_dir'1618_unmerged_deduplicated.fq'
+read_dir='/media/4TB1/diplonema/reads/genome/used/1601/'
+p1_1=$read_dir'1601_merged_deduplicated.fq'
+p1_2=$read_dir'1601_unmerged_deduplicated.fq'
 
 alignment=$base_name'.sam'
 report=$base_name'_report.txt'
@@ -16,8 +16,7 @@ unmapped_unpaired=$base_name'_unmapped_unpaired.fq'
 unmapped_paired=$base_name'_unmapped_paired.fq'
 
 $bw2_dir'bowtie2' --very-fast-local -p 32 -I 0 -X 5 \
--x $base_name -U $p1_1 --un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $alignment 2> $report
-# ,$p1_2
+-x $base_name -U $p1_1,$p1_2 --un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $alignment 2> $report
 
 samfile=$alignment
 bamfile=$base_name'_unsorted.bam'
