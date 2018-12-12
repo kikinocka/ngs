3752aad5cdcf330ac64d05b24b67569270eebdbd
KikaZah <kika.zahonova@gmail.com>
2018-12-12 12:11:17 +0100

    bla

--- bash_scripts/bbmerge_interleaved_trypka.sh
+++ bash_scripts/bbmerge_interleaved_trypka.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 read_dir='/media/4TB1/diplonema/reads/genome/used/1601/'
-reads=$read_dir'1601_used_reads.fastq'
+reads=$read_dir'1601_used_reads_rest.fastq'
 
-name='1601_'
+name='1601_rest_'
 merged=$read_dir$name'merged.fq'
 unmerged=$read_dir$name'unmerged.fq'
 report=$read_dir$name'report_bbmerge.txt'
