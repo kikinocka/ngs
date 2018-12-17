20c422799e580d523d96dcf2ce59a738b18bc34f
KikaZah <kika.zahonova@gmail.com>
2018-12-17 21:20:33 +0100

    bla

--- py_scripts/blastocrithidia/seqfire_indel_analysis.py
+++ py_scripts/blastocrithidia/seqfire_indel_analysis.py
@@ -4,15 +4,18 @@ import re
 from Bio import AlignIO
 from collections import defaultdict
 
-os.chdir('/home/kika/ownCloud/blastocrithidia/seqfire/complex_ins/')
+os.chdir('/media/4TB1/blastocrithidia/seqfire/apicomplexans_aln/')
 files = sorted(os.listdir())
-# aln_out = open('aln_len.tsv', 'w')
+aln_out = open('aln_len.tsv', 'w')
 
 def find_species(file):
 	species = set()
 	for line in open(file):
-		if '_' in line:
-			sp = line.split(' ')[0].split('_')[0]
+		if '__' in line:
+			# #tryps
+			# sp = line.split(' ')[0].split('_')[0]
+			#apicomplexans
+			sp = line.split('__')[1].split(' ')[0]#.replace('_', '')[:4]
 			species.add(sp)
 	return species
 
@@ -20,22 +23,22 @@ def find_ins(file, species):
 	ins_dict = defaultdict(list)
 	file_name = file.split('_')[0]
 	for line in open(file):
-		if line.startswith(species):
+		if species in line:
 			ins = re.sub(r'.*: \w  (.*)  \w', r'\g<1>', line)
 			ins = ins.replace('-', '').replace('  **', '')[:-1]
 			ins_dict[file_name].append(ins)
 	return ins_dict
 
 
-# for file in files:
-# 	if file.endswith('.aln'):
-# 		f_name = file.split('.')[0]
-# 		aln = AlignIO.read(file, 'fasta')
-# 		length = aln.get_alignment_length()
-# 		aln_out.write('{}\t{}\n'.format(f_name, length))
+for file in files:
+	if file.endswith('.aln'):
+		f_name = file.split('.')[0]
+		aln = AlignIO.read(file, 'fasta')
+		length = aln.get_alignment_length()
+		aln_out.write('{}\t{}\n'.format(f_name, length))
 
 for file in files:
-	if file.endswith('.indel'):
+	if file.endswith('_replaced.indel'):
 		print(file)
 		species = find_species(file)
 		for sp in species:
@@ -55,4 +58,4 @@ for file in files:
 						full_len += len(x)
 						if len(x) != 0:
 							non_zero += 1
-				out2.write('{}\t{}\t{}\t{}\n'.format(key, index, non_zero, full_len))
+					out2.write('{}\t{}\t{}\t{}\n'.format(key, index, non_zero, full_len))
