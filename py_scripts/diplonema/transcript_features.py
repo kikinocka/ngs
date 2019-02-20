f09ac1c7edcb9fbbeecf64752d19ae681fabaf09
KikaZah <kika.zahonova@gmail.com>
2019-02-20 13:03:27 +0100

    bla

--- py_scripts/diplonema/transcript_features.py
+++ py_scripts/diplonema/transcript_features.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/home/kika/MEGAsync/diplonema_mt/1618/transcripts/spades/gff/')
+os.chdir('/home/kika/MEGAsync/diplonema_mt/1621/transcripts/gff/')
 files = sorted(os.listdir())
-out = open('1618_transcripts_stats.tsv', 'w')
+out = open('1621_transcripts_stats.tsv', 'w')
 
-out.write('Gene\tNo. of modules\tA-to-I\tC-to-U\tNo. of SNPs\tU-appendage (length)\n')
+out.write('Gene\tNo. of modules\tA-to-I\tC-to-U\ttransam\tNo. of SNPs\tU-appendage (length)\n')
 
 for file in files:
 	if file.endswith('.gff'):
@@ -13,6 +13,7 @@ for file in files:
 		gene = file.split('_')[0]
 		A_to_I = 0
 		C_to_T = 0
+		transam = 0
 		SNP = 0
 		U_length = []
 		for line in open(file):
@@ -25,11 +26,13 @@ for file in files:
 					A_to_I += 1
 				if 'C>T' in line.split('\t')[8]:
 					C_to_T += 1
+			elif line.split('\t')[2] == 'Modified_base':
+				transam += 1
 			elif line.split('\t')[2] == 'polymorphism' or line.split('\t')[2] == 'SNP':
 				SNP += 1
 			elif line.split('\t')[2] == 'poly-u':
 				U_length.append(line.split('\t')[8].split('-pU')[1][:-1])
-		out.write('{}\t{}\t{}\t{}\t{}\t{} ({})\n'.format(gene, modules, A_to_I, C_to_T, SNP, len(U_length),
+		out.write('{}\t{}\t{}\t{}\t{}\t{}\t{} ({})\n'.format(gene, modules, A_to_I, C_to_T, transam, SNP, len(U_length),
 			str(U_length).replace('\'', '').replace('[', '').replace(']', '')))
 
 out.close()
