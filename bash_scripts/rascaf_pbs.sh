386754ed2001d6880c9bb12709bd604443f51b00
KikaZah <kika.zahonova@gmail.com>
2019-02-21 08:11:09 +0100

    bla

--- bash_scripts/rascaf_pbs.sh
+++ bash_scripts/rascaf_pbs.sh
@@ -14,10 +14,13 @@ cp pelomyxa_clean.fa $SCRATCHDIR
 cd /auto/brno3-cerit/nfs4/home/kika/pelomyxa/mapping/
 cp pelo_clean_bw2_sorted.bam $SCRATCHDIR
 
+cd /auto/brno2/home/kika/tools/rascaf/
+cp rascaf
+
 #compute on scratch
 cd $SCRATCHDIR
 
-rascaf='/auto/brno2/home/kika/tools/rascaf/rascaf'
+rascaf='rascaf'
 assembly='pelomyxa_clean.fa'
 bam_file='pelo_clean_bw2_sorted.bam'
 base_name='pelo_clean_rascaf'
@@ -25,4 +28,4 @@ base_name='pelo_clean_rascaf'
 $rascaf -b $bam -f $assembly -o $base_name
 
 #copy files back
-cp *rascaf* /auto/brno3-cerit/nfs4/home/kika/pelomyxa/mapping/.
+cp *pelo_clean_rascaf* /auto/brno3-cerit/nfs4/home/kika/pelomyxa/mapping/.
