a3d7083662768e30a8f8596c146c4bb1389742a8
KikaZah <kika.zahonova@gmail.com>
2019-02-25 12:59:20 +0100

    bla

--- bash_scripts/bowtie2_rna_pbs.sh
+++ bash_scripts/bowtie2_rna_pbs.sh
@@ -12,21 +12,21 @@ module add bowtie2-2.3.0
 module add samtools-1.3.1
 
 #copy files to scratch
-cd /auto/brno3-cerit/nfs4/home/kika/pelomyxa/genome_assembly/
+cd /auto/brno3-cerit/nfs4/home/kika/pelomyxa/genome_assembly/clean/
 cp pelomyxa_clean.fa $SCRATCHDIR
 
 cd /auto/brno3-cerit/nfs4/home/kika/pelomyxa/reads/transcriptome/
-cp *.fq.gz $SCRATCHDIR
+cp merged_trimmed* $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='pelo_clean_bw2'
+base_name='pelo_clean_merged_bw2'
 ref='pelomyxa_clean.fa'
 bowtie2-build --threads $PBS_NUM_PPN $ref $base_name
 
-p1_1='pelo1_trimmed_1.fq.gz','pelo2_trimmed_1.fq.gz','pelo3_trimmed_1.fq.gz','pelo5_trimmed_1.fq.gz','pelo6_trimmed_1.fq.gz'
-p1_2='pelo1_trimmed_2.fq.gz','pelo2_trimmed_2.fq.gz','pelo3_trimmed_2.fq.gz','pelo5_trimmed_2.fq.gz','pelo6_trimmed_2.fq.gz'
+p1_1='merged_trimmed_1.fq.gz'
+p1_2='merged_trimmed_2.fq.gz'
 
 samfile=$base_name'.sam'
 report=$base_name'.txt'
@@ -42,7 +42,7 @@ sorted_file=$sorted'.bam'
 samtools view -bS $samfile > $bamfile -@ $PBS_NUM_PPN
 bamfile='pelo_clean_bw2_unsorted.bam'
 sorted_file='pelo_clean_bw2_sorted.bam'
-samtools sort -o $sorted_file -@ 20 $bamfile 
+samtools sort -o $sorted_file -@ PBS_NUM_PPN $bamfile 
 samtools index $sorted_file
 
 #copy files back
