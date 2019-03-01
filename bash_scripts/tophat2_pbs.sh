ced22d145861eab25f602e8f8aa8671195c8e563
KikaZah <kika.zahonova@gmail.com>
2019-03-01 13:22:23 +0100

    bla

--- bash_scripts/tophat2_pbs.sh
+++ bash_scripts/tophat2_pbs.sh
@@ -13,31 +13,37 @@ module add bowtie2-2.3.0
 module add samtools-1.3.1
 
 #copy files to scratch
-cd /storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/clean/
-cp pelomyxa_clean.fa $SCRATCHDIR/pelo_clean_merged_bw2.fa
+# cd /storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/clean/
+# cp pelomyxa_clean.fa $SCRATCHDIR/pelo_clean_merged_bw2.fa
 
-cd /storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome/
-cp merged_trimmed* $SCRATCHDIR
+# cd /storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome/
+# cp merged_trimmed* $SCRATCHDIR
 
+cd /storage/brno3-cerit/home/kika/pelomyxa/mapping/tophat2/
+cp accepted_hits.bam $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-genome='pelo_clean_merged_bw2.fa'
-index='pelo_clean_merged_bw2'
-fwd='merged_trimmed_1.fq.gz'
-rv='merged_trimmed_2.fq.gz'
-out='tophat_out/'
+# genome='pelo_clean_merged_bw2.fa'
+# index='pelo_clean_merged_bw2'
+# fwd='merged_trimmed_1.fq.gz'
+# rv='merged_trimmed_2.fq.gz'
+# out='tophat_out/'
 bam='accepted_hits.bam'
 sam='accepted_hits.sam'
 
-bowtie2-build --threads $PBS_NUM_PPN $genome $index
+# bowtie2-build --threads $PBS_NUM_PPN $genome $index
 
-tophat2 -r 50 --mate-std-dev 50 -p $PBS_NUM_PPN -o $out $index $fwd $rv
+# tophat2 -r 50 --mate-std-dev 50 -i 30 -p $PBS_NUM_PPN -o $out $index $fwd $rv
 
-samtools view -h $out$bam > $out$sam
-samtools index $out$bam
+# samtools view -h $out$bam > $out$sam
+# samtools index $out$bam
+
+samtools view -h $out$bam > $sam
+samtools index $bam
 
 #copy files back
-cd $out
+# cd $out
+rm accepted_hits.bam
 cp -r * /storage/brno3-cerit/home/kika/pelomyxa/mapping/tophat2/.
