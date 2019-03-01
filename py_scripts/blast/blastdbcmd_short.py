caf6f9185d11cd10b938b0691204a569b5b36c94
KikaZah <kika.zahonova@gmail.com>
2019-03-01 14:30:52 +0100

    bla

--- py_scripts/blast/blastdbcmd_short.py
+++ py_scripts/blast/blastdbcmd_short.py
@@ -2,11 +2,11 @@
 import subprocess
 
 file = '/home/kika/MEGAsync/Euglena_longa/2013_Sekvenovanie/Phylloquinone/assembly_MenDHC/input.fasta'
-one = 'NODE_36_length_42357_cov_125.809'
+one = 'NODE_5002_length_1763_cov_68.7658'
 positions = '43693-47247'
 strand = 'plus'
-db = '/home/kika/programs/blast-2.5.0+/bin/jaculum_scaffolds_transc.fasta'
-out = '/home/kika/ownCloud/blastocrithidia/genes/aa-tRNA-synthetases/additional/jac_NODE.fa'
+db = '/home/kika/programs/blast-2.5.0+/bin/triat_scaffolds_transc.fasta'
+out = '/home/kika/ownCloud/blastocrithidia/genes/aa-tRNA-synthetases/additional/triat_NODE.fa'
 
 # subprocess.call('blastdbcmd -entry_batch {} -db {} -out {}'.format(file, db, out), shell=True)
 subprocess.call('blastdbcmd -entry {} -db {} -out {} -strand={}'.format(one, db, out, strand), 
