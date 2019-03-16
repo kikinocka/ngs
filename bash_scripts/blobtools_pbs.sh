2a771fb0d2eb2c258aef3358c883406bb92eddc0
KikaZah <kika.zahonova@gmail.com>
2019-03-16 14:09:00 +0100

    bla

--- bash_scripts/blobtools_pbs.sh
+++ bash_scripts/blobtools_pbs.sh
@@ -11,12 +11,9 @@ cat $PBS_NODEFILE
 module add blobtools-1.0
 
 #copy files to scratch
-cd /storage/brno3-cerit/home/kika/blobtools/
-cp prot.accession2taxid $SCRATCHDIR
-
 cd /storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/
-cp blobtools/pelo_trinity_dmnd_bx.out $SCRATCHDIR/pelo_trinity.out
 cp pelomyxa_trinity.fa $SCRATCHDIR
+cp blobtools/pelo_trinity.taxified.out $SCRATCHDIR
 
 cd /storage/brno3-cerit/home/kika/pelomyxa/mapping/bowtie2/RNA_to_transcriptome/
 cp pelo_trinity_bw2.sam $SCRATCHDIR
@@ -26,17 +23,14 @@ cp pelo_trinity_bw2.sam $SCRATCHDIR
 cd $SCRATCHDIR
 
 transcriptome='pelomyxa_trinity.fa'
-dmnd='pelo_trinity.out'
-taxmap='prot.accession2taxid'
 sam='pelo_trinity_bw2.sam'
 taxified='pelo_trinity.taxified.out'
 base='blobDB'
 
-blobtools taxify -f $dmnd -m $taxmap -s 0 -t 1
 blobtools create -i $transcriptome -s $sam -t $taxified -o $base
 blobtools view -i $base'.blobDB.json' --cov -o table
 blobtools plot -i $base'.blobDB.json' -o plot 
 
 #copy files back
-rm $transcriptome $dmnd $taxmap $sam 
+rm $transcriptome $sam $taxified
 cp -r * /storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/blobtools/. || export CLEAN_SCRATCH=false
