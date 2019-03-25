11744fbbc6f37c6fc8707a24317049a0cb7627ea
KikaZah <kika.zahonova@gmail.com>
2019-03-25 13:31:03 +0100

    bla

--- bash_scripts/ra.sh
+++ bash_scripts/ra.sh
@@ -3,8 +3,7 @@
 ra='/home/kika/tools/ra/build/bin/ra'
 read_dir='/media/4TB1/blastocrithidia/reads/genome/'
 tgs=$read_dir'pacbio/all_reads_merged.fq'
-fw=$read_dir'trimmed/p57_trimmed_1.fq'
-rv=$read_dir'trimmed/p57_trimmed_2.fq'
+hiseq=$read_dir'p57_all_reads_merged.fq'
 assembly='/media/4TB1/blastocrithidia/genome_assembly/p57_ra/p57_ra_assembly.fa'
 
-$ra -t 32 -x pb $tgs $fw $rv > $assembly
+$ra -t 32 -x pb $tgs $hiseq > $assembly
