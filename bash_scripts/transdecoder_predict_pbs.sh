bfd20858db2374733abed9ed5d7dcfc6e7acec31
KikaZah <kika.zahonova@gmail.com>
2019-05-06 14:25:56 +0200

    bla

--- bash_scripts/transdecoder_predict_pbs.sh
+++ bash_scripts/transdecoder_predict_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N TransDecoder_Predict
-#PBS -l select=1:ncpus=10:mem=20gb:scratch_local=50gb
-#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=50gb
+#PBS -l walltime=10:00:00
 #PBS -m ae
 #PBS -j oe
 
