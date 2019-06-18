f38c3d14decfd0f2afcd15b911c6b754cb3c12bd
KikaZah <kika.zahonova@gmail.com>
2019-06-18 15:15:44 +0200

    bla

--- bash_scripts/tophat2_pbs.sh
+++ bash_scripts/tophat2_pbs.sh
@@ -20,7 +20,7 @@ cd /storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/
 cp pelomyxa_final_genome.fa $SCRATCHDIR/pelo_final_bw2.fa
 
 cd /storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome/
-cp merged_trimmed* $SCRATCHDIR
+cp merged_trimmed_renamed* $SCRATCHDIR
 
 
 #compute on scratch
@@ -28,8 +28,8 @@ cd $SCRATCHDIR
 
 genome='pelo_final_bw2.fa'
 index='pelo_final_bw2'
-fwd='merged_trimmed_1.fq.gz'
-rv='merged_trimmed_2.fq.gz'
+fwd='merged_trimmed_renamed_1.fq.gz'
+rv='merged_trimmed_renamed_2.fq.gz'
 out='tophat_out/'
 bam=$out'accepted_hits.bam'
 sam=$out'accepted_hits.sam'
@@ -38,9 +38,9 @@ bowtie2-build --threads $PBS_NUM_PPN $genome $index
 
 tophat2 -r 50 --mate-std-dev 50 -i 30 -p $PBS_NUM_PPN -o $out $index $fwd $rv
 
-samtools view -bS $bam > $sam -@ $PBS_NUM_PPN
-samtools index $bam
+# samtools view -bS $bam > $sam -@ $PBS_NUM_PPN
+# samtools index $bam
 
 #copy files back
 cd $out
-cp -r * /storage/brno3-cerit/home/kika/pelomyxa/mapping/tophat2/pelomyxa_final/.
+cp -r * /storage/brno3-cerit/home/kika/pelomyxa/mapping/tophat2/for_augustus/.
