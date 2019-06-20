8a4a6891837cc047b2bc51d18066464c0583c361
KikaZah <kika.zahonova@gmail.com>
2019-06-20 11:50:03 +0200

    bla

--- bash_scripts/augustus_pbs.sh
+++ bash_scripts/augustus_pbs.sh
@@ -2,7 +2,7 @@
 #PBS -N Augustus
 #PBS -q default
 #PBS -l select=1:ncpus=1:mem=5gb:scratch_local=30gb:os=debian9
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -23,7 +23,7 @@ datadir='/storage/brno3-cerit/home/kika/pelomyxa/augustus/'
 #augustus runs on 1 core only
 cd $SCRATCHDIR
 
-# #1) CONVERT GFF FILE TO GENBANK
+# #1) CONVERT GFF FILE TO GENBANK (1 min, 250 MB)
 # cp $datadir'pelo_final.corrected.gff' $SCRATCHDIR
 # cp /storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/pelomyxa_final_genome.fa $SCRATCHDIR
 # gff2gbSmallDNA.pl pelo_final.corrected.gff pelomyxa_final_genome.fa 60 pelo_final.corrected.gb
@@ -39,22 +39,22 @@ cd $SCRATCHDIR
 # mkdir $augustus_configs/pelomyxa
 # cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/pelomyxa/.
 
-#4) MAKE AN INITIAL TRAINING
-cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
-etraining --species=pelomyxa pelo_final.corrected.gb
-cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
-rm pelo_final.corrected.gb
+# #4) MAKE AN INITIAL TRAINING (1 min, 150 MB)
+# cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
+# etraining --species=pelomyxa pelo_final.corrected.gb
+# cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
+# rm pelo_final.corrected.gb
 
 # #not run if 2) is skipped
 # cp $datadir'pelo_final.gb.test' $SCRATCHDIR
 # augustus --species=pelomyxa pelo_final.gb.test | tee pelo_first_test.out
 # cp pelo_first_test.out $datadir
 
-# #5) OPTIMIZE AUGUSTUS (~ 2.30h)
-# cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
-# optimize_augustus.pl --species=pelomyxa --cpus=$PBS_NUM_PPN pelo_final.corrected.gb
-# cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
-# rm pelo_final.corrected.gb
+#5) OPTIMIZE AUGUSTUS (~ 2.30h)
+cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
+optimize_augustus.pl --species=pelomyxa --cpus=$PBS_NUM_PPN pelo_final.corrected.gb
+cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
+rm pelo_final.corrected.gb
 
 # #6) RETRAIN AUGUSTUS
 # cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
@@ -62,7 +62,7 @@ rm pelo_final.corrected.gb
 # cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
 # rm pelo_final.corrected.gb
 
-# #7) PREDICT GENES (less than 4h)
+# #7) PREDICT GENES
 # cp '/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/pelomyxa_final_genome.fa' $SCRATCHDIR
 # augustus --protein=on --cds=on --outfile=pelo_augustus_fly.gff --species=fly pelomyxa_final_genome.fa
 # rm pelomyxa_final_genome.fa
