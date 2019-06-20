fd2f7799d5541c5c939523ca40fcecb9e67509e5
KikaZah <kika.zahonova@gmail.com>
2019-06-20 20:05:59 +0200

    bla

--- bash_scripts/augustus_pbs.sh
+++ bash_scripts/augustus_pbs.sh
@@ -39,22 +39,22 @@ cd $SCRATCHDIR
 # mkdir $augustus_configs/pelomyxa
 # cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/pelomyxa/.
 
-#4) MAKE AN INITIAL TRAINING (1 min, 150 MB)
-cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
-etraining --species=pelomyxa pelo_final.corrected.gb
-cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
-rm pelo_final.corrected.gb
+# #4) MAKE AN INITIAL TRAINING (1 min, 150 MB)
+# cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
+# etraining --species=pelomyxa pelo_final.corrected.gb
+# cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
+# rm pelo_final.corrected.gb
 
 # #not run if 2) is skipped
 # cp $datadir'pelo_final.gb.test' $SCRATCHDIR
 # augustus --species=pelomyxa pelo_final.gb.test | tee pelo_first_test.out
 # cp pelo_first_test.out $datadir
 
-# #5) OPTIMIZE AUGUSTUS (~ 1h, 150 MB)
-# cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
-# optimize_augustus.pl --species=pelomyxa --cpus=$PBS_NUM_PPN pelo_final.corrected.gb
-# cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
-# rm pelo_final.corrected.gb
+#5) OPTIMIZE AUGUSTUS (~ 1h, 150 MB)
+cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
+optimize_augustus.pl --species=pelomyxa --cpus=$PBS_NUM_PPN pelo_final.corrected.gb
+cp -r $SCRATCHDIR/augustus_configs/species/pelomyxa/* $augustus_configs/species/pelomyxa/.
+rm pelo_final.corrected.gb
 
 # #6) RETRAIN AUGUSTUS (2 min, 150 MB)
 # cp $datadir'pelo_final.corrected.gb' $SCRATCHDIR
