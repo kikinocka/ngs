2ee7d171451716369ed856e4af7ceb73834408ae
KikaZah <kika.zahonova@gmail.com>
2019-06-21 10:46:19 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 os.chdir('/home/kika/MEGAsync/diplonema_catalase/')
-files = [x for x in os.listdir() if x.endswith('.fasta')]
+files = [x for x in os.listdir() if x.endswith('.fas')]
 
 for file in files:
 	print(file)
