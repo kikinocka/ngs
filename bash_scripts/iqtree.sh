b3f084a79a67022a65b3091bbbd8fc4c1720a0ba
KikaZah <kika.zahonova@gmail.com>
2019-07-09 14:09:56 +0200

    bl

--- bash_scripts/iqtree.sh
+++ bash_scripts/iqtree.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-aln='/home/kika/ownCloud/euglenophytes/trees/q1010219/q1010219_trimal_automated1.aln'
-bb=1000
-alrt=5000
-nm=5000
+data_dir='/home/kika/MEGAsync/diplonema_paramylon/beta-1,3-glucan_synthase/domain/'
+aln=$data_dir'glucane_synthase_trimal_0.5.aln'
+guide=$data_dir'guide_glucane_synthase'
+guide_tree=$data_dir$guide'.treefile'
 
-iqtree-omp -s $aln -bb $bb -nt 4 -m TEST
+iqtree-omp -m LG+F+G -nt 4 -s $aln -pre $guide
+iqtree-omp -m LG+C20+F+G -nt 4 -bb $bb -s $aln -ft $guide_tree
 
+# iqtree -s $aln -bb $bb -nt AUTO -m TEST
+# iqtree-omp -s $aln -bb $bb -nt 4 -m TEST
 # -alrt $alrt -nm $nm
+# nm=5000
+# alrt=5000
