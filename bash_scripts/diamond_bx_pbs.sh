f579df19cfdfd6ae861fd19a6b8755fb775ff714
KikaZah <kika.zahonova@gmail.com>
2019-07-22 18:11:55 +0200

    bla

--- bash_scripts/diamond_bx_pbs.sh
+++ bash_scripts/diamond_bx_pbs.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 #PBS -N diamond-bx
-#PBS -l select=1:ncpus=20:mem=50gb:scratch_local=50gb
-#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=10:mem=50gb:scratch_local=100gb
+#PBS -l walltime=10:00:00
 #PBS -m ae
 #PBS -j oe
+
+#RUN IN ZOLI'S FOLDER !!!
 
 cat $PBS_NODEFILE
 
 #add module
 module add diamond-0.8.29
 
-#copy files to scratch
-cd /storage/brno3-cerit/home/fussyz01/DMND/
-cp refseq_ryby.dmnd $SCRATCHDIR/refseq.dmnd
+diamond_dir='/storage/brno3-cerit/home/fussyz01/DMND/'
+data_dir='/storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/'
 
-cd /storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/
-cp pelomyxa_trinity.fa $SCRATCHDIR
+#copy files to zoli's folder
+cp $data_dir'pelomyxa_transcriptome_clean.fa' $diamond_dir
 
-query='pelomyxa_trinity.fa'
-db='refseq.dmnd'
-out='pelo_trinity_dmnd_bx.out'
+query='pelomyxa_transcriptome_clean.fa'
+db='refseq_ryby.dmnd'
+out='pelo_clean_dmnd_bx.out'
 
 
-#compute on scratch
-cd $SCRATCHDIR
-
-diamond blastx -q $query -d $db -o $out -p $PBS_NUM_PPN -f 6 --sensitive --max-target-seqs 1 --evalue 1e-5
+#compute in zoli's folder
+cd $diamond_dir
+./diamond blastx -q $query -d $db -o $out -p $PBS_NUM_PPN -f 6 --sensitive --max-target-seqs 1 --evalue 1e-5
 
 #copy files back
-cp $out /storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/. || export CLEAN_SCRATCH=false
+cp $out $data_dir'blobtools/pelo_clean/.' || export CLEAN_SCRATCH=false
