c89bc024f2d474322ee4a40fb7895069c02249ce
KikaZah <kika.zahonova@gmail.com>
2019-07-22 16:12:46 +0200

    bla

--- bash_scripts/bowtie2_rna_pbs.sh
+++ bash_scripts/bowtie2_rna_pbs.sh
@@ -13,18 +13,18 @@ module add samtools-1.3.1
 
 #copy files to scratch
 cd /storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/
-cp pelomyxa_trinity.fa $SCRATCHDIR
+cp pelomyxa_transcriptome_clean.fa $SCRATCHDIR
 
 cd /storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome
-cp merged_trimmed* $SCRATCHDIR
+cp merged_trimmed_1.fq.gz $SCRATCHDIR
+cp merged_trimmed_2.fq.gz $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='pelo_trinity_bw2'
-ref='pelomyxa_trinity.fa'
-
+base_name='pelo_trinity_clean_bw2'
+ref='pelomyxa_transcriptome_clean.fa'
 p1_1='merged_trimmed_1.fq.gz'
 p1_2='merged_trimmed_2.fq.gz'
 
@@ -37,7 +37,6 @@ bamfile=$base_name'_unsorted.bam'
 sorted=$base_name'_sorted.bam'
 
 bowtie2-build --threads $PBS_NUM_PPN $ref $base_name
-
 bowtie2 --very-sensitive -p $PBS_NUM_PPN -x $base_name -1 $p1_1 -2 $p1_2 --un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $samfile 2> $report
 
 samtools view -bS $samfile > $bamfile -@ $PBS_NUM_PPN
@@ -45,4 +44,4 @@ samtools sort -o $sorted -@ PBS_NUM_PPN $bamfile
 samtools index $sorted
 
 #copy files back
-cp *bw2* /storage/brno3-cerit/home/kika/pelomyxa/mapping/bowtie2/RNA_to_transcriptome/.
+cp *bw2* /storage/brno3-cerit/home/kika/pelomyxa/mapping/bowtie2/RNA_to_transcriptome/pelo_clean/.
