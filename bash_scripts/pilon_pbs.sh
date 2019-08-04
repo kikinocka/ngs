968a736f6d5b7db83d49f17e5f1c77032fa4026c
KikaZah <kika.zahonova@gmail.com>
2019-08-04 10:49:19 +0200

    bla

--- bash_scripts/pilon_pbs.sh
+++ bash_scripts/pilon_pbs.sh
@@ -13,11 +13,11 @@ module add openjdk-10
 data_dir='/storage/brno3-cerit/home/kika/p57/'
 
 #copy files to scratch
-cp '/storage/brno2/home/kika/tools/pilon-1.23.jar' $SCRATCHDIR
 cp $data_dir'p57_ra.fa' $SCRATCHDIR
-cp $data_dir'p57_ra_DNA_bw2_sorted.bam*' $SCRATCHDIR
+cp $data_dir'p57_ra_DNA_bw2_sorted.bam' $SCRATCHDIR
+cp $data_dir'p57_ra_DNA_bw2_sorted.bam.bai' $SCRATCHDIR
 
-pilon='pilon-1.23.jar'
+pilon='/storage/brno2/home/kika/tools/pilon-1.23.jar'
 assembly='p57_ra.fa'
 bam='p57_ra_DNA_bw2_sorted.bam'
 index='p57_ra_DNA_bw2_sorted.bam.bai'
