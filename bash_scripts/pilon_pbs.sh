0091662ffcf07b7888050dc9695f21569d1bcf1d
KikaZah <kika.zahonova@gmail.com>
2019-08-08 13:08:17 +0200

    bla

--- bash_scripts/pilon_pbs.sh
+++ bash_scripts/pilon_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N Pilon
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=100gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,14 +13,14 @@ module add openjdk-10
 data_dir='/storage/brno3-cerit/home/kika/p57/'
 
 #copy files to scratch
-cp $data_dir'pilon1/p57_pilon1.fa' $SCRATCHDIR
-cp $data_dir'pilon1/p57_pilon1_bw2_sorted.bam' $SCRATCHDIR
-cp $data_dir'pilon1/p57_pilon1_bw2_sorted.bam.bai' $SCRATCHDIR
+cp $data_dir'pilon2/p57_pilon2.fa' $SCRATCHDIR
+cp $data_dir'pilon2/p57_pilon2_bw2_sorted.bam' $SCRATCHDIR
+cp $data_dir'pilon2/p57_pilon2_bw2_sorted.bam.bai' $SCRATCHDIR
 
 pilon='/storage/brno2/home/kika/tools/pilon-1.23.jar'
-assembly='p57_pilon1.fa'
-bam='p57_pilon1_bw2_sorted.bam'
-index='p57_pilon1_bw2_sorted.bam.bai'
+assembly='p57_pilon2.fa'
+bam='p57_pilon2_bw2_sorted.bam'
+index='p57_pilon2_bw2_sorted.bam.bai'
 
 #compute on scratch
 cd $SCRATCHDIR
