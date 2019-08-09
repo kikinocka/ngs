7d969bcdede0fb0dec50af5ea6e077e47b17aa76
KikaZah <kika.zahonova@gmail.com>
2019-08-09 22:06:10 +0200

    bla

---
+++ bash_scripts/pasa_evm/awk_name.sh
@@ -0,0 +1,19 @@
+
+###
+FNR == 1 { 
+count = 1
+
+gsub(/evm\.[a-zA-Z]+\.scaffold[0-9]+-[0-9]+\.[0-9]+/,"BlNa"count)
+print # print the line after replace
+}
+FNR > 1 { 
+	if ($1 == "###"){ # it is a new gene so moving on
+	count++
+	print $0
+	}
+	else{ # still the same gene
+		gsub(/evm\.[a-zA-Z]+\.scaffold[0-9]+-[0-9]+\.[0-9]+/,"BlNa"count)
+		print
+	}
+}
+
