ec1d8f6d0ef1712de109a0ec5bd9d6daf389f9ce
KikaZah <kika.zahonova@gmail.com>
2019-08-09 13:25:25 +0200

    bla

--- bash_scripts/pasa_evm/evm_step4.sh
+++ bash_scripts/pasa_evm/evm_step4.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#change WEIGHTS
 
 evm_path='/opt/evm/EVM_r2012-06-25/EvmUtils'
 
 genome='/home/kika/pelomyxa_schiedti/genome_assembly/pelomyxa_final_genome.fa'
-transdecoder='pasa1/pelomyxa_pasa_mysql.assemblies.fasta.transdecoder.gff3'
-pasa_gff='pasa2/pelomyxa_pasa_mysql.gene_structures_post_PASA_updates.23203.gff3'
-
-#converting augustus gff 
-augustus='/home/kika/pelomyxa_schiedti/augustus/pelo_augustus_100_hints.gff'
-prediction='pelo_augustus_100_hints.gff3'
-$evm_path'misc/augustus_to_GFF3.pl' $augustus > $prediction
+transdecoder='pasa1/pelomyxa_pasa_mysql.assemblies.fasta.transdecoder.genome.gff3'
+pasa_genes='pasa2/pelomyxa_pasa_mysql.gene_structures_post_PASA_updates.updated.gff3'
+pasa_gff='pasa1/pelomyxa_pasa_mysql.pasa_assemblies.gff3'
 
 #merging the prediction
-merged='evm1/all_merged.gff3'
-cat $prediction $transdecoder > $merged
+merged='evm2/all_merged.gff3'
+cat $pasa_genes $transdecoder > $merged
 
 #generate partitions
 partitions='partitions_list.out'
