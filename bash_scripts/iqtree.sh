45ce32a9d75464f9ffce10d2931356a44e08d665
KikaZah <kika.zahonova@gmail.com>
2019-08-22 19:53:33 +0200

    bla

--- bash_scripts/iqtree.sh
+++ bash_scripts/iqtree.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-data_dir='/home/kika/MEGAsync/diplonema/catalase/'
-aln=$data_dir'apx_tree/apx_trimal_0.5.aln'
+data_dir='/home/kika/MEGAsync/diplonema/catalase/apx_tree/'
+aln=$data_dir'apx_trimal_0.5.aln'
 guide=$data_dir'guide_apx'
 guide_tree=$guide'.treefile'
 bb=1000
 
 iqtree -m LG+F+G -nt 4 -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt 4 -bb $bb -s $aln -ft $guide_tree
+# iqtree -m LG+C20+F+G -nt 4 -bb $bb -s $aln -ft $guide_tree
 
 # iqtree -s $aln -bb $bb -nt AUTO -m TEST
 # iqtree-omp -s $aln -bb $bb -nt 4 -m TEST
--- bash_scripts/iqtree_pbs.sh
+++ bash_scripts/iqtree_pbs.sh
@@ -20,12 +20,13 @@ cp $datadir'apx_trimal_0.5.aln' $SCRATCHDIR
 cd $SCRATCHDIR
 aln='apx_trimal_0.5.aln'
 guide='guide_apx'
-guide_tree=$guide'.treefile'
+# guide_tree=$guide'.treefile'
+guide_tree='guide_apx.treefile'
 bb=1000
 
 # iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST
 
-iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
 iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree
 
 #copy files back
