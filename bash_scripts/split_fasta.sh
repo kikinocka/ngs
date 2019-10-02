2c48d3bc16925b3705e7281f28db8cd9ad475c58
KikaZah <kika.zahonova@gmail.com>
2019-10-02 12:58:51 +0200

    bla

--- bash_scripts/split_fasta.sh
+++ bash_scripts/split_fasta.sh
@@ -3,5 +3,5 @@
 workdir='/home/kika/pelomyxa_schiedti/predicted_proteins/'
 seqs=$workdir'pelo_transcriptome_clean.fa.transdecoder.5prime_complete.clustered.pep'
 
-awk 'BEGIN {n_seq=0;} /^>/ {if(n_seq%1500==0){file=sprintf("myseq%d.fa",n_seq);} print >> file; n_seq++; next;} \
+awk 'BEGIN {n_seq=0;} /^>/ {if(n_seq%1200==0){file=sprintf("myseq%d.fa",n_seq);} print >> file; n_seq++; next;} \
 { print >> file; }' < $seqs
