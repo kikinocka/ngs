593380a91aa26ebb52eb25b5b05b349aa9f1ae31
KikaZah <kika.zahonova@gmail.com>
2019-10-02 10:31:47 +0200

    bla

--- bash_scripts/targetp_veles.sh
+++ bash_scripts/targetp_veles.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-targetp='/home/kika/tools/targetp-1.1/targetp'
 workdir='/home/kika/pelomyxa_schiedti/predicted_proteins/'
 infile=$workdir'pelo_transcriptome_clean.fa.transdecoder.5prime_complete.clustered.pep'
 outfile=$workdir'pelo_transcriptome_clean.fa.transdecoder.5prime_complete.clustered.targetp.txt'
 plant='P'
 non_plant='N'
 
-$targetp -$non_plant -c $infile > $outfile
+targetp -$non_plant -c $infile > $outfile
