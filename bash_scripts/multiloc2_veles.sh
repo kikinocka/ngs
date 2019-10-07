586e5f75d63b9994cb5b9c062291ff5ef7d2336b
KikaZah <kika.zahonova@gmail.com>
2019-10-07 09:55:55 +0200

    bla

--- bash_scripts/multiloc2_veles.sh
+++ bash_scripts/multiloc2_veles.sh
@@ -2,8 +2,8 @@
 
 multiloc='/home/kika/tools/MultiLoc2/src/multiloc2_prediction.py'
 work_dir='/home/kika/pelomyxa_schiedti/predicted_proteins/'
-fa=$work_dir'myseq3000.fa'
-r=$work_dir'myseq3000.multiloc_animal.txt'
+fa=$work_dir'myseq7000.fa'
+r=$work_dir'myseq7000.multiloc_animal.txt'
 a=animal
 p=plant
 f=fungal
