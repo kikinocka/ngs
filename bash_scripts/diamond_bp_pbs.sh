e78a6783e2641a1f319727bb6d9fd385ebf94393
KikaZah <kika.zahonova@gmail.com>
2019-10-11 21:27:45 +0200

    bla

--- bash_scripts/diamond_bp_pbs.sh
+++ bash_scripts/diamond_bp_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N diamond-bp
 #PBS -l select=1:ncpus=10:mem=50gb:scratch_local=100gb
-#PBS -l walltime=10:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,19 +13,20 @@ cat $PBS_NODEFILE
 module add diamond-0.8.29
 
 diamond_dir='/storage/brno3-cerit/home/fussyz01/DMND/'
-data_dir='/storage/brno3-cerit/home/kika/pelomyxa/predicted_proteins_transdecoder/'
+data_dir='/storage/brno3-cerit/home/kika/diplonema/catalase/apx_tree/ver17/'
 
 #copy files to zoli's folder
-cp $data_dir'pelo.mit_predicted.fa' $diamond_dir
+cp $data_dir'1618.fa' $diamond_dir
 
-query='pelo.mit_predicted.fa'
-db='nr2.dmnd'
-out='pelo.mit_predicted.dmnd_bp.out'
+query='1618.fa'
+db='refseq_excavata.dmnd'
+out='1618.dmnd_bp.out'
 
 
 #compute in zoli's folder
 cd $diamond_dir
-./diamond blastp -p $PBS_NUM_PPN -d $db -q $query -o $out -f 6 qseqid sseqid stitle evalue bitscore full_sseq --sensitive --max-target-seqs 1 --evalue 1e-5
+./diamond blastp -p $PBS_NUM_PPN -d $db -q $query -o $out -f 6 qseqid sseqid stitle evalue bitscore full_sseq --sensitive --max-target-seqs 30 --evalue 1e-4
 
 #copy files back
-cp $out $data_dir/.
+rm $query
+mv $out $data_dir/.
