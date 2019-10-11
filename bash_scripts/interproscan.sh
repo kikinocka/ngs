508e9b0efab4d3a77c9207f15f87c925d269804a
KikaZah <kika.zahonova@gmail.com>
2019-10-11 12:23:49 +0200

    bla

--- bash_scripts/interproscan.sh
+++ bash_scripts/interproscan.sh
@@ -11,16 +11,18 @@ cat $PBS_NODEFILE
 #add module
 module add interproscan-5.20-59.0
 
-datadir='/storage/brno3-cerit/home/kika/elonga/'
+datadir='/storage/brno3-cerit/home/kika/pelomyxa/predicted_proteins_transdecoder/'
 
 #copy files to scratch
-cp $datadir'EL_hits_aa.fa' $SCRATCHDIR
+cp $datadir'pelo.mit_predicted.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
+input='pelo.mit_predicted.fa'
+out='pelo.mit_predicted.interpro'
 
-interproscan.sh -dp -f TSV,GFF3 -T $SCRATCHDIR -i EL_hits_aa.fa -b EL_hits_interpro -appl PRINTS,Pfam,Hamap,ProSitePatterns,ProSiteProfiles,Panther -goterms --pathways -iprlookup
+interproscan.sh -dp -f TSV,GFF3 -T $SCRATCHDIR -i $input -b $out -appl PRINTS,Pfam,Hamap,ProSitePatterns,ProSiteProfiles,Panther -goterms --pathways -iprlookup
 
 #copy files back
-rm EL_hits_aa.fa
+rm $input
 cp -r * $datadir
