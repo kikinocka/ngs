a46c9ac08923350dcae06144ea7ac07557184aa5
KikaZah <kika.zahonova@gmail.com>
2019-11-05 20:09:15 +0100

    bla

--- bash_scripts/blast_many_pbs.sh
+++ bash_scripts/blast_many_pbs.sh
@@ -17,7 +17,7 @@ outfmt=7
 word=3
 files=$datadir'*.fna'
 
-for db in files; do
+for db in $files; do
 	echo $db
 	out=${db%.*}'.tblastn.out'
 	$program -query $query -db $db -out $out -outfmt $outfmt -word_size $word -num_threads $PBS_NUM_PPN
