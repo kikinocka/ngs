10e9c72de6f9dde2825143869394d2d087a6f397
KikaZah <kika.zahonova@gmail.com>
2019-11-06 10:43:41 +0100

    bla

--- py_scripts/blast/check_BBH.py
+++ py_scripts/blast/check_BBH.py
@@ -5,7 +5,7 @@ import os
 os.chdir('/storage/brno3-cerit/home/kika/elonga_bct_genomes/')
 files = [x for x in os.listdir() if x.endswith('.tblastn.out')]
 
-with open('bct_SDR_a4.presnce.tsv', 'w') as result:
+with open('bct_SDR_a4.presence.tsv', 'w') as result:
 	for file in files:
 		print(file)
 		for line in open(file):
@@ -13,5 +13,5 @@ with open('bct_SDR_a4.presnce.tsv', 'w') as result:
 				pass
 			else:
 				if float(line.split('\t')[10]) < 0.001:
-					new_line = '{}\t1\n'.format(file.split('_')[0]+file.split('_')[1])
+					new_line = '{}\t1\t\n'.format(file.split('_')[0]+file.split('_')[1], line.split('\t')[10])
 					result.write(new_line)
