889512f686e3821ac3398d9ca8a3086fd904fcf2
KikaZah <kika.zahonova@gmail.com>
2019-11-06 10:46:28 +0100

    bla

--- py_scripts/blast/check_BBH.py
+++ py_scripts/blast/check_BBH.py
@@ -13,5 +13,5 @@ with open('bct_SDR_a4.presence.tsv', 'w') as result:
 				pass
 			else:
 				if float(line.split('\t')[10]) < 0.001:
-					new_line = '{}\t1\t\n'.format(file.split('_')[0]+file.split('_')[1], line.split('\t')[10])
+					new_line = '{}\t1\t{}\n'.format(file.split('_')[0]+file.split('_')[1], line.split('\t')[10])
 					result.write(new_line)
