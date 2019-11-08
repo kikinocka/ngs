0bf396ad9b6df5200748884357b1bc4bb0a89488
KikaZah <kika.zahonova@gmail.com>
2019-11-08 12:21:26 +0100

    bla

--- bash_scripts/trim_galore.sh
+++ bash_scripts/trim_galore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -N trim_galore
-#PBS -l select=1:ncpus=20:mem=100gb:scratch_local=100gb
+#PBS -l select=1:ncpus=10:mem=10gb:scratch_local=50gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
