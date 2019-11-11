e2240bca01d7a3e9190c649722f650aef81330a4
KikaZah <kika.zahonova@gmail.com>
2019-11-11 14:37:14 +0100

    bla

--- bash_scripts/bbmap_trypka.sh
+++ bash_scripts/bbmap_trypka.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 bbmap='/home/kika/tools/bbmap/bbmap.sh'
-read_dir='/media/4TB1/novymonas/transcriptome/reads/trimmed_reads/'
-fw=$read_dir'wt_rna_trimmed_1.fq.gz'
-rv=$read_dir'wt_rna_trimmed_2.fq.gz'
+read_dir='/home/kika/work_dir/'
+fw=$read_dir'SRR2048655_trimmed_1.fq.gz'
+rv=$read_dir'SRR2048655_trimmed_2.fq.gz'
 # all=$read_dir'EG_GEFR_SRR2628535.1.fq'
-ref='/media/4TB1/novymonas/transcriptome/assembly/genome_based_trinity/Trinity-GG.fasta'
+ref='/home/kika/work_dir/TriTrypDB-46_LseymouriATCC30220_AnnotatedCDSs.fasta'
 
 out_dir='/home/kika/work_dir/'
-sam=$out_dir'novymonas_bb_rna.sam'
-rpkm=$out_dir'novymonas_rpkm.txt'
-report=$out_dir'novymonas_bbmap.report'
+sam=$out_dir'lsey_35_bb_rna.sam'
+rpkm=$out_dir'lsey_35_rpkm.txt'
+report=$out_dir'lsey_35_bbmap.report'
 
-$bbmap in=$fw in2=$rv out=$sam ref=$ref rpkm=$rpkm threads=20 2> $report
+$bbmap in=$fw in2=$rv out=$sam ref=$ref rpkm=$rpkm threads=30 2> $report
 # $bbmap in=$all out=$sam ref=$ref rpkm=$rpkm threads=30 2> $report
--- bash_scripts/quast.sh
+++ bash_scripts/quast.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-f='/home/kika/ownCloud/pelomyxa_schiedti/transcriptome_assembly/pelomyxa_transcriptome_clean.fa'
-output='/home/kika/ownCloud/pelomyxa_schiedti/transcriptome_assembly/quast/pelo_clean/'
+f='/home/kika/ownCloud/SAGs/reassembly/EU1718_contigs.fa'
+output='/home/kika/ownCloud/SAGs/reassembly/quast/'
 
 /usr/bin/python3.5 /home/kika/programs/quast-4.4/quast.py $f -o $output -t 4
