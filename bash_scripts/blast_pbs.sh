26d119709d52afa3a40098d47d344e6cd8ea6adb
KikaZah <kika.zahonova@gmail.com>
2019-11-13 18:41:52 +0100

    bla

--- bash_scripts/blast_pbs.sh
+++ bash_scripts/blast_pbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N tblastn
+#PBS -N blastn-megablast
 #PBS -l select=1:ncpus=20:mem=50gb:scratch_local=100gb
 #PBS -l walltime=24:00:00
 #PBS -m ae
@@ -15,7 +15,7 @@ query=$datadir'spades/contigs.fasta'
 out=$datadir'blast/EU1718.fa_vs_nt_1e-10.megablast'
 report=$datadir'blast/EU1718.fa_vs_nt_1e-10.megablast.report'
 
-program=blast
+program=blastn
 task=megablast
 db='/storage/brno3-cerit/home/kika/ncbi_db/nt'
 outfmt='6 qseqid staxids bitscore std'
