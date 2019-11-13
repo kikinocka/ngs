3dc3bf0e47a482ff3823b53dfc1872bcbc2986be
KikaZah <kika.zahonova@gmail.com>
2019-11-13 16:19:05 +0100

    bla

--- bash_scripts/get_sra_fastq.sh
+++ bash_scripts/get_sra_fastq.sh
@@ -1,4 +1,40 @@
 #!/bin/sh
 
-raw_dir='/home/kika/'
-fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip -O $raw_dir SRR2048652
+# raw_dir='/home/kika/'
+# fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip -O $raw_dir SRR2048652
+
+
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos/sra-pub-run-2/SRR2048653/SRR2048653.1
+echo 'SRA SRR2048653.1 downloaded'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip ./SRR2048653.1
+echo 'SRA SRR2048653.1 split to pair-end files'
+echo '-----------------------------'
+echo
+
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos/sra-pub-run-2/SRR2048654/SRR2048654.1
+echo 'SRA SRR2048654.1 downloaded'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip ./SRR2048654.1
+echo 'SRA SRR2048654.1 split to pair-end files'
+echo '-----------------------------'
+echo
+
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos/sra-pub-run-2/SRR2048655/SRR2048655.1
+echo 'SRA SRR2048655.1 downloaded'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip ./SRR2048655.1
+echo 'SRA SRR2048655.1 split to pair-end files'
+echo '-----------------------------'
+echo
+
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos/sra-pub-run-2/SRR2048656/SRR2048656.1
+echo 'SRA SRR2048656.1 downloaded'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip ./SRR2048656.1
+echo 'SRA SRR2048656.1 split to pair-end files'
+echo '-----------------------------'
+echo
+
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos/sra-pub-run-2/SRR2048657/SRR2048657.1
+echo 'SRA SRR2048657.1 downloaded'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip ./SRR2048657.1
+echo 'SRA SRR2048657.1 split to pair-end files'
+echo '-----------------------------'
+echo
