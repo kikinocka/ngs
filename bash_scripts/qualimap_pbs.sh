b9d431eda4472c850a9b9a42fb2e213cd1412bc5
KikaZah <kika.zahonova@gmail.com>
2019-11-13 13:24:42 +0100

    bla

--- bash_scripts/qualimap_pbs.sh
+++ bash_scripts/qualimap_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N qualiMap
 #PBS -q default
-#PBS -l select=1:ncpus=15:mem=1gb:scratch_local=10gb:os=debian9
+#PBS -l select=1:ncpus=10:mem=1gb:scratch_local=10gb:os=debian9
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
@@ -11,13 +11,13 @@ cat $PBS_NODEFILE
 #add module
 module ad qualiMap-11_12-16
 
-mapping='/storage/brno3-cerit/home/kika/sags/reassembly/mapping_bwa/'
-outdir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/qualimap_bwa/'
+mapping='/storage/brno3-cerit/home/kika/sags/reassembly/mapping/bowtie2/'
+outdir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/qualimap/bowtie2/'
 
 #copy files to scratch
-cp $mapping'EU1718_bwa_mapped_all.sorted.bam' $SCRATCHDIR
+cp $mapping'EU1718_bw2_sorted.bam' $SCRATCHDIR
 
-bam='EU1718_bwa_mapped_all.sorted.bam'
+bam='EU1718_bw2_sorted.bam'
 
 
 #compute on scratch
