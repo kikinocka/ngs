33b1caa0fb98ebd458a7709f6a79c070b7994977
KikaZah <kika.zahonova@gmail.com>
2019-11-14 17:46:01 +0100

    bla

--- bash_scripts/quast_pbs.sh
+++ bash_scripts/quast_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N Quast
 #PBS -l select=1:ncpus=10:mem=3gb:scratch_local=100gb
-#PBS -l walltime=00:10:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,19 +10,24 @@ cat $PBS_NODEFILE
 #add module
 module add quast-4.6.3
 
-assembly='/auto/brno3-cerit/nfs4/home/kika/pelomyxa/genome_assembly/all_reads_k-mers/K121/'
-outdir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/all_reads_k-mers/quast/k121/'
+assembly='/storage/brno3-cerit/home/kika/sags/reassembly/spades/'
+mapping='/auto/brno3-cerit/nfs4/home/kika/sags/reassembly/mapping/bbmap/'
+outdir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/'
 
 #copy assembly to scratch
-cd $assembly
-cp final_contigs.fasta $SCRATCHDIR
+cp $assembly'contigs.fasta' $SCRATCHDIR
+cp $mapping'EU1718_bbm_mapped_all.sorted.bam' $SCRATCHDIR
 
-f='final_contigs.fasta'
-output='k121/'
+f='contigs.fasta'
+bam='EU1718_bbm_mapped_all.sorted.bam'
+output='quast/'
+min_contig=500
+
 
 #compute on scratch
-quast.py $f -o $output -t $PBS_NUM_PPN
+cd $SCRATCHDIR
+quast.py -o $output -t PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote --bam $bam $f
 
 #copy results to your folder
-cd output
-cp -R * $outdir
+rm $f $bam
+cp -r * $outdir
