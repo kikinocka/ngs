3c43c681f2a93edb3e1c017aae1b7c2243013cd0
KikaZah <kika.zahonova@gmail.com>
2019-11-14 15:20:39 +0100

    bla

--- bash_scripts/bbmap_rpkm_pbs.sh
+++ bash_scripts/bbmap_rpkm_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -N bbmap_rpkm
-#PBS -l select=1:ncpus=20:mem=10gb:scratch_local=50gb
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=50gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
@@ -10,19 +10,19 @@ cat $PBS_NODEFILE
 #add module
 module add bbmap-36.92
 
-data='/storage/brno3-cerit/home/kika/cther/'
+data='/storage/brno3-cerit/home/kika/lsey/'
 
 #copy files to scratch
-cp $data'Trinity_job_11485672.fasta' $SCRATCHDIR
-cp $data'37all_trimmed_1.fq.gz' $data'37all_trimmed_2.fq.gz' $SCRATCHDIR
+cp $data'TriTrypDB-46_LseymouriATCC30220_AnnotatedCDSs.fasta' $SCRATCHDIR
+cp $data'23all_1.fq.gz' $data'23all_2.fq.gz' $SCRATCHDIR
 
-assembly='Trinity_job_11485672.fasta'
-fw='37all_trimmed_1.fq.gz'
-rv='37all_trimmed_2.fq.gz'
+assembly='TriTrypDB-46_LseymouriATCC30220_AnnotatedCDSs.fasta'
+fw='23all_1.fq.gz'
+rv='23all_2.fq.gz'
 
-sam=$out_dir'cther_37_bbmap_rna.sam'
-rpkm=$out_dir'cther_37_bbmap.rpkm'
-report=$out_dir'cther_37_bbmap.report'
+sam=$out_dir'lsey_23_bbmap_rna.sam'
+rpkm=$out_dir'lsey_23_bbmap.rpkm'
+report=$out_dir'lsey_23_bbmap.report'
 
 
 #compute on scratch
