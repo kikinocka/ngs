19acb1ef029d4b10ce064553b3400d9c4fc1f527
KikaZah <kika.zahonova@gmail.com>
2019-11-14 17:48:53 +0100

    bla

--- bash_scripts/blast_pbs.sh
+++ bash_scripts/blast_pbs.sh
@@ -14,15 +14,17 @@ datadir='/storage/brno3-cerit/home/kika/sags/reassembly/'
 query=$datadir'spades/contigs.fasta'
 out=$datadir'blast/EU1718.fa_vs_nt_1e-10.megablast'
 report=$datadir'blast/EU1718.fa_vs_nt_1e-10.megablast.report'
+db_dir='/storage/brno3-cerit/home/kika/ncbi_db/'
 
 program=blastn
 task=megablast
-db='/storage/brno3-cerit/home/kika/ncbi_db/nt'
+db='nt'
 # outfmt=6 'qseqid staxids bitscore std'
 max_seqs=1
 max_hsps=1
 evalue=1e-10
 
-
+#run in DB folder
+cd $db_dir
 $program -task $task -query $query -db $db -out $out -num_threads $PBS_NUM_PPN \
 -outfmt '6 qseqid staxids bitscore std' -max_target_seqs $max_seqs -max_hsps $max_hsps -evalue $evalue 2>$report
