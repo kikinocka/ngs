27b4548ca44fd42be1051a0a8ce31b9a2f26d7d8
KikaZah <kika.zahonova@gmail.com>
2019-11-20 12:30:28 +0100

    bla

--- bash_scripts/blobtools_pbs.sh
+++ bash_scripts/blobtools_pbs.sh
@@ -10,67 +10,53 @@ cat $PBS_NODEFILE
 #add module
 module add blobtools-1.0
 
-cp /storage/brno3-cerit/home/kika/sags/reassembly/mapping/bbmap/EU1718_bbm_mapped_all.sorted.bam $SCRATCHDIR
-cp /storage/brno3-cerit/home/kika/sags/reassembly/spades/contigs.fasta $SCRATCHDIR
+assembly_dir='/storage/brno3-cerit/home/kika/sags/reassembly/spades/'
+blast_dir='/storage/brno3-cerit/home/kika/sags/reassembly/blast/'
+mapping_dir='/storage/brno3-cerit/home/kika/sags/reassembly/mapping/bbmap/'
+ncbi='/storage/brno3-cerit/home/kika/ncbi_db/'
+blob_dir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/blobtools/'
+
+#copy files to scratch
+cp $assembly_dir'contigs.fasta' $SCRATCHDIR
+cp $blast_dir'EU1718.fa_vs_nt_1e-10.megablast' $SCRATCHDIR
+cp $mapping_dir'EU1718_bbm_mapped_all.sorted.bam' $SCRATCHDIR
+cp $ncbi'nodes.dmp' $ncbi'names.dmp' $SCRATCHDIR
+
+#compute on scratch
+cd $SCRATCHDIR
+mkdir tables
+mkdir images
 
 genome='contigs.fasta'
+blast='EU1718.fa_vs_nt_1e-10.megablast'
 bam='EU1718_bbm_mapped_all.sorted.bam'
-out='EU1718_blobtools'
+tax_nodes='nodes.dmp'
+tax_names='names.dmp'
+base='EU1718_blobtools'
 
-cd $SCRATCHDIR
-blobtools map2cov -i $genome -b $bam -o $out
+echo 'Running BlobTools create'
+blobtools create -i $genome --nodes $tax_nodes --names $tax_names -t $blast -b $bam -o $base
+echo 'Running BlobTools View - Phylum'
+blobtools view -i $base'.blobDB.json' --out tables/phylum --rank 'phylum'
+echo 'Running BlobTools View - Super Kingdom'
+blobtools view -i $base'.blobDB.json' --out tables/superkingdom --rank 'superkingdom'
+echo 'Running BlobTools Plot PNG - Phylum'
+blobtools plot -i $base'.blobDB.json' --out images/. --rank 'phylum' 
+echo 'Running BlobTools Plot PNG - Super Kingdom'
+blobtools plot -i $base'.blobDB.json' --out images/. --rank 'superkingdom' 
+echo 'Running BlobTools Plot SVG - Phylum'
+blobtools plot -i $base'.blobDB.json' --out images/. --rank 'phylum' --format 'svg'
+echo 'Running BlobTools Plot SVG - Super Kingdom'
+blobtools plot -i $base'.blobDB.json' --out images/. --rank 'superkingdom' --format 'svg'
 
-rm $genome $bam
-cp -r * /storage/brno3-cerit/home/kika/sags/reassembly/reports/.
+# blobtools taxify -f $dmnd -m $taxid -s 1 -t 2
+# blobtools create -i $transcriptome -s $sam -t $taxified -o $base
+# blobtools view -i $base'.blobDB.json' --cov -o table
+# blobtools plot -i $base'.blobDB.json' -r $rank -o plot 
 
+#copy files back
+rm $genome $blast $bam $tax_names $tax_nodes
+cp -r * $blob_dir
 
-# assembly_dir='/storage/brno3-cerit/home/kika/sags/reassembly/spades/'
-# blast_dir='/storage/brno3-cerit/home/kika/sags/reassembly/blast/'
-# mapping_dir='/storage/brno3-cerit/home/kika/sags/reassembly/mapping/bbmap/'
-# ncbi='/storage/brno3-cerit/home/kika/ncbi_db/'
-# blob_dir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/blobtools/'
-
-# #copy files to scratch
-# cp $assembly_dir'contigs.fasta' $SCRATCHDIR
-# cp $blast_dir'EU1718.fa_vs_nt_1e-10.megablast' $SCRATCHDIR
-# cp $mapping_dir'EU1718_bbm_mapped_all.sorted.bam' $SCRATCHDIR
-# cp $ncbi'nodes.dmp' $ncbi'names.dmp' $SCRATCHDIR
-
-# #compute on scratch
-# cd $SCRATCHDIR
-# mkdir tables
-# mkdir images
-
-# genome='contigs.fasta'
-# blast='EU1718.fa_vs_nt_1e-10.megablast'
-# bam='EU1718_bbm_mapped_all.sorted.bam'
-# tax_nodes='nodes.dmp'
-# tax_names='names.dmp'
-# base='EU1718_blobtools'
-
-# echo 'Running BlobTools create'
-# blobtools create -i $genome --nodes $tax_nodes --names $tax_names -t $blast -b $bam -o $base
-# echo 'Running BlobTools View - Phylum'
-# blobtools view -i $base'.blobDB.json' --out tables/phylum --rank 'phylum'
-# echo 'Running BlobTools View - Super Kingdom'
-# blobtools view -i $base'.blobDB.json' --out tables/superkingdom --rank 'superkingdom'
-# echo 'Running BlobTools Plot PNG - Phylum'
-# blobtools plot -i $base'.blobDB.json' --out images/. --rank 'phylum' 
-# echo 'Running BlobTools Plot PNG - Super Kingdom'
-# blobtools plot -i $base'.blobDB.json' --out images/. --rank 'superkingdom' 
-# echo 'Running BlobTools Plot SVG - Phylum'
-# blobtools plot -i $base'.blobDB.json' --out images/. --rank 'phylum' --format 'svg'
-# echo 'Running BlobTools Plot SVG - Super Kingdom'
-# blobtools plot -i $base'.blobDB.json' --out images/. --rank 'superkingdom' --format 'svg'
-
-# # blobtools taxify -f $dmnd -m $taxid -s 1 -t 2
-# # blobtools create -i $transcriptome -s $sam -t $taxified -o $base
-# # blobtools view -i $base'.blobDB.json' --cov -o table
-# # blobtools plot -i $base'.blobDB.json' -r $rank -o plot 
-
-# #copy files back
-# rm $genome $blast $bam $tax_names $tax_nodes
-# cp -r * $blob_dir
-
-# # rm $transcriptome $sam $dmnd $taxid
-# # cp -r * $data_dir'blobtools/pelo_clean/' || export CLEAN_SCRATCH=false
+# rm $transcriptome $sam $dmnd $taxid
+# cp -r * $data_dir'blobtools/pelo_clean/' || export CLEAN_SCRATCH=false
--- bash_scripts/busco_pbs.sh
+++ bash_scripts/busco_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N BUSCO
 #PBS -q default
-#PBS -l select=1:ncpus=1:mem=1gb:scratch_local=10gb:os=debian9
+#PBS -l select=1:ncpus=5:mem=1gb:scratch_local=10gb:os=debian9
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
@@ -19,28 +19,27 @@ cp -r $augustus_configs/* $SCRATCHDIR/augustus_configs/ || exit 1
 export AUGUSTUS_CONFIG_PATH=$SCRATCHDIR/augustus_configs
 export PATH=$PATH:/software/augustus/3.3.1/src/bin:/software/augustus/3.3.1/src/scripts
 
-# assembly_dir='/storage/brno3-cerit/home/kika/sags/reassembly/spades/'
-# lin_dir='/software/busco/3.0.2/src/db/'
-# busco_dir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/busco/'
-sumaries='/storage/brno3-cerit/home/kika/sags/reassembly/reports/busco/sumaries/'
+assembly_dir='/storage/brno3-cerit/home/kika/archamoebae/'
+busco_dir=$assembly_dir'busco/'
+lin_dir='/software/busco/3.0.2/src/db/'
+# sumaries='/storage/brno3-cerit/home/kika/sags/reassembly/reports/busco/sumaries/'
 
-#copy files to scratch
-# cp $assembly_dir'contigs.fasta' $SCRATCHDIR
-# cp -r $lin_dir'protists_ensembl/' $SCRATCHDIR
+copy files to scratch
+cp $assembly_dir'mab_trinity_050516_renamed_nucl.fasta' $SCRATCHDIR
+cp -r $lin_dir'protists_ensembl/' $SCRATCHDIR
 
-# assembly='contigs.fasta'
-# base='protists_ensembl'
-# lineage='protists_ensembl/'
-# mode='genome'
-# species='toxoplasma'
+assembly='mab_trinity_050516_renamed_nucl.fasta'
+base='eukaryota_odb9'
+lineage='eukaryota_odb9/'
+mode='transcriptome'
 
 
 #compute on scratch
 cd $SCRATCHDIR
-# run_BUSCO.py -i $assembly -o $base -l $lineage -m $mode -c $PBS_NUM_PPN -sp $species
-generate_plot.py -wd $sumaries
+run_BUSCO.py -i $assembly -o $base -l $lineage -m $mode -c $PBS_NUM_PPN
+# generate_plot.py -wd $sumaries
 
 #copy files back
-# rm -r $assembly $lineage augustus_configs
-# cp -r * $busco_dir
-cp -r * $sumaries
+rm -r $assembly $lineage augustus_configs
+cp -r * $busco_dir
+# cp -r * $sumaries
