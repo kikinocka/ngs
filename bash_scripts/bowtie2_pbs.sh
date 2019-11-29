31945d58ced8674a6bf875631b8926c06b4967c0
KikaZah <kika.zahonova@gmail.com>
2019-11-29 12:09:07 +0100

    bla

--- bash_scripts/bowtie2_pbs.sh
+++ bash_scripts/bowtie2_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N Bowtie2
-#PBS -l select=1:ncpus=25:mem=100gb:scratch_local=120gb
-#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=25:mem=100gb:scratch_local=200gb
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -18,7 +18,7 @@ outdir=$data'mapping/bowtie2/DNA_to_genome/'
 
 #copy files to scratch
 cp $spades'pelomyxa_final_genome.fa' $SCRATCHDIR
-cp $reads'all_trimmed_1.fq.gz' $reads'all_trimmed_2.fq.gz' $SCRATCHDIR
+# cp $reads'all_trimmed_1.fq.gz' $reads'all_trimmed_2.fq.gz' $SCRATCHDIR
 
 
 #compute on scratch
@@ -26,8 +26,8 @@ cd $SCRATCHDIR
 
 base_name='pelo_DNA_bw2'
 ref='pelomyxa_final_genome.fa'
-p1_1='all_trimmed_1.fq.gz'
-p1_2='all_trimmed_2.fq.gz'
+p1_1=$data'all_trimmed_1.fq.gz'
+p1_2=$data'all_trimmed_2.fq.gz'
 samfile=$base_name'.sam'
 unmapped_unpaired=$base_name'_unmapped_unpaired.fq'
 unmapped_paired=$base_name'_unmapped_paired.fq'
