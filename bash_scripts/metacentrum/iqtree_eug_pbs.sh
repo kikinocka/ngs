9ce7b5ef55e35a8b00eb56f41b87acae4421f163
KikaZah <kika.zahonova@gmail.com>
2019-12-19 18:41:29 +0100

    bla

--- bash_scripts/metacentrum/iqtree_eug_pbs.sh
+++ bash_scripts/metacentrum/iqtree_eug_pbs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #PBS -N IQTree
 #PBS -q default
-#PBS -l select=1:ncpus=15:mem=15gb:scratch_local=30gb:os=debian9
-#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=10gb:os=debian9
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,15 +11,15 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/euglenophytes/trees/gyraseB/'
+datadir='/storage/brno3-cerit/home/kika/diplonema/cardiolipin/phospholipasesD/'
 
 #copy files to scratch
-cp $datadir'gyrB_trimal_automated1.aln' $SCRATCHDIR
+cp $datadir'phospholipasesD_trimal_0.5.aln' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='gyrB_trimal_automated1.aln'
-guide='guide_gyrB'
+aln='phospholipasesD_trimal_0.5.aln'
+guide='guide_phospholipasesD'
 guide_tree=$guide'.treefile'
 bb=1000
 
