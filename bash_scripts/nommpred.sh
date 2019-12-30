eb2fc19e4f9288c258341d608701881751990875
KikaZah <kika.zahonova@gmail.com>
2019-12-30 11:03:40 +0100

    bla

---
+++ bash_scripts/nommpred.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+workdir='/mnt/mokosz/home/kika/'
+input=$workdir'diplo_all.fa'
+out=$workdir'diplo_all.nommpred.out'
+lineage=1
+# 1) Mt
+# 2) MRO
+# 3) Piroplasma
+# 4) Chlorophyta
+# 5) Dictyostelium
+# 6) Plasmodium
+# 7) stramenopiles
+# 8) Toxoplasma
+# 9) Trypanosomatida
+
+python3 NommPred.py -i $input -o $out -l $lineage --overwrite
