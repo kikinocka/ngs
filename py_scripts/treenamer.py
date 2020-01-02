7b55f3b36950095d69ec4f0bf5c045ed18ac5e18
KikaZah <kika.zahonova@gmail.com>
2020-01-02 10:23:59 +0100

    bla

--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/home/kika/MEGAsync/diplonema/cardiolipin/CLS_cap/ver3/')
+os.chdir('/home/kika/MEGAsync/diplonema/cardiolipin/CLS_cap/ver4/')
 
 #file in format Acc. number \t name of organism \n
 names = open('cls-cap_names.txt')
