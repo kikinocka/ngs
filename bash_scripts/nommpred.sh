42628039a84c815c6e5742568e2f99ae27d20556
KikaZah <kika.zahonova@gmail.com>
2020-01-07 17:34:07 +0100

    bla

--- bash_scripts/nommpred.sh
+++ bash_scripts/nommpred.sh
@@ -2,8 +2,8 @@
 
 workdir='/mnt/mokosz/home/kika/pelomyxa_schiedti/'
 input=$workdir'pelo_others.fa'
-out=$workdir'pelo_others.nommpred_dict.tsv'
-lineage=5
+out=$workdir'pelo_others.nommpred_mro.tsv'
+lineage=2
 # 1) Mt
 # 2) MRO
 # 3) Piroplasma
