e4526684770d6366d568db0801f7b4adcb21c135
KikaZah <kika.zahonova@gmail.com>
2020-01-15 18:22:33 +0100

    bla

--- bash_scripts/wget.sh
+++ bash_scripts/wget.sh
@@ -1,40 +1,20 @@
 #!/bin/bash
 
-cd /storage/brno3-cerit/home/kika/sl_euglenozoa/
+cd /storage/brno3-cerit/home/kika/cther/
 
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/1_CACTGT_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/1_CACTGT_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/2_GATCTG_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/2_GATCTG_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/3_GAACGA_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/3_GAACGA_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/4_TACAAG_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/4_TACAAG_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/5_TACGGA_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/5_TACGGA_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/6_ACATCG_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/6_ACATCG_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/7_CCGCAT_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/7_CCGCAT_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/8_GCAGTA_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/8_GCAGTA_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/9_GCTTAC_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/9_GCTTAC_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/10_GCGATT_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/10_GCGATT_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/11_TCAAGT_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/11_TCAAGT_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/12_AGGCCT_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/12_AGGCCT_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/13_CGTGAT_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/13_CGTGAT_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/14_TGGTCA_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/14_TGGTCA_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/15_GTAGCC_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/15_GTAGCC_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/16_CTGATC_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/16_CTGATC_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/17_ATTGGC_L001_R1_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/17_ATTGGC_L001_R2_001.fastq.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/deep_sea_V9_swarm2_global_derep.fas.bz2
-wget http://richardslab.exeter.ac.uk/downloads/raquel/V9/deep_sea_V9_swarm2_global_derep_1f_OTU.table.bz2
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/CT1_1.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/CT1_2.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/CT2_1.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/CT2_2.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/CT3_1.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/CT3_2.fastq.gz
+
+
+cd /storage/brno3-cerit/home/kika/lsey/
+
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/LS1_1.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/LS1_2.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/LS2_1.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/LS2_2.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/LS3_1.fastq.gz
+wget https://data.macrogen.com/~macro3/HiSeq02//20200114/HN00120597/LS3_2.fastq.gz
