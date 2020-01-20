797b121cfb79794bc0df858a98d4f1c2c108d11a
KikaZah <kika.zahonova@gmail.com>
2020-01-20 17:50:36 +0100

    bla

---
+++ bash_scripts/cat.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /storage/brno3-cerit/home/kika/cther/
+cat '14I_trimmed_1.fastq.gz' '14II_trimmed_1.fastq.gz' '14III_trimmed_1.fastq.gz' > '14all_trimmed_1.fq.gz'
+cat '14I_trimmed_2.fastq.gz' '14II_trimmed_2.fastq.gz' '14III_trimmed_2.fastq.gz' > '14all_trimmed_2.fq.gz'
