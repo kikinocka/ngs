1257e5b8bae3313130e9a8296c8f5044c5b09e5a
KikaZah <kika.zahonova@gmail.com>
2020-01-20 16:57:02 +0100

    bla

--- bash_scripts/metacentrum/fastqc_pbs.sh
+++ bash_scripts/metacentrum/fastqc_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N FastQC
 #PBS -l select=1:ncpus=1:mem=50gb:scratch_local=100gb
-#PBS -l walltime=0:30:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 #hashes explained: 
@@ -12,16 +12,19 @@ cat $PBS_NODEFILE
 #add modules
 module add fastQC-0.11.5
 
-read_dir='/storage/brno3-cerit/home/kika/sags/reassembly/trimmed_reads/'
-out_dir='/storage/brno3-cerit/home/kika/sags/reassembly/trimmed_reads/fastqc/'
+read_dir='/storage/brno3-cerit/home/kika/cther/'
+out_dir='/storage/brno3-cerit/home/kika/cther/fastqc/'
+
 
 #copy data to scratch
-cp $read_dir'EU17_r1_trimmed.fq.gz' $read_dir'EU17_r2_trimmed.fq.gz' $read_dir'EU18_r1_trimmed.fq.gz' $read_dir'EU18_r2_trimmed.fq.gz' $SCRATCHDIR
+cp $read_dir'14I_1.fastq.gz' $read_dir'14I_2.fastq.gz' $read_dir'14II_1.fastq.gz' $read_dir'14II_2.fastq.gz' $read_dir'14III_1.fastq.gz' $read_dir'14III_2.fastq.gz' $SCRATCHDIR
 
 
 #chdir to scratch and perform operations
 cd $SCRATCHDIR
-fastqc -o $out_dir 'EU17_r1_trimmed.fq.gz'
-fastqc -o $out_dir 'EU17_r2_trimmed.fq.gz'
-fastqc -o $out_dir 'EU18_r1_trimmed.fq.gz'
-fastqc -o $out_dir 'EU18_r2_trimmed.fq.gz'
+fastqc -o $out_dir '14I_1.fastq.gz'
+fastqc -o $out_dir '14I_2.fastq.gz'
+fastqc -o $out_dir '14II_1.fastq.gz'
+fastqc -o $out_dir '14II_2.fastq.gz'
+fastqc -o $out_dir '14III_1.fastq.gz'
+fastqc -o $out_dir '14III_2.fastq.gz'
