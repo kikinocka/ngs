82fedea85506b7dd771d067168b387c8bb229372
KikaZah <kika.zahonova@gmail.com>
2020-01-24 13:21:15 +0100

    bla

--- bash_scripts/metacentrum/bbmerge_pbs.sh
+++ bash_scripts/metacentrum/bbmerge_pbs.sh
@@ -14,14 +14,14 @@ raw='/storage/brno3-cerit/home/kika/sl_euglenozoa/raw_reads/'
 res='/storage/brno3-cerit/home/kika/sl_euglenozoa/merged_reads/'
 
 #copy data to scratch
-cp $raw'7_CCGCAT_L001_R1_001.fastq.bz2' $raw'7_CCGCAT_L001_R2_001.fastq.bz2' $SCRATCHDIR
+cp $raw'8_GCAGTA_L001_R1_001.fastq.bz2' $raw'8_GCAGTA_L001_R2_001.fastq.bz2' $SCRATCHDIR
 
-fw='7_CCGCAT_L001_R1_001.fastq.bz2'
-rv='7_CCGCAT_L001_R2_001.fastq.bz2'
-merged='7_CCGCAT_merged.fq.gz'
-un1='7_CCGCAT_unmerged_R1.fq.gz'
-un2='7_CCGCAT_unmerged_R2.fq.gz'
-report='7_CCGCAT_bbmerge_report.txt'
+fw='8_GCAGTA_L001_R1_001.fastq.bz2'
+rv='8_GCAGTA_L001_R2_001.fastq.bz2'
+merged='8_GCAGTA_merged.fq.gz'
+un1='8_GCAGTA_unmerged_R1.fq.gz'
+un2='8_GCAGTA_unmerged_R2.fq.gz'
+report='8_GCAGTA_bbmerge_report.txt'
 
 
 #run on scratch
