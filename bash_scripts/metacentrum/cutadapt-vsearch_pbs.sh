f8ab4aed63f522097e5f4694b65002b7f44debe7
KikaZah <kika.zahonova@gmail.com>
2020-02-05 15:47:21 -0700

    bla

--- bash_scripts/metacentrum/cutadapt-vsearch_pbs.sh
+++ bash_scripts/metacentrum/cutadapt-vsearch_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N cutadapt-vsearch
-#PBS -l select=1:ncpus=10:mem=50gb:scratch_local=50gb
+#PBS -l select=1:ncpus=1:mem=50gb:scratch_local=50gb
 #PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
