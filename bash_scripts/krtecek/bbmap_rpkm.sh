6b83584686dc6271cafb740e36de238a13d17f3a
KikaZah <kika.zahonova@gmail.com>
2020-02-05 19:04:59 -0700

    bla

--- bash_scripts/krtecek/bbmap_rpkm.sh
+++ bash_scripts/krtecek/bbmap_rpkm.sh
@@ -3,10 +3,10 @@
 cd /home/users/kika/diplonema/
 
 assembly='dpap_transcripts.fa'
-fw='/home/users/kika/diplonema/reads_trimmed/h5_r1.fq.gz'
-rv='/home/users/kika/diplonema/reads_trimmed/h5_r2.fq.gz'
-sam='dpap_rich_bbmap_rna.sam'
-rpkm='dpap_rich_bbmap.rpkm'
-report='dpap_rich_bbmap.report'
+fw='/home/users/kika/diplonema/reads_trimmed/d5_r1.fq.gz'
+rv='/home/users/kika/diplonema/reads_trimmed/d5_r2.fq.gz'
+sam='dpap_poor_bbmap_rna.sam'
+rpkm='dpap_poor_bbmap.rpkm'
+report='dpap_poor_bbmap.report'
 
 bbmap.sh in=$fw in2=$rv out=$sam ref=$assembly rpkm=$rpkm threads=10 2> $report
