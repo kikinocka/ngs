ad20407800ecc40d1b32506a37c13bf97e2ab9f4
KikaZah <kika.zahonova@gmail.com>
2020-02-07 10:51:34 -0700

    bla

--- bash_scripts/metacentrum/mafft_pbs.sh
+++ bash_scripts/metacentrum/mafft_pbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N MAFFT
+#PBS -N mafft
 #PBS -l select=1:ncpus=20:mem=15gb:scratch_local=1gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
@@ -10,17 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add mafft-7.313
 
-data_dir='/storage/brno3-cerit/home/kika/sags/alignments/'
+data_dir='/storage/brno3-cerit/home/kika/sags/alignments'
 
 #copy files to scratch
-cp $data_dir'*.faa' $SCRATCHDIR
+cp $data_dir'/'*.faa $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
 for f in *.faa ; do
  aln=${f%.faa}.mafft.aln
- mafft --thread $PBS_NUM_PPN --inputorder --auto ${f} > $aln
+ mafft --thread $PBS_NUM_PPN --maxiterate 100 --inputorder --auto ${f} > ${aln}
 done
 
 
