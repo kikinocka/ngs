521797f8b272bc5de30a392520e441a73d272f7d
KikaZah <kika.zahonova@gmail.com>
2020-02-17 21:30:50 -0700

    bla

--- bash_scripts/metacentrum/fastqc_pbs.sh
+++ bash_scripts/metacentrum/fastqc_pbs.sh
@@ -17,17 +17,12 @@ out_dir='/storage/brno3-cerit/home/kika/egracilis/fastqc/'
 
 
 #copy data to scratch
-cp $read_dir'Dark_L004_R1.fastq.gz' $read_dir'Dark_L004_R2.fastq.gz' $read_dir'dark_trimmed_1.fq.gz' $read_dir'dark_trimmed_2.fq.gz' $SCRATCHDIR
+cp $read_dir'Light_L004_R1.fastq.gz' $read_dir'Light_L004_R2.fastq.gz' $read_dir'light_trimmed_1.fq.gz' $read_dir'light_trimmed_2.fq.gz' $SCRATCHDIR
 
 
 #chdir to scratch and perform operations
 cd $SCRATCHDIR
-# fastqc -o $out_dir 'Light_L004_R1.fastq.gz'
-# fastqc -o $out_dir 'Light_L004_R2.fastq.gz'
-# fastqc -o $out_dir 'light_trimmed_1.fq.gz'
-# fastqc -o $out_dir 'light_trimmed_2.fq.gz'
-
-fastqc -o $out_dir 'Dark_L004_R1.fastq.gz'
-fastqc -o $out_dir 'Dark_L004_R2.fastq.gz'
-fastqc -o $out_dir 'dark_trimmed_1.fq.gz'
-fastqc -o $out_dir 'dark_trimmed_2.fq.gz'
+fastqc -o $out_dir 'Light_L004_R1.fastq.gz'
+fastqc -o $out_dir 'Light_L004_R2.fastq.gz'
+fastqc -o $out_dir 'light_trimmed_1.fq.gz'
+fastqc -o $out_dir 'light_trimmed_2.fq.gz'
