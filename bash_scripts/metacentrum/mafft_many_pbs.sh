9428bef376401f5b5e2d979440e539c55b5564e0
KikaZah <kika.zahonova@gmail.com>
2020-02-18 16:37:06 -0700

    bla

--- bash_scripts/metacentrum/mafft_many_pbs.sh
+++ bash_scripts/metacentrum/mafft_many_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N mafft
-#PBS -l select=1:ncpus=20:mem=15gb:scratch_local=1gb
+#PBS -l select=1:ncpus=10:mem=5gb:scratch_local=1gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
