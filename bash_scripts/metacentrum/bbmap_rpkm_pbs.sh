c2a281c7c3e5052b6e56bfa818a3fa38200634bb
KikaZah <kika.zahonova@gmail.com>
2020-02-17 21:34:10 -0700

    bla

--- bash_scripts/metacentrum/bbmap_rpkm_pbs.sh
+++ bash_scripts/metacentrum/bbmap_rpkm_pbs.sh
@@ -14,17 +14,17 @@ data='/storage/brno3-cerit/home/kika/egracilis/'
 
 #copy files to scratch
 cp $data'GEFR01.1.fsa_nt' $SCRATCHDIR
-cp $data'dark_trimmed_1.fq.gz' $SCRATCHDIR
-cp $data'dark_trimmed_2.fq.gz' $SCRATCHDIR
+cp $data'light_trimmed_1.fq.gz' $SCRATCHDIR
+cp $data'light_trimmed_2.fq.gz' $SCRATCHDIR
 
 assembly='GEFR01.1.fsa_nt'
-fw='dark_trimmed_1.fq.gz'
-rv='dark_trimmed_2.fq.gz'
+fw='light_trimmed_1.fq.gz'
+rv='light_trimmed_2.fq.gz'
 # all='el_reads_new.fa'
 
-sam='eg_dark_bbmap_rna.sam'
-rpkm='eg_dark_bbmap.rpkm'
-report='eg_dark_bbmap.report'
+sam='eg_light_bbmap_rna.sam'
+rpkm='eg_light_bbmap.rpkm'
+report='eg_light_bbmap.report'
 
 
 #compute on scratch
