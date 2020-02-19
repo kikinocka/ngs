4005281232fa30a3e5e8caad37e8264a6518d533
KikaZah <kika.zahonova@gmail.com>
2020-02-19 22:31:16 +0100

    bla

--- bash_scripts/metacentrum/diamond_bp_pbs.sh
+++ bash_scripts/metacentrum/diamond_bp_pbs.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 #PBS -N diamond-bp
 #PBS -l select=1:ncpus=10:mem=50gb:scratch_local=100gb
-#PBS -l walltime=10:00:00
+#PBS -l walltime=2:00:00
 #PBS -m ae
 #PBS -j oe
-
-#RUN IN ZOLI'S FOLDER !!!
 
 cat $PBS_NODEFILE
 
 #add module
 module add diamond-0.8.29
 
-diamond_dir='/storage/brno3-cerit/home/fussyz01/DMND/'
-data_dir='/storage/brno3-cerit/home/kika/diplonema/catalase/apx_tree/ver17/'
+diamond_dir='/storage/brno3-cerit/home/kika/refseq/'
+data_dir='/storage/brno3-cerit/home/kika/proteromonas/peroxisomal/'
 
-#copy files to zoli's folder
-cp $data_dir'1618.fa' $diamond_dir
+#copy files to db's folder
+cp $data_dir'possibly_peroxisomal.fa' $diamond_dir
 
-query='1618.fa'
-db='nr2.dmnd'
-out='1618.dmnd_bp.out'
+query='possibly_peroxisomal.fa'
+db='refseq.dmnd'
+out='peroxisomal.dmnd_bp.out'
 
 
-#compute in zoli's folder
+#compute in db's folder
 cd $diamond_dir
-./diamond blastp -p $PBS_NUM_PPN -d $db -q $query -o $out -f 6 qseqid sseqid stitle evalue bitscore full_sseq --sensitive --max-target-seqs 30 --evalue 1e-3
+diamond blastp -p $PBS_NUM_PPN -d $db -q $query -o $out -f 6 qseqid sseqid stitle evalue bitscore --sensitive --max-target-seqs 1 --evalue 1e-3
 
 #copy files back
 rm $query
--- py_scripts/motif_search.py
+++ py_scripts/motif_search.py
@@ -3,21 +3,22 @@ import os
 import re
 from Bio import SeqIO
 
-os.chdir('/Dcko/MEGAsync/diplonema/catalase/targeting/')
-proteins = SeqIO.parse('all.fa', 'fasta')
+os.chdir('/Dcko/MEGAsync/Data/stramenopiles/')
+proteins = SeqIO.parse('Proteromonas_l_proteins.fasta', 'fasta')
 
-#kinetoplastids
-pts1 = r'(S|A|G|C|N|P)(R|H|K|N|Q)(L|I|V|F|A|M|Y)'
-pts2 = r'^M\w{0,20}(R|K)(L|V|I)\w{5}(H|K|Q|R)(L|A|I|V|F|Y)'
+# #kinetoplastids
+# pts1 = r'(S|A|G|C|N|P)(R|H|K|N|Q)(L|I|V|F|A|M|Y)'
+# pts2 = r'^M\w{0,20}(R|K)(L|V|I)\w{5}(H|K|Q|R)(L|A|I|V|F|Y)'
 
-# pts1 = r'(S|A|C)(K|R|H|Q)(L|M)'
-# pts2 = r'^\w{1,21}R(L|I|V|Q)\w{2}(L|I|V|Q|H)(L|S|G|A)\w{1}(H|Q)(L|A)'
+pts1 = r'(S|A|C)(K|R|H|Q)(L|M)'
+pts2 = r'^\w{1,21}R(L|I|V|Q)\w{2}(L|I|V|Q|H)(L|S|G|A)\w{1}(H|Q)(L|A)'
 
-with open('all.possibly_peroxisomal.fa', 'w') as out:
+with open('proteromonas.possibly_peroxisomal.fa', 'w') as out:
 	for protein in proteins:
 		# print(protein.seq)
 		# print(str(protein.seq)[-3:])
-		if str(protein.seq)[-3:] == pts1:
+		# if str(protein.seq)[-3:] == pts1:
+		if re.search(pts1, str(protein.seq)[-3:]):
 			out.write('>{} @PTS1:{}\n{}\n'.format(protein.description, protein.seq[-3:], protein.seq))
 		elif re.search(pts2, str(protein.seq)):
 			# match = re.match(pts2, str(protein.seq))
