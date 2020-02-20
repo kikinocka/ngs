03bdd49493b4d13f4a8897c276d86cc4907f6a97
KikaZah <kika.zahonova@gmail.com>
2020-02-19 20:09:55 -0700

    bla

--- bash_scripts/metacentrum/v9_analysis/5_global_dereplicate.sh
+++ bash_scripts/metacentrum/v9_analysis/5_global_dereplicate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N cutadapt-vsearch
+#PBS -N vsearch
 #PBS -l select=1:ncpus=1:mem=50gb:scratch_local=50gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
@@ -17,27 +17,28 @@ cp $trimmed'/'*.fas $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
+out='global_dereplicated.fa'
 export TMPDIR=$SCRATCHDIR
 
 TMP_FASTA=$(mktemp)
 TMP_FASTA_DEREPLICATED=$(mktemp)
 
-if [ $# != 1 ]; then
-    echo "You need to supply an output filename";
-    exit 1;
-fi
+# if [ $# != 1 ]; then
+#     echo "You need to supply an output filename";
+#     exit 1;
+# fi
 
 cat ./*.fas > ${TMP_FASTA}
 
 # Dereplicate (vsearch)
-vsearch --threads $PBS_NUM_PPN \
+vsearch --threads 1 \
     --derep_fulllength ${TMP_FASTA} \
     --sizein \
     --sizeout \
     --fasta_width 0 \
-    --output $1
+    --output ${out}
 
-bzip2 -9k $1 &
+bzip2 -9k ${out} &
 
 #copy files back
 rm ${TMP_FASTA} ${TMP_FASTA_DEREPLICATED}
