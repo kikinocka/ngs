4101a62bfe503803206b23226078f77563f9e623
KikaZah <kika.zahonova@gmail.com>
2020-02-20 14:46:44 -0700

    bla

--- bash_scripts/metacentrum/v9_analysis/7a_swarm.sh
+++ bash_scripts/metacentrum/v9_analysis/7a_swarm.sh
@@ -13,22 +13,28 @@ data='/storage/brno3-cerit/home/kika/sl_euglenozoa/'
 script='/storage/brno2/home/kika/scripts/kika/bash_scripts/metacentrum/v9_analysis/'
 
 #copy files to scratch
-# cp $data'global_dereplicated.fa' $SCRATCHDIR
-# cp $script'7b_swarm_fastidious.sh' $SCRATCHDIR
-cp $script'7b_swarm_fastidious.sh' $data
+cp $data'global_dereplicated.fa' $SCRATCHDIR
+cp $script'7b_swarm_fastidious.sh' $SCRATCHDIR
 
 #compute on scratch
-# cd $SCRATCHDIR
+cd $SCRATCHDIR
 # if [ $# != 1 ]; then
 #     echo "You need to supply an input filename - this is your global dereplicated fasta file (not the bzip)!";
 #     exit 1;
 # fi
-cd $data
-fasta='global_dereplicated.fa'
-swarm_sc='7b_swarm_fastidious.sh'
 
-./$swarm_sc $fasta $PBS_NUM_PPN
+fasta='global_dereplicated.fa'
+echo '******'
+echo $fasta
+echo '******'
+
+swarm_sc='7b_swarm_fastidious.sh'
+echo '******'
+echo $swarm_sc
+echo '******'
+
+$swarm_sc $fasta $PBS_NUM_PPN
 
 #copy files back
-# rm $fasta $swarm_sc
-# cp * $data
+rm $fasta $swarm_sc
+cp * $data
