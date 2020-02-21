ea015d46cef9b4f0867e543d5dd449a86d830178
KikaZah <kika.zahonova@gmail.com>
2020-02-21 13:38:49 -0700

    bla

---
+++ py_scripts/databases/uniprot_search.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+from bioservices import UniProt as up
+
+os.chdir('/Dcko/ownCloud/proteromonas/PXMP2_tree/ver2/')
+acc_files = [x for x in os.listdir() if x.endswith('.acc')]
+
+for acc_file in acc_files:
+	print(acc_file)
+	name = acc_file.split('_')[0]
+	with open('{}_uniprot.fa'.format(name), 'w') as result:
+		for acc in open(acc_file):
+			print(acc.strip())
+			seq = up().retrieve(acc.strip(), 'fasta')
+			result.write('{}'.format(seq))
