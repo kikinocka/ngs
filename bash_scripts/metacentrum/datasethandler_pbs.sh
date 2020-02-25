fd1b7e7e416725ebcc45ce7ae51f1c0761cb941a
KikaZah <kika.zahonova@gmail.com>
2020-02-25 22:40:23 +0100

    bla

--- bash_scripts/metacentrum/datasethandler_pbs.sh
+++ bash_scripts/metacentrum/datasethandler_pbs.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #PBS -N datasethandler
-#PBS -l select=1:ncpus=10:mem=20gb:scratch_local=2gb
-#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=2gb
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -22,7 +22,21 @@ cp $DATADIR'/'*.fa $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-./datasethandler-server.py -a mafft -t iqtree -i batch -b -s
+./datasethandler-server.py -a mafft -t iqtree -i batch -b --trimalparams '-gt 0.5'
+
+#'-d', '--directory', help='Change working directory', default='.'
+#'-i', '--infile', help='Fasta/Phylip set to be analyzed', default="batch"
+#'-a', '--aligner', help='Aligner', default='mafft'
+#'-t', '--treemaker', help='Program for tree inference', default='iqtree'
+#'-n', '--no_dedupe', help='Do not filter duplicates', action='store_true'
+#'--alignerparams', help='Custom aligner parameters, check with manual', default=''
+#'--trimalparams', help='Custom TrimAl parameters, check with manual', default=''
+#'-b', '--ufbootstrap', help='Ultra-fast boostrap calculation', action='store_true'
+#'-B', '--bootstrap', help='Boostrap calculation', action='store_true'
+#'--shalrt', help='Calculate SH-aLRT', action='store_true'
+#'-g', '--no_guide', help='Do not perform guide tree inference', action='store_true'
+#'--treeparams', help='Custom tree inference parameters, check with manual', default=''
+#'-s', '--mark_similarity', help='Mark similarity on branches', action='store_true'
 
 #copy files back
 cp -R RESULT $DATADIR
