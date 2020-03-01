27e55717702028abd83ed6a6a5fabfa15956a965
KikaZah <kika.zahonova@gmail.com>
2020-03-01 02:24:39 +0100

    bla

--- bash_scripts/targetp_veles.sh
+++ bash_scripts/targetp_veles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-workdir='/home/kika/pelomyxa_schiedti/predicted_proteins/'
-infile=$workdir'pelo_transcriptome_clean.fa.transdecoder.5prime_complete.clustered.pep'
-outfile=$workdir'pelo_transcriptome_clean.fa.transdecoder.5prime_complete.clustered.targetp.txt'
+workdir='/mnt/mokosz/home/kika/workdir/'
+infile=$workdir'pelo_formate.fasta'
+outfile=$workdir'pelo_formate.clustered.targetp.txt'
 plant='P'
 non_plant='N'
 
