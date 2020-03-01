90bf8c0751d19db6c6433d526b87672729f9831b
KikaZah <kika.zahonova@gmail.com>
2020-03-01 04:55:02 +0100

    bla

--- bash_scripts/targetp_veles.sh
+++ bash_scripts/targetp_veles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 workdir='/mnt/mokosz/home/kika/workdir/'
-infile=$workdir'pelo_formate.fasta'
-outfile=$workdir'pelo_formate.clustered.targetp.txt'
+infile=$workdir'pelo_formate_transporter.fasta'
+outfile=$workdir'pelo_formate_transporter.targetp.txt'
 plant='P'
 non_plant='N'
 
