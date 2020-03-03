9565a1bfb556a485fa055b0db0fb4d7b060ab796
KikaZah <kika.zahonova@gmail.com>
2020-03-03 11:27:45 -0700

    bla

---
+++ bash_scripts/metacentrum/ra_pbs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#PBS -N ra
+#PBS -q default
+#PBS -l select=1:ncpus=20:mem=50gb:scratch_local=50gb
+#PBS -l walltime=24:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add ra-032020
+
+data='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/wt/'
+
+#copy files to scratch
+cp $data'reads/wt_pacbio_all.fq.gz' $SCRATCHDIR
+cp $data'reads/wt_illumina_all.fq.gz' $SCRATCHDIR
+
+tgs='wt_pacbio_all.fq.gz'
+hiseq='wt_illumina_all.fq.gz'
+report='wt_ra_report.txt'
+
+#run on scratch
+cd $SCRATCHDIR
+ra -t $PBS_NUM_PPN -x pb $tgs $hiseq 2> $report
+
+#copy files back
+rm $tgs $hiseq
+cp -r * $data
