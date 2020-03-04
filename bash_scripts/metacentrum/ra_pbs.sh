78dbee0cea9edea97ee237c8a61ceb394b85818d
KikaZah <kika.zahonova@gmail.com>
2020-03-04 10:55:22 -0700

    bla

--- bash_scripts/metacentrum/ra_pbs.sh
+++ bash_scripts/metacentrum/ra_pbs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #PBS -N ra
 #PBS -q default
-#PBS -l select=1:ncpus=20:mem=50gb:scratch_local=50gb
-#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=20:mem=70gb:scratch_local=50gb
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,19 +11,20 @@ cat $PBS_NODEFILE
 #add module
 module add ra-032020
 
-data='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/ku80/'
+data='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/ku70/'
 
 #copy files to scratch
-cp $data'reads/ku80_pacbio_all.fq.gz' $SCRATCHDIR
-cp $data'reads/ku80_illumina_all.fq.gz' $SCRATCHDIR
+cp $data'reads/ku70_pacbio_all.fq.gz' $SCRATCHDIR
+cp $data'reads/ku70_illumina_all.fq.gz' $SCRATCHDIR
 
-tgs='ku80_pacbio_all.fq.gz'
-hiseq='ku80_illumina_all.fq.gz'
-report='ku80_ra_report.txt'
+tgs='ku70_pacbio_all.fq.gz'
+hiseq='ku70_illumina_all.fq.gz'
+report='ku70_ra_report.txt'
+assembly='ku70_ra.fa'
 
 #run on scratch
 cd $SCRATCHDIR
-ra -t $PBS_NUM_PPN -x pb $tgs $hiseq 2> $report
+ra -t $PBS_NUM_PPN -x pb $tgs $hiseq > $assembly 2> $report
 
 #copy files back
 rm $tgs $hiseq
