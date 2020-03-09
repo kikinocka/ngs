bb8074d9eb9e112f6ad39761ece04c70e7eed6a2
KikaZah <kika.zahonova@gmail.com>
2020-03-08 19:24:09 -0600

    bla

--- bash_scripts/metacentrum/bowtie2_pbs.sh
+++ bash_scripts/metacentrum/bowtie2_pbs.sh
@@ -11,25 +11,25 @@ cat $PBS_NODEFILE
 module add bowtie2-2.3.0
 module add samtools-1.3.1
 
-data='/storage/brno3-cerit/home/kika/kinetoplastids/cfas_genome/'
+data='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/wt/'
 outdir=$data'bw2_mapping/'
 
 #copy files to scratch
-cp $data'cfas_AODS02.fasta' $SCRATCHDIR
-cp $data'reads/'*.fq.gz $SCRATCHDIR
+cp $data'wt_ra.fa' $SCRATCHDIR
+cp $data'reads/'*.fq $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='cfas_bw2'
-ref='cfas_AODS02.fasta'
-p1_1='SRR1028161_trimmed_1.fq.gz'
-p1_2='SRR1028161_trimmed_2.fq.gz'
-p2_1='SRR1593518_trimmed_1.fq.gz'
-p2_2='SRR1593518_trimmed_2.fq.gz'
-p3_1='SRR834693_trimmed_1.fq.gz'
-p3_2='SRR834693_trimmed_2.fq.gz'
+base_name='lmex_bw2'
+ref='wt_ra.fa'
+p1_1='L_mex_wt_trimmed_1.fq'
+p1_2='L_mex_wt_trimmed_2.fq'
+# p2_1='SRR1593518_trimmed_1.fq.gz'
+# p2_2='SRR1593518_trimmed_2.fq.gz'
+# p3_1='SRR834693_trimmed_1.fq.gz'
+# p3_2='SRR834693_trimmed_2.fq.gz'
 samfile=$base_name'.sam'
 unmapped_unpaired=$base_name'_unmapped_unpaired.fq'
 unmapped_paired=$base_name'_unmapped_paired.fq'
@@ -41,8 +41,8 @@ sorted=$base_name'_sorted.bam'
 bowtie2-build --threads $PBS_NUM_PPN $ref $base_name
 bowtie2 --very-sensitive -p $PBS_NUM_PPN \
 	-x $base_name \
-	-1 $p1_1,$p2_1,$p3_1 \
-	-2 $p1_2,$p2_2,$p3_2 \
+	-1 $p1_1 \
+	-2 $p1_2 \
 	--un-gz $unmapped_unpaired \
 	--un-conc-gz $unmapped_paired \
 	-S $samfile 2> $report
--- py_scripts/blast/blast.py
+++ py_scripts/blast/blast.py
@@ -2,13 +2,13 @@
 import subprocess
 from Bio.Blast import NCBIXML
 
-cmd = 'blastx'
-task = 'blastx'
-query = '/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs_hits.fa'
-# db = '/Dcko/MEGAsync/Data/diplonemids_transcriptomes/1608_Trinity.fasta'
-db = '/Dcko/ownCloud/membrane-trafficking/Rab_db/RABs_deduplicated.fa'
+cmd = 'tblastn'
+task = 'tblastn'
+query = '/Dcko/ownCloud/membrane-trafficking/RhuRAB_aa.txt'
+db = '/Dcko/MEGAsync/Data/diplonemids_transcriptomes/1608_Trinity.fasta'
+# db = '/Dcko/ownCloud/membrane-trafficking/Rab_db/RABs_deduplicated.fa'
 # subject = '/home/kika/MEGAsync/diplonema_mt/1621/transcripts/y8/y8.fasta'
-out = '/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs_hits.blast.xml'
+out = '/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs.blast.xml'
 evalue = 1
 outfmt = 5
 hits = 10
@@ -31,8 +31,8 @@ print('writing BLAST results to tables')
 
 result_handle = open(out)
 blast_records = NCBIXML.parse(result_handle)
-output = open('/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs_hits.blast.tsv', 'w')
-out_best = open('/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs_hits.best_blast.tsv', 'w')
+output = open('/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs.blast.tsv', 'w')
+out_best = open('/Dcko/ownCloud/membrane-trafficking/rhynchopus_humris_1608/rhRABs.best_blast.tsv', 'w')
 
 output.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format('qseqid', 'qlen', 'sseqid', 'sseqdef',
 	'slen', 'alen', 'evalue', 'pident', 'bitscore', 'mismatch', 'gaps', 'qstart', 'qend', 'sstart', 'send', 
