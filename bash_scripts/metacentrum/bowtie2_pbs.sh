989293a9029594a9b2a4bba3e1037663c4d92ee7
KikaZah <kika.zahonova@gmail.com>
2020-03-09 09:36:55 -0600

    bla

--- bash_scripts/metacentrum/bowtie2_pbs.sh
+++ bash_scripts/metacentrum/bowtie2_pbs.sh
@@ -12,18 +12,18 @@ module add bowtie2-2.3.0
 module add samtools-1.3.1
 
 data='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/wt/'
-outdir=$data'bw2_mapping/pilon4/'
+outdir=$data'bw2_mapping/pilon5/'
 
 #copy files to scratch
-cp $data'wt_pilon4.fa' $SCRATCHDIR
+cp $data'wt_pilon5.fa' $SCRATCHDIR
 cp $data'reads/'*.fq $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='wt_p4_bw2'
-ref='wt_pilon4.fa'
+base_name='wt_p5_bw2'
+ref='wt_pilon5.fa'
 p1_1='L_mex_wt_trimmed_1.fq'
 p1_2='L_mex_wt_trimmed_2.fq'
 # p2_1='SRR1593518_trimmed_1.fq.gz'
