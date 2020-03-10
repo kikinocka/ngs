ebc0c6c493db5b270e04b0c1ff8f011c5ac123e1
KikaZah <kika.zahonova@gmail.com>
2020-03-10 17:22:33 -0600

    bla

--- bash_scripts/metacentrum/transdecoder_predict_pbs.sh
+++ bash_scripts/metacentrum/transdecoder_predict_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N TransDecoder_Predict
 #PBS -l select=1:ncpus=10:mem=10gb:scratch_local=50gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,27 +11,30 @@ cat $PBS_NODEFILE
 module add hmmer-3.2
 module add transdecoder-3.0.1
 
-data_dir='/storage/brno3-cerit/home/kika/pelomyxa/transcriptome_assembly/'
-pfam_dir='/storage/brno3-cerit/home/kika/pfam/'
+data_dir='/storage/brno3-cerit/home/kika/diplonemids/trafficking/'
+# pfam_dir='/storage/brno3-cerit/home/kika/pfam/'
 
 #copy files to scratch
-cp $pfam_dir'Pfam-A.hmm' $SCRATCHDIR
+# cp $pfam_dir'Pfam-A.hmm' $SCRATCHDIR
+cp $data_dir'rhRABs.fwd_hits.fa' $SCRATCHDIR
+# cp -r $data_dir'pelomyxa_transcriptome_clean.fa.transdecoder_dir' $SCRATCHDIR
+cp $data_dir'rhRABs_rev.blastx.out' $SCRATCHDIR
 
-cp $data_dir'pelomyxa_transcriptome_clean.fa' $SCRATCHDIR
-cp -r $data_dir'pelomyxa_transcriptome_clean.fa.transdecoder_dir' $SCRATCHDIR
-
-hmm='Pfam-A.hmm'
-transcriptome='pelomyxa_transcriptome_clean.fa'
-orfs='pelomyxa_transcriptome_clean.fa.transdecoder_dir/longest_orfs.pep'
+# hmm='Pfam-A.hmm'
+transcriptome='rhRABs.fwd_hits.fa'
+blast='rhRABs_rev.blastx.out'
+# orfs='pelomyxa_transcriptome_clean.fa.transdecoder_dir/longest_orfs.pep'
 
 
 #compute on scratch
 cd $SCRATCHDIR
-hmmpress $hmm
-hmmscan --cpu $PBS_NUM_PPN --domtblout pfam.domtblout $hmm $orfs
-TransDecoder.Predict -t $transcriptome --retain_pfam_hits pfam.domtblout
+# hmmpress $hmm
+# hmmscan --cpu $PBS_NUM_PPN --domtblout pfam.domtblout $hmm $orfs
+TransDecoder.Predict -t $transcriptome --retain_blastp_hits $blast #--retain_pfam_hits pfam.domtblout
 
 
 #copy files back
-rm -r $transcriptome $hmm 'pelomyxa_transcriptome_clean.fa.transdecoder_dir/'
-cp -r * $data_dir'pelomyxa_transcriptome_clean.fa.transdecoder_dir/'
+# rm -r $transcriptome $hmm 'pelomyxa_transcriptome_clean.fa.transdecoder_dir/'
+# cp -r * $data_dir'pelomyxa_transcriptome_clean.fa.transdecoder_dir/'
+rm $transcriptome $blast
+cp -r * $data_dir
