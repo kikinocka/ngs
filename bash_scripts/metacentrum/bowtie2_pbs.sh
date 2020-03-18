77878ca3503f38d73e3bdf7fe1e26e09d2914355
KikaZah <kika.zahonova@gmail.com>
2020-03-18 15:22:35 -0600

    bla

--- bash_scripts/metacentrum/bowtie2_pbs.sh
+++ bash_scripts/metacentrum/bowtie2_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N Bowtie2
 #PBS -l select=1:ncpus=20:mem=50gb:scratch_local=50gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -12,18 +12,18 @@ module add bowtie2-2.3.0
 module add samtools-1.3.1
 
 data='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/ku70/'
-outdir=$data'bw2_mapping/ra/'
+outdir=$data'bw2_mapping/pilon1/'
 
 #copy files to scratch
-cp $data'ku70_ra.fa' $SCRATCHDIR
+cp $data'ku70_pilon1.fa' $SCRATCHDIR
 cp $data'reads/'*.fq $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='ku70_ra_bw2'
-ref='ku70_ra.fa'
+base_name='ku70_p1_bw2'
+ref='ku70_pilon1.fa'
 p1_1='Lmex_Ku70_trimmed_1.fq'
 p1_2='Lmex_Ku70_trimmed_2.fq'
 # p2_1='SRR1593518_trimmed_1.fq.gz'
