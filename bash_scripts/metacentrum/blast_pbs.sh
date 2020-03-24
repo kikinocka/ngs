f5b91437a281d9b52d0e069400d03f3e9812d9a1
KikaZah <kika.zahonova@gmail.com>
2020-03-24 16:10:48 -0600

    bla

--- bash_scripts/metacentrum/blast_pbs.sh
+++ bash_scripts/metacentrum/blast_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N blastp
 #PBS -l select=1:ncpus=20:mem=80gb:scratch_local=50gb
-#PBS -l walltime=48:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,18 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add blast+-2.7.1
 
-datadir='/storage/brno3-cerit/home/kika/proteromonas/peroxisomal/'
-query=$datadir'possibly_peroxisomal.fa'
-out=$datadir'peroxisomal.blastp_nr.xml'
-report=$datadir'peroxisomal.blastp_nr.report'
+datadir='/storage/brno3-cerit/home/kika/proteromonas/rabs/'
+query=$datadir'Plac.fwd_hits.fa'
+out=$datadir'Plac.rev_nr.blast.xml'
+report=$datadir'Plac.rev_nr.blast.report'
 db_dir='/storage/projects/BlastDB/'
 db=$db_dir'nr'
-
 program=blastp
 task=blastp
 outfmt=5
+eval=1e-3
 # max_seqs=1
-# eval=1e-3
 
 #run in DB folder
 # cd $db_dir
@@ -30,7 +29,7 @@ $program -task $task \
 	-db $db \
 	-out $out \
 	-outfmt $outfmt \
-	# -max_target_seqs $max_seqs \
-	# -evalue $eval \
+	-evalue $eval \
 	-num_threads $PBS_NUM_PPN \
 	2>$report
+	# -max_target_seqs $max_seqs \
