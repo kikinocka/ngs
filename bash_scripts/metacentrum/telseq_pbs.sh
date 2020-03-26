21deae65132ced35169180ae321502eeaa7b9393
KikaZah <kika.zahonova@gmail.com>
2020-03-26 16:29:45 -0600

    bla

--- bash_scripts/metacentrum/telseq_pbs.sh
+++ bash_scripts/metacentrum/telseq_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N telseq
-#PBS -l select=1:ncpus=1:mem=100gb:scratch_local=20gb
+#PBS -l select=1:ncpus=1:mem=10gb:scratch_local=20gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
@@ -11,14 +11,14 @@ cat $PBS_NODEFILE
 module add telseq-0.0.2
 
 #copy files to scratch
-datadir='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/ku70/'
-cp $datadir'bw2_mapping/pilon10/ku70_p10_bw2_sorted.bam' $SCRATCHDIR
+datadir='/storage/brno3-cerit/home/kika/kinetoplastids/lmex_genome/ku80/'
+cp $datadir'bw2_mapping/pilon10/ku80_p10_bw2_sorted.bam' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-bam='ku70_p10_bw2_sorted.bam'
-out='ku70_telseq.out'
+bam='ku80_p10_bw2_sorted.bam'
+out='ku80_telseq.out'
 pattern='TTAGGG'
 
 telseq -z $pattern -o $out $bam
