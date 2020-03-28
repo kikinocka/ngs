d342f4bb3d7cf0c2b2782bb96af7e99bd8fc1409
KikaZah <kika.zahonova@gmail.com>
2020-03-28 14:02:44 -0600

    bla

--- bash_scripts/metacentrum/makeblastdb_many_pbs.sh
+++ bash_scripts/metacentrum/makeblastdb_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N MakeBlastDB
-#PBS -l select=1:ncpus=1:mem=50gb:scratch_local=50gb
-#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=1:mem=5gb:scratch_local=1gb
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,8 +10,8 @@ cat $PBS_NODEFILE
 #add module
 module add blast+-2.7.1
 
-datadir='/storage/brno3-cerit/home/kika/elonga_bct_genomes/'
-files=$datadir'*.fna'
+datadir='/auto/brno3-cerit/nfs4/home/fussyz01/hampllab/'
+files=$datadir'*.nt.fa.txt'
 dbtype=nucl
 
 for file in $files; do
