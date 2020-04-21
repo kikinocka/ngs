593c0c72a42a2ff1f39a52086ccf36a33557b130
Kristina Zahonova <kika@Kristinas-MacBook-Air.local>
2020-04-21 15:09:39 +0200

    bla

---
+++ bash_scripts/multiloc2_leshy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+multiloc='/opt/multiloc2/git/MultiLoc2/MultiLoc2/src/multiloc2_prediction.py'
+work_dir='/mnt/mokosz/home/kika/workdir/'
+files=$workdir'*.fa'
+a=animal
+p=plant
+f=fungal
+
+cd $workdir
+for file in $files; do
+	echo $file
+	out=${file%.*}'.multiloc_fungal.txt'
+	python $multiloc -fasta=$file -origin=$f -predictor=LowRes -result=$out -output=simple
+done
