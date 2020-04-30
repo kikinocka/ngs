6d3b2c2553c8890759fde058ab256eb2ad1b7f42
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-04-30 12:13:22 +0200

    bla

--- bash_scripts/metacentrum/fastqc_pbs.sh
+++ bash_scripts/metacentrum/fastqc_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N FastQC
-#PBS -l select=1:ncpus=1:mem=50gb:scratch_local=20gb
-#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=1:mem=50gb:scratch_local=30gb
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,8 +10,8 @@ cat $PBS_NODEFILE
 #add modules
 module add fastQC-0.11.5
 
-read_dir='/storage/brno3-cerit/home/kika/prototheca/wickerhamii'
-out_dir='/storage/brno3-cerit/home/kika/prototheca/wickerhamii/fastqc/'
+read_dir='/storage/brno3-cerit/home/kika/prototheca/zopfii'
+out_dir='/storage/brno3-cerit/home/kika/prototheca/zopfii/fastqc/'
 
 
 #copy data to scratch
@@ -25,11 +25,9 @@ files='*.gz'
 
 for file in $files; do
 	echo $file
-	fastqc -o $out_dir $file
+	fastqc -o $SCRATCHDIR $file
 done
 
-
-# fastqc -o $out_dir 'Light_L004_R1.fastq.gz'
-# fastqc -o $out_dir 'Light_L004_R2.fastq.gz'
-# fastqc -o $out_dir 'light_trimmed_1.fq.gz'
-# fastqc -o $out_dir 'light_trimmed_2.fq.gz'
+#copy files back
+rm files
+cp -r * $out_dir
