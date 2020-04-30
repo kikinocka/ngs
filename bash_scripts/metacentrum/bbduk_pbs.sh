78dcbdc0cfe3390706cc82bef84f46d547b3d6d5
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-04-30 16:44:26 +0200

    bla

--- bash_scripts/metacentrum/bbduk_pbs.sh
+++ bash_scripts/metacentrum/bbduk_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N bbduk
-#PBS -l select=1:ncpus=10:mem=15gb:scratch_local=20gb
+#PBS -l select=1:ncpus=20:mem=15gb:scratch_local=20gb
 #PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
@@ -27,7 +27,11 @@ report=$name'bbduk_report.txt'
 
 #compute on scratch
 cd $SCRATCHDIR
-bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
+bbduk.sh overwrite=true \
+	in1=$fw in2=$rv \
+	out1=$trimmed_fw out2=$trimmed_rv \
+	ref=$adapt \
+	usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
 
 #copy files back
 rm $fw $rv $adapters
