67d99040887a6db87e80a29b5e7d77f44414235f
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-04-30 12:18:50 +0200

    bla

--- bash_scripts/metacentrum/bbduk_pbs.sh
+++ bash_scripts/metacentrum/bbduk_pbs.sh
@@ -4,28 +4,31 @@
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
+
 cat $PBS_NODEFILE
 
 module add bbmap-36.92
 
 adapt='/storage/brno2/home/kika/tools/bbmap/resources/adapters.fa'
 
-read_dir='/storage/brno3-cerit/home/kika/cther/genome/'
-fw='SRR834693_1.fastq.gz'
-rv='SRR834693_2.fastq.gz'
-
-# trimdir='/storage/brno3-cerit/home/kika/pelomyxa/reads/transcriptome/'
-name='SRR834693'
-trimmed_fw=$name'_trimmed_1.fq.gz'
-trimmed_rv=$name'_trimmed_2.fq.gz'
-report=$name"_report.txt"
+read_dir='/storage/brno3-cerit/home/kika/prototheca/wickerhamii/'
+fw=$read_dir'BILC-read_1.fq.gz'
+rv=$read_dir'BILC-read_2.fq.gz'
 
 #copy data to scratch
-cd $read_dir
 cp $adapt $SCRATCHDIR
 cp $fw $rv $SCRATCHDIR
 
+name='BILC'
+trimmed_fw=$name'_trimmed_1.fq.gz'
+trimmed_rv=$name'_trimmed_2.fq.gz'
+report=$name'_report.txt'
+
+
+#compute on scratch
 cd $SCRATCHDIR
 bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
 
-cp $trimmed_fw $trimmed_rv $report $read_dir
+#copy files back
+rm $fw $rv
+cp -r * $read_dir
