90867a4a76562f502391aa41f6e6576608f48d08
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-01 10:05:12 +0200

    bla

--- bash_scripts/sed_read_headers.sh
+++ bash_scripts/sed_read_headers.sh
@@ -4,7 +4,7 @@ cd /storage/brno3-cerit/home/kika/prototheca/zopfii/
 
 files=*trimmed*.fq.gz
 
-for file in files; do
+for file in $files; do
 	echo $file
 done	
 # sed -E 's/(@.*)\/([[:digit:]]).*/\1_\2/' SRR8447029_1.fastq > SRR8447029_renamed_1.fastq
