cb3a02272262c46348cb814d9c60497e54b4eb55
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-01 09:56:34 +0200

    bla

--- bash_scripts/sed_read_headers.sh
+++ bash_scripts/sed_read_headers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /storage/brno3-cerit/home/kika/prototheca/zopfi/
+cd /storage/brno3-cerit/home/kika/prototheca/zopfii/
 
 sed -E 's/(@.*)\/([[:digit:]]).*/\1_\2/' SRR8447029_1.fastq > SRR8447029_renamed_1.fastq
