7c178313641cf9239a7810b10b462a15b3c9b0f9
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-01 17:03:40 +0200

    bla

--- bash_scripts/metacentrum/sed_read_headers_pbs.sh
+++ bash_scripts/metacentrum/sed_read_headers_pbs.sh
@@ -2,14 +2,25 @@
 
 cd /storage/brno3-cerit/home/kika/prototheca/zopfii/
 
-files=*trimmed*.fq.gz
+fw=*trimmed_1*.fq.gz
+rv=*trimmed_2*.fq.gz
 
-for file in $files; do
+for file in $fw; do
+	unzipped=${file%.gz}
+	renamed_fq=${unzipped/trimmed_/trimmed_renamed_}.gz
+
+	echo $file
+	zcat $file | awk '{{print (NR%4 == 1) ? "@1_" ++i "/1": $0}}' | gzip -c > $renamed_fq
+	echo 'Reads renamed:' $renamed_fq
+	echo '==================================================='
+done
+
+for file in $fw; do
 	unzipped=${file%.gz}
 	renamed_fq=${unzipped/trimmed_/trimmed_renamed_}.gz
 
 	echo $file
 	zcat $file | awk '{{print (NR%4 == 1) ? "@1_" ++i "/2": $0}}' | gzip -c > $renamed_fq
 	echo 'Reads renamed:' $renamed_fq
-	echo '==============================================='
+	echo '==================================================='
 done
