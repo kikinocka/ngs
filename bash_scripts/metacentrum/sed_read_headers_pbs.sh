9d59c0c345b27ac2210770de66409cd8c0bd2bae
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-01 16:22:50 +0200

    bla

--- bash_scripts/metacentrum/sed_read_headers_pbs.sh
+++ bash_scripts/metacentrum/sed_read_headers_pbs.sh
@@ -7,23 +7,9 @@ files=*trimmed*.fq.gz
 for file in $files; do
 	unzipped=${file%.gz}
 	renamed_fq=${unzipped/trimmed_/trimmed_renamed_}.gz
+
 	echo $file
-	echo $renamed_fq
-
-	# echo 'Unzipping ' $file
-	# gzip -k -d $file
-
-	# echo 'Replacing "/" by "_" in ' $unzipped
-	# sed -E 's/(@.*)\/([[:digit:]]).*/\1_\2/' $unzipped > $renamed_fq
-	
-	# echo 'Zipping ' $renamed_fq
-	# gzip $renamed_fq
-
-	# echo 'Removing ' $unzipped
-	# rm $unzipped
-
-	# echo '==============================================='
+	zcat $file | awk '{{print (NR%4 == 1) ? "@1_" ++i "/2": $0}}' | gzip -c > $renamed_fq
+	echo 'Reads renamed:' $renamed_fq
+	echo '==============================================='
 done
-
-
-# zcat $file | awk '{{print (NR%4 == 1) ? "@1_" ++i "/2": $0}}' | gzip -c > SRR8447028_trimmed_renamed_1.fq.gz 
