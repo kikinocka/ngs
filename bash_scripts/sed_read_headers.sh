e152e6ce84e173fdcfa147cd7aafc093b1a81b76
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-01 11:15:40 +0200

    bla

--- bash_scripts/sed_read_headers.sh
+++ bash_scripts/sed_read_headers.sh
@@ -5,14 +5,18 @@ cd /storage/brno3-cerit/home/kika/prototheca/zopfii/
 files=*trimmed*.fq.gz
 
 for file in $files; do
-	echo $file
-	first=${file/trimmed_/trimmed_renamed_}
-	first=${first%.gz}
-	# sed 's/_trimmed/_trimmed_renamed/' $file
-	# new=${file%.fq.gz}_renamed.fq
-	echo $first
-done	
-# sed -E 's/(@.*)\/([[:digit:]]).*/\1_\2/' SRR8447029_1.fastq > SRR8447029_renamed_1.fastq
-# SRR8447029_trimmed_1.fq.gz 
-# SRR8447029_trimmed_renamed_1.fq
-# aln=${f%.mafft.aln}.trimal_0.5.aln
+	unzipped=${file%.gz}
+	renamed_fq=${unzipped/trimmed_/trimmed_renamed_}
+
+	echo 'Unzipping ' $file
+	gzip -k -d $file
+
+	echo 'Replacing "/" by "_" in ' $unzipped
+	sed -E 's/(@.*)\/([[:digit:]]).*/\1_\2/' $unzipped > $renamed_fq
+	
+	echo 'Zipping ' $renamed_fq
+	gzip $renamed_fq
+
+	echo 'Removing ' $unzipped
+	rm $unzipped
+done
