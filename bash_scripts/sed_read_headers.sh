ae4fef13e067a4f29f45d3dd6e28f95288016afb
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-01 10:14:00 +0200

    bla

--- bash_scripts/sed_read_headers.sh
+++ bash_scripts/sed_read_headers.sh
@@ -6,5 +6,11 @@ files=*trimmed*.fq.gz
 
 for file in $files; do
 	echo $file
+	new=${file_trimmed%_\d.fq.gz}._renamed
+	echo $new
 done	
 # sed -E 's/(@.*)\/([[:digit:]]).*/\1_\2/' SRR8447029_1.fastq > SRR8447029_renamed_1.fastq
+# SRR8447029_trimmed_1.fq.gz 
+
+# for f in *.mafft.aln ; do
+#  aln=${f%.mafft.aln}.trimal_0.5.aln
