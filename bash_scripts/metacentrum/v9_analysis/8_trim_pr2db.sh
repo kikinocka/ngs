1e9e6a6617311e6dec753151eda5bfdaafd635d0
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-04 12:10:36 +0200

    bla

--- bash_scripts/metacentrum/v9_analysis/8_trim_pr2db.sh
+++ bash_scripts/metacentrum/v9_analysis/8_trim_pr2db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N trim_pr2db
-#PBS -l select=1:ncpus=15:mem=10gb:scratch_local=50gb
+#PBS -l select=1:ncpus=1:mem=5gb:scratch_local=10gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
