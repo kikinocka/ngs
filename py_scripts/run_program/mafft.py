6ce588ff91ae14ed12f224ca0953fd8d4cac736f
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-11 19:19:56 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -4,7 +4,7 @@ import subprocess
 
 mafft = '/Users/kika/miniconda3/bin/mafft'
 
-os.chdir('/Users/kika/ownCloud/SAGs/phylogenomics/concatenated/ver3/dataset/')
+os.chdir('/Users/kika/ownCloud/SAGs/phylogenomics/concatenated/ver4_GL/dataset/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
