ae520e8a6950652f2c59a5cb6894e050de990f4c
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-21 17:12:35 +0200

    bla

--- bash_scripts/get_sra_fastq.sh
+++ bash_scripts/get_sra_fastq.sh
@@ -8,8 +8,8 @@ module add sratools-2.3.2
 # cd '/storage/brno3-cerit/home/kika/prototheca'
 # wget https://sra-download.ncbi.nlm.nih.gov/traces/sra75/SRR/008249/SRR8447028
 # echo 'SRA SRR8447028 downloaded'
-fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip SRR8447030
-echo 'SRA SRR8447028 split to pair-end files'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip SRR8179913
+echo 'SRA SRR8179913 split to pair-end files'
 echo '-----------------------------'
 echo
 
