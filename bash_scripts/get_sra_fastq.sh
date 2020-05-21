32ced5e79365aa96503881217b2498046afc404e
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-21 17:28:56 +0200

    bla

--- bash_scripts/get_sra_fastq.sh
+++ bash_scripts/get_sra_fastq.sh
@@ -6,9 +6,9 @@
 module add sratools-2.3.2
 
 cd '/storage/brno3-cerit/home/kika/kinetoplastids/lguy_genome/reads'
-# wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-1/SRR8179913/SRR8179913.1
-# echo 'SRA SRR8179913 downloaded'
-fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip SRR8179913.1
+wget ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR817/003/SRR8179913
+echo 'SRA SRR8179913 downloaded'
+fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip SRR8179913
 echo 'SRA SRR8179913 split to pair-end files'
 echo '-----------------------------'
 echo
