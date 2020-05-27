6873cab9f22477d7151b51b0562081d1ee09fa74
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-27 16:06:30 +0200

    bla

--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/SNARE/')
-trees = [x for x in os.listdir() if x.endswith('iq.treefile')]
+os.chdir('/Users/kika/ownCloud/SAGs/SSUs/euglenozoans/ver9/')
+trees = [x for x in os.listdir() if x.endswith('.treefile')]
 
 #file in format Acc. number \t name of organism \n
-names = open('qa_names.txt')
+names = open('ssu_names.txt')
 
 name_dict = {}
 for name in names:
 	split_line = name.strip().split('\t')
-	new = split_line[1]
-	# new = split_line[0] + ' ' + split_line[1]
+	# new = split_line[1]
+	new = split_line[0] + ' ' + split_line[1]
 	name_dict[split_line[0]] = new
 
 for tree in trees:
