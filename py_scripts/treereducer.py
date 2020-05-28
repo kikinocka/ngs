5ea377dd8c873525b618a0906a4052e0b449f853
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-05-28 18:49:02 +0200

    bla

--- py_scripts/treereducer.py
+++ py_scripts/treereducer.py
@@ -2,16 +2,16 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Dcko/ownCloud/proteromonas/RABs/tree/ver6/')
+os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/mito_proteins/fes_cluster_assembly/nif/nifS_tree/ver10/')
 inacc = open('delete.acc')
-infasta = SeqIO.parse('rabome_prot.fa', 'fasta')
+infasta = SeqIO.parse('nifS.fa', 'fasta')
 
 omitted = []
 for line in inacc:
 	omitted.append(line.strip())
 
 c = 0
-with open('rabome_prot_reduced.fa', 'w') as result:
+with open('nifS_reduced.fa', 'w') as result:
 	for seq in infasta:
 		if seq.description in omitted:
 			c += 1
