8c7b51a4e51b3cc5912f514eae6e063f848fa8dc
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-04 12:41:36 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -N IQT-SSUeuk
+#PBS -N IQT-nifU
 #PBS -q default
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb:os=debian9
-#PBS -l walltime=96:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,24 +11,24 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/sags/ssus/eukaryotes/bs/'
+datadir='/storage/brno3-cerit/home/kika/pelomyxa/trees/nifU/ver9/'
 
 #copy files to scratch
-cp $datadir'ssus_eukaryotes.trimal_gt_0.8.aln' $SCRATCHDIR
+cp $datadir'nifU_v9.trimal_gt_0.5.aln' $SCRATCHDIR
 # cp $datadir'guide_acsl_seqs.treefile' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='ssus_eukaryotes.trimal_gt_0.8.aln'
-# guide='guide_concat'
-# guide_tree=$guide'.treefile'
-bb=100
+aln='nifU_v9.trimal_gt_0.5.aln'
+guide='guide_nifU'
+guide_tree=$guide'.treefile'
+bb=1000
 
 # iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m GTR+G -quiet
-iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m GTR+G -quiet
+# iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m GTR+G -quiet
 
-# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree -wsr
+iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,14 +3,14 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/SAGs/SSUs/euglenozoans/ver10/')
+os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/mito_proteins/fes_cluster_assembly/nif/nifU_tree/ver9/')
 files = [x for x in os.listdir() if 'mafft.aln' in x]
 
 for file in files:
 	print(file)
 	file_name = file.split('.')[0] #+ '_' + file.split('_')[1]
 	aut = 'automated1'
-	gt = 0.8 #fraction of sequences with a gap allowed
+	gt = 0.5 #fraction of sequences with a gap allowed
 	st = 0.001 #minimum average similarity allowed.
 
 	# output = '{}.trimal_{}.aln'.format(file_name, aut)
