811c5b93376903bc099411d3fd69675fe7cb4fff
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-04 15:26:56 +0200

    bla

--- bash_scripts/metacentrum/spades_pbs.sh
+++ bash_scripts/metacentrum/spades_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N SPAdes
 #PBS -l select=1:ncpus=10:ompthreads=10:mem=30gb:scratch_local=30gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,18 +10,22 @@ cat $PBS_NODEFILE
 #add modules
 module add spades-3.14.0
 
-reads='/storage/brno3-cerit/home/kika/kinetoplastids/cbom_genome/reads/'
-outdir='/storage/brno3-cerit/home/kika/kinetoplastids/cbom_genome/'
+datadir='/storage/brno3-cerit/home/kika/kinetoplastids/'
+reads=$datadir'cbom_genome/reads/'
+outdir=$datadir'cbom_genome/'
+cfasdir=$datadir'/cfas_genome/'
 
 #copy reads to scratch
 cp $reads'AK08_047.R1.trim.fq.gz' $reads'AK08_047.R2.trim.fq.gz' $SCRATCHDIR
+cp $cfasdir'cfas_AODS02.fasta' $SCRATCHDIR
 
 pe1_1='AK08_047.R1.trim.fq.gz'
 pe1_2='AK08_047.R2.trim.fq.gz'
+cfas='cfas_AODS02.fasta'
 
 #compute on scratch
 cd $SCRATCHDIR
-spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --careful -t $PBS_NUM_PPN -o spades
+spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --untrusted-contigs $cfas --careful -t $PBS_NUM_PPN -o spades-cfas
 
 
 # spades.py --sc --careful -t $PBS_NUM_PPN -o out \
@@ -29,5 +33,5 @@ spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --careful -t $PBS_NUM_PPN -o spades
 # --pe2-m $pe2m --pe2-1 $pe21 --pe2-2 $pe22 --pe2-s $pe2u \
 
 #copy results back
-rm $pe1_1 $pe1_2
+rm $pe1_1 $pe1_2 $cfas
 cp -r * $outdir
