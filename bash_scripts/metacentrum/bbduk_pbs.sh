756c3d9f4ecce6d0e13b5e89be2501be825caa29
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-08 11:17:10 +0200

    bla

--- bash_scripts/metacentrum/bbduk_pbs.sh
+++ bash_scripts/metacentrum/bbduk_pbs.sh
@@ -28,11 +28,13 @@ trimmed_fw=$name'_trimmed.fq.gz'
 # trimmed_rv=$name'trimmed_2.fq.gz'
 report=$name'_bbduk_report.txt'
 
-bbduk.sh overwrite=true in1=$fw out1=$trimmed_fw\
-	# in1=$fw in2=$rv \
-	# out1=$trimmed_fw out2=$trimmed_rv \
-	ref=$adapt \
-	usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
+# bbduk.sh overwrite=true \
+# 	in1=$fw in2=$rv \
+# 	out1=$trimmed_fw out2=$trimmed_rv \
+# 	ref=$adapt \
+# 	usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
+
+bbduk.sh in=$fw out=$trimmed_fw ref=$adapt k=23 ktrim=r mink=11 edist=1 qtrim=rl trimq=15 t=$PBS_NUM_PPN 2> $report
 
 #copy files back
 rm $fw $adapt
