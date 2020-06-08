d8e0f75d41ca72760f49e1de464afdbb072c9538
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-08 18:06:46 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -11,7 +11,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/sags/mit/ver2/concat/'
+datadir='/storage/brno3-cerit/home/kika/sags/mit/ver3/'
 
 #copy files to scratch
 cp $datadir'mit_concat.aln' $SCRATCHDIR
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -4,7 +4,7 @@ import subprocess
 
 mafft = '/Users/kika/miniconda3/bin/mafft'
 
-os.chdir('/Users/kika/ownCloud/SAGs/mit/phylogenomics/ver2/dataset/')
+os.chdir('/Users/kika/ownCloud/SAGs/mit/phylogenomics/ver3/dataset/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
