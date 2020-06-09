90b85a20050d61174f5934354a3a6c75dc37a036
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-09 09:34:24 +0200

    bla

--- bash_scripts/metacentrum/quast_pbs.sh
+++ bash_scripts/metacentrum/quast_pbs.sh
@@ -12,13 +12,13 @@ module add quast-4.6.3
 
 datadir='/storage/brno3-cerit/home/kika/kinetoplastids/cbom_genome/'
 # mapping=$datadir'bw2_mapping/pilon10/'
-outdir=$datadir'quast/cbom_trusted'
+outdir=$datadir'quast'
 
 #copy files to scratch
 # cp $mapping'ku80_p10_bw2_sorted.bam' $SCRATCHDIR
 cp $datadir'/'*trusted.fa $SCRATCHDIR
 
-assemblies='*_trusted.fa'
+assemblies='*trusted.fa'
 # bam='ku80_p10_bw2_sorted.bam'
 min_contig=500
 
@@ -29,11 +29,14 @@ cd $SCRATCHDIR
 
 for fasta in $assemblies; do
 	echo $fasta
+	out=`echo $fasta | cut -d '.' -f 2`
+	full_out=$outdir'/'$out
+
 	quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $fasta
+	mv -r $full_out
 done
 
 
 #copy results to your folder
 # rm $f $bam
 rm $assemblies
-cp -r * $outdir
