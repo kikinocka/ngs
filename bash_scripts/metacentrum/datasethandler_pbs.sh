b53db7aa515dbc0582baa11f48c50101029b0d1a
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-10 18:00:50 +0200

    bla

--- bash_scripts/metacentrum/datasethandler_pbs.sh
+++ bash_scripts/metacentrum/datasethandler_pbs.sh
@@ -14,7 +14,7 @@ module add mafft-7.313
 module add trimal-1.4
 
 #copy files to scratch
-DATADIR='/storage/brno3-cerit/home/kika/trafficking/SNARE/Qa/ver3'
+DATADIR='/storage/brno3-cerit/home/kika/trafficking/SNARE/Qb/ver2'
 
 cp '/storage/brno2/home/kika/scripts/kika/py_scripts/datasethandler-server.py' $SCRATCHDIR
 cp $DATADIR'/'*.fa $SCRATCHDIR
