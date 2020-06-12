b49dea7ebaaec24978c2e273690347a976cc4cd6
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-12 14:56:10 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N IQT-mitEbs
 #PBS -q default
-#PBS -l select=1:ncpus=10:mem=10gb:scratch_local=1gb:os=debian9
-#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb:os=debian9
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
