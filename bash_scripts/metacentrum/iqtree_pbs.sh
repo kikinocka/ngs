59d63275712c2f42a5aca96fa872a61a491e9e29
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-14 09:06:17 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -2,7 +2,7 @@
 #PBS -N IQT-conUFB
 #PBS -q default
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb:os=debian9
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,7 +11,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/sags/phylogenomics/concat_ver5_C20_ufb/'
+datadir='/storage/brno3-cerit/home/kika/sags/phylogenomics/concat_ver5_LG4X_ufb/'
 
 #copy files to scratch
 cp $datadir'concat.aln' $SCRATCHDIR
@@ -25,10 +25,10 @@ guide_tree=$guide'.treefile'
 bb=1000
 
 # iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST -quiet -wsr
-# iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m GTR+G -quiet
+iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet -wsr
 
-iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
+# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
