f64b60829fc287544e89806b59c355f28e3b7f47
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-14 09:29:16 +0200

    bla

--- bash_scripts/metacentrum/iqtree_eug_pbs.sh
+++ bash_scripts/metacentrum/iqtree_eug_pbs.sh
@@ -28,7 +28,7 @@ bb=1000
 # iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet -wsr
 
 iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
+iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree -wsr
 
 #copy files back
 rm $aln
