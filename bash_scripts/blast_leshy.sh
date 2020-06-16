1472eeeacc14894f4be6953989b822b09bd11e15
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-06-16 10:15:57 +0200

    bla

--- bash_scripts/blast_leshy.sh
+++ bash_scripts/blast_leshy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 datadir='/mnt/mokosz/home/kika/prototheca/'
-query=$datadir'pzop_hits.fa'
-out=$datadir'pzop_hits.nr.blast.xml'
+query=$datadir'pwic_hits.fa'
+out=$datadir'pwic_hits.nr.blast.xml'
 db='/opt/databases/nr/nr'
 program=blastx
 task=blastx
 outfmt=5
 eval=1e-3
 max_seqs=5
-cpu=20
+cpu=4
 
 $program -task $task \
 	-query $query \
