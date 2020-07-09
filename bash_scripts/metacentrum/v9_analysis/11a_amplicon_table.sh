4f9e1726eac0e16489fbf0b2ed50ef74b6fd64a1
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-07-09 15:14:14 +0200

    bla

--- bash_scripts/metacentrum/v9_analysis/11a_amplicon_table.sh
+++ bash_scripts/metacentrum/v9_analysis/11a_amplicon_table.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N amplicons
 #PBS -l select=1:ncpus=15:mem=10gb:scratch_local=50gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,12 +11,13 @@ module add vsearch-1.4.4
 
 data='/storage/brno3-cerit/home/kika/sl_euglenozoa/trimmed_cutadapt'
 script='/storage/brno2/home/kika/scripts/kika/bash_scripts/metacentrum/v9_analysis/'
+out='/storage/brno3-cerit/home/kika/sl_euglenozoa/'
 
 #copy files to scratch
 cp $data'/'*.fas $SCRATCHDIR
 cp $script'11b_amplicon_contingency_table.py' $SCRATCHDIR
 
-OUTPUT=
+OUTPUT='amplicon_table.out'
 SCRIPT='11b_amplicon_contingency_table.py'
 
 
@@ -33,8 +34,7 @@ ls -1 ./[1-9]*.fas | cut -d '/' -f 2 | sort -d | uniq -d
 
 python '${SCRIPT}' ./[1-9]*.fas > ${OUTPUT} &
 
-/var/spool/pbs/mom_priv/jobs/721817.cerit-pbs.cerit-sc.cz.SC: line 39: ${OUTPUT}: ambiguous redirect
 
 #copy files back
-rm $fasta $SCRIPT
-cp -r * $data
+rm *fas $SCRIPT
+cp -r * $out
