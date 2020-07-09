5dbbb1fb25056d005bd5b32d0dd2473cf3da55ba
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-07-09 22:22:10 +0200

    bla

--- bash_scripts/metacentrum/datasethandler_pbs.sh
+++ bash_scripts/metacentrum/datasethandler_pbs.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #PBS -N datasethandler
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=10gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -14,7 +14,7 @@ module add mafft-7.453
 module add trimal-1.4
 
 #copy files to scratch
-DATADIR='/storage/brno3-cerit/home/kika/pelomyxa/trees/acs'
+DATADIR='/storage/brno3-cerit/home/kika/trafficking/COPII/ver2'
 
 cp '/storage/brno2/home/kika/scripts/kika/py_scripts/datasethandler-server.py' $SCRATCHDIR
 cp $DATADIR'/'*.fa $SCRATCHDIR
@@ -25,7 +25,7 @@ python datasethandler-server.py \
 	-i batch \
 	-a mafft \
 	--alignerparams='--anysymbol' \
-	--trimalparams='-gt 0.5' \
+	--trimalparams='-gt 0.8' \
 	-t iqtree \
 	-b \
 	--maxcores $PBS_NUM_PPN
