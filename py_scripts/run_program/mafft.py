8f1d6c64c831047eac07f965e9c86375095c7206
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-07-17 15:43:02 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -15,6 +15,6 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 #add to aligned sequences
 os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
 existing = 'queries/SNAREs/Qb_160302.afa'
-new = 'trees/SNARE/Qb/ver6/euglenozoa.fa'
-out = 'trees/SNARE/Qb/ver6/qb_v6.mafft.aln'
+new = 'trees/SNARE/Qb/ver7/euglenozoa.fa'
+out = 'trees/SNARE/Qb/ver7/qb_v7.mafft.aln'
 subprocess.call('{} --add {} --thread 7 --inputorder {} > {}'.format(mafft, new, existing, out), shell=True)
