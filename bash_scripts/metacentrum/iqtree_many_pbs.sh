27c452cb18cc89811e78a377fb655761c85b297f
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-07-27 12:22:34 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many_pbs.sh
+++ bash_scripts/metacentrum/iqtree_many_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/trafficking/SNARE/Qc/ver10'
+data_dir='/storage/brno3-cerit/home/kika/sags/nad11/ver3'
 
 #copy files to scratch
 cp $data_dir'/'*.aln $SCRATCHDIR
@@ -20,12 +20,12 @@ cp $data_dir'/'*.aln $SCRATCHDIR
 cd $SCRATCHDIR
 
 for f in *.aln ; do
- guide=guide_${f%.trimal_gt_0.8.aln}
- guide_tree=$guide'.treefile'
+ # guide=guide_${f%.trimal_gt_0.8.aln}
+ # guide_tree=$guide'.treefile'
  bb=1000
- # iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
- iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
- iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
+ iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
+ # iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
+ # iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
 done
 
 #copy files back
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -4,17 +4,17 @@ import subprocess
 
 mafft = '/Users/kika/miniconda3/bin/mafft'
 
-# os.chdir('/Users/kika/ownCloud/SAGs/mit/nad11/ver2/')
-# files = [x for x in os.listdir() if x.endswith('.fa')]
+os.chdir('/Users/kika/ownCloud/SAGs/mit/nad11/ver3/')
+files = [x for x in os.listdir() if x.endswith('.fa')]
 
-# for file in files:
-# 	print(file)
-# 	out = '{}.mafft.aln'.format(file.split('.fa')[0])
-# 	subprocess.call('{} --thread 6 --maxiterate 100 --inputorder --auto {} > {}'.format(mafft, file, out), shell=True)
+for file in files:
+	print(file)
+	out = '{}.mafft.aln'.format(file.split('.fa')[0])
+	subprocess.call('{} --thread 6 --maxiterate 100 --inputorder --auto {} > {}'.format(mafft, file, out), shell=True)
 
-#add to aligned sequences
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
-existing = 'queries/SNAREs/Qc_160302_2.afa'
-new = 'trees/SNARE/Qc/ver9/euglenozoa.fa'
-out = 'trees/SNARE/Qc/ver9/qc_v9.mafft.aln'
-subprocess.call('{} --add {} --thread 6 --inputorder {} > {}'.format(mafft, new, existing, out), shell=True)
+# #add to aligned sequences
+# os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
+# existing = 'queries/SNAREs/Qc_160302_2.afa'
+# new = 'trees/SNARE/Qc/ver9/euglenozoa.fa'
+# out = 'trees/SNARE/Qc/ver9/qc_v9.mafft.aln'
+# subprocess.call('{} --add {} --thread 6 --inputorder {} > {}'.format(mafft, new, existing, out), shell=True)
