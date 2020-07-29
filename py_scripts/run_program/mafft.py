e5b04d583202b97f569ab9507a4f19f706bced27
Kristina Zahonova <kika@Kikas-Macbook.local>
2020-07-29 12:09:25 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -15,6 +15,6 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 #add to aligned sequences
 os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
 existing = 'queries/SNAREs/R_SNARES_all.afa'
-new = 'trees/SNARE/R/ver2/euglenozoa.fa'
-out = 'trees/SNARE/R/ver2/r_v2.mafft.aln'
+new = 'trees/SNARE/R/ver3/euglenozoa.fa'
+out = 'trees/SNARE/R/ver3/r_v3.mafft.aln'
 subprocess.call('{} --add {} --thread 6 --inputorder {} > {}'.format(mafft, new, existing, out), shell=True)
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/SNARE/R/ver2/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/SNARE/R/ver3/')
 files = [x for x in os.listdir() if 'mafft.aln' in x]
 
 for file in files:
