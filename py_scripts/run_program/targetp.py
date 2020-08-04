828b1defd4f1314e2e9dda8e48424ccf0d68e134
kikinocka <kika.zahonova@gmail.com>
2020-08-04 17:28:46 +0200

    bla

--- py_scripts/run_program/targetp.py
+++ py_scripts/run_program/targetp.py
@@ -4,8 +4,8 @@ import subprocess
 
 targetp='/Users/kika/programs/targetp-2.0/bin/targetp'
 
-os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/mito_proteins/energy_generation/')
-files = [x for x in os.listdir() if x.endswith('carboxylase.fa')]
+os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/mito_proteins/sulfate_activation/')
+files = [x for x in os.listdir() if x.endswith('transporter.fa')]
 
 for file in files:
 	print(file)
