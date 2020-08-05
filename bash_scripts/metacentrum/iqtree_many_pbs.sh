8d2364e4c185f820cbc04d62e5251c2036780236
kikinocka <kika.zahonova@gmail.com>
2020-08-05 15:29:40 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many_pbs.sh
+++ bash_scripts/metacentrum/iqtree_many_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/pelomyxa/trees/MCF_transporters/ver2'
+data_dir='/storage/brno3-cerit/home/kika/pelomyxa/trees/MCF_transporters/ver3'
 
 #copy files to scratch
 cp $data_dir'/'*.aln $SCRATCHDIR
