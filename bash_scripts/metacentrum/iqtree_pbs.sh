537504373f63b0cbb4ef9d3c7989135eae994e88
kikinocka <kika.zahonova@gmail.com>
2020-08-07 11:07:06 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -N IQT-LG4X
+#PBS -N IQT-mitBS
 #PBS -q default
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb:os=debian9
-#PBS -l walltime=336:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,7 +11,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/sags/phylogenomics/concat_ver8_LG4X_bs'
+datadir='/storage/brno3-cerit/home/kika/sags/mit/ver5/concat_bs'
 
 #copy files to scratch
 cp $datadir'/'* $SCRATCHDIR
@@ -19,12 +19,13 @@ cp $datadir'/'* $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='concat.aln'
+aln='mit_concat.aln'
 # guide='guide_concat'
 # guide_tree=$guide'.treefile'
 bb=1000
 
-iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet -wsr
+iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST -quiet -wsr
+# iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet -wsr
 
 # iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
 # iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
