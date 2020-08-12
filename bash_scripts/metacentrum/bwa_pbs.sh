586184a25df91619e4a980ac36fef57ab021cfcd
kikinocka <kika.zahonova@gmail.com>
2020-08-12 10:46:30 +0200

    bla

--- bash_scripts/metacentrum/bwa_pbs.sh
+++ bash_scripts/metacentrum/bwa_pbs.sh
@@ -8,39 +8,31 @@
 cat $PBS_NODEFILE
 
 #add module
-module add bwa-0.7.3a
+module add bwa-0.7.17
 module add samtools-1.3.1
 
-sags='/storage/brno3-cerit/home/kika/sags/reassembly/'
-reads=$sags'trimmed_reads/'
-spades=$sags'spades/'
-outdir=$sags'mapping_bwa/'
+sags='/storage/brno3-cerit/home/kika/sags/mapping/EU2/'
+outdir=$sags'bwa/'
 
 #copy files to scratch
-cp $spades'contigs.fasta' $SCRATCHDIR
-cp $reads'all_r1_trimmed.fq.gz' $reads'all_r2_trimmed.fq.gz' $reads'all_unpaired.fq.gz' $SCRATCHDIR
+cp $spades'EU2_contigs.fasta' $SCRATCHDIR
+cp $sags'EU2_r1_val_1.fq.gz' $sags'EU2_r2_val_2.fq.gz' $sags'unpaired_all.fq.gz' $SCRATCHDIR
 
-assembly='contigs.fasta'
-fw='all_r1_trimmed.fq.gz'
-rv='all_r2_trimmed.fq.gz'
-unpaired='all_unpaired.fq.gz'
+assembly='EU2_contigs.fasta'
+fw='EU2_r1_val_1.fq.gz'
+rv='EU2_r2_val_2.fq.gz'
+unpaired='unpaired_all.fq.gz'
 
-base_name='EU1718_bwa_'
+base_name='EU2_bwa_'
 index_report=$base_name'index_genome.report'
 
 stat_mapped_paired=$base_name'mapped_paired.flagstat'
-sam_mapped_paired=$base_name'mapped_paired.sam'
-bam_mapped_paired=$base_name'mapped_paired.bam'
 sorted_mapped_paired=$base_name'mapped_paired.sorted.bam'
-# bai_mapped_paired=$base_name'mapped_paired.sorted.bam.bai'
-report_mapped_paired=$base_name'mapped_paired.report'
+bai_mapped_paired=$base_name'mapped_paired.sorted.bam.bai'
 
 stat_mapped_unpaired=$base_name'mapped_unpaired.flagstat'
-sam_mapped_unpaired=$base_name'mapped_unpaired.sam'
-bam_mapped_unpaired=$base_name'mapped_unpaired.bam'
 sorted_mapped_unpaired=$base_name'mapped_unpaired.sorted.bam'
-# bai_mapped_unpaired=$base_name'mapped_unpaired.bam.bai'
-report_mapped_unpaired=$base_name'mapped_unpaired.report'
+bai_mapped_unpaired=$base_name'mapped_unpaired.bam.bai'
 
 bam=$base_name'mapped_all.bam'
 sorted=$base_name'mapped_all.sorted.bam'
@@ -50,17 +42,17 @@ sorted=$base_name'mapped_all.sorted.bam'
 cd $SCRATCHDIR
 bwa index -a bwtsw $assembly 2>$index_report
 
-bwa mem -t $PBS_NUM_PPN $assembly $fw $rv > $sam_mapped_paired 2> $report_mapped_paired
-samtools view -bS $sam_mapped_paired > $bam_mapped_paired -@ $PBS_NUM_PPN
-samtools flagstat $bam_mapped_paired > $stat_mapped_paired
-samtools sort -O BAM -o $sorted_mapped_paired -@ PBS_NUM_PPN $bam_mapped_paired
-samtools index $sorted_mapped_paired
+bwa mem -t $PBS_NUM_PPN $assembly $fw $rw \
+	| tee >(samtools flagstat - > $stat_mapped_paired) \
+	| samtools sort -O BAM \
+	| tee $sorted_mapped_paired \
+	| samtools index - $bai_mapped_paired
 
-bwa mem -t $PBS_NUM_PPN $assembly $unpaired > $sam_mapped_unpaired 2> $report_mapped_unpaired
-samtools view -bS $sam_mapped_unpaired > $bam_mapped_unpaired -@ $PBS_NUM_PPN
-samtools flagstat $bam_mapped_unpaired > $stat_mapped_unpaired
-samtools sort -O BAM -o $sorted_mapped_unpaired -@ PBS_NUM_PPN $bam_mapped_unpaired
-samtools index $sorted_mapped_unpaired
+bwa mem -t $PBS_NUM_PPN $assembly $fw $rw \
+	| tee >(samtools flagstat - > $stat_mapped_unpaired) \
+	| samtools sort -O BAM \
+	| tee $sorted_mapped_unpaired \
+	| samtools index - $bai_mapped_unpaired
 
 samtools merge -@ $PBS_NUM_PPN -f $bam $sorted_mapped_paired $sorted_mapped_unpaired
 samtools sort -@ $PBS_NUM_PPN -o $sorted $bam
