2ee61444d6161a75dfc200abd1dac06a4b99a9b7
kikinocka <kika.zahonova@gmail.com>
2020-08-13 17:01:47 +0200

    bla

--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/SAGs/mit/phylogenomics/ver5/concat_bs/')
+os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/trees/NaS_transporter/ver4/')
 trees = [x for x in os.listdir() if x.endswith('aln.treefile')]
 
 #file in format Acc. number \t name of organism \n
-names = open('mit_names.txt')
+names = open('nast_names.txt')
 
 name_dict = {}
 for name in names:
