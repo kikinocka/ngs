bfca39d736e641fafd9cda8e7dc75e86d5f534ca
kikinocka <kika.zahonova@gmail.com>
2020-08-28 13:07:13 +0200

    bla

--- bash_scripts/metacentrum/hisat2_pbs.sh
+++ bash_scripts/metacentrum/hisat2_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N Hisat2
-#PBS -l select=1:ncpus=20:mem=50gb:scratch_local=50gb
-#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=20:mem=70gb:scratch_local=50gb
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
--- bash_scripts/metacentrum/quast_pbs.sh
+++ bash_scripts/metacentrum/quast_pbs.sh
@@ -11,21 +11,18 @@ cat $PBS_NODEFILE
 module add quast-4.6.3
 
 datadir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly/'
-# mapping=$datadir'bw2_mapping/pilon10/'
 outdir=$datadir'quast'
 
 #copy files to scratch
-# cp $mapping'ku80_p10_bw2_sorted.bam' $SCRATCHDIR
 cp $datadir'pelomyxa_final_corr_genome.fa' $SCRATCHDIR
 
 assemblies='pelomyxa_final_corr_genome.fa'
-# bam='ku80_p10_bw2_sorted.bam'
 min_contig=500
 
 
 #compute on scratch
 cd $SCRATCHDIR
-quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote --bam $bam $assemblies
+quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $assemblies
 
 # for fasta in $assemblies; do
 # 	echo $fasta
