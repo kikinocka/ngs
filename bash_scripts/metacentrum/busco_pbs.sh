cd2d68f93bb3c3f73ccdda1d09bd01a035f92bc3
kikinocka <kika.zahonova@gmail.com>
2020-08-28 13:20:15 +0200

    bla

--- bash_scripts/metacentrum/busco_pbs.sh
+++ bash_scripts/metacentrum/busco_pbs.sh
@@ -19,7 +19,7 @@ cp -r $augustus_configs/* $SCRATCHDIR/augustus_configs/ || exit 1
 export AUGUSTUS_CONFIG_PATH=$SCRATCHDIR/augustus_configs
 export PATH=$PATH:/software/augustus/3.3.1/src/bin:/software/augustus/3.3.1/src/scripts
 
-assembly_dir='/storage/brno3-cerit/home/kika/archamoebae'
+assembly_dir='/storage/brno3-cerit/home/kika/pelomyxa/genome_assembly'
 # busco_dir=$assembly_dir'busco/'
 # summaries=$busco_dir'summaries/'
 lin_dir='/software/busco/3.0.2/src/db/'
@@ -31,14 +31,14 @@ lin_dir='/software/busco/3.0.2/src/db/'
 # ln -s $busco_dir'rli_eukaryota_odb9/short_summary_eukaryota_odb9.txt' $summaries'short_summary_eukaryota_odb9_rli.txt'
 
 #copy files to scratch
-cp $assembly_dir'/'*.fa $SCRATCHDIR
+cp $assembly_dir'/pelomyxa_predicted_proteins_corr.fa' $SCRATCHDIR
 cp -r $lin_dir'eukaryota_odb9/' $SCRATCHDIR
 
 assemblies='*.fa'
 # base='eukaryota_odb9'
 lineage='eukaryota_odb9/'
 mode='proteins'
-species='leishmania_tarentolae'
+species='pelomyxa'
 
 
 #compute on scratch
@@ -47,11 +47,11 @@ cd $SCRATCHDIR
 for fasta in $assemblies; do
 	echo $fasta
 	base=${fasta%.fa}_eukaryota_odb9
-	run_BUSCO.py -i $fasta -o $base -l $lineage -m $mode -c $PBS_NUM_PPN #-sp $species 
+	run_BUSCO.py -i $fasta -o $base -l $lineage -m $mode -c $PBS_NUM_PPN -sp $species 
 done
 # generate_plot.py -wd $summaries
 
 #copy files back
-rm -r $assemblies $lineage augustus_configs
-cp -r * $assembly_dir
+rm -r $assemblies $lineage
+cp -r * $assembly_dir'/busco/'
 # cp -r * $summaries
