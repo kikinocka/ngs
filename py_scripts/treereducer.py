acb53136f8bf10b9194c7c3ba008e2cf7174946e
kikinocka <kika.zahonova@gmail.com>
2020-08-28 14:00:36 +0200

    bla

--- py_scripts/treereducer.py
+++ py_scripts/treereducer.py
@@ -2,16 +2,16 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/trees/NaS_transporter/ver3')
+os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/trees/NifU/ver9')
 inacc = open('delete.acc')
-infasta = SeqIO.parse('nast_v3.fa', 'fasta')
+infasta = SeqIO.parse('nifU_v9.fa', 'fasta')
 
 omitted = []
 for line in inacc:
 	omitted.append(line.strip())
 
 c = 0
-with open('nast_reduced.fa', 'w') as result:
+with open('nifU_v10.fa', 'w') as result:
 	for seq in infasta:
 		if seq.description in omitted:
 			c += 1
