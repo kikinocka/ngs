c51162bfd27dd5f8feacd1a5853871656ff10493
kikinocka <kika.zahonova@gmail.com>
2020-09-08 10:51:32 +0200

    bla

--- bash_scripts/metacentrum/bowtie2_pbs.sh
+++ bash_scripts/metacentrum/bowtie2_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N Bowtie2
 #PBS -l select=1:ncpus=30:mem=50gb:scratch_local=20gb
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,25 +11,27 @@ cat $PBS_NODEFILE
 module add bowtie2-2.3.0
 module add samtools-1.3.1
 
-data='/storage/brno3-cerit/home/kika/sags/mapping/EU2-2/'
+data='/storage/brno3-cerit/home/kika/kinetoplastids/cfas_genome/'
 
 #copy files to scratch
-cp $data'EU2_contigs.fasta' $SCRATCHDIR
-cp $data'EU2_r1_val_1.fq.gz' $SCRATCHDIR
-cp $data'EU2_r2_val_2.fq.gz' $SCRATCHDIR
-cp $data'EU2_r1_unpaired_1.fq.gz' $SCRATCHDIR
-cp $data'EU2_r2_unpaired_2.fq.gz' $SCRATCHDIR
+cp $data'cfas_AODS02.fasta' $SCRATCHDIR
+cp $data'reads/SRR1028161_trimmed_1.fq.gz' $SCRATCHDIR
+cp $data'reads/SRR1028161_trimmed_2.fq.gz' $SCRATCHDIR
+cp $data'reads/SRR1593518_trimmed_1.fq.gz' $SCRATCHDIR
+cp $data'reads/SRR1593518_trimmed_2.fq.gz' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='EU2_bw2'
-ref='EU2_contigs.fasta'
-p1_1='EU2_r1_val_1.fq.gz'
-p1_2='EU2_r2_val_2.fq.gz'
-r1='EU2_r1_unpaired_1.fq.gz'
-r2='EU2_r2_unpaired_2.fq.gz'
+base_name='cfas_bw2'
+ref='cfas_AODS02.fasta'
+p1_1='SRR1028161_trimmed_1.fq.gz'
+p1_2='SRR1028161_trimmed_2.fq.gz'
+p2_1='SRR1593518_trimmed_1.fq.gz'
+p2_2='SRR1593518_trimmed_2.fq.gz'
+# r1='EU2_r1_unpaired_1.fq.gz'
+# r2='EU2_r2_unpaired_2.fq.gz'
 
 samfile=$base_name'.sam'
 unmapped_unpaired=$base_name'_unmapped_unpaired.fq'
@@ -41,16 +43,16 @@ sorted=$base_name'_sorted.bam'
 bowtie2-build --threads $PBS_NUM_PPN $ref $base_name
 bowtie2 --very-sensitive -p $PBS_NUM_PPN \
 	-x $base_name \
-	-1 $p1_1 \
-	-2 $p1_2 \
-	-U $r1,$r2 \
+	-1 $p1_1,$p2_1 \
+	-2 $p1_2,$p2_2 \
 	--un-gz $unmapped_unpaired \
 	--un-conc-gz $unmapped_paired \
 	-S $samfile 2> $report
+	# -U $r1,$r2 \
 
 samtools view -bS $samfile > $bamfile -@ $PBS_NUM_PPN
 samtools sort -o $sorted -@ PBS_NUM_PPN $bamfile 
 samtools index $sorted
 
 #copy files back
-cp *bw2* $data
+cp *bw2* $data'bw2_mapping/'
