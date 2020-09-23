87a9a33655d6034523d3d7443b8b27bed6314120
kikinocka <kika.zahonova@gmail.com>
2020-09-23 16:07:39 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -16,6 +16,6 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 #add to aligned sequences
 os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
 existing = 'queries/ESCRTs/CHMP7.R9.mask.aln'
-new = 'trees/ESCRTs/chmp7/ver2/euglenozoa_chmp7.fa'
-out = 'trees/ESCRTs/chmp7/ver2/chmp7.mafft.aln'
+new = 'trees/ESCRTs/chmp7/ver3/discoba_chmp7.fa'
+out = 'trees/ESCRTs/chmp7/ver3/chmp7.mafft.aln'
 subprocess.call('{} --add {} --thread 6 --inputorder {} > {}'.format(mafft, new, existing, out), shell=True)
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/ESCRTs/chmp7/ver2/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/ESCRTs/chmp7/ver3/')
 files = [x for x in os.listdir() if 'mafft.aln' in x]
 
 for file in files:
