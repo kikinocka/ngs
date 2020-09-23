b4f987a47e69fda19099a30f508a63fb39d795dc
kikinocka <kika.zahonova@gmail.com>
2020-09-23 19:15:55 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many_pbs.sh
+++ bash_scripts/metacentrum/iqtree_many_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/trafficking/ESCRT/chmp7/ver3'
+data_dir='/storage/brno3-cerit/home/kika/pelomyxa/trees/pfo/ver2'
 
 #copy files to scratch
 cp $data_dir'/'*.aln $SCRATCHDIR
--- py_scripts/deduplicate_sequence_names.py
+++ py_scripts/deduplicate_sequence_names.py
@@ -4,40 +4,40 @@ from Bio import SeqIO
 from collections import OrderedDict
 from collections import defaultdict
 
-os.chdir('/Dcko/ownCloud/membrane-trafficking/Rab_db/')
-infile = SeqIO.parse('RABs_deduplicated_seqs.fa', 'fasta')
-# out_fasta = open('RABs_deduplicated_seqs_names.fa', 'w')
-# out_names = open('RABs_dupl-names_seqs_names.txt', 'w')
+os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/trees/PFO/ver2/')
+infile = SeqIO.parse('pfo.fa', 'fasta')
+out_fasta = open('pfo_deduplicated.fa', 'w')
+out_names = open('pfo_names.txt', 'w')
 
-names = []
-with open('RABs_deduplicated.fa', 'w') as out:	
-	for seq in infile:
-		if seq.name.lower() not in names:
-			names.append(seq.name.lower())
-			new_name = seq.name.replace('|', '_')
-			new_desc = '{} {}'.format(new_name, ' '.join(seq.description.split()[1:]))
-			out.write('>{}\n{}\n'.format(new_desc[:50], seq.seq))
-			print(seq.name + ' not in names')
-		else:
-			names.append(seq.name.lower())
-			new_name = '{}_{}'.format(seq.name.replace('|', '_'), names.count(seq.name.lower()))
-			new_desc = '{} {}'.format(new_name, ' '.join(seq.description.split()[1:]))
-			out.write('>{}\n{}\n'.format(new_desc[:50], seq.seq))
-			print('{} changed to {}'.format(seq.name, new_name))
+# names = []
+# with open('RABs_deduplicated.fa', 'w') as out:	
+# 	for seq in infile:
+# 		if seq.name.lower() not in names:
+# 			names.append(seq.name.lower())
+# 			new_name = seq.name.replace('|', '_')
+# 			new_desc = '{} {}'.format(new_name, ' '.join(seq.description.split()[1:]))
+# 			out.write('>{}\n{}\n'.format(new_desc[:50], seq.seq))
+# 			print(seq.name + ' not in names')
+# 		else:
+# 			names.append(seq.name.lower())
+# 			new_name = '{}_{}'.format(seq.name.replace('|', '_'), names.count(seq.name.lower()))
+# 			new_desc = '{} {}'.format(new_name, ' '.join(seq.description.split()[1:]))
+# 			out.write('>{}\n{}\n'.format(new_desc[:50], seq.seq))
+# 			print('{} changed to {}'.format(seq.name, new_name))
 
-# multiplications = defaultdict(list)
-# seq_dict = OrderedDict()
-# for sequence in infile:
-# 	multiplications[sequence.name].append(sequence.description)
-# 	if sequence.name not in seq_dict:
-# 		seq_dict[sequence.name] = [sequence.description, sequence.seq]
+multiplications = defaultdict(list)
+seq_dict = OrderedDict()
+for sequence in infile:
+	multiplications[sequence.name].append(sequence.description)
+	if sequence.name not in seq_dict:
+		seq_dict[sequence.name] = [sequence.description, sequence.seq]
 
-# for key, value in seq_dict.items():
-# 	out_fasta.write('>{}\n{}\n'.format(key, value))
+for key, value in seq_dict.items():
+	out_fasta.write('>{}\n{}\n'.format(key, value))
 
-# for key, value in multiplications.items():
-#     if len(value) > 1:
-#         out_names.write('{}\n'.format(str(value)))
+for key, value in multiplications.items():
+    if len(value) > 1:
+        out_names.write('{}\n'.format(str(value)))
 
-# out_fasta.close()
-# out_names.close()
+out_fasta.close()
+out_names.close()
