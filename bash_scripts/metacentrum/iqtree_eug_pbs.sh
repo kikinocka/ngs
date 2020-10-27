6f9e6187ef4d4b1b56da444b3ee3e9cdd12db966
kikinocka <kika.zahonova@gmail.com>
2020-10-27 12:35:07 +0100

    bla

--- bash_scripts/metacentrum/iqtree_eug_pbs.sh
+++ bash_scripts/metacentrum/iqtree_eug_pbs.sh
@@ -2,7 +2,7 @@
 #PBS -N IQT-mitUFB
 #PBS -q default
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb:os=debian9
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,24 +11,24 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/sags/mit/ver5/concat_ufb'
+datadir='/storage/brno3-cerit/home/kika/proteromonas/rabs/ver13b'
 
 #copy files to scratch
 cp $datadir'/'* $SCRATCHDIR
-# cp $datadir'guide_acsl_seqs.treefile' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='mit_concat.aln'
-# guide='guide_concat'
-# guide_tree=$guide'.treefile'
+aln='rabs.trimal_gt_0.8.aln'
+guide='guide_rabs.trimal_gt_0.8.aln'
+guide_tree=$guide'.treefile'
 bb=1000
+nm=2000
 
-iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST -quiet -wsr
+# iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST -quiet -wsr
 # iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet -wsr
 
-# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-# iqtree -m LG+C60+F+G -nt AUTO -ntmax $PBS_NUM_PPN -b $bb -quiet -s $aln -ft $guide_tree -wsr
+iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+iqtree -m LG+C60+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -nm $nm -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
