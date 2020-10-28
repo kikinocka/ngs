d69d7fbd63c2a3e75044e025cc5110ce1573a65b
kikinocka <kika.zahonova@gmail.com>
2020-10-28 09:27:14 +0100

    bla

--- bash_scripts/iqtree_leshy.sh
+++ bash_scripts/iqtree_leshy.sh
@@ -5,7 +5,7 @@ aln='rabs.trimal_gt_0.8.aln'
 guide='guide_rabs'
 guide_tree=$guide'.treefile'
 bb=10000
-
+threads=10
 
 iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
 iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln -ft $guide_tree 
