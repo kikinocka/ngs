83f74336916b47a08b0af2b3b6aa588732a043c2
kikinocka <kika.zahonova@gmail.com>
2020-11-24 09:47:05 +0100

    bla

--- bash_scripts/metacentrum/iqtree_many_pbs.sh
+++ bash_scripts/metacentrum/iqtree_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N iqtree
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,21 +10,22 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/trafficking/SNARE/rhabdomonas/qc'
+data_dir='/storage/brno3-cerit/home/kika/mic60-dynamins'
 
 #copy files to scratch
-cp $data_dir'/'*.aln $SCRATCHDIR
+cp $data_dir'/'*trimal*.aln $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
 for f in *.aln ; do
- guide=guide_${f%.trimal_gt_0.8.aln}
+ guide=guide_${f%.aln}
  guide_tree=$guide'.treefile'
  bb=1000
+ iqtree -m LG+F+R8 -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
  # iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
- iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
- iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
+ # iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
+ # iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
 done
 
 #copy files back
