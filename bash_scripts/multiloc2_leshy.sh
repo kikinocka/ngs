c82728db5110eb2b90ec35b305e0050ba0fe4cb7
kikinocka <kika.zahonova@gmail.com>
2020-11-24 15:26:10 +0100

    bla

--- bash_scripts/multiloc2_leshy.sh
+++ bash_scripts/multiloc2_leshy.sh
@@ -10,7 +10,7 @@ f=fungal
 cd $workdir
 for file in $files; do
 	echo $file
-	# out=${file%.*}'.multiloc_animal.txt'
-	out=${file%.*}'.multiloc_fungal.txt'
-	python $multiloc -fasta=$file -origin=$f -predictor=LowRes -result=$out -output=simple
+	out=${file%.*}'.multiloc_animal.txt'
+	# out=${file%.*}'.multiloc_fungal.txt'
+	python $multiloc -fasta=$file -origin=$a -predictor=LowRes -result=$out -output=simple
 done
