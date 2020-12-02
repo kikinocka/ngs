0175b84779872cb798e0ef6a36a5dea9114de1db
kikinocka <kika.zahonova@gmail.com>
2020-12-02 11:50:11 +0100

    bla

---
+++ bash_scripts/mitofates_leshy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mitofates='/mnt/mokosz/home/kika/MitoFates/MitoFates.pl'
+workdir='/mnt/mokosz/home/zoli/localize/'
+files=$workdir'*.fa'
+m=matazoa
+p=plant
+f=fungi
+
+cd $workdir
+for file in $files; do
+	echo $file
+	
+	out=${file%.*}'.mitofates_fungal.txt'
+	perl $mitofates $file $f > $outfile
+	
+	# out=${file%.*}'.multiloc_animal.txt'
+	# python $multiloc -fasta=$file -origin=$a -predictor=LowRes -result=$out -output=simple
+done
