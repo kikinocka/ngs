412876a81ba2655a2b0516af91f6b681a4f6d064
kikinocka <kika.zahonova@gmail.com>
2020-12-04 16:26:10 +0100

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N IQT-ufb
+#PBS -N IQT-c20
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=02:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.8
 
-datadir='/storage/brno3-cerit/home/kika/sags/mit/ver6/concat_C20_ufb'
+datadir='/storage/brno3-cerit/home/kika/sags/mit/ver6/concat_C20_bs'
 
 #copy files to scratch
 cp $datadir'/'* $SCRATCHDIR
@@ -27,7 +27,7 @@ bb=1000
 # iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet -wsr
 
 iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
+iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -b $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
