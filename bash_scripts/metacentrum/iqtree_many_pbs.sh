f5be1d6f28db8fd1d3dd362cf6dd0cb60a73651e
kikinocka <kika.zahonova@gmail.com>
2021-02-15 11:01:02 +0100

    bla

--- bash_scripts/metacentrum/iqtree_many_pbs.sh
+++ bash_scripts/metacentrum/iqtree_many_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/mic60-dynamins/ver2'
+data_dir='/storage/brno3-cerit/home/kika/trafficking/RABs/ver4'
 
 #copy files to scratch
 cp $data_dir'/'*trimal*.aln $SCRATCHDIR
@@ -22,10 +22,10 @@ for f in *.aln ; do
  guide=guide_${f%.aln}
  guide_tree=$guide'.treefile'
  bb=1000
- iqtree -m LG+F+R8 -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
+ # iqtree -m LG+F+R8 -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
  # iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f}
- # iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
- # iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
+ iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
+ iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
 done
 
 #copy files back
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/ETFb/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABs/ver4/')
 files = [x for x in os.listdir() if '.mafft.aln' in x]
 
 for file in files:
