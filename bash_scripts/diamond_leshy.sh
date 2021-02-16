2094c50c02d69b03dacd1c297549f84664211624
kikinocka <kika.zahonova@gmail.com>
2021-02-16 11:18:14 +0100

    bla

--- bash_scripts/diamond_leshy.sh
+++ bash_scripts/diamond_leshy.sh
@@ -5,18 +5,16 @@ query='/mnt/mokosz/home/kika/rhizomastix_reassembly/rhizomastix_reassembly.trini
 out='/mnt/mokosz/home/kika/rhizomastix_reassembly/rhizomastix_reassembly.trinity.NTfilt.fasta.transdecoder_dir/longest_orfs.nr.dmnd.out'
 db='/opt/databases/nr_auto/2021-02-15/diamond/nr.dmnd'
 # outfmt=5
-eval=1e-2
+eval=1e-5
 max_seqs=20
-max_hsps=1
-cpu=4
+cpu=10
 
 diamond $task \
 	-q $query \
 	-d $db \
 	-o $out \
-	--outfmt "6 qseqid staxids bitscore sseqid qcovs pident" \
+	--outfmt "6 qseqid bitscore sseqid qcovhsp pident qlen length" \
 	--threads $cpu \
 	--evalue $eval \
 	--max-target-seqs $max_seqs \
-	--max-hsps $max_hsps \
 	--sensitive
