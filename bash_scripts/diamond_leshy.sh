7c345334036054d4fddb643c66ec197dabe89a46
kikinocka <kika.zahonova@gmail.com>
2021-02-17 23:08:44 +0100

    bla

--- bash_scripts/diamond_leshy.sh
+++ bash_scripts/diamond_leshy.sh
@@ -4,7 +4,7 @@ task=blastp
 query='/mnt/mokosz/home/kika/mastigamoeba_abducta_CHOM/Trinity_CHOM1_050516_renamed_prot.fas_50'
 out='/mnt/mokosz/home/kika/mastigamoeba_abducta_CHOM/Trinity_CHOM1_050516_renamed_prot.fas_50.dmnd.out'
 db='/opt/databases/nr_auto/2021-02-15/diamond/nr.dmnd'
-# taxify='/mnt/mokosz/home/kika/rhizomastix_reassembly/taxify_DMND_nr_gz.py'
+taxify='/mnt/mokosz/home/kika/rhizomastix_reassembly/taxify_DMND_nr_gz.py'
 # outfmt=5
 eval=1e-5
 max_seqs=20
@@ -20,4 +20,4 @@ diamond $task \
 	--max-target-seqs $max_seqs \
 	--sensitive
 
-# python3 $taxify -i $out 
+python3 $taxify -i $out 
