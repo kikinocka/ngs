b305bec1787af13672584858a0be6a67bdfef0ad
kikinocka <kika.zahonova@gmail.com>
2021-02-22 16:43:33 +0100

    bla

--- bash_scripts/metacentrum/busco_pbs.sh
+++ bash_scripts/metacentrum/busco_pbs.sh
@@ -30,7 +30,7 @@ lin_dir='/software/busco/3.0.2/src/db/'
 # ln -s $busco_dir'rli_eukaryota_odb9/short_summary_eukaryota_odb9.txt' $summaries'short_summary_eukaryota_odb9_rli.txt'
 
 #copy files to scratch
-cp $assembly_dir'/rhizomastix_reassembly.trinity.NRfilt-p70.fna' $SCRATCHDIR
+cp $assembly_dir'/*.faa' $SCRATCHDIR
 
 assemblies='*.faa'
 lineage=$lin_dir'eukaryota_odb9/'
--- bash_scripts/metacentrum/quast_pbs.sh
+++ bash_scripts/metacentrum/quast_pbs.sh
@@ -10,19 +10,19 @@ cat $PBS_NODEFILE
 #add module
 module add quast-4.6.3
 
-datadir='/storage/brno3-cerit/home/kika/archamoebae/prot_assemblies-NRfilt/'
-outdir=$datadir'quast_Mabd'
+datadir='/storage/brno3-cerit/home/kika/archamoebae/rhizomastix_libera/'
+outdir=$datadir'quast_rhizomastix_reassembly.NRfilt'
 
 #copy files to scratch
-cp $datadir'Mastigabducta_CHOM1_050516_renamed_prot.NRfilt.faa' $SCRATCHDIR
+cp $datadir'rhizomastix_reassembly.trinity.NRfilt-p70.fna' $SCRATCHDIR
 
-assemblies='Mastigabducta_CHOM1_050516_renamed_prot.NRfilt.faa'
+assemblies='rhizomastix_reassembly.trinity.NRfilt-p70.fna'
 min_contig=500
 
 
 #compute on scratch
 cd $SCRATCHDIR
-quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --min-contig $min_contig --eukaryote $assemblies
+quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $assemblies
 
 # for fasta in $assemblies; do
 # 	echo $fasta
