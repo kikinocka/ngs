d9f2e708282f150c8f0dd0f31f976bf4f1c6caf6
kikinocka <kika.zahonova@gmail.com>
2021-03-04 14:59:40 +0100

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -5,7 +5,7 @@ import subprocess
 mafft = '/Users/kika/miniconda3/bin/mafft'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/GCS-H/')
+os.chdir('/Users/kika/ownCloud/Naegleria/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
