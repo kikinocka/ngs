0ab886bae85ffa9720b1692aff205b6a792de3c5
kikinocka <kika.zahonova@gmail.com>
2021-03-18 13:15:16 +0100

    bla

--- bash_scripts/metacentrum/rabifier_pbs.sh
+++ bash_scripts/metacentrum/rabifier_pbs.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
-#PBS -N Bowtie2
-#PBS -l select=1:ncpus=30:mem=80gb:scratch_local=150gb
-#PBS -l walltime=24:00:00
+#PBS -N rabifier
+#PBS -l select=1:ncpus=10:mem=10gb:scratch_local=10gb
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
-module add bowtie2-2.4.2
-module add samtools-1.11
+module add python36-modules-gcc
+
+datadir='/storage/brno12-cerit/home/kika/anaeramoeba/RABs'
+
+#copy files to scratch
+cp $datadir'/'*.fa $SCRATCHDIR
 
 
-/storage/brno12-cerit/home/kika/anaeramoeba/RABs
+#run on scratch
+cd $SCRATCHDIR
+
+for fasta in *.fa; do
+	echo $fasta
+	rabifier --cpu $PBS_NUM_PPN $fasta
+	echo ***Rabifier done***
+done
+# optional arguments:
+#   -h, --help            show this help message and exit
+#   -v, --version         show program's version number and exit
+#   -o OUTPUT, --output OUTPUT
+#                         output file name [-]
+#   --outfmt {text,json,csv}
+#                         output format [text]
+#   --show_positive       include only Rab positive predictions in the output
+#   --cpu CPU             maximal number of threads to use [1]
+#   --fast                phase 1 speedup: stop if it is not a G protein (does
+#                         not show the full summary)
+#   --bh_evalue BH_EVALUE
+#                         e-value threshold for best hit search [1e-10]
+#   --motif_evalue MOTIF_EVALUE
+#                         e-value threshold for motif search [1e-04]
+#   --motif_number MOTIF_NUMBER
+#                         minimum number of RabF motifs [1]
+#   --subfamily_identity SUBFAMILY_IDENTITY
+#                         minimum sequence identity with subfamily's best hit
+#                         [0.4]
+#   --subfamily_score SUBFAMILY_SCORE
+#                         minimum subfamily score [0.2]
+
+
+#copy files back
+rm *.fa
+cp -r * $datadir
