af68c0b46085e9d6e24e714ca49c33e409217638
kikinocka <kika.zahonova@gmail.com>
2021-03-31 18:53:20 +0200

    bla

--- bash_scripts/metacentrum/metaquast_pbs.sh
+++ bash_scripts/metacentrum/metaquast_pbs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#PBS -N quast
-#PBS -l select=1:ncpus=10:mem=3gb:scratch_local=5gb
-#PBS -l walltime=02:00:00
+#PBS -N metaquast
+#PBS -l select=1:ncpus=15:mem=5gb:scratch_local=10gb
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
+module add blast+-2.8.0a
 module add quast-4.6.3
 
 datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/'
@@ -20,11 +21,12 @@ cp $datadir'reads/BML_trimmed_2.fq.gz' $SCRATCHDIR
 assemblies='scaffolds.fasta'
 fwd='BML_trimmed_1.fq.gz'
 rev='BML_trimmed_2.fq.gz'
+blast='/storage/projects/BlastDB/nt'
 
 
 #compute on scratch
 cd $SCRATCHDIR
-metaquast.py -o $SCRATCHDIR -t $PBS_NUM_PPN -1 $fwd -2 $rev $assemblies
+metaquast.py -o $SCRATCHDIR -t $PBS_NUM_PPN -1 $fwd -2 $rev --blast-db $blast $assemblies
 
 
 #copy results to your folder
