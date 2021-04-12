f84764e7865d5dd31a49fc9a53bd9c509e522905
kikinocka <kika.zahonova@gmail.com>
2021-04-12 12:29:56 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-c20
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-datadir='/storage/brno3-cerit/home/kika/sags/mit/ver7'
+datadir='/storage/brno3-cerit/home/kika/diplonema/ocdh/ver4'
 
 #copy files to scratch
 cp $datadir'/'*.aln $SCRATCHDIR
@@ -18,8 +18,8 @@ cp $datadir'/'*.aln $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='concat.aln'
-guide='guide_concat'
+aln='ocdh.trimal_gt_0.8.aln'
+guide='guide_ocdh'
 guide_tree=$guide'.treefile'
 bb=1000
 
@@ -27,8 +27,8 @@ bb=1000
 # iqtree -m LG4X -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 
 iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -b $bb -quiet -s $aln -ft $guide_tree #-wsr
+iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
-cp * $datadir'/concat_C20_bs/'
+cp * $datadir
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/PFO/')
+os.chdir('/Users/kika/ownCloud/diplonema/pyruvate_metabolism/octopine_superfamily/ver4/')
 files = [x for x in os.listdir() if 'mafft.aln' in x]
 
 for file in files:
