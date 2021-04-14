5e463f91761b3fa1b12bb606f6d5d7e033a40ed6
kikinocka <kika.zahonova@gmail.com>
2021-04-14 12:25:29 +0200

    bla

--- bash_scripts/metacentrum/spades_pbs.sh
+++ bash_scripts/metacentrum/spades_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N SPAdes
-#PBS -l select=1:ncpus=50:ompthreads=50:mem=100gb:scratch_local=30gb
+#PBS -l select=1:ncpus=50:ompthreads=50:mem=150gb:scratch_local=30gb
 #PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
@@ -18,21 +18,21 @@ cp $reads'BML_trimmed_1.fq.gz' $reads'BML_trimmed_2.fq.gz' $SCRATCHDIR
 
 pe1_1='BML_trimmed_1.fq.gz'
 pe1_2='BML_trimmed_2.fq.gz'
-
+out='spades_kmers'
 
 #compute on scratch
 cd $SCRATCHDIR
 
-#metagenome assembly
-metaspades.py --pe-1 1 $pe1_1 --pe-2 1 $pe1_2 -t $PBS_NUM_PPN -o BML_metagenome
+# #metagenome assembly
+# metaspades.py --pe-1 1 $pe1_1 --pe-2 1 $pe1_2 -t $PBS_NUM_PPN -o BML_metagenome
 
-# #metagenome adding more k-mers
-# metaspades.py --pe-1 1 $pe1_1 --pe-2 1 $pe1_2 -k 21,33,55,77,99,111  -t $PBS_NUM_PPN -o BML_metagenome
+#metagenome specifying k-mers
+metaspades.py --pe-1 1 $pe1_1 --pe-2 1 $pe1_2 -k 21,33,55,77,99,111  -t $PBS_NUM_PPN -o $out
 
 # #using reference genome
 # spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --trusted-contigs $cbom --careful -t $PBS_NUM_PPN -o spades_cbom_trusted
 
-# #using several libraries
+# #single-cell using several libraries
 # spades.py --sc --careful -t $PBS_NUM_PPN -o out \
 # --pe1-m $pe1m --pe1-1 $pe11 --pe1-2 $pe12 --pe1-s $pe1u \
 # --pe2-m $pe2m --pe2-1 $pe21 --pe2-2 $pe22 --pe2-s $pe2u \
