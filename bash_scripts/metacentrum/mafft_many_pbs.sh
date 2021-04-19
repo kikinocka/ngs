2f26d93bb373ebf1d85d125038aeb126ec5c2670
kikinocka <kika.zahonova@gmail.com>
2021-04-19 16:46:43 +0200

    bla

--- bash_scripts/metacentrum/mafft_many_pbs.sh
+++ bash_scripts/metacentrum/mafft_many_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add mafft-7.453
 
-data_dir='/storage/brno3-cerit/home/kika/diplonema/ocdh/ver6'
+data_dir='/storage/brno3-cerit/home/kika/diplonema/ocdh/ver7'
 
 #copy files to scratch
 cp $data_dir'/'*.fa $SCRATCHDIR
