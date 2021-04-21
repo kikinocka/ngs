da954dc1355aa8129ba6cad5174458bf6527b71f
kikinocka <kika.zahonova@gmail.com>
2021-04-21 16:13:10 +0200

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/anaeramoeba/trees/RABs/')
+os.chdir('/Users/kika/ownCloud/anaeramoeba/trees/RABs/endocytic/')
 files = [x for x in os.listdir() if 'mafft.aln' in x]
 
 for file in files:
