513cfdcac8e4b97574b2ff5564978ca4d0fa17a5
kikinocka <kika.zahonova@gmail.com>
2021-04-30 17:51:06 +0200

    bla

--- bash_scripts/iqtree_leshy.sh
+++ bash_scripts/iqtree_leshy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd /mnt/mokosz/home/kika/archam_trees/d-ldh/
+cd /mnt/mokosz/home/kika/archam_trees/ak/
 
-aln='d-ldh.trimal_gt_0.8.aln'
-guide='guide_d-ldh'
+aln='ak.trimal_gt_0.8.aln'
+guide='guide_ak'
 guide_tree=$guide'.treefile'
 bb=1000
 threads=10
 
-iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln -ft $guide_tree 
+# iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
+# iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln -ft $guide_tree 
 
-# iqtree -m TEST -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln
+iqtree -m TEST -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln
