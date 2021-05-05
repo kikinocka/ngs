99349cb7fc295208772d7a2eb8df373ab7538f12
kikinocka <kika.zahonova@gmail.com>
2021-05-05 21:53:14 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N IQT-c20
+#PBS -N IQT
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/18S_tree/ver3'
+datadir='/storage/brno3-cerit/home/kika/anaeramoeba/tbcs/ver3'
 
 #copy files to scratch
 cp $datadir'/'*.aln $SCRATCHDIR
@@ -18,13 +18,13 @@ cp $datadir'/'*.aln $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='18S.trimal_gt_0.8.aln'
+aln='TBCs.mafft.trimal_gt_0.9.aln'
 # guide='guide_ocdh'
 # guide_tree=$guide'.treefile'
 bb=1000
 
-# iqtree -m TEST -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
-iqtree -m GTR+G -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
+iqtree -m TEST -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
+# iqtree -m GTR+G -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 
 # iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
 # iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -4,22 +4,22 @@ import subprocess
 
 mafft = '/Users/kika/miniconda3/bin/mafft'
 
-#align de-novo
-os.chdir('/Users/kika/ownCloud/manuscripts/Blastocrithidia_genetic_code_figured/eRF1/')
-files = [x for x in os.listdir() if x.endswith('eRF1.fa')]
+# #align de-novo
+# os.chdir('/Users/kika/ownCloud/manuscripts/Blastocrithidia_genetic_code_figured/eRF1/')
+# files = [x for x in os.listdir() if x.endswith('eRF1.fa')]
 
-for file in files:
-	print(file)
-	out = '{}.mafft.aln'.format(file.split('.fa')[0])
-	log = '{}.mafft.log'.format(file.split('.fa')[0])
-	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
-		mafft, file, out, log), shell=True)
+# for file in files:
+# 	print(file)
+# 	out = '{}.mafft.aln'.format(file.split('.fa')[0])
+# 	log = '{}.mafft.log'.format(file.split('.fa')[0])
+# 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
+# 		mafft, file, out, log), shell=True)
 
-# #add to aligned sequences
-# os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/trees/PFO/ver5/')
-# existing = 'pfo-wo-ACZ97600.mafft.aln'
-# add = 'pelo3613.fa'
-# out = 'pfo.mafft.aln'
-# log = 'pfo.mafft.log'
-# subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
-# # subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
+#add to aligned sequences
+os.chdir('/Users/kika/ownCloud/anaeramoeba/trees/TBCs/ver3/')
+existing = 'TBC_all_parabasalids-reduced_masked.fa.aln'
+add = 'tbcB.fa'
+out = 'TBCs.mafft.aln'
+log = 'TBCs.mafft.log'
+subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
+# subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
