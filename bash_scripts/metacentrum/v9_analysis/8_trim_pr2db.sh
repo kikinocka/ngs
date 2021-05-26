2170f5002b6cfbe785a6d8252325bd449bb26e40
kikinocka <kika.zahonova@gmail.com>
2021-05-26 16:10:33 +0200

    bla

--- bash_scripts/metacentrum/v9_analysis/8_trim_pr2db.sh
+++ bash_scripts/metacentrum/v9_analysis/8_trim_pr2db.sh
@@ -10,19 +10,25 @@ cat $PBS_NODEFILE
 module add python27-modules-gcc
 # module add python36-modules-gcc
 
-DATADIR='/storage/brno3-cerit/home/kika/pr2db/'
+DATADIR='/storage/brno3-cerit/home/kika/pr2db/4.13.0/'
 
 #copy files to scratch
-cp $DATADIR'pr2_version_4.12.0_18S_taxo_long.fasta' $SCRATCHDIR 
+cp $DATADIR'pr2_version_4.13.0_18S_UTAX.fasta' $SCRATCHDIR 
 
 
 #run on scratch
 cd $SCRATCHDIR
 
-SOURCE='pr2_version_4.12.0_18S_taxo_long.fasta'
+SOURCE='pr2_version_4.13.0_18S_UTAX.fasta'
 
-PRIMER_F="CCAGCASCYGCGGTAATTCC"
-PRIMER_R="TYRATCAAGAACGAAAGT"
+# #V4 primers
+# PRIMER_F="CCAGCASCYGCGGTAATTCC"
+# PRIMER_R="TYRATCAAGAACGAAAGT"
+
+# #V9 primers
+PRIMER_F="TTGTACACACCGCCC"
+PRIMER_R="GTAGGTGAACCTGCNGAAGG"
+
 OUTPUT="${SOURCE/_long*/}_${PRIMER_F}_${PRIMER_R}.fas"
 LOG="${OUTPUT/.fas/.log}"
 MIN_LENGTH=32
