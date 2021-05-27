ea5785d1e63afd0693ce1f75028404600f2adc4b
kikinocka <kika.zahonova@gmail.com>
2021-05-27 16:20:21 +0200

    bla

--- bash_scripts/metacentrum/samtools_view.sh
+++ bash_scripts/metacentrum/samtools_view.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N samtools_view
 #PBS -l select=1:ncpus=5:mem=50gb:scratch_local=150gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,15 +10,15 @@ cat $PBS_NODEFILE
 #add module
 module add samtools-1.11
 
-mapping_dir='/storage/brno3-cerit/home/kika/pelomyxa/mapping/bowtie2/DNA_to_genome/'
+mapping_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/bw2_mapping/eukaryotes_prokka/'
 
 #copy files to scratch
-cp $mapping_dir'pelo_genome_bw2_sorted.bam' $SCRATCHDIR
+cp $mapping_dir'bml_euk_prokka_bw2_sorted.bam' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
-base='pelo_genome_bw2'
+base='bml_euk_prokka_bw2'
 all=$base'_sorted.bam'
 mapped=$base'_mapped_sorted.bam'
 
