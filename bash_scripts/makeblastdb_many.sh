894c2740fe19f0802ff0ea978babe4f7b7c2b1b9
kikinocka <kika.zahonova@gmail.com>
2021-05-28 22:37:52 +0200

    bla

--- bash_scripts/makeblastdb_many.sh
+++ bash_scripts/makeblastdb_many.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-datadir='/home/kika/MEGAsync/Euglena_longa/2013_Sekvenovanie/Tetrapyrroles/precorrin-2_dehydrogenase/'
-files=$datadir'*.fna'
+cd '/home/kristinaz/eukprot/'
 
-for file in $files; do
+for file in *.fasta; do
 	echo $file
-	# makeblastdb -in $file -dbtype nucl -parse_seqids
+	makeblastdb -in $file -dbtype prot -parse_seqids
 	echo $file': BLASTable database done'
 done
