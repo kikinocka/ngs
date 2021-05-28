fec53e18a397da8b25e2e4d315bde0cd8d9379ec
kikinocka <kika.zahonova@gmail.com>
2021-05-28 14:30:53 +0200

    bla

---
+++ bash_scripts/bbduk_leshy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bbduk='/mnt/mokosz/home/kika/tools/bbmap/bbduk.sh'
+adapt='/mnt/mokosz/home/kika/tools/bbmap/resources/adapters.fa'
+
+cd '/mnt/mokosz/home/kika/endolimax_nana/reads/'
+name='Endo1'
+fw=$name'_1.fastq.gz'
+rv=$name'_2.fastq.gz'
+trimmed_fw=$name'_trimmed_1.fq.gz'
+trimmed_rv=$name'_trimmed_2.fq.gz'
+report=$name'_bbduk_report.txt'
+
+#illumina reads
+$bbduk overwrite=true \
+	in1=$fw in2=$rv \
+	out1=$trimmed_fw out2=$trimmed_rv \
+	ref=$adapt \
+	usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=10 2> $report
+
--- bash_scripts/fastqc_leshy.sh
+++ bash_scripts/fastqc_leshy.sh
@@ -6,7 +6,7 @@ out_dir=$read_dir'fastqc/'
 cd $read_dir
 
 for reads in *fastq.gz ; do
-	echo 'running FASTQC on ' $reads
+	echo 'running FastQC on ' $reads
 	fastqc -t 10 -o $out_dir $reads
 done
 
