09c5f939ce4fcb28a1b998314a6bb25425c2a020
kikinocka <kika.zahonova@gmail.com>
2021-05-28 14:56:50 +0200

    bla

---
+++ bash_scripts/metacentrum/kma_pbs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -N kma
+#PBS -l select=1:ncpus=20:mem=70gb:scratch_local=15gb
+#PBS -l walltime=04:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+module add kma-1.3.0
+
+reads='/storage/brno3-cerit/home/kika/oil_sands/metagenome/reads/'
+
+#copy files to scratch
+cp $reads'BML_trimmed_1.fq.gz' $SCRATCHDIR
+cp $reads'BML_trimmed_2.fq.gz' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+fwd='BML_trimmed_1.fq.gz'
+rev='BML_trimmed_2.fq.gz'
+out='bml_kma'
+
+kma -ipe $fwd $rev -o $out -t_db $db -t $th -1t1 -mem_mode -and -apm f
