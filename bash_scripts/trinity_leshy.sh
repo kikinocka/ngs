72819b2c64e72f7a06beccb5cbf1bc5866478d28
kikinocka <kika.zahonova@gmail.com>
2021-05-30 16:46:17 +0200

    bla

--- bash_scripts/trinity_leshy.sh
+++ bash_scripts/trinity_leshy.sh
@@ -2,9 +2,9 @@
 
 cd '/mnt/mokosz/home/kika/endolimax_nana/'
 
-fwd1='reads/RV1_trimmed_1.fq.gz'
-rev1='reads/RV1_trimmed_2.fq.gz'
-fwd2='reads/RV2_trimmed_1.fq.gz'
-rev2='reads/RV2_trimmed_2.fq.gz'
+fwd1='reads/Endo1_trimmed_1.fq.gz'
+rev1='reads/Endo1_trimmed_2.fq.gz'
+fwd2='reads/Endo2_trimmed_1.fq.gz'
+rev2='reads/Endo2_trimmed_2.fq.gz'
 
 Trinity --seqType fq --left $fwd1,$fwd2 --right $rev1,$rev2 --max_memory 100G --CPU 15
