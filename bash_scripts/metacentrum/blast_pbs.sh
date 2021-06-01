028d2dbdc71c231182a1784c0b384e69ecc258c2
kikinocka <kika.zahonova@gmail.com>
2021-06-01 14:12:30 +0200

    bla

--- bash_scripts/metacentrum/blast_pbs.sh
+++ bash_scripts/metacentrum/blast_pbs.sh
@@ -10,32 +10,36 @@ cat $PBS_NODEFILE
 #add module
 module add blast+-2.8.0a
 
-datadir='/storage/brno3-cerit/home/kika/trafficking/RABs/1604/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/'
 
 #copy files to scratch
-cp $datadir'fwd_hits.fa' $SCRATCHDIR
+cp $datadir'bml_meta.spades_def.fa' $SCRATCHDIR
 
-query='fwd_hits.fa'
-out='rev_nr.blast.xml'
-db='/storage/projects/BlastDB/nr'
-program=blastx
-task=blastx
-outfmt=5
-eval=1
-max_seqs=1
 
 #run on scratch
 cd $SCRATCHDIR
+
+query='bml_meta.spades_def.fa'
+out='bml_meta.spades_def.blast'
+db='/storage/projects/BlastDB/nt'
+program=blastn
+task=blastn
+# outfmt=5
+eval=1e-02
+max_seqs=5
+max_hsps=1
 
 $program -task $task \
 	-query $query \
 	-db $db \
 	-out $out \
-	-outfmt $outfmt \
+	-outfmt "6 qseqid staxids bitscore sseqid qcovs pident" \
 	-num_threads $PBS_NUM_PPN \
 	-evalue $eval \
 	-max_target_seqs $max_seqs \
+	-max_hsps $max_hsps 
+
 
 #copy files back
 rm $query
-cp -R * $datadir
+cp -R * $datadir'filtration'
