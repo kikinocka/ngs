b5f22b254a6d46004b8313b6b9ac7d52645bb526
kikinocka <kika.zahonova@gmail.com>
2021-06-09 09:31:03 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,7 +13,7 @@ module add iqtree-1.6.12
 datadir='/storage/brno3-cerit/home/kika/diplonema/elongases'
 
 #copy files to scratch
-cp $datadir'/'*.aln $SCRATCHDIR
+cp $datadir'/'* $SCRATCHDIR
 
 
 #compute on scratch
@@ -26,7 +26,7 @@ bb=1000
 # iqtree -m TEST -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 # iqtree -m GTR+G -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 
-iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
 iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
