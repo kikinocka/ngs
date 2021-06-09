95bfde35ceb21554c6214ec92bf3cb1597881b0f
kikinocka <kika.zahonova@gmail.com>
2021-06-09 17:28:28 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -13,21 +13,21 @@ module add iqtree-1.6.12
 datadir='/storage/brno3-cerit/home/kika/diplonema/elongases'
 
 #copy files to scratch
-cp $datadir'/'* $SCRATCHDIR
+cp $datadir'/'*.aln $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 aln='elongases.trimal_gt_0.8.aln'
-guide='guide_elongases'
-guide_tree=$guide'.treefile'
+# guide='guide_elongases'
+# guide_tree=$guide'.treefile'
 bb=1000
 
-# iqtree -m TEST -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
+iqtree -m TEST -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 # iqtree -m GTR+G -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 
 # iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
+# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
