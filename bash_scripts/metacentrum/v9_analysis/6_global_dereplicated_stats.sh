1c1230f0432f0fd9cb8a5e76c4e98c3fdee82e1d
kikinocka <kika.zahonova@gmail.com>
2021-06-17 15:08:55 +0200

    bla

--- bash_scripts/metacentrum/v9_analysis/6_global_dereplicated_stats.sh
+++ bash_scripts/metacentrum/v9_analysis/6_global_dereplicated_stats.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-raw='/storage/brno3-cerit/home/kika/sl_euglenozoa/'
-fasta=$raw'global_dereplicated.fa'
+data='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/'
+fasta=$data'global_dereplicated.fa'
+out=$data'global.stats.txt'
 
-cd $raw
+cd $data
 # if [ $# != 1 ]; then
 #     echo "You need to supply an input filename";
 #     exit 1;
 # fi
 
-echo "Unique Seqs | Raw Reads"
-awk -F "[;=]" '/^>/ {s += $3 ; c += 1} END {print s, c}' $fasta
+echo 'I am in: ' $pwd
+echo 'Running global stats on: ' $fasta
+echo "Unique Seqs | Raw Reads" >$out
+awk -F "[;=]" '/^>/ {s += $3 ; c += 1} END {print s, c}' $fasta >> $out
+echo 'awk done'
+echo '******************'
