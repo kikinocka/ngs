f5dff4afc4885825b5675306a56fd95f33d398e4
kikinocka <kika.zahonova@gmail.com>
2021-06-17 11:54:28 +0200

    bla

--- bash_scripts/metacentrum/v9_analysis/3_assembly_stats.sh
+++ bash_scripts/metacentrum/v9_analysis/3_assembly_stats.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 raw='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/merged_pear/'
-
+path1='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/raw_reads/'
+path2='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/trimmed_cutadapt/'
 cd $raw
 for TARGET in *.assembled.fastq ; do
+	cd $raw
     ASSEMBLED=$(wc -l < ${TARGET})
+    cd $path1
     RAW=$(sed '/^$/d' ${TARGET/.assembled/_L001_R1_001} | wc -l)
+    cd $path2
     TRIMMED=$(tail -n 1 ${TARGET/.fastq/.log} | cut -f 2)
     awk -v after=${ASSEMBLED} \
         -v before=${RAW} \
