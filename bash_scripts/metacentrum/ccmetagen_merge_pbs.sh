5afdf9654f3d5d5d9e8e9b642d32c5de39738e62
kikinocka <kika.zahonova@gmail.com>
2021-06-28 18:42:01 +0200

    bla

--- bash_scripts/metacentrum/ccmetagen_merge_pbs.sh
+++ bash_scripts/metacentrum/ccmetagen_merge_pbs.sh
@@ -8,8 +8,7 @@ datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta/'
 # $datadir must contain .res.csv file with CCMetagen results !
 
 level='Superkingdom'
-lineage='Bacteria'
+lineage='Bacteria,Viruses'
 out=$datadir'eukaryotes.table'
 
 CCMetagen_merge.py -i $datadir -kr r -l $level -tlist $lineage -o $out
-# CCMetagen_merge.py -i /storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta/ -kr k -l Superkingdom -tlist Eukaryota -o eukaryotes.table
