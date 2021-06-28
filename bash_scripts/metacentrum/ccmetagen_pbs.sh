f6659214d7686779fb5d16154ef863f1aca925fd
kikinocka <kika.zahonova@gmail.com>
2021-06-28 16:14:38 +0200

    bla

--- bash_scripts/metacentrum/ccmetagen_pbs.sh
+++ bash_scripts/metacentrum/ccmetagen_pbs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #PBS -N CCMetagen
 #PBS -l select=1:ncpus=20:mem=1gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
-module add python36-modules-gcc
+module add ccmetagen-1.2.5
 
 
 datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta/'
