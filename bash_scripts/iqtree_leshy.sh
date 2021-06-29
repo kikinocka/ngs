b8289d0fa9bc5c23d493a055a3455589dc20bc9f
kikinocka <kika.zahonova@gmail.com>
2021-06-29 17:15:28 +0200

    bla

--- bash_scripts/iqtree_leshy.sh
+++ bash_scripts/iqtree_leshy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd /mnt/mokosz/home/kika/archam_trees/ak/ver2/
+cd /mnt/mokosz/home/kika/archam_trees/tom7/
 
-aln='ak.trimal_gt_0.8.aln'
-guide='guide_ak'
-guide_tree=$guide'.treefile'
+aln='tom7.trimal_gt_0.8.aln'
+# guide='guide_ak'
+# guide_tree=$guide'.treefile'
 bb=1000
 threads=10
 
-iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln -ft $guide_tree 
+# iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
+# iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln -ft $guide_tree 
 
-# iqtree -m TEST -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln
+iqtree -m TEST -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/retromer/')
+os.chdir('/Users/kika/ownCloud/archamoebae/trees/')
 files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 
 for file in files:
