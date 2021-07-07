8d76e7e8ff9c2af4e32e03f839e5b02d8f5b243a
kikinocka <kika.zahonova@gmail.com>
2021-07-07 15:55:17 +0200

    bla

--- bash_scripts/qualimap_leshy.sh
+++ bash_scripts/qualimap_leshy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 qualimap='/mnt/mokosz/home/kika/tools/qualimap_v2.2.1/qualimap'
-bam='/mnt/mokosz/home/kika/endolimax_nana/mapping/endo1/endo1_bw2_sorted.bam'
+bam='/mnt/mokosz/home/kika/endolimax_nana/mapping/endo2/endo2_bw2_sorted.bam'
 outdir='/mnt/mokosz/home/kika/endolimax_nana/qualimap/'
-out='endo1_qualimap.pdf'
+out='endo2_qualimap.pdf'
 cpu=10
 
 $qualimap bamqc -nt $cpu -bam $bam -outdir $outdir -outfile $out -outformat pdf
