851a90286c53e6cb7f095b3e403ecf965aabb61d
kikinocka <kika.zahonova@gmail.com>
2021-07-07 16:27:46 +0200

    bla

--- bash_scripts/bowtie2_leshy.sh
+++ bash_scripts/bowtie2_leshy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd '/mnt/mokosz/home/kika/rhizomastix_vacuolata/mapping/rvac2/'
+cd '/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/mapping/'
 assembly_dir='/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/rhizomastix_reassembly_NR/'
-read_dir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/reads/'
+read_dir='/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/reads/'
 
 base_name='rvac2_bw2'
 ref=$assembly_dir'rhizomastix_reassembly.trinity.NRfilt.fasta'
-p1_1=$read_dir'RV2_trimmed_1.fq.gz'
-p1_2=$read_dir'RV2_trimmed_2.fq.gz'
+p1_1=$read_dir'SRR5396411_trimmed_1.fq.gz'
+p1_2=$read_dir'SRR5396411_trimmed_2.fq.gz'
+p2_1=$read_dir'SRR5396412_trimmed_1.fq.gz'
+p2_2=$read_dir'SRR5396412_trimmed_2.fq.gz'
 cpu=10
 
 samfile=$base_name'.sam'
@@ -18,8 +20,8 @@ sorted=$base_name'_sorted.bam'
 bowtie2-build --threads $cpu $ref $base_name
 bowtie2 --very-sensitive -p $cpu \
 	-x $base_name \
-	-1 $p1_1 \
-	-2 $p1_2 \
+	-1 $p1_1,$p2_1 \
+	-2 $p1_2,$p2_2 \
 	--no-unal \
 	-S $samfile 2> $report
 
