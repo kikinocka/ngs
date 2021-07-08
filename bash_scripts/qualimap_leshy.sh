0add3a82a7c335d6ecc6bfe98c93195b5026bfc2
kikinocka <kika.zahonova@gmail.com>
2021-07-08 09:22:31 +0200

    bla

--- bash_scripts/qualimap_leshy.sh
+++ bash_scripts/qualimap_leshy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 qualimap='/mnt/mokosz/home/kika/tools/qualimap_v2.2.1/qualimap'
-bam='/mnt/mokosz/home/kika/rhizomastix_vacuolata/mapping/rvac2/rvac2_bw2_sorted.bam'
+bam='/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/mapping/rlib_bw2_sorted.bam'
 outdir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/qualimap/'
-out='rvac2_qualimap.pdf'
+out='rlib_qualimap.pdf'
 cpu=10
 
 $qualimap bamqc -nt $cpu -bam $bam -outdir $outdir -outfile $out -outformat pdf
