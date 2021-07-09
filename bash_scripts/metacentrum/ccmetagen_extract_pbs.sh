01a493b126bbf57308de6eda1d8f0c49a4d8c23b
kikinocka <kika.zahonova@gmail.com>
2021-07-09 11:08:49 +0200

    bla

--- bash_scripts/metacentrum/ccmetagen_extract_pbs.sh
+++ bash_scripts/metacentrum/ccmetagen_extract_pbs.sh
@@ -3,7 +3,7 @@
 
 module add ccmetagen-1.2.5
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta_reads/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta_assembly/'
 
 csv=$datadir'CCMetagen.res.csv'
 frag=$datadir'bml_kma.frag'
