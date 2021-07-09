8db0bf737453913bee2d96fb06795932a15fc552
kikinocka <kika.zahonova@gmail.com>
2021-07-09 11:10:41 +0200

    bla

--- bash_scripts/metacentrum/ccmetagen_extract_pbs.sh
+++ bash_scripts/metacentrum/ccmetagen_extract_pbs.sh
@@ -7,8 +7,8 @@ datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta_assembly
 
 csv=$datadir'CCMetagen.res.csv'
 frag=$datadir'bml_kma.frag'
-out=$datadir'bacteria'
+out=$datadir'unknown'
 level='Superkingdom'
-lineage='Bacteria'
+lineage='unk_s'
 
 CCMetagen_extract_seqs.py -iccm $csv -ifrag $frag -l $level -t $lineage -o $out
