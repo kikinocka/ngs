69ba1c4bf2ba95baf3ea21944e24087e27c0f286
kikinocka <kika.zahonova@gmail.com>
2021-07-09 11:05:59 +0200

    bla

--- bash_scripts/metacentrum/ccmetagen_merge_pbs.sh
+++ bash_scripts/metacentrum/ccmetagen_merge_pbs.sh
@@ -3,7 +3,7 @@
 
 module add ccmetagen-1.2.5
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenome/kma-ccmeta_assembly/'
 
 # $datadir must contain .res.csv file with CCMetagen results !
 
