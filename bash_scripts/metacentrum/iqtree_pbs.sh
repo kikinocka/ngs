e22e47bc9a485c53c2473383e2985bb0886dc32a
kikinocka <kika.zahonova@gmail.com>
2021-07-14 21:51:07 +0200

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-datadir='/storage/brno3-cerit/home/kika/trafficking/retromer'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/fornicata'
 
 #copy files to scratch
 cp $datadir'/'*.aln $SCRATCHDIR
@@ -18,16 +18,16 @@ cp $datadir'/'*.aln $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='vps29.trimal_gt_0.8.aln'
-guide='guide_vps29'
-guide_tree=$guide'.treefile'
+aln='fornicata.trimal_gt_0.1.aln'
+# guide='guide_vps29'
+# guide_tree=$guide'.treefile'
 bb=1000
 
 # iqtree -m TEST -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
-# iqtree -m GTR+G -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
+iqtree -m GTR+G -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 
-iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
+# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,21 +3,21 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/Mic60-Mgm1-Opa1/coevolution/opa1/4-no_redundancy_trimmed_enol-orgn/')
+os.chdir('/Users/kika/ownCloud/oil_sands/Lane26_18S_V9/metamonads/18S_fornicata/')
 files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 
 for file in files:
 	print(file)
 	file_name = file.split('.')[0] #+ '_' + file.split('_')[1]
 	aut = 'automated1'
-	gt = 0.8 #fraction of sequences with a gap allowed
+	gt = 0.1 #fraction of sequences with a gap allowed
 	st = 0.001 #minimum average similarity allowed.
 
-	output = '{}.trimal_{}.aln'.format(file_name, aut)
-	os.system('{} -in {} -out {} -{} -fasta'.format(trimal, file, output, aut))
+	# output = '{}.trimal_{}.aln'.format(file_name, aut)
+	# os.system('{} -in {} -out {} -{} -fasta'.format(trimal, file, output, aut))
 	
-	# output = '{}.trimal_gt_{}.aln'.format(file_name, gt)
-	# os.system('{} -in {} -out {} -gt {} -fasta'.format(trimal, file, output, gt))
+	output = '{}.trimal_gt_{}.aln'.format(file_name, gt)
+	os.system('{} -in {} -out {} -gt {} -fasta'.format(trimal, file, output, gt))
 
 	# output = '{}.trimal_gt_{}_st_{}.aln'.format(file_name, gt, st)
 	# os.system('{} -in {} -out {} -gt {} -st {} -fasta'.format(trimal, file, output, gt, st))
