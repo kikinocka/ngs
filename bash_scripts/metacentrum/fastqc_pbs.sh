00a1157d300383f88f357d6680ff610e0983034f
kikinocka <kika.zahonova@gmail.com>
2021-07-14 18:07:57 +0200

    bla

--- bash_scripts/metacentrum/fastqc_pbs.sh
+++ bash_scripts/metacentrum/fastqc_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add modules
 module add fastQC-0.11.5
 
-read_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P2S_1-01A_L001-ds.9f42a90caf694c0ab5686f0e22e79319/reads'
+read_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3B_1-06D_L001-ds.435324be81dc4260a8e3e8dbb5ed960c/reads'
 
 
 #copy data to scratch
