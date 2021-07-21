8341a121b4eaf031ae219ea095fdf4aede74ab21
kikinocka <kika.zahonova@gmail.com>
2021-07-21 21:52:55 +0200

    bla

--- bash_scripts/trinity_leshy.sh
+++ bash_scripts/trinity_leshy.sh
@@ -6,5 +6,7 @@ fwd1='reads/Endo1_trimmed_1.fq.gz'
 rev1='reads/Endo1_trimmed_2.fq.gz'
 fwd2='reads/Endo2_trimmed_1.fq.gz'
 rev2='reads/Endo2_trimmed_2.fq.gz'
+fwd3='reads/Endo1-2_trimmed_1.fq.gz'
+rev3='reads/Endo1-2_trimmed_2.fq.gz'
 
-Trinity --seqType fq --left $fwd1,$fwd2 --right $rev1,$rev2 --max_memory 100G --CPU 15
+Trinity --seqType fq --left $fwd1,$fwd2,$fwd3 --right $rev1,$rev2,$rev3 --max_memory 100G --CPU 15
