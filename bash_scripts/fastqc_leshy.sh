ac2893c7be03ca17fe4aa2ccd7a6bd90a18ea8b4
kikinocka <kika.zahonova@gmail.com>
2021-07-21 20:54:52 +0200

    bla

--- bash_scripts/fastqc_leshy.sh
+++ bash_scripts/fastqc_leshy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-read_dir='/mnt/mokosz/home/kika/endolimax_nana/reads/'
+read_dir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/reads/'
 out_dir=$read_dir'fastqc/'
 
 cd $read_dir
 
-for reads in Endo1-2_*fq.gz ; do
+for reads in RV1-2_*.gz ; do
 	echo 'running FastQC on ' $reads
 	fastqc -t 10 -o $out_dir $reads
 done
