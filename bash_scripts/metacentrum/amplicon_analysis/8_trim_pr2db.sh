495eeb190815741b8fc35787de3a215f22ce0db3
kikinocka <kika.zahonova@gmail.com>
2021-07-21 22:16:11 +0200

    bla

--- bash_scripts/metacentrum/amplicon_analysis/8_trim_pr2db.sh
+++ bash_scripts/metacentrum/amplicon_analysis/8_trim_pr2db.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 module add python27-modules-gcc
 # module add python36-modules-gcc
 
-DATADIR='/storage/brno3-cerit/home/kika/pr2db/4.13.0/'
+DATADIR='/storage/brno3-cerit/home/kika/databases/pr2db/4.13.0/'
 
 #copy files to scratch
 cp $DATADIR'pr2_version_4.13.0_18S_UTAX.fasta' $SCRATCHDIR 
