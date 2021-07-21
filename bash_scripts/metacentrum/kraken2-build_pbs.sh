49cb1f91dddd40546ab00f96ae86628cdb7a1b93
kikinocka <kika.zahonova@gmail.com>
2021-07-21 22:01:39 +0200

    bla

--- bash_scripts/metacentrum/kraken2-build_pbs.sh
+++ bash_scripts/metacentrum/kraken2-build_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N kraken-build
-#PBS -l select=1:ncpus=20:ompthreads=20:mem=100gb:scratch_local=650gb
-#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=100gb:scratch_local=650gb
+#PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
 
