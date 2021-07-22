9f3ab3c6f98586d811d594d946273564160984a5
kikinocka <kika.zahonova@gmail.com>
2021-07-22 11:36:04 +0200

    bla

--- bash_scripts/metacentrum/iqtree_eug_pbs.sh
+++ bash_scripts/metacentrum/iqtree_eug_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-rabs
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-datadir='/storage/brno3-cerit/home/kika/anaeramoeba/rabs/trees/2-4-14/ver1_bs'
+datadir='/storage/brno3-cerit/home/kika/anaeramoeba/hops-corvet/ver2_bs'
 
 #copy files to scratch
 cp $datadir'/'*.aln $SCRATCHDIR
@@ -18,14 +18,13 @@ cp $datadir'/'*.aln $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='2-4-14.trimal_gt_0.8.aln'
-guide='guide_2-4-14'
+aln='hops_corvet.trimal_gt_0.8.aln'
+guide='guide_hops-corvet'
 guide_tree=$guide'.treefile'
 bb=100
 nm=2000
 
 # iqtree -s $aln -b $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST -quiet
-# iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m TEST -quiet
 # iqtree -s $aln -bb $bb -nt AUTO -ntmax $PBS_NUM_PPN -m LG4X -quiet #-wsr
 
 iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
