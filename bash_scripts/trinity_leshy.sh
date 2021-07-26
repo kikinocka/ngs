9d25e18d3e27f15ac73bc4421c9dd744485cdc8b
kikinocka <kika.zahonova@gmail.com>
2021-07-26 09:30:53 +0200

    bla

--- bash_scripts/trinity_leshy.sh
+++ bash_scripts/trinity_leshy.sh
@@ -2,19 +2,19 @@
 
 #CALCULATE IN TMP DIR!!!
 
-datadir='/mnt/mokosz/home/kika/endolimax_nana'
+datadir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/'
 
-cp $datadir'reads/'*_trimmed* /tmp/kika/
+cp $datadir'reads/'*trimmed* /tmp/kika/
 
 
 cd '/tmp/kika/'
 
-fwd1='Endo1_trimmed_1.fq.gz'
-rev1='Endo1_trimmed_2.fq.gz'
-fwd2='Endo2_trimmed_1.fq.gz'
-rev2='Endo2_trimmed_2.fq.gz'
-fwd3='Endo1-2_trimmed_1.fq.gz'
-rev3='Endo1-2_trimmed_2.fq.gz'
+fwd1='RV1_trimmed_1.fq.gz'
+rev1='RV1_trimmed_2.fq.gz'
+fwd2='RV2_trimmed_1.fq.gz'
+rev2='RV2_trimmed_2.fq.gz'
+fwd3='RV1-2_trimmed_1.fq.gz'
+rev3='RV1-2_trimmed_2.fq.gz'
 
 Trinity --seqType fq --left $fwd1,$fwd2,$fwd3 --right $rev1,$rev2,$rev3 --max_memory 100G --CPU 15
 
