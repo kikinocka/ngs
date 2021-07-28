bf0ec7beb3ee36b75f55f76efd3b5dec68c56824
kikinocka <kika.zahonova@gmail.com>
2021-07-28 14:52:41 +0200

    bla

--- bash_scripts/metacentrum/iqtree_diplo.sh
+++ bash_scripts/metacentrum/iqtree_diplo.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-datadir='/storage/brno3-cerit/home/kika/trafficking/SNARE/Qa/syn16'
+datadir='/storage/brno3-cerit/home/kika/trafficking/RABs/rab32/ver2'
 
 #copy files to scratch
 cp $datadir'/'*.aln $SCRATCHDIR
