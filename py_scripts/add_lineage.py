33b07fc3834918f9097322e4bc78b6430a509864
kikinocka <kika.zahonova@gmail.com>
2021-08-02 17:18:58 +0200

    bla

--- py_scripts/add_lineage.py
+++ py_scripts/add_lineage.py
@@ -4,8 +4,19 @@ import os
 os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/')
 table = open('otu_table.V9DS.tsv')
 
-for line in table:
-	if line.startswith('OTU'):
-		print(line)
-	# if line.split('\t')[22] == 'No_hit':
-	# 	print(line)
+with open('otu_table.V9DS_updated.tsv', 'w') as out:
+	for line in table:
+		if line.startswith('OTU'):
+			new_line = '{}\tlineage\t{}'.format('\t'.join(line.split('\t')[0:22]), '\t'.join(line.split('\t')[22:]))
+			out.write(new_line)
+		elif line.split('\t')[22] == 'No_hit':
+			new_line = '{}\tNo_hit|No_hit|No_hit|No_hit|No_hit|No_hit|No_hit|No_hit\t{}'.format('\t'.join(line.split('\t')[0:22]), 
+				'\t'.join(line.split('\t')[22:]))
+			out.write(new_line)
+		elif line.split('\t')[22] == 'Eukaryota|*|*|*|*|*|*|*':
+			new_line = '{}\tEukaryota|Eukaryota_X|Eukaryota_XX|Eukaryota_XXX|Eukaryota_XXXX|Eukaryota_XXXXX|Eukaryota_XXXXXX|Eukaryota_XXXXXX_sp\t{}'.format(
+				'\t'.join(line.split('\t')[0:22]), '\t'.join(line.split('\t')[22:]))
+			out.write(new_line)
+		else:
+			new_line = '{}\t{}\t{}'.format('\t'.join(line.split('\t')[0:22]), line.split('\t')[22], '\t'.join(line.split('\t')[22:]))
+			out.write(new_line)
