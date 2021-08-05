2135516b8a45c78ecdfbd46e8301195354b76248
kikinocka <kika.zahonova@gmail.com>
2021-08-05 15:02:03 +0200

    bla

--- bash_scripts/qualimap_leshy.sh
+++ bash_scripts/qualimap_leshy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 qualimap='/mnt/mokosz/home/kika/tools/qualimap_v2.2.1/qualimap'
-bam='/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/mapping/rlib_bw2_sorted.bam'
-outdir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/qualimap/'
-out='rlib_qualimap.pdf'
+bam='/mnt/mokosz/home/kika/endolimax_nana/mapping2/enan_bw2_sorted.bam'
+outdir='/mnt/mokosz/home/kika/endolimax_nana/qualimap2/'
+out='enan_qualimap.pdf'
 cpu=10
 mem=5G
 
