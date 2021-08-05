f8be3d75f8cfe08b5935ba108804d3a572c8c430
kikinocka <kika.zahonova@gmail.com>
2021-08-05 12:49:04 +0200

    bla

--- bash_scripts/bowtie2_leshy.sh
+++ bash_scripts/bowtie2_leshy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd '/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/mapping/'
-assembly_dir='/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/rhizomastix_reassembly_NR/'
-read_dir='/mnt/mokosz/home/kika/rhizomastix_libera+elongata_reassembly/reads/'
+cd '/mnt/mokosz/home/kika/endolimax_nana/mapping2/'
+assembly_dir='/mnt/mokosz/home/kika/endolimax_nana/filtration2/enan_NR/'
+read_dir='/mnt/mokosz/home/kika/endolimax_nana/reads/'
 
-base_name='rlib_bw2'
-ref=$assembly_dir'rhizomastix_reassembly.trinity.NRfilt.fasta'
-p1_1=$read_dir'SRR5396411_trimmed_1.fq.gz'
-p1_2=$read_dir'SRR5396411_trimmed_2.fq.gz'
-p2_1=$read_dir'SRR5396412_trimmed_1.fq.gz'
-p2_2=$read_dir'SRR5396412_trimmed_2.fq.gz'
+base_name='enan_bw2'
+ref=$assembly_dir'enan.trinity.NRfilt.fna'
+p1_1=$read_dir'Endo1-2_trimmed_1.fq.gz'
+p1_2=$read_dir'Endo1-2_trimmed_2.fq.gz'
+p2_1=$read_dir'Endo1_trimmed_1.fq.gz'
+p2_2=$read_dir'Endo1_trimmed_2.fq.gz'
+p3_1=$read_dir'Endo2_trimmed_1.fq.gz'
+p3_2=$read_dir'Endo2_trimmed_2.fq.gz'
 cpu=10
 
 samfile=$base_name'.sam'
@@ -20,8 +22,8 @@ sorted=$base_name'_sorted.bam'
 bowtie2-build --threads $cpu $ref $base_name
 bowtie2 --very-sensitive -p $cpu \
 	-x $base_name \
-	-1 $p1_1,$p2_1 \
-	-2 $p1_2,$p2_2 \
+	-1 $p1_1,$p2_1,$p3_1 \
+	-2 $p1_2,$p2_2,$p3_2 \
 	--no-unal \
 	-S $samfile 2> $report
 
--- bash_scripts/mirrortree.sh
+++ bash_scripts/mirrortree.sh
@@ -2,9 +2,9 @@
 
 mt='/Users/kika/programs/MirrorTree/mirrortree_mac-uni'
 matrix='/Users/kika/programs/MirrorTree/Maxhom_McLachlan.metric'
-aln='/Users/kika/ownCloud/Mic60-Mgm1-Opa1/coevolution/hsp70/mgm1/2-enol-orgn/concat_hsp70-mgm1.aln'
-out='/Users/kika/ownCloud/Mic60-Mgm1-Opa1/coevolution/hsp70/mgm1/2-enol-orgn/concat_hsp70-mgm1.MT.txt'
-first=663
-second=616
+aln='/Users/kika/ownCloud/Mic60-Mgm1-Opa1/coevolution/hsp70/opa1/2-enol-orgn/concat_hsp70-opa1.aln'
+out='/Users/kika/ownCloud/Mic60-Mgm1-Opa1/coevolution/hsp70/opa1/2-enol-orgn/concat_hsp70-opa1.MT.txt'
+first=630
+second=670
 
 $mt $aln $matrix $first $second 2> $out 1> $out
