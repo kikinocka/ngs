30d743f571891b40f30d8e23e17c5d48a86c6fce
kikinocka <kika.zahonova@gmail.com>
2021-08-09 09:30:22 +0200

    bla

---
+++ bash_scripts/metacentrum/krona_pbs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -N krona
+#PBS -l select=1:ncpus=15:mem=5gb:scratch_local=1gb
+#PBS -l walltime=02:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+module add krona-2.8
+
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/20xx0821_BML-B-first/kraken2/'
+
+#copy files to scratch
+cp $datadir'bml_meta.spades_def.fa' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+report='bml_meta.kraken.report'
+out='bml_meta.kraken.html'
+
+
+ImportTaxonomy.pl -m $PBS_NUM_PPN -t 5 $report -o $out
+
+#copy files back
+rm $report
+cp -R * $datadir
