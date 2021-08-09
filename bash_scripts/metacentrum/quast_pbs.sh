628bbb091706296f97300fb0e1651a2e8583f783
kikinocka <kika.zahonova@gmail.com>
2021-08-09 12:58:55 +0200

    bla

--- bash_scripts/metacentrum/quast_pbs.sh
+++ bash_scripts/metacentrum/quast_pbs.sh
@@ -10,32 +10,31 @@ cat $PBS_NODEFILE
 #add module
 module add quast-4.6.3
 
-datadir='/storage/brno3-cerit/home/kika/kinetoplastids/20210809'
+datadir='/storage/brno3-cerit/home/kika/p57_genome'
 outdir=$datadir'/quast/'
 
 #copy files to scratch
-cp $datadir'/'*.fasta $SCRATCHDIR
+cp $datadir'p57_polished.fa' $SCRATCHDIR
 
-assemblies=*.fasta
+assemblies='p57_polished.fa'
 min_contig=500
 
 
 #compute on scratch
 cd $SCRATCHDIR
 # quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --min-contig $min_contig $assemblies
-# quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $assemblies
+quast.py -o $SCRATCHDIR -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $assemblies
 
-for fasta in $assemblies; do
-	echo $fasta
-	out=`echo $fasta | cut -d '.' -f 1`
-	full_out=$outdir'/'$out
+# for fasta in $assemblies; do
+# 	echo $fasta
+# 	out=`echo $fasta | cut -d '.' -f 1`
+# 	full_out=$outdir'/'$out
 
-	quast.py -o $full_out -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $fasta
-	cp -r * $full_out
-done
+# 	quast.py -o $full_out -t $PBS_NUM_PPN --glimmer --min-contig $min_contig --eukaryote $fasta
+# done
 
 
-#copy results to your folder
+copy results to your folder
 # rm $f $bam
-# rm $assemblies
-# cp -r * $outdir
+rm $assemblies
+cp -r * $outdir
