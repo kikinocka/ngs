661708cb0f5d552f7a90ecf27bc62022ebcb7fc1
kikinocka <kika.zahonova@gmail.com>
2021-08-10 15:18:12 +0200

    bla

--- bash_scripts/NR_filt.sh
+++ bash_scripts/NR_filt.sh
@@ -3,7 +3,6 @@
 filt_script='/mnt/mokosz/home/kika/scripts/py_scripts/NR_filt.py'
 
 datadir='/mnt/mokosz/home/kika/endolimax_nana/'
-outdir=$datadir'filtration3b_NR-amoebozoa/'
 diamond=$datadir'filtration2/enan_NR/enan.trinity.NTfilt.dmnd.out'
 transcriptome=$datadir'filtration3a_NR-only_eukaryota/enan_NR/enan.trinity.NRfilt.fna'
 proteins=$datadir'filtration3a_NR-only_eukaryota/enan_NR/enan.trinity.NRfilt.faa'
@@ -11,7 +10,6 @@ coverage='70' #default: 50
 identity='90' #default: 75
 taxon='Amoebozoa'
 
-cd $outdir
 $filt_script -i $diamond -d $datadir -nt $transcriptome -aa $proteins -q $coverage -p $identity -g $taxon
 
 # -i: --infile
