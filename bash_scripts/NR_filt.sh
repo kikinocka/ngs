b668cec2da0fd38c58743db81037ce95444a352b
kikinocka <kika.zahonova@gmail.com>
2021-08-10 14:14:18 +0200

    bla

--- bash_scripts/NR_filt.sh
+++ bash_scripts/NR_filt.sh
@@ -6,8 +6,8 @@ datadir='/mnt/mokosz/home/kika/endolimax_nana/'
 diamond=$datadir'filtration2/enan_NR/enan.trinity.NTfilt.dmnd.out'
 transcriptome=$datadir'filtration2/enan_trinity_NT/enan_trinity.NTfilt.fasta'
 proteins=$datadir'filtration2/enan_trinity.NTfilt.fasta.transdecoder_dir/longest_orfs.pep'
-coverage='50' #default: 50
-identity='70' #default: 75
+coverage='70' #default: 50
+identity='90' #default: 75
 taxon='Eukaryota'
 
 $filt_script -i $diamond -d $datadir -nt $transcriptome -aa $proteins -q $coverage -p $identity -g $taxon
