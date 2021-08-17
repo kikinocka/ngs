ffa3c1e1f3c87b67f49a2276ed45cae56ec7ddd0
kikinocka <kika.zahonova@gmail.com>
2021-08-17 10:36:59 +0200

    bla

--- bash_scripts/metacentrum/krona_pbs.sh
+++ bash_scripts/metacentrum/krona_pbs.sh
@@ -9,17 +9,17 @@ cat $PBS_NODEFILE
 
 module add krona-2.8
 
-datadir='/auto/brno3-cerit/nfs4/home/kika/oil_sands/metagenomes/P3S_1-02B_L001-ds.971c07c67a83443891de04bf749cee0b/4-kraken2_assembly/'
+datadir='/auto/brno3-cerit/nfs4/home/kika/oil_sands/metagenomes/P2S_1-01A_L001-ds.9f42a90caf694c0ab5686f0e22e79319/4-kraken2_assembly/'
 
 #copy files to scratch
-cp $datadir'P3S.kraken.report' $SCRATCHDIR
+cp $datadir'P2S.kraken.report' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-report='P3S.kraken.report'
-out='P3S.kraken.html'
+report='P2S.kraken.report'
+out='P2S.kraken.html'
 
 
 ImportTaxonomy.pl -m $PBS_NUM_PPN -t 5 $report -o $out
--- bash_scripts/qualimap_leshy.sh
+++ bash_scripts/qualimap_leshy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 qualimap='/mnt/mokosz/home/kika/tools/qualimap_v2.2.1/qualimap'
-bam='/mnt/mokosz/home/kika/endolimax_nana/mapping2/enan_bw2_sorted.bam'
-outdir='/mnt/mokosz/home/kika/endolimax_nana/qualimap2/'
+bam='/mnt/mokosz/home/kika/rhizomastix_vacuolata/mapping2/rvac_bw2_sorted.bam'
+outdir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/mapping2/'
 out='enan_qualimap.pdf'
 cpu=10
 mem=5G
