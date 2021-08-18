877e0b099108fa8936fdcf906aa747f9a4632c50
kikinocka <kika.zahonova@gmail.com>
2021-08-18 21:25:45 +0200

    bla

--- bash_scripts/metacentrum/krona_pbs.sh
+++ bash_scripts/metacentrum/krona_pbs.sh
@@ -9,17 +9,17 @@ cat $PBS_NODEFILE
 
 module add krona-2.8
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3B_1-06D_L001-ds.435324be81dc4260a8e3e8dbb5ed960c/4b-kraken2-microbial_reads/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3S_1-02B_L001-ds.971c07c67a83443891de04bf749cee0b/4b-kraken2-microbial_reads/'
 
 #copy files to scratch
-cp $datadir'P3B.kraken.report' $SCRATCHDIR
+cp $datadir'P3S.kraken.report' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-report='P3B.kraken.report'
-out='P3B.kraken.html'
+report='P3S.kraken.report'
+out='P3S.kraken.html'
 
 
 ImportTaxonomy.pl -m $PBS_NUM_PPN -t 5 $report -o $out
