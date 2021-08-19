75a005aebceabcd0675c046d4fb2ff1d2c7fdc0a
kikinocka <kika.zahonova@gmail.com>
2021-08-19 10:27:36 +0200

    bl

---
+++ bash_scripts/metacentrum/raxml-epa_pbs.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#PBS -N raxml-epa
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l walltime=02:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add raxml-8.2.8
+
+data='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/18S_trees/metamonads_EPA/'
+
+#copy files to scratch
+cp $data'metamonads_renamed.tree' $SCRATCHDIR
+cp $data'mlsb_metamonads.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+aln='mlsb_metamonads.trimal_gt-0.25_cons-50.aln'
+tree='metamonads_renamed.tree'
+out='EPARUN'
+
+raxmlHPC-PTHREADS-SSE3 -f v -G 0.2 -m GTRCATI -n $out -s $aln -t $tree -T $PBS_NUM_PPN
+
+
+#copy files back
+rm $aln $tree
+cp -R * $data
