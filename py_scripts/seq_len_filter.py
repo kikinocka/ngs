81f4f02fd849d17442eb836a3c2e1bf3b03cbe48
kikinocka <kika.zahonova@gmail.com>
2021-08-20 18:06:53 +0200

    bla

--- py_scripts/seq_len_filter.py
+++ py_scripts/seq_len_filter.py
@@ -6,7 +6,7 @@ from Bio import SeqIO
 os.chdir('/storage/brno3-cerit/home/kika/databases/pr2db/4.14.0/')
 db = SeqIO.parse('pr2_version_4.14.0_SSU_UTAX.fasta', 'fasta')
 
-with open('pr2_version_4.14.0_SSU_UTAX.longer1000.fasta', 'w') as update:
+with open('pr2_version_4.14.0_SSU_UTAX.longer400.fasta', 'w') as update:
 	for seq in db:
 		print(seq.name)
 		if len(seq.seq) < 1000:
