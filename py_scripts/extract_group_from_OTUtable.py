0faa18510ba6f7f5628f2ec64c4a8e78c5804fa5
kikinocka <kika.zahonova@gmail.com>
2021-08-23 12:09:02 +0200

    bla

--- py_scripts/extract_group_from_OTUtable.py
+++ py_scripts/extract_group_from_OTUtable.py
@@ -5,10 +5,10 @@ os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/')
 table = open('otu_table.V9DS_updated.no_chimera.tsv')
 
 groups = {}
-with open('breviatea_counts.tsv', 'w') as counts, open('breviatea.V9DS_updated.no_chimera.tsv', 'w') as out:
+with open('metazoa_counts.tsv', 'w') as counts, open('metazoa.V9DS_updated.no_chimera.tsv', 'w') as out:
 	for line in table:
 		taxonomy = line.split('\t')[22]
-		if 'Breviata' in taxonomy:
+		if 'Metazoa' in taxonomy:
 			out.write(line)
 			if taxonomy.split('|')[3] not in groups:
 				groups[taxonomy.split('|')[3]] = 1
