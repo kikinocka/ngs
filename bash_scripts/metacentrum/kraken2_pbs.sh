325b548e37956ab06f0a64bf256e45fca0bd341f
kikinocka <kika.zahonova@gmail.com>
2021-08-24 10:03:21 +0200

    bla

--- bash_scripts/metacentrum/kraken2_pbs.sh
+++ bash_scripts/metacentrum/kraken2_pbs.sh
@@ -14,41 +14,41 @@ kraken2DB='/storage/brno3-cerit/home/kika/databases/kraken2DB-eukprot'
 datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
 
 #copy files to scratch
-# cp $datadir'1-reads/P1B_trimmed_1.fq.gz' $SCRATCHDIR
-# cp $datadir'1-reads/P1B_trimmed_2.fq.gz' $SCRATCHDIR
-cp $datadir'2-spades/scaffolds.fasta' $SCRATCHDIR
+cp $datadir'1-reads/P1B_trimmed_1.fq.gz' $SCRATCHDIR
+cp $datadir'1-reads/P1B_trimmed_2.fq.gz' $SCRATCHDIR
+# cp $datadir'2-spades/scaffolds.fasta' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-# fwd='P1B_trimmed_1.fq.gz'
-# rev='P1B_trimmed_2.fq.gz'
-# classified='P1B.classified#.fq'
-# unclassified='P1B.unclassified#.fq'
-assembly='scaffolds.fasta'
-classified='P1B.classified.fa'
-unclassified='P1B.unclassified.fa'
+fwd='P1B_trimmed_1.fq.gz'
+rev='P1B_trimmed_2.fq.gz'
+classified='P1B.classified#.fq'
+unclassified='P1B.unclassified#.fq'
+# assembly='scaffolds.fasta'
+# classified='P1B.classified.fa'
+# unclassified='P1B.unclassified.fa'
 out='P1B.kraken.out'
 report='P1B.kraken.report'
 krona='P1B.kraken.html'
 
-# #on reads
-# kraken2 --db $kraken2DB --threads $PBS_NUM_PPN \
-#   --classified-out $classified \
-#   --unclassified-out $unclassified \
-#   --report $report \
-#   --paired --gzip-compressed $fwd $rev > $out
-
-#on assembly
+#on reads
 kraken2 --db $kraken2DB --threads $PBS_NUM_PPN \
   --classified-out $classified \
   --unclassified-out $unclassified \
-  --report $report $assembly > $out
+  --report $report \
+  --paired --gzip-compressed $fwd $rev > $out
+
+# #on assembly
+# kraken2 --db $kraken2DB --threads $PBS_NUM_PPN \
+#   --classified-out $classified \
+#   --unclassified-out $unclassified \
+#   --report $report $assembly > $out
 
 ImportTaxonomy.pl -m $PBS_NUM_PPN -t 5 $report -o $krona
 
 
 #copy files back
-# rm $fwd $rev
-rm $assembly
-cp -R * $datadir'4c-kraken2-eukprot_assembly/'
+rm $fwd $rev
+# rm $assembly
+cp -R * $datadir'4b-kraken2-eukprot_reads/'
