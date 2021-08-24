fad7b1125b3fe8fc5f243de246fa6cd08f1db196
kikinocka <kika.zahonova@gmail.com>
2021-08-24 09:25:24 +0200

    bla

--- bash_scripts/metacentrum/kraken2_pbs.sh
+++ bash_scripts/metacentrum/kraken2_pbs.sh
@@ -11,26 +11,26 @@ module add kraken2-1.2
 module add krona-2.8
 
 kraken2DB='/storage/brno3-cerit/home/kika/databases/kraken2DB-eukprot'
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P2S_1-01A_L001-ds.9f42a90caf694c0ab5686f0e22e79319/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
 
 #copy files to scratch
-# cp $datadir'1-reads/P2S_trimmed_1.fq.gz' $SCRATCHDIR
-# cp $datadir'1-reads/P2S_trimmed_2.fq.gz' $SCRATCHDIR
+# cp $datadir'1-reads/P1B_trimmed_1.fq.gz' $SCRATCHDIR
+# cp $datadir'1-reads/P1B_trimmed_2.fq.gz' $SCRATCHDIR
 cp $datadir'2-spades/scaffolds.fasta' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-# fwd='P2S_trimmed_1.fq.gz'
-# rev='P2S_trimmed_2.fq.gz'
-# classified='P2S.classified#.fq'
-# unclassified='P2S.unclassified#.fq'
+# fwd='P1B_trimmed_1.fq.gz'
+# rev='P1B_trimmed_2.fq.gz'
+# classified='P1B.classified#.fq'
+# unclassified='P1B.unclassified#.fq'
 assembly='scaffolds.fasta'
-classified='P2S.classified.fa'
-unclassified='P2S.unclassified.fa'
-out='P2S.kraken.out'
-report='P2S.kraken.report'
-krona='P2S.kraken.html'
+classified='P1B.classified.fa'
+unclassified='P1B.unclassified.fa'
+out='P1B.kraken.out'
+report='P1B.kraken.report'
+krona='P1B.kraken.html'
 
 # #on reads
 # kraken2 --db $kraken2DB --threads $PBS_NUM_PPN \
