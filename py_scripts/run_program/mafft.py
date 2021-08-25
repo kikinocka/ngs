5644f6101e8e4131e7a50add3437b1852a5a236a
kikinocka <kika.zahonova@gmail.com>
2021-08-25 15:26:11 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/all_adaptors/ver2/')
+os.chdir('/Users/kika/ownCloud/proteromonas/peroxisome/pexins/pex19/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -4,7 +4,7 @@ import os
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
 os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/all_adaptors/ver2/')
-files = [x for x in os.listdir() if x.endswith('large-beta.mafft.aln')]
+files = [x for x in os.listdir() if x.endswith('.aln')]
 
 for file in files:
 	print(file)
