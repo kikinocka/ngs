4fbae9aa3bb16949fdafe1c830156b441ac955de
kikinocka <kika.zahonova@gmail.com>
2021-08-27 13:50:46 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABS/Rab5/ver2/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABS/Rab5/ver3/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
