a34cf919a317937496953c2596e2ec40f552bf8c
kikinocka <kika.zahonova@gmail.com>
2021-08-31 18:04:03 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -16,28 +16,28 @@ maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 # 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
 # 		mafft, file, out, log), shell=True)
 
-# #add to aligned sequences
-# os.chdir('/Users/kika/ownCloud/oil_sands/amplicons/Lane26_18S_V9/metamonads/MLSB/placement/')
-# existing = 'ver2/mlsb_metamonads.mafft_man.aln'
-# add = 'ver3/anaeramoeba.fa'
-# out = 'ver3/mlsb_metamonads.mafft.aln'
-# log = 'ver3/mlsb_metamonads.mafft.log'
-# subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
-# # subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
+#add to aligned sequences
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/ARFs/')
+existing = 'ScrollSaw_output_untrimmed_masked_348seq.updated.aln'
+add = 'diplo_sarB.fa'
+out = 'sarB/diplo_sarB.mafft.aln'
+log = 'sarB/diplo_sarB.mafft.log'
+subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
+# subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
 
-#merge alignments
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/discoba/alns')
-aln1 = 'Diplonemea_Kinetoplastida_alignment.aln'
-aln2 = 'Euglenida_alignment.aln'
-# aln3 = 'preaxostyla_eukref.aln'
-# fasta = 'barthelonids.fa'
-input = 'euglenozoa.in'
-table = 'euglenozoa.table'
-out = 'euglenozoa.mafft.aln'
-log = 'euglenozoa.mafft.log'
-subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
-print('Aligments concatenated\n\n')
-subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
-print('Table prepared\n\n')
-subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-print('Alignments merged')
+# #merge alignments
+# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/discoba/alns')
+# aln1 = 'Diplonemea_Kinetoplastida_alignment.aln'
+# aln2 = 'Euglenida_alignment.aln'
+# # aln3 = 'preaxostyla_eukref.aln'
+# # fasta = 'barthelonids.fa'
+# input = 'euglenozoa.in'
+# table = 'euglenozoa.table'
+# out = 'euglenozoa.mafft.aln'
+# log = 'euglenozoa.mafft.log'
+# subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
+# print('Aligments concatenated\n\n')
+# subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
+# print('Table prepared\n\n')
+# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# print('Alignments merged')
