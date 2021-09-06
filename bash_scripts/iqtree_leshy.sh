6a76068453b5a2859da936c6b4919aa95363c267
kikinocka <kika.zahonova@gmail.com>
2021-09-06 16:46:20 +0200

    bla

--- bash_scripts/iqtree_leshy.sh
+++ bash_scripts/iqtree_leshy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cd /mnt/mokosz/home/kika/archam_trees/cpn10/ver2/
+cd /mnt/mokosz/home/kika/pelomyxa_schiedti/trees/vdac/
 
-aln='cpn10.trimal_gt-0.8.aln'
-# guide='guide_ak'
-# guide_tree=$guide'.treefile'
+aln='porins.trimal_gt-0.8.aln'
+guide='guide_porins'
+guide_tree=$guide'.treefile'
 bb=1000
-# nm=2000
+nm=5000
 threads=10
 
-# iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
-# iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln -ft $guide_tree 
+iqtree -m LG+F+G -nt AUTO -ntmax $threads -quiet -s $aln -pre $guide
+iqtree -m LG+C20+F+G -nt AUTO -ntmax $threads -bb $bb -nm $nm -quiet -s $aln -ft $guide_tree 
 
-iqtree -m TEST -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln
+# iqtree -m TEST -nt AUTO -ntmax $threads -bb $bb -quiet -s $aln
 # -nm $nm 
