e4fefb875c053bc488f87e85d39a31da190a9cdf
kikinocka <kika.zahonova@gmail.com>
2021-09-17 16:44:18 +0200

    bla

--- bash_scripts/sina_leshy.sh
+++ bash_scripts/sina_leshy.sh
@@ -6,4 +6,4 @@ database='/mnt/mokosz/home/kika/silva_ssu_ref-nr90/SILVA_138.1_SSURef_NR99_12_06
 fasta='/mnt/mokosz/home/kika/workdir/metamonads_eukref_V9.fa'
 fasta='/mnt/mokosz/home/kika/workdir/metamonads_eukref_V9.silva.aln'
 
-$sina -i $fasta --intype fasta -o $aln --outtype fasta -ptdb $database
+$sina -i $fasta --intype fasta -o $aln --outtype fasta --ptdb $database
