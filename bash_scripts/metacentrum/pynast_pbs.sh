95147df0bc068bf5bba5127c679e9f21f9ee0516
kikinocka <kika.zahonova@gmail.com>
2021-09-17 11:55:25 +0200

    bla

--- bash_scripts/metacentrum/pynast_pbs.sh
+++ bash_scripts/metacentrum/pynast_pbs.sh
@@ -14,14 +14,14 @@ data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonada/
 
 #copy files to scratch
 cp $data_dir'arb-silva.de_2021-09-16_id1053505.upd.aln' $SCRATCHDIR
-cp $data_dir'metamonads_eukref-otus.fa' $SCRATCHDIR
+cp $data_dir'metamonads_eukref-otus.unambiguous.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
 
 existing='arb-silva.de_2021-09-16_id1053505.upd.aln'
-add='metamonads_eukref-otus.fa'
+add='metamonads_eukref-otus.unambiguous.fa'
 aln='metamonads_V9.pynast.aln'
 log='metamonads_V9.pynast.log'
 length=50
--- py_scripts/remove_seqs.py
+++ py_scripts/remove_seqs.py
@@ -2,17 +2,41 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/')
-remove = open('ncbi_submission/genome/remove.acc')
-fasta = SeqIO.parse('pasa-evm/pelomyxa_introns_final.fa', 'fasta')
+# os.chdir('/Users/kika/ownCloud/pelomyxa_schiedti/')
+# remove = open('ncbi_submission/genome/remove.acc')
+# fasta = SeqIO.parse('pasa-evm/pelomyxa_introns_final.fa', 'fasta')
 
-acc = set()
-for line in remove:
-	acc.add(line.strip())
+# acc = set()
+# for line in remove:
+# 	acc.add(line.strip())
 
-with open('pasa-evm/pelomyxa_introns_final-removed.fa', 'w') as out:
-	for seq in fasta:
-		if seq.name in acc:
-			print(seq.name)
+# with open('pasa-evm/pelomyxa_introns_final-removed.fa', 'w') as out:
+# 	for seq in fasta:
+# 		if seq.name in acc:
+# 			print(seq.name)
+# 		else:
+# 			out.write('>{}\n{}\n'.format(seq.description, seq.seq))
+
+
+os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/metamonada/')
+infasta = SeqIO.parse('metamonads_eukref-otus.fa', 'fasta')
+
+c = 0
+with open('metamonads_eukref-otus.unambiguous.fa', 'w') as out:
+	for seq in infasta:
+		if ('N' in seq.seq.upper()) or \
+		('W' in seq.seq.upper()) or \
+		('S' in seq.seq.upper()) or \
+		('M' in seq.seq.upper()) or \
+		('K' in seq.seq.upper()) or \
+		('R' in seq.seq.upper()) or \
+		('Y' in seq.seq.upper()) or \
+		('B' in seq.seq.upper()) or \
+		('D' in seq.seq.upper()) or \
+		('H' in seq.seq.upper()) or \
+		('V' in seq.seq.upper()):
+			c +=1
 		else:
 			out.write('>{}\n{}\n'.format(seq.description, seq.seq))
+
+print('ambiguous sequences: {}'.format(c))
