8720e098db2fcc37523a274bd9bd3aa9d3d20b3c
kikinocka <kika.zahonova@gmail.com>
2021-09-20 15:06:58 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many_pbs.sh
+++ bash_scripts/metacentrum/iqtree_many_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/trafficking/RABs'
+data_dir='/storage/brno3-cerit/home/kika/trafficking/RABs/rabs1-8-18/ver2'
 
 #copy files to scratch
 cp $data_dir'/'*.aln $SCRATCHDIR
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABs/rab32/ver3/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABs/rabs1-8-18/ver2/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
