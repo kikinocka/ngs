9bc3dc7135adcf724e784d711b7e222ea8cfe139
kikinocka <kika.zahonova@gmail.com>
2021-09-20 16:25:42 +0200

    bla

--- bash_scripts/metacentrum/mafft_pbs.sh
+++ bash_scripts/metacentrum/mafft_pbs.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 #PBS -N mafft
 #PBS -l select=1:ncpus=20:mem=50gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
-module add conda-modules-py37
-conda activate mafft
+# module add conda-modules-py37
+# conda activate mafft
+module add mafft-7.453
 
-data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/discoba/'
+data_dir='/storage/brno3-cerit/home/kika/trafficking/RABs/ver12/'
 
 #copy files to scratch
-cp $data_dir'discobids_eukref.mafft_merge.aln' $SCRATCHDIR
-cp $data_dir'discobids_otus.fa' $SCRATCHDIR
+cp $data_dir'rabs.fa' $SCRATCHDIR
+# cp $data_dir'discobids_otus.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-# #align de-novo
-# fa='rabs.fa'
-# aln=${fa%.fa}.mafft.aln
-# log=${fa%.fa}.mafft.log
+#align de-novo
+fa='rabs.fa'
+aln=${fa%.fa}.mafft.aln
+log=${fa%.fa}.mafft.log
 
-# mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --inputorder ${fa} > ${aln} 2> ${log}
+mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --inputorder ${fa} > ${aln} 2> ${log}
 
 
-#add to aligned sequences
-existing='discobids_eukref.mafft_merge.aln'
-add='discobids_otus.fa'
-aln='discoba_V9.mafft.aln'
-log='discoba_V9.mafft.log'
+# #add to aligned sequences
+# existing='discobids_eukref.mafft_merge.aln'
+# add='discobids_otus.fa'
+# aln='discoba_V9.mafft.aln'
+# log='discoba_V9.mafft.log'
 
-# mafft --add $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
-mafft --addfragments $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
+# # mafft --add $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
+# mafft --addfragments $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
 
 
 #copy files back
-# rm $fa
-rm $existing $add
+rm $fa
+# rm $existing $add
 cp * $data_dir
