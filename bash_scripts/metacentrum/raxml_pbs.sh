a0a24e7d2a494c6335a74c770784ce6c24264e12
kikinocka <kika.zahonova@gmail.com>
2021-09-21 20:01:28 +0200

    bla

--- bash_scripts/metacentrum/raxml_pbs.sh
+++ bash_scripts/metacentrum/raxml_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=48:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,10 +10,11 @@ cat $PBS_NODEFILE
 #add module
 module add raxml-8.2.8
 
-data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonada/reference_tree/'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonada/reference_tree'
 
 #copy files to scratch
-cp $data'metamonads_eukref.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+# cp $data'metamonads_eukref.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'/'* $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
@@ -21,9 +22,9 @@ cd $SCRATCHDIR
 aln='metamonads_eukref.trimal_gt-0.25_cons-50.aln'
 out='metamonads_eukref'
 
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -T 1
+# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -T 1$PBS_NUM_PPN
 
 
 #copy files back
