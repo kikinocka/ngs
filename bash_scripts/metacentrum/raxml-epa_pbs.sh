6b8f4931df1d3915cf33e585040f6d0dceee1312
kikinocka <kika.zahonova@gmail.com>
2021-09-22 15:51:47 +0200

    bla

--- bash_scripts/metacentrum/raxml-epa_pbs.sh
+++ bash_scripts/metacentrum/raxml-epa_pbs.sh
@@ -10,17 +10,18 @@ cat $PBS_NODEFILE
 #add module
 module add raxml-8.2.8
 
-data='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/18S_trees/metamonads_EPA/ver4/'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonada/'
 
 #copy files to scratch
-cp $data'mlsb_metamonads.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
-cp $data'mlsb_metamonads.trimal_gt-0.25_cons-50.no_V9.aln.treefile' $SCRATCHDIR
+cp $data'placement/metamonads_eukref_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'reference_tree/RAxML_bipartitions.metamonads_eukref3' $SCRATCHDIR
+
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='mlsb_metamonads.trimal_gt-0.25_cons-50.aln'
-tree='mlsb_metamonads.trimal_gt-0.25_cons-50.no_V9.aln.treefile'
+aln='metamonads_eukref_V9.trimal_gt-0.25_cons-50.aln'
+tree='RAxML_bipartitions.metamonads_eukref3'
 out='EPARUN'
 
 raxmlHPC-PTHREADS -f v -G 0.2 -m GTRCATI -n $out -s $aln -t $tree -T $PBS_NUM_PPN
