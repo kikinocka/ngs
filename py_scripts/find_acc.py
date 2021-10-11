49c3e20d213c8b59ecac82d7765c1b0fc91706e1
kikinocka <kika.zahonova@gmail.com>
2021-10-11 17:09:33 +0200

    bla

--- py_scripts/find_acc.py
+++ py_scripts/find_acc.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 Entrez.email = 'kika.zahonova@gmail.com'
 
-os.chdir('/Users/kika/ownCloud/archamoebae/beta-barrels/')
+os.chdir('/Users/kika/ownCloud/archamoebae/beta-barrel/')
 accessions = open('ena.acc')
 blast = open('enan.trinity.NTfilt.dmnd.out')
 out = open('ena.blast_hits.defline.tsv', 'w')
