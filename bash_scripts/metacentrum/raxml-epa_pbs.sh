4019d2d80bbc31853fccd1ac40f8966acb0c2447
kikinocka <kika.zahonova@gmail.com>
2021-11-04 12:28:01 +0100

    bla

--- bash_scripts/metacentrum/raxml-epa_pbs.sh
+++ bash_scripts/metacentrum/raxml-epa_pbs.sh
@@ -13,14 +13,14 @@ module add raxml-8.2.8
 data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonada/'
 
 #copy files to scratch
-cp $data'placement/ver2/metamonads_eukref_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'placement/ver2/metamonads_eukref_V9_above100.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
 cp $data'reference_tree/ver2/RAxML_bipartitions.metamonads_eukref3' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='metamonads_eukref_V9.trimal_gt-0.25_cons-50.aln'
+aln='metamonads_eukref_V9_above100.trimal_gt-0.25_cons-50.aln'
 tree='RAxML_bipartitions.metamonads_eukref3'
 out='EPARUN'
 
