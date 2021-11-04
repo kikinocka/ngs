e0114fc6114508129d455f5c8d18917993655e3f
kikinocka <kika.zahonova@gmail.com>
2021-11-04 15:30:21 +0100

    bla

--- bash_scripts/mafft_leshy.sh
+++ bash_scripts/mafft_leshy.sh
@@ -7,3 +7,5 @@ for f in *.fa ; do
 	log=${f%.fa}.mafft.log
 	mafft --thread 15 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
 done
+
+python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT done
