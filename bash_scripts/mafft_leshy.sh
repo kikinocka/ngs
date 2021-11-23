827dfd902ac004993f66f40ef70a6dbf19fa7492
kikinocka <kika.zahonova@gmail.com>
2021-11-23 12:32:41 +0100

    bla

--- bash_scripts/mafft_leshy.sh
+++ bash_scripts/mafft_leshy.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 
-cd '/mnt/mokosz/home/kika/archam_trees/'
+#align denovo
+# cd '/mnt/mokosz/home/kika/archam_trees/'
 
-for f in *.fa ; do
-	aln=${f%.fa}.mafft.aln
-	log=${f%.fa}.mafft.log
-	mafft --thread 5 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
-done
+# for f in *.fa ; do
+# 	aln=${f%.fa}.mafft.aln
+# 	log=${f%.fa}.mafft.log
+# 	mafft --thread 5 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
+# done
+
+# source ~/.profile
+# python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT done
+
+
+#merge alignments
+cd '/mnt/mokosz/home/kika/workdir/'
+
+maketable='/mnt/mokosz/home/kika/miniconda3/bin/makemergetable.rb'
+aln1='ciliophora_eukref.aln'
+aln2='V9_above99.mafft.aln'
+input='ciliates_V9.in'
+table='ciliates_V9.table'
+out='ciliates_V9_above99.mafft.aln'
+log='ciliates_V9_above99.mafft.log'
+
+cat $aln1 $aln2 > $input
+echo 'Alignments concatenated'
+ruby $maketable $aln1 $aln2 > $table
+echo 'Table prepared'
+mafft --thread 7 --localpair --maxiterate 1000 --merge $table $in > $out 2> $log
+echo 'Alignments merged'
 
 source ~/.profile
-python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT done
+python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT merge done
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -5,16 +5,16 @@ import subprocess
 mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
-#align de-novo
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/ciliates/')
-files = [x for x in os.listdir() if x.endswith('.fa')]
+# #align de-novo
+# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/ciliates/')
+# files = [x for x in os.listdir() if x.endswith('.fa')]
 
-for file in files:
-	print(file)
-	out = '{}.mafft.aln'.format(file.split('.fa')[0])
-	log = '{}.mafft.log'.format(file.split('.fa')[0])
-	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
-		mafft, file, out, log), shell=True)
+# for file in files:
+# 	print(file)
+# 	out = '{}.mafft.aln'.format(file.split('.fa')[0])
+# 	log = '{}.mafft.log'.format(file.split('.fa')[0])
+# 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
+# 		mafft, file, out, log), shell=True)
 
 # #add to aligned sequences
 # os.chdir('/Users/kika/ownCloud/')
@@ -26,19 +26,19 @@ for file in files:
 # # subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
 
 
-# #merge alignments
-# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/heterolobosea/join/')
-# aln1 = 'good.aln'
-# aln2 = 'rest.aln'
-# # aln3 = 'preaxostyla_eukref.aln'
-# # fasta = 'outgroup_nogaps.fa'
-# input = 'heterolob_v9.in'
-# table = 'heterolob_v9.table'
-# out = 'heterolob_v9.mafft.aln'
-# log = 'heterolob_v9.mafft.log'
-# subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
-# print('Alignments concatenated\n\n')
-# subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
-# print('Table prepared\n\n')
-# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-# print('Alignments merged')
+#merge alignments
+os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/ciliates/')
+aln1 = 'alns/ciliophora_eukref.aln'
+aln2 = 'V9_above99.mafft.aln'
+# aln3 = 'preaxostyla_eukref.aln'
+# fasta = 'outgroup_nogaps.fa'
+input = 'ciliates_v9.in'
+table = 'ciliates_v9.table'
+out = 'ciliates_V9_above99.mafft.aln'
+log = 'ciliates_V9_above99.mafft.log'
+subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
+print('Alignments concatenated\n\n')
+subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
+print('Table prepared\n\n')
+subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+print('Alignments merged')
