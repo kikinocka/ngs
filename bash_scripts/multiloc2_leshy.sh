d04708ecddcde09df690655d4155795ef1d15bb4
kikinocka <kika.zahonova@gmail.com>
2021-11-29 10:49:29 +0100

    bla

--- bash_scripts/multiloc2_leshy.sh
+++ bash_scripts/multiloc2_leshy.sh
@@ -12,10 +12,10 @@ for file in $files; do
 	echo $file
 	
 	out=${file%.*}'.multiloc_fungal.txt'
-	python $multiloc -fasta=$file -origin=$f -predictor=LowRes -result=$out -output=simple
+	python2 $multiloc -fasta=$file -origin=$f -predictor=LowRes -result=$out -output=simple
 	
-	out=${file%.*}'.multiloc_animal.txt'
-	python2 $multiloc -fasta=$file -origin=$a -predictor=LowRes -result=$out -output=simple
+	# out=${file%.*}'.multiloc_animal.txt'
+	# python2 $multiloc -fasta=$file -origin=$a -predictor=LowRes -result=$out -output=simple
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MultiLoc2 done
