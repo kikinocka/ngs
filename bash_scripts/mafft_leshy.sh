350559f44ff9e8363b303baaf010ef80425587f0
kikinocka <kika.zahonova@gmail.com>
2021-11-29 15:23:38 +0100

    bla

--- bash_scripts/mafft_leshy.sh
+++ bash_scripts/mafft_leshy.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-#align denovo
-cd '/mnt/mokosz/home/kika/workdir'
-
-for f in *.fa ; do
-	aln=${f%.fa}.mafft.aln
-	log=${f%.fa}.mafft.log
-	mafft --thread 10 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
-done
-
-source ~/.profile
-python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT done
-
-
-# #merge alignments
+# #align denovo
 # cd '/mnt/mokosz/home/kika/workdir'
 
-# aln1='Euglenozoa_alignment.aln'
-# aln2='V9_above99.mafft.aln'
-# # fasta='outgroup_nogaps.fa'
-# input='euglenozoa_outgroup.in'
-# table='euglenozoa_outgroup.table'
-# out='euglenozoa_outgroup.mafft.aln'
-# log='euglenozoa_outgroup.mafft.log'
+# for f in *.fa ; do
+# 	aln=${f%.fa}.mafft.aln
+# 	log=${f%.fa}.mafft.log
+# 	mafft --thread 10 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
+# done
 
-# cat $aln1 $aln2 > $input
-# # cat $aln1 $fasta > $input
-# echo 'Alignments concatenated'
-# makemergetable $aln1 $aln2 > $table
-# # makemergetable $aln1 > $table
-# echo 'Table prepared'
-# mafft --thread 10 --localpair --maxiterate 1000 --merge $table $input > $out 2> $log
-# echo 'Alignments merged'
+# source ~/.profile
+# python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT done
 
-# # source ~/.profile
-# python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT merge done
+
+#merge alignments
+cd '/mnt/mokosz/home/kika/workdir'
+
+aln1='Euglenozoa_alignment.aln'
+aln2='V9_above99.mafft.aln'
+# fasta='outgroup_nogaps.fa'
+input='euglenozoa_V9_above99.in'
+table='euglenozoa_V9_above99.table'
+out='euglenozoa_V9_above99.mafft.aln'
+log='euglenozoa_V9_above99.mafft.log'
+
+cat $aln1 $aln2 > $input
+# cat $aln1 $fasta > $input
+echo 'Alignments concatenated'
+makemergetable $aln1 $aln2 > $table
+# makemergetable $aln1 > $table
+echo 'Table prepared'
+mafft --thread 10 --localpair --maxiterate 1000 --merge $table $input > $out 2> $log
+echo 'Alignments merged'
+
+# source ~/.profile
+python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT merge done
