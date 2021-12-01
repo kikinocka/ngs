814737ff1198c5d66413ac34193b11c436c31353
kikinocka <kika.zahonova@gmail.com>
2021-12-01 14:33:56 +0100

    bla

--- bash_scripts/hmmsearch_leshy.sh
+++ bash_scripts/hmmsearch_leshy.sh
@@ -4,7 +4,7 @@ cd '/mnt/mokosz/home/kika/tom60/'
 
 db='/mnt/mokosz/home/zoli/DMND/EukProt_v2_renamed.faa'
 
-for profile in *.hmm_profile ; do:
+for profile in *.hmm_profile ; do
 	output=${profile%.hmm_profile}.hmm_search.out
 	threads=10
 	hmmsearch -o $output --cpu $threads $profile $db
