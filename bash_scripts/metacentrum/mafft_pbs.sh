558492a495b217cf7039e380855a98fdebaf05ec
kikinocka <kika.zahonova@gmail.com>
2021-12-02 11:40:06 +0100

    bla

--- bash_scripts/metacentrum/mafft_pbs.sh
+++ bash_scripts/metacentrum/mafft_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N mafft
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,11 +13,11 @@ cat $PBS_NODEFILE
 # module add mafft-7.453
 module add mafft-7.487 
 
-data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/euglenozoa/'
+data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/ciliates/'
 
 #copy files to scratch
-cp $data_dir'Euglenozoa_alignment.aln' $SCRATCHDIR
-cp $data_dir'outgroup_nogaps.fa' $SCRATCHDIR
+cp $data_dir'ciliates_V9_above99.mafft.aln' $SCRATCHDIR
+cp $data_dir'outgroup.mafft.aln' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
@@ -42,23 +42,25 @@ cd $SCRATCHDIR
 
 #merge alignments and fasta files
 maketable='/software/mafft/7.487/core/makemergetable.rb'
-aln1='Euglenozoa_alignment.aln'
-fasta='outgroup_nogaps.fa'
-input='euglenozoa_outgroup.in'
-table='euglenozoa_outgroup.table'
-out='euglenozoa_outgroup.mafft.aln'
-log='euglenozoa_outgroup.mafft.log'
+aln1='ciliates_V9_above99.mafft.aln'
+# fasta='outgroup.fa'
+aln2='outgroup.mafft.aln'
+input='ciliates_outgroup_V9_above99.in'
+table='ciliates_outgroup_V9_above99.table'
+out='ciliates_outgroup_V9_above99.mafft.aln'
+log='ciliates_outgroup_V9_above99.mafft.log'
 
-cat $aln1 $fasta > $input
+cat $aln1 $aln2 > $input
+# cat $aln1 $fasta > $input
 echo 'Alignments concatenated'
-ruby $maketable $aln1 > $table
+makemergetable $aln1 $aln2 > $table
+# makemergetable $aln1 > $table
 echo 'Table prepared'
-mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --merge $table $in > $out 2> $log
+mafft --thread 10 --localpair --maxiterate 1000 --merge $table $input > $out 2> $log
 echo 'Alignments merged'
-
 
 #copy files back
 # rm $fa
 # rm $existing $add
-rm $aln1 $fasta
+rm $aln1 $aln2
 cp * $data_dir
