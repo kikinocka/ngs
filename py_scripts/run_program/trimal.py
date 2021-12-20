c4d0cd80e7a89716e8a5afeac78883998ab9bfe7
kikinocka <kika.zahonova@gmail.com>
2021-12-20 09:22:09 +0100

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -10,14 +10,14 @@ for file in files:
 	print(file)
 	file_name = file.split('.')[0] #+ '_' + file.split('_')[1]
 	aut = 'automated1'
-	gt = 0.25 #fraction of sequences with a gap allowed
+	gt = 0.8 #fraction of sequences with a gap allowed
 	st = 0.001 #minimum average similarity allowed
 
 	# output = '{}.trimal_{}.aln'.format(file_name, aut)
 	# os.system('{} -in {} -out {} -{} -fasta'.format(trimal, file, output, aut))
 	
-	output = '{}.trimal_gt-{}_cons-50.aln'.format(file_name, gt)
-	os.system('{} -in {} -out {} -gt {} -cons 50 -fasta'.format(trimal, file, output, gt))
+	output = '{}.trimal_gt-{}.aln'.format(file_name, gt)
+	os.system('{} -in {} -out {} -gt {} -fasta'.format(trimal, file, output, gt))
 
 	# output = '{}.trimal_gt_{}_st_{}.aln'.format(file_name, gt, st)
 	# os.system('{} -in {} -out {} -gt {} -st {} -fasta'.format(trimal, file, output, gt, st))
