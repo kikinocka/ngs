82937f10f29c44e9a9d1112f5f62298ef712ecbe
kikinocka <kika.zahonova@gmail.com>
2021-12-22 12:02:33 +0100

    bla

---
+++ bash_scripts/metacentrum/raxml2_pbs.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#PBS -N raxml-tree
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l walltime=168:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add raxml-8.2.8
+
+data='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/placement/above99/only_tree/'
+
+#copy files to scratch
+cp $data'mlsb_metamonads.trimal_gt-0.8.aln' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+aln='mlsb_metamonads.trimal_gt-0.8.aln'
+out='mlsb_metamonads'
+
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T 1$PBS_NUM_PPN
+
+
+#copy files back
+rm $aln
+cp -R * $data
