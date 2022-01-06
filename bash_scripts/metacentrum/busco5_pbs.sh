89e3fa33e0176020397d6357ef9ac8f0aa58fbce
kikinocka <kika.zahonova@gmail.com>
2022-01-06 16:50:03 +0100

    bla

--- bash_scripts/metacentrum/busco5_pbs.sh
+++ bash_scripts/metacentrum/busco5_pbs.sh
@@ -15,29 +15,32 @@ conda activate busco
 # #available datasets
 # busco --list-datasets
 
-assembly_dir='/storage/brno3-cerit/home/kika/p57/predicted_proteins/'
+assembly_dir='/storage/brno3-cerit/home/kika/eupelagonemids/assemblies/'
 
 #copy files to scratch
-cp $assembly_dir'annotation_peptides.fasta' $SCRATCHDIR
+cp $assembly_dir'*.pep' $SCRATCHDIR
 
-assemblies='*.fasta'
+assemblies='*.pep'
 mode='proteins'
 lineage='protist_odb10'
+
 
 #compute on scratch
 cd $SCRATCHDIR
 
+mkdir BUSCO_summaries
+
 for fasta in $assemblies; do
 	echo $fasta
-	base=${fasta%.fasta}_$lineage
+	base=${fasta%.fasta}_pep_$lineage
 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
+	
+	cp $base'/shortshort_summary.specific.'$lineage$base'.txt' BUSCO_summaries
 done
 
-#copy files back
-cp -r * $assembly_dir
+generate_plot.py -wd BUSCO_summaries
 
-# #generate plot (in terminal)
-# cd $assembly_dir
-# mkdir BUSCO_summaries
-# cp $base'/shortshort_summary.specific.'$lineage$base'.txt' BUSCO_summaries
-# generate_plot.py -wd BUSCO_summaries
+
+#copy files back
+rm $assemblies
+cp -r * $assembly_dir
