95dea24d7c7cd9c761be7572d1af0af781984f85
kikinocka <kika.zahonova@gmail.com>
2022-01-17 11:22:27 +0100

    bla

--- bash_scripts/blast_leshy.sh
+++ bash_scripts/blast_leshy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 datadir='/mnt/mokosz/home/kika/workdir/'
-query=$datadir'v9.no_chimera.above99.fa'
-out=$datadir'v9.no_chimera.above99.blast.xml'
-db='/opt/databases/nt_auto/current/blast/nt'
-program=blastn
-task=blastn
+query=$datadir'pbei_aceE.fa'
+out=$datadir'pbei_aceE.blast.out'
+db='/opt/databases/eukprot/current/blast/eukprot'
+program=blastp
+task=blastp
 outfmt=5
 eval=1e-05
 max_seqs=1
@@ -19,8 +19,8 @@ $program -task $task \
 	-outfmt $outfmt \
 	-num_threads $cpu \
 	-evalue $eval \
-	-max_target_seqs $max_seqs \
-	-max_hsps $max_hsps
+	# -max_target_seqs $max_seqs \
+	# -max_hsps $max_hsps
 	# -outfmt "6 qseqid staxids bitscore sseqid qcovs pident" \
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py BLAST done
