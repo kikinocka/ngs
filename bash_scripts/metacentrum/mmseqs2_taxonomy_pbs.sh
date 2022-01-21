dd8da4f04358439f2a5988f6367470433327e2b5
kikinocka <kika.zahonova@gmail.com>
2022-01-21 15:35:25 +0100

    bla

--- bash_scripts/metacentrum/mmseqs2_taxonomy_pbs.sh
+++ bash_scripts/metacentrum/mmseqs2_taxonomy_pbs.sh
@@ -9,7 +9,7 @@ cat $PBS_NODEFILE
 
 mmseqs='/storage/brno3-cerit/home/kika/miniconda3/bin/mmseqs'
 db_dir='/storage/brno3-cerit/home/kika/databases/eukprotDB'
-data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
+data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P2S_1-01A_L001-ds.9f42a90caf694c0ab5686f0e22e79319/'
 
 #copy files to scratch
 cp $db_dir'/'* $SCRATCHDIR
