763c11a999416dec71065ccc704a2f1a2dc534cb
kikinocka <kika.zahonova@gmail.com>
2022-01-21 11:41:59 +0100

    bla

--- bash_scripts/metacentrum/metaeuk_pbs.sh
+++ bash_scripts/metacentrum/metaeuk_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N metaeuk
 #PBS -l select=1:ncpus=20:mem=500gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -9,7 +9,8 @@ cat $PBS_NODEFILE
 
 data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P2S_1-01A_L001-ds.9f42a90caf694c0ab5686f0e22e79319/'
 metaeuk='/storage/brno3-cerit/home/kika/miniconda3/bin/metaeuk'
-database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC'
+# database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC'
+database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC_profiles'
 
 #copy files to scratch
 cp $data_dir'5-tiara/eukarya.fasta' $SCRATCHDIR
@@ -25,4 +26,4 @@ $metaeuk easy-predict --threads $PBS_NUM_PPN $contigs $database $out $SCRATCHDIR
 
 #copy files back
 rm $contigs
-cp -r * $data_dir'6-metaeuk/database/'
+cp -r * $data_dir'6-metaeuk/profiles/'
