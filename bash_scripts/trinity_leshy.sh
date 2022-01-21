a7be959a3b4f385a5c9e48aab110ccb9d25ded4f
kikinocka <kika.zahonova@gmail.com>
2022-01-21 17:47:50 +0100

    bla

--- bash_scripts/trinity_leshy.sh
+++ bash_scripts/trinity_leshy.sh
@@ -2,21 +2,22 @@
 
 #CALCULATE IN TMP DIR!!!
 
-datadir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/'
+datadir='/mnt/mokosz/home/kika/mastigamoeba_abducta_CHOM1/'
 
 cp $datadir'reads/'*trimmed* /tmp/kika/
 
 
 cd '/tmp/kika/'
 
-fwd1='RV1_trimmed_1.fq.gz'
-rev1='RV1_trimmed_2.fq.gz'
-fwd2='RV2_trimmed_1.fq.gz'
-rev2='RV2_trimmed_2.fq.gz'
-fwd3='RV1-2_trimmed_1.fq.gz'
-rev3='RV1-2_trimmed_2.fq.gz'
+fwd1='CHOM1_trimmed_1.fq.gz'
+rev1='CHOM1_trimmed_2.fq.gz'
+# fwd2='RV2_trimmed_1.fq.gz'
+# rev2='RV2_trimmed_2.fq.gz'
+# fwd3='RV1-2_trimmed_1.fq.gz'
+# rev3='RV1-2_trimmed_2.fq.gz'
 
-Trinity --seqType fq --left $fwd1,$fwd2,$fwd3 --right $rev1,$rev2,$rev3 --max_memory 100G --CPU 15
+Trinity --seqType fq --left $fwd1 --right $rev1 --max_memory 100G --CPU 15
+# Trinity --seqType fq --left $fwd1,$fwd2,$fwd3 --right $rev1,$rev2,$rev3 --max_memory 100G --CPU 15
 
 rm *fq.gz
 cp -r * $datadir
