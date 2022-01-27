b2c74c960e69fa6a93affc3b87a2c3bc87fa53fc
kikinocka <kika.zahonova@gmail.com>
2022-01-27 15:10:34 +0100

    bla

--- bash_scripts/NR_filt.sh
+++ bash_scripts/NR_filt.sh
@@ -2,14 +2,15 @@
 
 filt_script='/mnt/mokosz/home/kika/scripts/py_scripts/NR_filt.py'
 
-datadir='/mnt/mokosz/home/kika/rhizomastix_vacuolata/filtration2/'
-diamond=$datadir'rvac_NR/rvac.trinity.NTfilt.dmnd.out'
-transcriptome=$datadir'rvac_trinity_NT/rvac_trinity.NTfilt.fasta'
-proteins=$datadir'rvac_trinity.NTfilt.fasta.transdecoder_dir/longest_orfs.pep'
+datadir='/mnt/mokosz/home/kika/endolimax_nana/filtration2/'
+diamond=$datadir'enan_NR/enan.trinity.NTfilt.dmnd.out'
+transcriptome=$datadir'enan_trinity_NT/enan_trinity.NTfilt.fasta'
+proteins=$datadir'enan_trinity_NT/enan_trinity.NTfilt.fasta.transdecoder_dir//longest_orfs.pep'
 coverage='50' #default: 50
 identity='70' #default: 75
 taxon='Amoebozoa'
 
+cd '/mnt/mokosz/home/kika/endolimax_nana/filtration_20220127/'
 $filt_script -i $diamond -d $datadir -nt $transcriptome -aa $proteins -q $coverage -p $identity -g $taxon
 
 # -i: --infile
