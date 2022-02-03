63bb6d0a00ff267b5327356167a048db7892d12b
kikinocka <kika.zahonova@gmail.com>
2022-02-03 17:16:54 +0100

    bla

--- bash_scripts/metacentrum/bbduk_pbs.sh
+++ bash_scripts/metacentrum/bbduk_pbs.sh
@@ -35,7 +35,7 @@ bbduk.sh overwrite=true \
 	in1=$fw in2=$rv \
 	out1=$trimmed_fw out2=$trimmed_rv \
 	ref=$adapt \
-	usejni=t qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
+	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
 
 # #454 reads
 # bbduk.sh in=$fw out=$trimmed_fw ref=$adapt k=23 ktrim=r mink=11 edist=1 qtrim=rl trimq=20 t=$PBS_NUM_PPN 2> $report
