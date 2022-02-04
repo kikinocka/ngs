57a6fa0afd4ddf37a537148eac93158d934ebc44
kikinocka <kika.zahonova@gmail.com>
2022-02-04 10:20:21 +0100

    bla

---
+++ bash_scripts/diamond_for_filtering_leshy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd '/mnt/mokosz/home/kika/mastigella_eilhardi_MAST/'
+
+task=blastp
+query='Trinity_Mastigella_150316_renamed_nucl.fasta.transdecoder_dir/longest_orfs.pep'
+out='mei.trinity.NTfilt.dmnd.out'
+db='/opt/databases/nr_auto/2021-02-15/diamond/nr.dmnd'
+taxify='/mnt/mokosz/home/kika/scripts/py_scripts/taxify_DMND_nr_gz.py'
+eval=1e-5
+max_seqs=1
+cpu=10
+
+diamond $task \
+	-q $query \
+	-d $db \
+	-o $out \
+	--outfmt 6 qseqid bitscore sseqid qcovhsp pident qlen length \
+	--threads $cpu \
+	--evalue $eval \
+	--max-target-seqs $max_seqs \
+	--sensitive
+
+python2 $taxify -i $out 
+
+python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py DMND done
