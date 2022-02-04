f94efe2134c876ef4df495cb3f92074cdbc80004
kikinocka <kika.zahonova@gmail.com>
2022-02-04 15:16:12 +0100

    bla

---
+++ bash_scripts/metacentrum/aragorn_pbs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -N aragorn
+#PBS -l select=1:ncpus=1:mem=1gb:scratch_local=5gb
+#PBS -l walltime=02:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+aragorn='/storage/brno3-cerit/home/kika/miniconda3/pkgs/aragorn-1.2.38-h779adbc_4/bin/aragorn'
+data_dir='/storage/brno3-cerit/home/kika/tRNAs-kinetoplastids/'
+
+#copy files to scratch
+cp $data_dir'TriTrypDB-55_TbruceiLister427_Genome.fasta' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+genome='TriTrypDB-55_TbruceiLister427_Genome.fasta'
+out='TriTrypDB-55_TbruceiLister427_Genome.aragorn.fa'
+
+aragorn -t -fo -o $out $genome
+
+
+#copy files back
+rm $genome
+cp * $data_dir
--- bash_scripts/tRNAscan.sh
+++ bash_scripts/tRNAscan.sh
@@ -2,12 +2,20 @@
 
 tRNAscan='/Users/kika/miniconda3/bin/tRNAscan-SE'
 
-cd /Users/kika/ownCloud/Rhabdomonas_costata/mitochondrion/
+cd '/Users/kika/ownCloud/tRNAs-kinetoplastids/'
 
-for assembly in atp6_contigs.fa ; do
-	echo $assembly
-	table=${assembly%.fasta}.tRNAscan_table.out
-	seq=${assembly%.fasta}.tRNAscan.fa
-	structures=${assembly%.fasta}.tRNAscan_structures.out
-	$tRNAscan --thread 5 -o $table -a $seq -f $structures ${assembly}
-done
+# for assembly in atp6_contigs.fa ; do
+# 	echo $assembly
+# 	table=${assembly%.fasta}.tRNAscan_table.out
+# 	seq=${assembly%.fasta}.tRNAscan.fa
+# 	structures=${assembly%.fasta}.tRNAscan_structures.out
+# 	$tRNAscan --thread 5 -o $table -a $seq -f $structures ${assembly}
+# done
+
+
+assembly='TriTrypDB-55_TbruceiLister427_Genome.fasta'
+table=${assembly%.fasta}.tRNAscan_table.out
+seq=${assembly%.fasta}.tRNAscan.fa
+structures=${assembly%.fasta}.tRNAscan_structures.out
+
+$tRNAscan --thread 5 -o $table -a $seq -f $structures ${assembly}
