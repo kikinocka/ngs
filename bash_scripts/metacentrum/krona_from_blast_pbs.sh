da5b4ef8a56cf4b9169e4944366253f0f9277133
kikinocka <kika.zahonova@gmail.com>
2022-02-07 10:46:17 +0100

    bla

--- bash_scripts/metacentrum/krona_from_blast_pbs.sh
+++ bash_scripts/metacentrum/krona_from_blast_pbs.sh
@@ -9,7 +9,7 @@ cat $PBS_NODEFILE
 
 module add krona-2.8.1
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/20200821_BML-P3B/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/8-blast-krona/'
 taxonomy='/storage/brno3-cerit/home/kika/databases/krona/'
 
 #copy files to scratch
@@ -20,7 +20,7 @@ cp $datadir'euk_metaeuk.blast_upd.out' $SCRATCHDIR
 cd $SCRATCHDIR
 
 blast='euk_metaeuk.blast_upd.out'
-krona='euk_metaeuk.blast_upd.krona.html'
+krona='euk_metaeuk.blast_upd.krona-2.8.1.html'
 
 ktImportBLAST $blast -o $krona -tax $taxonomy
 
