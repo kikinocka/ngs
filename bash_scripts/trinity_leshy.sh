8b28b2ffcec00d62bde40d7a48f2bcf8348ddea6
kikinocka <kika.zahonova@gmail.com>
2022-02-14 11:37:39 +0100

    bla

--- bash_scripts/trinity_leshy.sh
+++ bash_scripts/trinity_leshy.sh
@@ -16,7 +16,7 @@ rev1='CHOM1_trimmed_2.fq.gz'
 # fwd3='RV1-2_trimmed_1.fq.gz'
 # rev3='RV1-2_trimmed_2.fq.gz'
 
-Trinity --seqType fq --left $fwd1 --right $rev1 --max_memory 100G --CPU 15
+Trinity --seqType fq --left $fwd1 --right $rev1 --max_memory 100G --CPU 20
 # Trinity --seqType fq --left $fwd1,$fwd2,$fwd3 --right $rev1,$rev2,$rev3 --max_memory 100G --CPU 15
 
 rm *fq.gz
