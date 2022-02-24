52d7e524fec8cf19bf73feedc535b5ea37f1d3fc
kikinocka <kika.zahonova@gmail.com>
2022-02-24 20:21:49 +0100

    bla

--- bash_scripts/metacentrum/bowtie2_pbs.sh
+++ bash_scripts/metacentrum/bowtie2_pbs.sh
@@ -11,56 +11,56 @@ cat $PBS_NODEFILE
 module add bowtie2-2.4.2
 module add samtools-1.11
 
-assembly_dir='/storage/brno3-cerit/home/kika/tRNAs-kinetoplastids/'
+assembly_dir='/storage/brno3-cerit/home/kika/p57/jaculum/'
 read_dir=$assembly_dir'reads/'
-mapping_dir=$assembly_dir'bw2/KP/'
+mapping_dir=$assembly_dir'bw2/'
 
 #copy files to scratch
-cp $assembly_dir'TriTrypDB-55_TbruceiLister427_Genome.fasta' $SCRATCHDIR
-cp $read_dir'T-brucei-cyto_trimmed.fq.gz' $SCRATCHDIR
-# cp $read_dir'P1-7_trimmed_2.fq.gz' $SCRATCHDIR
+cp $assembly_dir'modryi_scaffolds_transc.fasta' $SCRATCHDIR
+cp $read_dir'jac_trimmed_1.fq.gz' $SCRATCHDIR
+cp $read_dir'jac_trimmed_2.fq.gz' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-base_name='Tbruc427_DNA.bw2'
-ref='TriTrypDB-55_TbruceiLister427_Genome.fasta'
-p1_1='T-brucei-cyto_trimmed.fq.gz'
-# p1_2='P1-7_trimmed_2.fq.gz'
+base_name='jac.bw2'
+ref='modryi_scaffolds_transc.fasta'
+p1_1='jac_trimmed_1.fq.gz'
+p1_2='jac_trimmed_2.fq.gz'
 # r1='EU2_r1_unpaired_1.fq.gz'
 # r2='EU2_r2_unpaired_2.fq.gz'
 
 samfile=$base_name'.sam'
 mapped=$base_name'_mapped.fq.gz'
-# unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
-# unmapped_paired=$base_name'_unmapped_paired.fq.gz'
-unaligned=$base_name'_unaligned.fq.gz'
+unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
+unmapped_paired=$base_name'_unmapped_paired.fq.gz'
+# unaligned=$base_name'_unaligned.fq.gz'
 report=$base_name'.report.txt'
 bamfile=$base_name'_unsorted.bam'
 sorted=$base_name'_sorted.bam'
 
 bowtie2-build --threads $PBS_NUM_PPN $ref $base_name
 
-# #paired-end reads
-# bowtie2 --very-sensitive -p $PBS_NUM_PPN \
-# 	-x $base_name \
-# 	-1 $p1_1 \
-# 	-2 $p1_2 \
-# 	--un-gz $unmapped_unpaired \
-# 	--un-conc-gz $unmapped_paired \
-# 	--al-conc-gz $mapped \
-# 	-S $samfile 2> $report
-# 	# -U $r1,$r2 \
-# 	#--no-unal \ #writes only mapped reads to sam file
-
-#single reads
+#paired-end reads
 bowtie2 --very-sensitive -p $PBS_NUM_PPN \
 	-x $base_name \
-	-U $p1_1 \
-	--al-gz $mapped \
-	--un-gz $unaligned \
+	-1 $p1_1 \
+	-2 $p1_2 \
+	--un-gz $unmapped_unpaired \
+	--un-conc-gz $unmapped_paired \
+	--al-conc-gz $mapped \
 	-S $samfile 2> $report
+	# -U $r1,$r2 \
+	#--no-unal \ #writes only mapped reads to sam file
+
+# #single reads
+# bowtie2 --very-sensitive -p $PBS_NUM_PPN \
+# 	-x $base_name \
+# 	-U $p1_1 \
+# 	--al-gz $mapped \
+# 	--un-gz $unaligned \
+# 	-S $samfile 2> $report
 
 # samtools view -bS -F 4 $samfile > $bamfile -@ $PBS_NUM_PPN #writes only mapped reads to bamfile
 samtools view -bS -@ $PBS_NUM_PPN $samfile > $bamfile
