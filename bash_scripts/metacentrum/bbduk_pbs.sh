9c9a414f44dc7b7efac8f0808f683faae879c000
kikinocka <kika.zahonova@gmail.com>
2022-02-24 18:08:45 +0100

    bla

--- bash_scripts/metacentrum/bbduk_pbs.sh
+++ bash_scripts/metacentrum/bbduk_pbs.sh
@@ -10,47 +10,47 @@ cat $PBS_NODEFILE
 module add bbmap-36.92
 
 
-raw_dir='/storage/brno3-cerit/home/kika/tRNAs-kinetoplastids/'
+raw_dir='/storage/brno3-cerit/home/kika/p57/jaculum/reads/'
 # trim_dir='/storage/brno3-cerit/home/kika/archamoebae/rhizomastix_libera/trimmed_reads/'
 
 #copy data to scratch
 cp '/storage/brno2/home/kika/tools/bbmap/resources/adapters.fa' $SCRATCHDIR
-cp $raw_dir'2-T-brucei-cyto.fastq.gz' $SCRATCHDIR
-# cp $raw_dir'June28-16-P1-7_S5_L001_R2_001.fastq.gz' $SCRATCHDIR
+cp $raw_dir'1_1.fastq.gz' $SCRATCHDIR
+cp $raw_dir'1_2.fastq.gz' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 adapt='adapters.fa'
-name='T-brucei-cyto'
-# fw='June28-16-P1-7_S5_L001_R1_001.fastq.gz'
-# rv='June28-16-P1-7_S5_L001_R2_001.fastq.gz'
-single='2-T-brucei-cyto.fastq.gz'
-# trimmed_fw=$name'_trimmed_1.fq.gz'
-# trimmed_rv=$name'_trimmed_2.fq.gz'
-trimmed='T-brucei-cyto_trimmed-AN.fq.gz'
-report=$name'_bbduk_report-AN.txt'
+name='jac'
+fw='1_1.fastq.gz'
+rv='1_2.fastq.gz'
+# single='2-T-brucei-cyto.fastq.gz'
+trimmed_fw=$name'_trimmed_1.fq.gz'
+trimmed_rv=$name'_trimmed_2.fq.gz'
+# trimmed='T-brucei-cyto_trimmed-AN.fq.gz'
+report=$name'_bbduk_report.txt'
 
-# #illumina pair-end reads
-# bbduk.sh overwrite=true \
-# 	in1=$fw in2=$rv \
-# 	out1=$trimmed_fw out2=$trimmed_rv \
-# 	ref=$adapt \
-# 	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
-
-#illumina single reads
+#illumina pair-end reads
 bbduk.sh overwrite=true \
-	in=$single \
-	out=$trimmed \
+	in1=$fw in2=$rv \
+	out1=$trimmed_fw out2=$trimmed_rv \
 	ref=$adapt \
-	ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN qtrim=rl trimq=20 2> $report
-	# qtrim=rl trimq=20 ktrim=r t=$PBS_NUM_PPN 2> $report
+	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
+
+# #illumina single reads
+# bbduk.sh overwrite=true \
+# 	in=$single \
+# 	out=$trimmed \
+# 	ref=$adapt \
+# 	ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN qtrim=rl trimq=20 2> $report
+# 	# qtrim=rl trimq=20 ktrim=r t=$PBS_NUM_PPN 2> $report
 
 # #454 reads
 # bbduk.sh in=$fw out=$trimmed_fw ref=$adapt k=23 ktrim=r mink=11 edist=1 qtrim=rl trimq=20 t=$PBS_NUM_PPN 2> $report
 
 #copy files back
-# rm $fw $rv $adapt
-rm $single $adapt
+rm $fw $rv $adapt
+# rm $single $adapt
 cp -r * $raw_dir
