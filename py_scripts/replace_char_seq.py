255551610715ed1f71b2fe3d8ccd0c8ecfa32df1
kikinocka <kika.zahonova@gmail.com>
2022-02-25 14:57:45 +0100

    bla

--- py_scripts/replace_char_seq.py
+++ py_scripts/replace_char_seq.py
@@ -6,6 +6,6 @@ os.chdir('/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/decontamina
 files = [x for x in os.listdir() if x.endswith('.fasta')]
 
 for file in files:
-	with open('{}_updated.fa'.format(file.split('.fasta')[0])) as out:
+	with open('{}_updated.fa'.format(file.split('.fasta')[0]), 'w') as out:
 		for seq in SeqIO.parse(file, 'fasta'):
 			out.write('>{}\n{}\n'.format(seq.description, (seq.seq).replace('.', '-').replace(' ', '')))
