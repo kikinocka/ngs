d960a1ad6a51ce0b80dfabffdbcc562c39726f7a
kikinocka <kika.zahonova@gmail.com>
2022-03-04 10:11:53 +0100

    bla

--- bash_scripts/metacentrum/blast_pbs.sh
+++ bash_scripts/metacentrum/blast_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N blast
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=3gb
-#PBS -l walltime=336:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,21 +10,20 @@ cat $PBS_NODEFILE
 #add module
 module add blast-plus/blast-plus-2.12.0-gcc-8.3.0-ohlv7t4
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/20200821_BML-P3B/'
+datadir='/storage/brno3-cerit/home/kika/trafficking/trappC13/'
 
 #copy files to scratch
-cp $datadir'8-metaeuk/profiles/euk_metaeuk.fas' $SCRATCHDIR
+cp $datadir'eukprot_trappc13.hmm_hits.fa' $SCRATCHDIR
 
 
 #run on scratch
 cd $SCRATCHDIR
 
-query='euk_metaeuk.fas'
-out='euk_metaeuk.blast.out'
+query='eukprot_trappc13.hmm_hits.fa'
+out='eukprot_trappc13.hmm_hits.rev_blast.xml'
 db='/storage/projects/BlastDB/nr'
 program=blastp
 task=blastp
-# outfmt=5
 eval=1e-05
 max_seqs=1
 max_hsps=1
@@ -33,12 +32,13 @@ $program -task $task \
 	-query $query \
 	-db $db \
 	-out $out \
-	-outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore ppos" \
+	-outfmt 5 \
 	-num_threads $PBS_NUM_PPN \
 	-evalue $eval \
 	-max_target_seqs $max_seqs \
 	-max_hsps $max_hsps
 	# -outfmt "6 qseqid staxids bitscore sseqid qcovs pident" \
+	# -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore ppos" \
 
 #copy files back
 rm $query
