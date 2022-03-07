f1803a927406bb9fc28049dcf1de743a459767e8
kikinocka <kika.zahonova@gmail.com>
2022-03-07 11:02:26 +0100

    bla

--- py_scripts/databases/ncbi_search.py
+++ py_scripts/databases/ncbi_search.py
@@ -8,8 +8,8 @@ from Bio import SeqIO
 
 Entrez.email = 'kika.zahonova@gmail.com'
 
-os.chdir('/Users/kika/ownCloud/oil_sands/metagenomes/20210222_BML-P3S/8-blast-krona/')
-acc = open('euk_metaeuk.blast.acc')
+os.chdir('/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/decontaminated/stramenopiles/')
+acc = open('str.acc')
 
 ids = []
 for line in acc:
@@ -27,29 +27,35 @@ for line in acc:
 # 		out.write('>{} {}\n{}\n'.format(prot_id[:-1], prot_record.description, prot_record.seq))
 
 
-with open('euk_metaeuk.blast.gi', 'w') as out:
+with open('str.lineage', 'w') as out, open('str.errors', 'w') as errors:
 	for prot_id in ids:
-		print(prot_id)
-
 		#get lineage based on accessions
-		# prot = Entrez.efetch(db='protein', id=prot_id, rettype='gb', retmode='text')
-		# prot_record = SeqIO.read(prot, 'genbank')
-		# print(prot_record)
-		# tax = prot_record.annotations['taxonomy']
-		# tax = prot_record.annotations['taxonomy'][::-1]
-		# tax = str(tax).replace('\'', '').replace('[', '').replace(']', '')#.replace(', ', '_')
-		# orgn = prot_record.annotations['organism']
-		# out.write('{}\t{}\t{}\n'.format(prot_id, orgn, tax))
-		# print(tax[-1:])
-		# out.write('{}\t{}\n'.format(prot_id, tax[-1:]))
-		# print(orgn)
-		# print(tax)
+		try:
+			print(prot_id)
+			prot = Entrez.efetch(db='nucleotide', id=prot_id, rettype='gb', retmode='text')
+			prot_record = SeqIO.read(prot, 'genbank')
+			# print(prot_record)
+			# tax = prot_record.annotations['taxonomy']
+			tax = prot_record.annotations['taxonomy'][1:-1]
+			tax = str(tax).replace('\'', '').replace('[', '').replace(']', '').replace(', ', '_')
+			orgn = prot_record.annotations['organism']
+			orgn = str(orgn).replace(' ', '_')
+			full = '{}_{}'.format(tax, orgn)
+			# print(orgn)
+			# print(tax)
+			# print(tax[-1:])
+			# print(full)
+			# out.write('{}\t{}\t{}\n'.format(prot_id, orgn, tax))
+			# out.write('{}\t{}\n'.format(prot_id, tax[-1:]))
+			out.write('{}\t{}\n'.format(prot_id, full))
+		except:
+			errors.write('{}\n'.format(prot_id))
 
-		#get GI numbers based on accessions
-		prot = Entrez.efetch(db='protein', id=prot_id, retmode='xml')
-		prot_record = Entrez.read(prot)
-		if prot_record[0]['GBSeq_other-seqids'][-1].startswith('gi'):
-			gi = prot_record[0]['GBSeq_other-seqids'][-1].split('|')[1]
-			out.write('{}\t{}\n'.format(prot_id, gi))
-		else:
-			out.write('{}\t-\n'.format(prot_id))
+		# #get GI numbers based on accessions
+		# prot = Entrez.efetch(db='protein', id=prot_id, retmode='xml')
+		# prot_record = Entrez.read(prot)
+		# if prot_record[0]['GBSeq_other-seqids'][-1].startswith('gi'):
+		# 	gi = prot_record[0]['GBSeq_other-seqids'][-1].split('|')[1]
+		# 	out.write('{}\t{}\n'.format(prot_id, gi))
+		# else:
+		# 	out.write('{}\t-\n'.format(prot_id))
