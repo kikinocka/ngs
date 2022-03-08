b9f0a6fe0c1e47722b87e4ef7d9a4aed50679abb
kikinocka <kika.zahonova@gmail.com>
2022-03-08 16:26:05 +0100

    bla

--- bash_scripts/metacentrum/metabinner_coverage_pbs.sh
+++ bash_scripts/metacentrum/metabinner_coverage_pbs.sh
@@ -13,11 +13,11 @@ module add conda-modules-py37
 
 cov_script='/storage/brno2/home/kika/.conda/envs/metabinner_env/bin/scripts/gen_coverage_file.sh'
 data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/20200821_BML-P3B/'
-assembly_dir=$data_dir'2a-spades_default/'
+assembly_dir=$data_dir'metabinner/'
 read_dir=$data_dir'1-reads/'
 
 #copy files to scratch
-cp $assembly_dir'scaffolds.fasta' $SCRATCHDIR
+cp $assembly_dir'scaffolds_len500.fa' $SCRATCHDIR
 cp $read_dir'BML_trimmed_1.fastq.gz' $SCRATCHDIR
 cp $read_dir'BML_trimmed_2.fastq.gz' $SCRATCHDIR
 
@@ -29,7 +29,7 @@ conda activate /storage/brno2/home/kika/.conda/envs/metabinner_env
 
 gzip -d *.gz
 
-assembly='scaffolds.fasta'
+assembly='scaffolds_len500.fa'
 fwd='BML_trimmed_1.fastq'
 rev='BML_trimmed_2.fastq'
 min_len=500
