ac56a875d11e11fb3fe4fb67c8db82c1a289357e
kikinocka <kika.zahonova@gmail.com>
2022-03-17 15:16:45 +0100

    bla

--- bash_scripts/metacentrum/orthofinder_pbs.sh
+++ bash_scripts/metacentrum/orthofinder_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N orthofinder
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=5gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,22 +10,22 @@ cat $PBS_NODEFILE
 #add module
 module add orthofinder-2.0.0
 
-data='/storage/brno3-cerit/home/kika/anaeramoeba/tbcs'
+data='/storage/brno3-cerit/home/kika/archamoebae/'
 
 #copy files to scratch
-cp $data'/'*.fa $SCRATCHDIR
-cp $data'/refs/'*.fa $SCRATCHDIR
+cp $data'prot_assemblies_filtration-20220127/'*.fa $SCRATCHDIR
+cp $data'refs/'*.faa $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
-files='*.fa'
-for file in $files; do
+
+for file in *.faa; do
 	echo $file
 done
 
 orthofinder -f $SCRATCHDIR
 
 #copy files back
-rm *.fa
+rm *.faa
 cp -R * $data
--- py_scripts/bar_plot_several.py
+++ py_scripts/bar_plot_several.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 os.chdir('/Users/kika/ownCloud/diplonema/pyruvate_metabolism/PDH/')
-df = pd.read_csv('pdh_frac.tsv', sep='\t')
+df = pd.read_csv('e1_frac.tsv', sep='\t')
 
 proteins = list(df.iloc[:, 0])
 mito = list(df.iloc[:, 1])
@@ -20,11 +20,12 @@ colors = [['#E55500', '#DD8558', '#BCBEC0'],
 		['#F2E500', '#D1C74E', '#BCBEC0']]
 
 x_axis = np.arange(len(proteins))
-plt.bar(x_axis - 0.2, mito, 0.2, yerr=mito_std, capsize=3, label='mito', color=colors[2][0])
-plt.bar(x_axis - 0.0, cyto, 0.2, yerr=cyto_std, capsize=3, label='cyto', color=colors[2][1])
-plt.bar(x_axis + 0.2, cell, 0.2, yerr=cell_std, capsize=3, label='cell', color=colors[2][2])
-plt.xticks(x_axis, proteins, rotation=10)
+plt.bar(x_axis - 0.2, mito, 0.2, yerr=mito_std, capsize=3, label='mito', color=colors[0][0])
+plt.bar(x_axis - 0.0, cyto, 0.2, yerr=cyto_std, capsize=3, label='cyto', color=colors[0][1])
+plt.bar(x_axis + 0.2, cell, 0.2, yerr=cell_std, capsize=3, label='cell', color=colors[0][2])
+plt.xticks(x_axis, proteins)
+plt.ylim([0,21000000])
 plt.legend(frameon=False)
-plt.title('PDH')
-plt.show()
-# plt.savefig('bckdh_frac.pdf', dpi=300)
+plt.title('E1')
+# plt.show()
+plt.savefig('e1_frac.pdf', dpi=300)
