7c364eac69fa84903d1fb0546c2dae6e384a8acc
kikinocka <kika.zahonova@gmail.com>
2022-03-21 11:23:26 +0100

    bla

--- bash_scripts/metacentrum/mafft_pbs.sh
+++ bash_scripts/metacentrum/mafft_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N mafft
-#PBS -l select=1:ncpus=30:mem=100gb:scratch_local=1gb
+#PBS -l select=1:ncpus=20:mem=80gb:scratch_local=1gb
 #PBS -l walltime=168:00:00
 #PBS -m ae
 #PBS -j oe
