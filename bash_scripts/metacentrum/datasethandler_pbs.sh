83b2a62ffe244803b86eac23671ed7e6ce9b0108
kikinocka <kika.zahonova@gmail.com>
2022-03-21 11:20:58 +0100

    bla

--- bash_scripts/metacentrum/datasethandler_pbs.sh
+++ bash_scripts/metacentrum/datasethandler_pbs.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #PBS -N datasethandler
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -14,7 +14,7 @@ module add mafft-7.453
 module add trimal-1.4
 
 #copy files to scratch
-DATADIR='/storage/brno3-cerit/home/kika/trafficking/COPII/ver2'
+DATADIR='/storage/brno3-cerit/home/kika/trafficking/COPII'
 
 cp '/storage/brno2/home/kika/scripts/py_scripts/datasethandler-server.py' $SCRATCHDIR
 cp $DATADIR'/'*.fa $SCRATCHDIR
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,8 +6,8 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99_decontaminated/stramenopiles/')
-files = [x for x in os.listdir() if x.endswith('v9.fa')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/COPII/ver2/')
+files = [x for x in os.listdir() if x.endswith('sec16.fa')]
 
 for file in files:
 	print(file)
