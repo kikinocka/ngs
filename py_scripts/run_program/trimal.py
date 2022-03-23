8f6b3fe07b625e2142043ef2c1ad475a11e98e61
kikinocka <kika.zahonova@gmail.com>
2022-03-23 15:09:35 +0100

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/ArfGAPs//')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/ArfGAPs/ver2/')
 files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 
 for file in files:
