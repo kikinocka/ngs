330c28fa38a1797d5f274bf0184c59752b3cedba
kikinocka <kika.zahonova@gmail.com>
2022-03-28 17:23:14 +0200

    bla

--- bash_scripts/metacentrum/iqtree_diplo_pbs.sh
+++ bash_scripts/metacentrum/iqtree_diplo_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/trafficking/ArfGAPs/ver2'
+data_dir='/storage/brno3-cerit/home/kika/trafficking/ArfGAPs/ag-smap'
 
 #copy files to scratch
 cp $data_dir'/'*.aln $SCRATCHDIR
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -18,32 +18,32 @@ maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 # 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
 # 		mafft, file, out, log), shell=True)
 
-# #add to aligned sequences
-# os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
-# existing = 'BLASTs/tca17_cdd.aln'
-# add = 'BLASTs/tca17.fa'
-# out = 'BLASTs/tca17_eug-cdd.mafft.aln'
-# log = 'BLASTs/tca17_eug-cdd.mafft.log'
-# # subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
+#add to aligned sequences
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
+existing = 'queries/ArfGAPs/Shweta_alns/Supplementary_File_2.aln'
+add = 'trees/ArfGAPs/ag-smap/euglenozoans.fa'
+out = 'trees/ArfGAPs/ag-smap/ag-smap_mask.mafft.aln'
+log = 'trees/ArfGAPs/ag-smap/ag-smap_mask.mafft.log'
+subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
 # subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
 
 
-#merge alignments
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99_decontaminated/stramenopiles/')
-aln1 = 'STR_5480seqs_230711_core_blast_min700bp.No_Chimera.align_V5.lineage.aln'
-aln2 = 'v9.mafft.aln'
-# aln3 = 'preaxostyla_eukref.aln'
-# fasta = 'otu.fa'
-input = 'placement/stramenopiles_v9.in'
-table = 'placement/stramenopiles_v9.table'
-out = 'placement/stramenopiles_v9.mafft.aln'
-log = 'placement/stramenopiles_v9.mafft.log'
-# subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
-subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
-print('Alignments concatenated\n\n')
-# subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
-subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
-print('Table prepared\n\n')
-# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-print('Alignments merged')
+# #merge alignments
+# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99_decontaminated/stramenopiles/')
+# aln1 = 'STR_5480seqs_230711_core_blast_min700bp.No_Chimera.align_V5.lineage.aln'
+# aln2 = 'v9.mafft.aln'
+# # aln3 = 'preaxostyla_eukref.aln'
+# # fasta = 'otu.fa'
+# input = 'placement/stramenopiles_v9.in'
+# table = 'placement/stramenopiles_v9.table'
+# out = 'placement/stramenopiles_v9.mafft.aln'
+# log = 'placement/stramenopiles_v9.mafft.log'
+# # subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
+# subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
+# print('Alignments concatenated\n\n')
+# # subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
+# subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
+# print('Table prepared\n\n')
+# # subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# print('Alignments merged')
