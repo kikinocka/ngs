fc73b80a7dfee132bc4dd280c342a091e3bd9b7e
kikinocka <kika.zahonova@gmail.com>
2022-03-28 11:19:52 +0200

    bla

--- bash_scripts/metacentrum/mafft_pbs.sh
+++ bash_scripts/metacentrum/mafft_pbs.sh
@@ -14,7 +14,7 @@ module add mafft-7.487
 data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/decontaminated/stramenopiles/placement'
 
 #copy files to scratch
-cp $data_dir'/'*.aln $SCRATCHDIR
+cp $data_dir'/'* $SCRATCHDIR
 # cp $data_dir'outgroup.mafft.aln' $SCRATCHDIR
 # cp $data_dir'ciliates_outgroup_V9_above99.table' $SCRATCHDIR
 # cp $data_dir'ciliates_outgroup_V9_above99.in' $SCRATCHDIR
@@ -32,38 +32,38 @@ cd $SCRATCHDIR
 # 	# mafft --auto --inputorder ${file} > ${aln} 2> ${log}
 # done
 
-# #add to aligned sequences
-# existing='discobids_eukref.mafft_merge.aln'
-# add='discobids_otus.fa'
-# aln='discoba_V9.mafft.aln'
-# log='discoba_V9.mafft.log'
+#add to aligned sequences
+existing='STR_5480seqs_230711_core_blast_min700bp.No_Chimera.align_V5.lineage.aln'
+add='v9.fa'
+aln='stramenopiles_V9.mafft.aln'
+log='stramenopiles_V9.mafft.log'
 
-# # mafft --add $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
-# mafft --addfragments $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
+# mafft --add $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
+mafft --addfragments $add --thread $PBS_NUM_PPN --inputorder $existing > $aln 2> $log
 
 
-#merge alignments and fasta files
-maketable='/software/mafft/7.487/core/makemergetable.rb'
-aln1='STR_5480seqs_230711_core_blast_min700bp.No_Chimera.align_V5.lineage.aln'
-# fasta='outgroup.fa'
-aln2='v9.mafft.aln'
-input='stramenopiles_V9_above99.in'
-table='stramenopiles_V9_above99.table'
-out='stramenopiles_V9_above99.mafft.aln'
-log='stramenopiles_V9_above99.mafft.log'
+# #merge alignments and fasta files
+# maketable='/software/mafft/7.487/core/makemergetable.rb'
+# aln1='STR_5480seqs_230711_core_blast_min700bp.No_Chimera.align_V5.lineage.aln'
+# # fasta='outgroup.fa'
+# aln2='v9.mafft.aln'
+# input='stramenopiles_V9_above99.in'
+# table='stramenopiles_V9_above99.table'
+# out='stramenopiles_V9_above99.mafft.aln'
+# log='stramenopiles_V9_above99.mafft.log'
 
-cat $aln1 $aln2 > $input
-# cat $aln1 $fasta > $input
-echo 'Alignments concatenated'
-ruby $makemergetable $aln1 $aln2 > $table
-# ruby $makemergetable $aln1 > $table
-echo 'Table prepared'
-# mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --merge $table $input > $out 2> $log
-mafft --thread $PBS_NUM_PPN --merge $table $input > $out 2> $log
-echo 'Alignments merged'
+# cat $aln1 $aln2 > $input
+# # cat $aln1 $fasta > $input
+# echo 'Alignments concatenated'
+# ruby $makemergetable $aln1 $aln2 > $table
+# # ruby $makemergetable $aln1 > $table
+# echo 'Table prepared'
+# # mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --merge $table $input > $out 2> $log
+# mafft --thread $PBS_NUM_PPN --merge $table $input > $out 2> $log
+# echo 'Alignments merged'
 
 #copy files back
 # rm *.fasta
-# rm $existing $add
-rm $aln1 $aln2
+rm $existing $add
+# rm $aln1 $aln2
 cp * $data_dir
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -5,18 +5,18 @@ import subprocess
 mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
-#align de-novo
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/LPLA/ver3/')
-files = [x for x in os.listdir() if x.endswith('.fa')]
+# #align de-novo
+# os.chdir('/Users/kika/ownCloud/archamoebae/trees/LPLA/ver3/')
+# files = [x for x in os.listdir() if x.endswith('.fa')]
 
-for file in files:
-	print(file)
-	out = '{}.mafft.aln'.format(file.split('.fa')[0])
-	log = '{}.mafft.log'.format(file.split('.fa')[0])
-	# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --op 10 --ep 0 --inputorder {} > {} 2> {}'.format(
-	# 	mafft, file, out, log), shell=True)
-	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
-		mafft, file, out, log), shell=True)
+# for file in files:
+# 	print(file)
+# 	out = '{}.mafft.aln'.format(file.split('.fa')[0])
+# 	log = '{}.mafft.log'.format(file.split('.fa')[0])
+# 	# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --op 10 --ep 0 --inputorder {} > {} 2> {}'.format(
+# 	# 	mafft, file, out, log), shell=True)
+# 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
+# 		mafft, file, out, log), shell=True)
 
 # #add to aligned sequences
 # os.chdir('/Users/kika/ownCloud/membrane-trafficking/')
@@ -28,22 +28,22 @@ for file in files:
 # subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
 
 
-# #merge alignments
-# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/decontaminated/ciliates/')
-# aln1 = 'help.aln'
-# # aln2 = 'v9.mafft.aln'
-# # aln3 = 'preaxostyla_eukref.aln'
+#merge alignments
+os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99_decontaminated/stramenopiles/')
+aln1 = 'STR_5480seqs_230711_core_blast_min700bp.No_Chimera.align_V5.lineage.aln'
+aln2 = 'v9.mafft.aln'
+# aln3 = 'preaxostyla_eukref.aln'
 # fasta = 'otu.fa'
-# input = 'ciliates_rest.in'
-# table = 'ciliates_rest.table'
-# out = 'ciliates_rest.mafft.aln'
-# log = 'ciliates_rest.mafft.log'
+input = 'placement/stramenopiles_v9.in'
+table = 'placement/stramenopiles_v9.table'
+out = 'placement/stramenopiles_v9.mafft.aln'
+log = 'placement/stramenopiles_v9.mafft.log'
 # subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
-# # subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
-# print('Alignments concatenated\n\n')
+subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
+print('Alignments concatenated\n\n')
 # subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
-# # subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
-# print('Table prepared\n\n')
-# # subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-# subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-# print('Alignments merged')
+subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
+print('Table prepared\n\n')
+# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+print('Alignments merged')
