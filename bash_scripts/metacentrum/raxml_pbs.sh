ed5347a26ef60eeec443af81584df7be08bc0802
kikinocka <kika.zahonova@gmail.com>
2022-03-28 15:41:57 +0200

    bla

--- bash_scripts/metacentrum/raxml_pbs.sh
+++ bash_scripts/metacentrum/raxml_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=336:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,17 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add raxml-8.2.8
 
-data='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/heterolobosea/'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/decontaminated/stramenopiles/reference_tree/'
 
 #copy files to scratch
-cp $data'heterolobosea_v9.trimal_gt-0.5.aln' $SCRATCHDIR
+cp $data'stramenopiles.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='heterolobosea_v9.trimal_gt-0.5.aln'
-out='heterolobosea'
+aln='stramenopiles.trimal_gt-0.25_cons-50.aln'
+out='stramenopiles'
 
 raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
 raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
