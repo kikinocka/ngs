359c590b44c764ad6950e2014d7920ef3e058bf7
kikinocka <kika.zahonova@gmail.com>
2022-05-20 10:36:56 +0200

    bla

---
+++ bash_scripts/metacentrum/phylomagnet.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#PBS -N phylomagnet
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=20gb
+#PBS -l walltime=02:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add phylomagnet-0.7
+
+data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3S_1-02B_L001-ds.971c07c67a83443891de04bf749cee0b/'
+eggnog_dir='/storage/brno3-cerit/home/kika/databases/'
+
+#copy files to scratch
+cp $data_dir'1-reads/P3S_all_trimmed_1.fq.gz' $SCRATCHDIR
+cp $data_dir'1-reads/P3S_all_trimmed_2.fq.gz' $SCRATCHDIR
+cp $eggnog_dir'eggnog_ogs.txt' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+reads='P3S_all_trimmed_'*'.fq.gz'
+eggnog='eggnog_ogs.txt'
+lineage='Cryptophyceae,Intramacronucleata,family'
+aligner='mafft-linsi'
+tree='iqtree'
+
+nextflow run /software/phylomagnet/0.7/main.nf \
+	--reference_classes $eggnog\
+	--fastq $reads \
+	--lineage $lineage \
+	--queries_dir 'queries_out'\
+	--reference_dir 'refs_out'\
+	--align_method $aligner\
+	--phylo_method $tree \
+	--cpus $PBS_NUM_PPN
+
+
+
+#copy results to your folder
+rm $reads $eggnog
+cp -r * $data_dir
--- py_scripts/duplicated_lines.py
+++ py_scripts/duplicated_lines.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+import os
 
-file = open('/home/kika/MEGAsync/diplonema_mt/comparison/in')
+os.chdir('/Users/kika/data/eggnog/')
+file = open('eggnog_ogs_dupl.txt')
 
 unique = set()
 for line in file:
 	if line not in unique:
 		unique.add(line)
 	else:
-		print(line)
+		pass
+
+with open('eggnog_ogs.txt', 'w') as out:
+	for item in unique:
+		out.write(item)
