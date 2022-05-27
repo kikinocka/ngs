56da0ce2cc5d1d257861e5aeaaf06f6e407aabc0
kikinocka <kika.zahonova@gmail.com>
2022-05-27 14:50:50 +0200

    bla

--- bash_scripts/iqtree_test_leshy.sh
+++ bash_scripts/iqtree_test_leshy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd '/mnt/mokosz/home/kika/archam_trees/sdhD/ver3/'
+cd '/mnt/mokosz/home/kika/archam_trees/sdhD/ver4/'
 
 for aln in *.aln ; do
 	echo $aln
--- bash_scripts/metacentrum/amplicon_analysis/13_filter_OTU_table.sh
+++ bash_scripts/metacentrum/amplicon_analysis/13_filter_OTU_table.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd '/Users/kika/ownCloud/SL_Euglenozoa/V9/above14/'
+cd '/Users/kika/ownCloud/SL_Euglenozoa/V9/'
 
-TABLE="v9.no_chimera.above14.no_prokaryota.tsv"
-MY_FAVORITE_TAXA="Obazoa"
+TABLE="otu_table.V9DS_updated.no_chimera.tsv"
+MY_FAVORITE_TAXA="Cestoda"
 LEAST_FAVORITE="Metazoa"
-# FILTERED="${TABLE/.tsv/.$MY_FAVORITE_TAXA.tsv}"
-FILTERED="${TABLE/.tsv/.non-$LEAST_FAVORITE$MY_FAVORITE_TAXA.tsv}"
+FILTERED="${TABLE/.tsv/.$MY_FAVORITE_TAXA.tsv}"
+# FILTERED="${TABLE/.tsv/.non-$LEAST_FAVORITE$MY_FAVORITE_TAXA.tsv}"
 
 head -n 1 "${TABLE}" > "${FILTERED}"
-# grep "${MY_FAVORITE_TAXA}" "${TABLE}" >> "${FILTERED}"
-grep "${MY_FAVORITE_TAXA}" "${TABLE}" | grep -v $LEAST_FAVORITE >> "${FILTERED}"
+grep "${MY_FAVORITE_TAXA}" "${TABLE}" >> "${FILTERED}"
+# grep "${MY_FAVORITE_TAXA}" "${TABLE}" | grep -v $LEAST_FAVORITE >> "${FILTERED}"
 # | \
 # awk '$7 == "N" && $9 <= 0.0002 && ($2 >= 3 || $8 >= 2)' >> "${FILTERED}"
