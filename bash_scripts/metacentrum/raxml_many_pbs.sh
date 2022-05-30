93b4802b34cf5db08ef25d7767feb91bb069c0fd
kikinocka <kika.zahonova@gmail.com>
2022-05-30 22:13:46 +0200

    bla

---
+++ bash_scripts/metacentrum/raxml_many_pbs.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#PBS -N raxml-eug
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l walltime=168:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add raxml-8.2.8
+
+data='/storage/brno3-cerit/home/kika/trafficking/all_coats/ver4'
+
+#copy files to scratch
+cp $data'/'*.aln $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+for aln in *.aln; do
+	out=${aln%.trimal_gt-0.8.aln}
+
+	#proteins
+	raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f d -T $PBS_NUM_PPN -x 123 -N 100 -p 12345 -s $aln -n $out
+
+	# #18S
+	# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+	# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+	# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T 1$PBS_NUM_PPN
+done
+
+#copy files back
+rm *.aln
+cp -R * $data
--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -5,20 +5,20 @@ import subprocess
 mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
-# #align de-novo
-# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99/heterolobosea/ver2/')
-# files = [x for x in os.listdir() if x.endswith('.fa')]
+#align de-novo
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/all_adaptors/ver4/')
+files = [x for x in os.listdir() if x.endswith('.fa')]
 
-# for file in files:
-# 	print(file)
-# 	out = '{}.mafft.aln'.format(file.split('.fa')[0])
-# 	log = '{}.mafft.log'.format(file.split('.fa')[0])
-# 	# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --op 10 --ep 0 --inputorder {} > {} 2> {}'.format(
-# 	# 	mafft, file, out, log), shell=True)
-# 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
-# 		mafft, file, out, log), shell=True)
-# 	# subprocess.call('{} --auto --inputorder {} > {} 2> {}'.format(
-# 	# 	mafft, file, out, log), shell=True)
+for file in files:
+	print(file)
+	out = '{}.mafft.aln'.format(file.split('.fa')[0])
+	log = '{}.mafft.log'.format(file.split('.fa')[0])
+	# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --op 10 --ep 0 --inputorder {} > {} 2> {}'.format(
+	# 	mafft, file, out, log), shell=True)
+	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
+		mafft, file, out, log), shell=True)
+	# subprocess.call('{} --auto --inputorder {} > {} 2> {}'.format(
+	# 	mafft, file, out, log), shell=True)
 
 
 # #add to aligned sequences
@@ -31,22 +31,22 @@ maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 # # subprocess.call('{} --addfragments {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
 
 
-#merge alignments
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99/heterolobosea/')
-aln1 = 'heterolobosea.mafft.aln'
-aln2 = 'ver2/v9.mafft.aln'
-# aln3 = 'preaxostyla_eukref.aln'
-# fasta = 'otu.fa'
-input = 'ver2/placement/heterolobosea_v9.in'
-table = 'ver2/placement/heterolobosea_v9.table'
-out = 'ver2/placement/heterolobosea_v9.mafft.aln'
-log = 'ver2/placement/heterolobosea_v9.mafft.log'
-# subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
-subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
-print('Alignments concatenated\n\n')
-# subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
-subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
-print('Table prepared\n\n')
-# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-print('Alignments merged')
+# #merge alignments
+# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99/heterolobosea/')
+# aln1 = 'heterolobosea.mafft.aln'
+# aln2 = 'ver2/v9.mafft.aln'
+# # aln3 = 'preaxostyla_eukref.aln'
+# # fasta = 'otu.fa'
+# input = 'ver2/placement/heterolobosea_v9.in'
+# table = 'ver2/placement/heterolobosea_v9.table'
+# out = 'ver2/placement/heterolobosea_v9.mafft.aln'
+# log = 'ver2/placement/heterolobosea_v9.mafft.log'
+# # subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
+# subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
+# print('Alignments concatenated\n\n')
+# # subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
+# subprocess.call('ruby {} {} {} > {}'.format(maketable, aln1, aln2, table), shell=True)
+# print('Table prepared\n\n')
+# # subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# print('Alignments merged')
