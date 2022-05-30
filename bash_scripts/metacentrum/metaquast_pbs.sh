eb359c6eae28831c19421a9b7c0d24961b0b667f
kikinocka <kika.zahonova@gmail.com>
2022-05-30 17:42:18 +0200

    bla

--- bash_scripts/metacentrum/metaquast_pbs.sh
+++ bash_scripts/metacentrum/metaquast_pbs.sh
@@ -11,20 +11,20 @@ cat $PBS_NODEFILE
 module add blast+-2.8.0a
 module add quast-4.6.3
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
+datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3S_1-02B_L001-ds.971c07c67a83443891de04bf749cee0b/'
 
 #copy files to scratch
 cp $datadir'2-spades/scaffolds.fasta' $SCRATCHDIR
-cp $datadir'1-reads/P1B_all_trimmed_1.fq.gz' $SCRATCHDIR
-cp $datadir'1-reads/P1B_all_trimmed_2.fq.gz' $SCRATCHDIR
+cp $datadir'1-reads/P3S_all_trimmed_1.fq.gz' $SCRATCHDIR
+cp $datadir'1-reads/P3S_all_trimmed_2.fq.gz' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 metagenome='scaffolds.fasta'
-fwd='P1B_all_trimmed_1.fq.gz'
-rev='P1B_all_trimmed_2.fq.gz'
+fwd='P3S_all_trimmed_1.fq.gz'
+rev='P3S_all_trimmed_2.fq.gz'
 
 metaquast.py -o $SCRATCHDIR -t $PBS_NUM_PPN -1 $fwd -2 $rev $metagenome
 
