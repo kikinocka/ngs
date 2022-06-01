f7c35f01ff2c4101f55521d85311b08a11de129f
kikinocka <kika.zahonova@gmail.com>
2022-06-01 17:00:21 +0200

    bla

--- bash_scripts/mrbayes_many_bioinfor.sh
+++ bash_scripts/mrbayes_many_bioinfor.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #PBS -N mrbayes-many
-#PBS -l nodes=1,walltime=24:00:00
+#PBS -l nodes=1,walltime=96:00:00
 #PBS -q default
 #PBS -M kika.zahonova@gmail.com
-#PBS -m abe
+#PBS -m ae
+#PBS -j oe
 
 ##print Node Hostname, the time and date, handy for trouble shooting
 echo $HOSTNAME
