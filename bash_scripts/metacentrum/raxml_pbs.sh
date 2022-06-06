490aac3897ea40f66c06bc257d477631e565fca4
kikinocka <kika.zahonova@gmail.com>
2022-06-06 17:05:10 +0200

    bla

--- bash_scripts/metacentrum/raxml_pbs.sh
+++ bash_scripts/metacentrum/raxml_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add raxml-8.2.8
 
-data='/storage/brno3-cerit/home/kika/trafficking/TBCs/'
+data='/storage/brno3-cerit/home/kika/trafficking/TBCs/ver5/'
 
 #copy files to scratch
 cp $data'tbcs.trimal_gt-0.8.aln' $SCRATCHDIR
