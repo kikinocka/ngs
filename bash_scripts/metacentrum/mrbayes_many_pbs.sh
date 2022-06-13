90996ea92979335d643d01e3ac1196c7115517b8
kikinocka <kika.zahonova@gmail.com>
2022-06-13 11:35:30 +0200

    bla

---
+++ bash_scripts/metacentrum/mrbayes_many_pbs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#PBS -N mrbayes
+#PBS -l select=1:ncpus=4:mem=20gb:scratch_local=1gb:os=debian11
+#PBS -l walltime=168:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add mrbayes-3.2.7a
+
+data='/storage/brno3-cerit/home/kika/trafficking/RABs/MrBayes'
+
+#copy files to scratch
+cp $data'/'*.nex $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+for aln in *.nex; do
+	echo $aln
+	mpirun -np 4 mb -i $aln
+done
+
+
+#copy files back
+rm *.nex
+cp -R * $data
