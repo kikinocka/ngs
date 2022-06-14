79ed0b467b64923b4b4d06867555eb5d05a82a1f
kikinocka <kika.zahonova@gmail.com>
2022-06-14 10:48:18 +0200

    bla

--- bash_scripts/metacentrum/mrbayes_many_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_many_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -N mrbayes-many
-#PBS -l select=1:ncpus=4:mem=20gb:scratch_local=1gb:os=debian11
+#PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
 #PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
