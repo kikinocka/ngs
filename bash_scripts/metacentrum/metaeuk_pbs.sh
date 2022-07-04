ba049869c464a16ddf1d2bdce6587b17432453c0
kikinocka <kika.zahonova@gmail.com>
2022-07-04 11:02:34 +0100

    bla

--- bash_scripts/metacentrum/metaeuk_pbs.sh
+++ bash_scripts/metacentrum/metaeuk_pbs.sh
@@ -9,7 +9,7 @@ cat $PBS_NODEFILE
 
 data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3S_1-02B_L001-ds.971c07c67a83443891de04bf749cee0b/'
 metaeuk='/storage/brno3-cerit/home/kika/miniconda3/bin/metaeuk'
-database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/ MMETSP_uniclust50_MERC_profiles'
+database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC_profiles'
 # database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC'
 
 #copy files to scratch
