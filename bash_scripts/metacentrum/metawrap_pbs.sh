998d8d85a8d09a397e1f1940adf3e0bd8322cc5d
kikinocka <kika.zahonova@gmail.com>
2022-07-05 15:19:29 +0100

    bla

--- bash_scripts/metacentrum/metawrap_pbs.sh
+++ bash_scripts/metacentrum/metawrap_pbs.sh
@@ -13,8 +13,8 @@ module add metawrap-1.3
 data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P2S_1-01A_L001-ds.9f42a90caf694c0ab5686f0e22e79319/'
 
 #copy files to scratch
-cp $data_dir'1-reads/P2S_trimmed_1.fq.gz' $SCRATCHDIR
-cp $data_dir'1-reads/P2S_trimmed_2.fq.gz' $SCRATCHDIR
+cp $data_dir'1-reads/P2S_trimmed_1.fastq' $SCRATCHDIR
+cp $data_dir'1-reads/P2S_trimmed_2.fastq' $SCRATCHDIR
 cp $data_dir'2-spades/scaffolds.fasta' $SCRATCHDIR
 
 
@@ -22,7 +22,8 @@ cp $data_dir'2-spades/scaffolds.fasta' $SCRATCHDIR
 cd $SCRATCHDIR
 
 assembly='scaffolds.fasta'
-metawrap binning -t $PBS_NUM_PPN -m 150 --metabat2 --maxbin2 --concoct -a $assembly -o initial_binning *fq.gz
+metawrap binning -t $PBS_NUM_PPN -m 150 --metabat2 --maxbin2 --concoct -a $assembly -o initial_binning *fastq
+#reads have to be unzipped
 
 #copy files back
 rm *fq.gz $assembly
