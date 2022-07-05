ba1994efef6c0ef07faa901ff37154f86f437bce
kikinocka <kika.zahonova@gmail.com>
2022-07-05 15:04:49 +0100

    bla

--- bash_scripts/metacentrum/metaeuk_pbs.sh
+++ bash_scripts/metacentrum/metaeuk_pbs.sh
@@ -7,23 +7,23 @@
 
 cat $PBS_NODEFILE
 
-data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P3B_1-06D_L001-ds.435324be81dc4260a8e3e8dbb5ed960c/'
+data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
 metaeuk='/storage/brno3-cerit/home/kika/miniconda3/bin/metaeuk'
 database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC_profiles'
 # database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC'
 
 #copy files to scratch
-cp $data_dir'5-tiara/eukarya.fasta' $SCRATCHDIR
+cp $data_dir'4-tiara/eukarya_P1B_scaffolds.fasta' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-contigs='eukarya.fasta'
-out='euk_P3B_metaeuk'
+contigs='eukarya_P1B_scaffolds.fasta'
+out='euk_P1B_metaeuk'
 
 $metaeuk easy-predict $contigs $database $out $SCRATCHDIR
 
 #copy files back
 rm $contigs
-cp -r * $data_dir'6-metaeuk/'
+cp -r * $data_dir'5-metaeuk/'
