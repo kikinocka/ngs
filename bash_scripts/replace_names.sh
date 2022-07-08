ba2433db821a6f145dcdf6bd1f99c8e05059b665
kikinocka <kika.zahonova@gmail.com>
2022-07-08 21:27:03 +0200

    bla

---
+++ bash_scripts/replace_names.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace_lael/name_replace.pl'
+
+cd '/Users/kika/ownCloud/membrane-trafficking/trees/COPII/ver6'
+
+for aln in *aln ; do
+	out=${aln%.mafft.aln}.coded.mafft.aln
+	table=${aln%.mafft.aln}.table
+	perl $replacer -f $aln $out $table
+done
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,8 +3,8 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/UbiA/')
-files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/COPII/ver6/')
+files = [x for x in os.listdir() if x.endswith('coded.mafft.aln')]
 
 for file in files:
 	print(file)
