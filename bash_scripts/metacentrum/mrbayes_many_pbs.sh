cfb703bcb809c2b3ecdb677367e050081fbf6a03
kikinocka <kika.zahonova@gmail.com>
2022-07-09 18:18:04 +0200

    bla

--- bash_scripts/metacentrum/mrbayes_many_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N mrbayes-many
 #PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
-#PBS -l walltime=24:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/SM/ver4/combined/')
-trees = [x for x in os.listdir() if x.endswith('raxml.tre')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/COPII/ver6/RAxML/')
+trees = [x for x in os.listdir() if x.endswith('bipartitions.sec31')]
 
 #file in format Acc. number \t name of organism \n
-names = open('/Users/kika/ownCloud/membrane-trafficking/trees/SM/ver4/sm_codes_names.txt')
+names = open('/Users/kika/ownCloud/membrane-trafficking/trees/COPII/ver6/sec31_codes_names.txt')
 
 name_dict = {}
 for name in names:
@@ -13,11 +13,12 @@ for name in names:
 	new = split_line[1]
 	# new = split_line[0] + ' ' + split_line[1]
 	name_dict[split_line[0]] = new
-
 # print(name_dict)
+
 for tree in trees:
 	print(tree)
-	name = tree.split('.tre')[0] + tree.split('.tre')[1]
+	# name = tree.split('.tre')[0] + tree.split('.tre')[1]
+	name = tree
 	tree_line = open(tree).readline()
 	for key in name_dict:
 		tree_line = tree_line.replace(key, name_dict[key])
