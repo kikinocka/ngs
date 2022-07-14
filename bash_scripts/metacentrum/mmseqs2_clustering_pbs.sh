a9075e5c81d65b5629b8606fa52fe0152a04a993
kikinocka <kika.zahonova@gmail.com>
2022-07-14 12:55:38 +0200

    bla

--- bash_scripts/metacentrum/mmseqs2_clustering_pbs.sh
+++ bash_scripts/metacentrum/mmseqs2_clustering_pbs.sh
@@ -8,18 +8,29 @@
 cat $PBS_NODEFILE
 
 mmseqs='/storage/brno3-cerit/home/kika/miniconda3/bin/mmseqs'
-data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/decontaminated/stramenopiles/'
+data_dir='/storage/brno3-cerit/home/kika/trafficking/wdr/'
 
 #copy files to scratch
-cp $data_dir'arb-silva.de_2022-02-25_id1126815.updated.fa' $SCRATCHDIR
+cp $data_dir'wdr_interpro.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-fasta='arb-silva.de_2022-02-25_id1126815.updated.fa'
-prefix='stram_silva'
+fasta='wdr_interpro.fa'
+prefix='wdr_interpro'
 
 $mmseqs easy-linclust $fasta $prefix tmp --min-seq-id 0.95 -c 0.3 --cov-mode 1 --cluster-mode 2
+# --min-seq-id FLOAT	List matches above this sequence identity (for clustering) (range 0.0-1.0) [0.000]
+# -c FLOAT         		List matches above this fraction of aligned (covered) residues (see --cov-mode) [0.800]
+# --cov-mode INT   		0: coverage of query and target
+#                   	1: coverage of target
+#                   	2: coverage of query
+#                   	3: target seq. length has to be at least x% of query length
+#                   	4: query seq. length has to be at least x% of target length
+#                   	5: short seq. needs to be at least x% of the other seq. length [0]
+# --cluster-mode INT	0: Set-Cover (greedy)
+#                   	1: Connected component (BLASTclust)
+#                   	2,3: Greedy clustering by sequence length (CDHIT) [0]
 
 
 #copy files back
