dc05504900b0510cc486610b21583bd284ce7924
kikinocka <kika.zahonova@gmail.com>
2022-07-15 15:58:35 +0200

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-placer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/hmmer_tools/boots_on_mb2.py'
+placer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/hmmer_tools/boots_on_mb_2.py'
 
 path='/Users/kika/ownCloud/membrane-trafficking/trees/retromer-retriever/vps26-DSCR3/ver2/combined/'
 
