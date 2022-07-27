efd160de2da76c49b3edcfeaa65277d4afbeb993
kikinocka <kika.zahonova@gmail.com>
2022-07-27 17:05:24 -0600

    bla

--- bash_scripts/raxml_many_leshy.sh
+++ bash_scripts/raxml_many_leshy.sh
@@ -11,12 +11,12 @@ for aln in *.aln; do
 	out=${aln%.trimal_gt-0.8.aln}
 
 	#proteins
-	raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T 10 -x 123 -N 100 -p 12345 -s $aln -n $out
+	raxmlHPC-PTHREADS-AVX  -m PROTGAMMALG4XF -f a -T 10 -x 123 -N 100 -p 12345 -s $aln -n $out
 
 	# #18S
-	# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
-	# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
-	# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T 1$PBS_NUM_PPN
+	# raxmlHPC-PTHREADS-AVX  -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+	# raxmlHPC-PTHREADS-AVX  -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+	# raxmlHPC-PTHREADS-AVX  -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T 1$PBS_NUM_PPN
 done
 
 cp * $data
