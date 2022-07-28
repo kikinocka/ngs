4059eeeece2802c424257d589242fe6ef0e37012
kikinocka <kika.zahonova@gmail.com>
2022-07-27 18:39:33 -0600

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/APSK/ver3/')
+os.chdir('/Users/kika/ownCloud/archamoebae/trees/HydA/ver6/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/APSK/ver3/')
+os.chdir('/Users/kika/ownCloud/archamoebae/trees/hydA/ver6/')
 files = [x for x in os.listdir() if x.endswith('mafft.aln')]
 
 for file in files:
