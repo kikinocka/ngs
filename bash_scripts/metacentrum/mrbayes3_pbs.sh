1937d3e178e1cab75555bce158490b968f4fb990
kikinocka <kika.zahonova@gmail.com>
2022-08-18 11:18:10 -0600

    bla

--- bash_scripts/metacentrum/mrbayes3_pbs.sh
+++ bash_scripts/metacentrum/mrbayes3_pbs.sh
@@ -13,14 +13,14 @@ module add mrbayes-3.2.7a
 data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes/'
 
 #copy files to scratch
-cp $data'nifU.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'apsk.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 #proteins
-aln='nifU.trimal_gt-0.8.nex'
+aln='apsk.trimal_gt-0.8.nex'
 
 mpirun -n $PBS_NUM_PPN mb-mpi $aln
 
