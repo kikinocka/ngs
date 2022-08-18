75b16d1ea70d145ce4756097153cf2b880ac8f05
kikinocka <kika.zahonova@gmail.com>
2022-08-17 22:04:07 -0600

    bla

---
+++ bash_scripts/metacentrum/mrbayes3_pbs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#PBS -N mrbayes
+#PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
+#PBS -l walltime=168:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+module add mrbayes-3.2.7a
+
+data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes/'
+
+#copy files to scratch
+cp $data'nifU.trimal_gt-0.8.nex' $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+#proteins
+aln='nifU.trimal_gt-0.8.nex'
+
+mpirun -n $PBS_NUM_PPN mb-mpi $aln
+
+
+#copy files back
+rm $aln
+cp -R * $data
