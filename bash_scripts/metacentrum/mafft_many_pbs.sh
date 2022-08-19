16eb0f5ad63a3a6c2c7cf9c01d02cb1f6aa35d52
kikinocka <kika.zahonova@gmail.com>
2022-08-19 16:10:42 -0600

    bla

--- bash_scripts/metacentrum/mafft_many_pbs.sh
+++ bash_scripts/metacentrum/mafft_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N mafft
 #PBS -l select=1:ncpus=20:mem=200gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,10 +10,10 @@ cat $PBS_NODEFILE
 #add module
 module add mafft-7.453
 
-data_dir='/storage/brno3-cerit/home/kika/trafficking/wdr'
+data_dir='/storage/brno3-cerit/home/kika/trafficking/wdr/ver8'
 
 #copy files to scratch
-cp $data_dir'/'*rep_seq.fasta $SCRATCHDIR
+cp $data_dir'/'*.fa $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/sec13-MS/wdr/tree/ver6/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/sec13-MS/wdr/tree/ver7/RAxML/')
 trees = [x for x in os.listdir() if x.endswith('RAxML_bipartitions.wdr.CD')]
 
 #file in format Acc. number \t name of organism \n
-names = open('/Users/kika/ownCloud/membrane-trafficking/sec13-MS/wdr/tree/ver6/wdr_codes_acc.txt')
+names = open('/Users/kika/ownCloud/membrane-trafficking/sec13-MS/wdr/tree/ver7/wdr_codes_acc.txt')
 
 name_dict = {}
 for name in names:
