44f60159e722e5bae3bb21483b6471d6648e3a87
kikinocka <kika.zahonova@gmail.com>
2022-08-18 20:15:51 -0600

    bla

--- bash_scripts/metacentrum/iqtree_pbs.sh
+++ bash_scripts/metacentrum/iqtree_pbs.sh
@@ -10,27 +10,27 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-datadir='/storage/brno3-cerit/home/kika/trafficking/ARFs/sar1-sarB/'
+datadir='/storage/brno3-cerit/home/kika/trafficking/wdr/ver7/'
 
 #copy files to scratch
 # cp $datadir'/'*.aln $SCRATCHDIR
-cp $datadir'sar1-sarB.CD.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $datadir'wdr.CD.trimal_gt-0.8.aln' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='sar1-sarB.CD.trimal_gt-0.8.aln'
+aln='wdr.CD.trimal_gt-0.8.aln'
 guide='guide_wdr'
 guide_tree=$guide'.treefile'
 bb=1000
 nm=5000
 
-iqtree -m TEST -bb $bb -nm $nm -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
+# iqtree -m TEST -bb $bb -nm $nm -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 # iqtree -m GTR+G -bb $bb -nm $nm -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 
-# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
-# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
+iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -pre $guide
+iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s $aln -ft $guide_tree #-wsr
 
 #copy files back
 rm $aln
-cp * $datadir
+cp * $datadir'IQ-TREE'
