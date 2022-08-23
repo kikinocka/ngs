c47315da28d6b4e34289467126ee5a2e1e75251c
kikinocka <kika.zahonova@gmail.com>
2022-08-23 09:09:50 -0600

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -3,6 +3,6 @@
 placer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/hmmer_tools/boots_on_mb_2.py'
 
 # path='/Users/kika/ownCloud/archamoebae/trees/APSK/ver3/combined/'
-path='/Users/kika/ownCloud/membrane-trafficking/trees/SNAREs/combined/r/'
+path='/Users/kika/ownCloud/membrane-trafficking/trees/SNAREs/combined/qb/'
 
 $placer $path
