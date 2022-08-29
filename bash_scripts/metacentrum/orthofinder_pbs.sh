2060c9a88216134129761f23bf3ed728b6fba232
kikinocka <kika.zahonova@gmail.com>
2022-08-29 15:18:06 +0200

    bla

--- bash_scripts/metacentrum/orthofinder_pbs.sh
+++ bash_scripts/metacentrum/orthofinder_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N orthofinder
-#PBS -l select=1:ncpus=20:mem=15gb:scratch_local=5gb
-#PBS -l walltime=336:00:00
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=20gb
+#PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,26 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add orthofinder-2.0.0
 
-data='/storage/brno3-cerit/home/kika/archamoebae/'
+data='/storage/brno3-cerit/home/kika/archamoebae/orthofinder'
 
 #copy files to scratch
-cp $data'prot_assemblies_filtration-20220127/'*_renamed.faa $SCRATCHDIR
-cp $data'prot_assemblies_filtration-20220127/AmoebaDB-53_EhistolyticaHM1IMSS_AnnotatedProteins.faa' $SCRATCHDIR
-cp $data'prot_assemblies_filtration-20220127/mei.trinity.NRfilt.faa' $SCRATCHDIR
-cp $data'prot_assemblies_filtration-20220127/Masba_prot_LATEST.faa' $SCRATCHDIR
-cp $data'prot_assemblies_filtration-20220127/pelomyxa_predicted_proteins_corr.faa' $SCRATCHDIR
-cp $data'refs/'*.faa $SCRATCHDIR
+cp $data'/'* $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-for file in *.faa; do
-	echo $file
-done
-
-orthofinder -f $SCRATCHDIR
+orthofinder -f $SCRATCHDIR -t $PBS_NUM_PPN
 
 #copy files back
-rm *.faa
+rm *.fa *.fasta
 cp -R * $data
