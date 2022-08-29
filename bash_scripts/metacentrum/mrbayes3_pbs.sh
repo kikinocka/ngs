632d752cd84a7acc2d4368b83dd9a8e43d6f357d
kikinocka <kika.zahonova@gmail.com>
2022-08-29 14:42:40 +0200

    bla

--- bash_scripts/metacentrum/mrbayes3_pbs.sh
+++ bash_scripts/metacentrum/mrbayes3_pbs.sh
@@ -13,7 +13,8 @@ module add mrbayes-3.2.7a
 data='/storage/brno3-cerit/home/kika/trafficking/COPII/ver7/MrBayes/'
 
 #copy files to scratch
-cp $data'sec24.CD.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'sec24.CD.trimal_gt-0.8.nex'* $SCRATCHDIR
+cp $data'sec24.mbLogFile.txt' $SCRATCHDIR
 
 
 #compute on scratch
