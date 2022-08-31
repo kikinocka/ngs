4f30ef9edb79438d4f3c8bdcd789f976838b0330
kikinocka <kika.zahonova@gmail.com>
2022-08-31 10:36:21 +0200

    bla

--- bash_scripts/ostrava/karect.sh
+++ bash_scripts/ostrava/karect.sh
@@ -1,6 +1,10 @@
-#PBS -d .
-#PBS -v PATH
-#PBS -l walltime=400:00:00,nodes=1:ppn=10
+#!/bin/bash
+#PBS -N karect
+#PBS -l nodes=1:ppn=10
+#PBS -l walltime=24:00:00
+#PBS -m ae
+#PBS -j oe
+
 
 read_dir='/mnt/data/bojana/Genomic/Obscuromonas_oborniki/Obscuromonas_oborniki_trimmed/trimmed75/'
 out_dir='/mnt/data/kika/blastocrithidia/o_oborniki/reads/'
@@ -11,13 +15,13 @@ log=$out_dir'o_oborniki.karect_correct.txt'
 cd $out_dir
 karect -correct -threads=10 -matchtype=hamming -celltype=diploid -inputfile=$fwd -inputfile=$rev 2> $log
 
-# karect -align -threads=12 -matchtype=hamming \
+# karect -align -threads=10 -matchtype=hamming \
 #       -inputfile=/mnt/data/bojana/Genomic/Obscuromonas_eliasi/Obscuromonas_eliasi_trimmed/trimmed75/PNG74_trimmed_75_1.fq \
 #       -inputfile=/mnt/data/bojana/Genomic/Obscuromonas_eliasi/Obscuromonas_eliasi_trimmed/trimmed75/PNG74_trimmed_75_1.fq \
 #       -refgenomefile=/mnt/data/bojana/Genomic/Obscuromonas_eliasi/Obscuromonas_eliasi_assembled/trimmed75/scaffolds.l500.fasta \
 #       -alignfile=O_eliasi_align.txt &> log_karect_align_o_eliasi.txt
 
-# karect -eval -threads=12 -matchtype=hamming \
+# karect -eval -threads=10 -matchtype=hamming \
 #       -inputfile=/mnt/data/bojana/Genomic/Obscuromonas_eliasi/Obscuromonas_eliasi_trimmed/trimmed75/PNG74_trimmed_75_1.fq \
 #       -inputfile=/mnt/data/bojana/Genomic/Obscuromonas_eliasi/Obscuromonas_eliasi_trimmed/trimmed75/PNG74_trimmed_75_2.fq \
 #       -resultfile=/mnt/data/bojana/Genomic/Obscuromonas_eliasi/Obscuromonas_eliasi_trimmed/trimmed75/karrect/karect_PNG74_trimmed_75_1.fq \
