0500ccb9c9074557945e9016d384b6ab7079bd16
kikinocka <kika.zahonova@gmail.com>
2022-08-31 18:01:37 +0200

    bla

--- bash_scripts/ostrava/fastqc.sh
+++ bash_scripts/ostrava/fastqc.sh
@@ -8,5 +8,6 @@
 cd '/mnt/data/kika/blastocrithidia/b_frustrata/reads/'
 fwd='karect_4FEM_trimmed_75_1.fq'
 rev='karect_4FEM_trimmed_75_2.fq'
+report='karect_4FEM_trimmed_75.fastqc.txt'
 
-fastqc -o ./fastqc -t 5 $fwd $rev
+fastqc -o ./fastqc -t 5 $fwd $rev 2> $report
