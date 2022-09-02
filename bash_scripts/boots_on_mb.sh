5cdff5e28d8de943d989947f8628910d604c6777
kikinocka <kika.zahonova@gmail.com>
2022-09-02 14:30:29 +0200

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -2,12 +2,12 @@
 
 placer='/Users/kika/scripts/py_scripts/boots_on_mb.py'
 
-# cd '/Users/kika/ownCloud/archamoebae/trees/D-LDH/ver4/combined/'
-cd '/Users/kika/ownCloud/membrane-trafficking/trees/RABs/endocytic/ver4/combined/'
+cd '/Users/kika/ownCloud/archamoebae/trees/hydA/ver6/combined/'
+# cd '/Users/kika/ownCloud/membrane-trafficking/trees/RABs/endocytic/ver4/combined/'
 # cd '/Users/kika/ownCloud/anaeramoeba/trees/TBCs/ver6/combined/'
 
-tree=$path'endocytic.CD.trimal_gt-0.8.nex.con.tre'
-bootstrap=$path'RAxML_bootstrap.endocytic.CD'
-out=$path'endocytic.CD.trimal_gt-0.8.nex.mb+raxml.tre'
+tree=$path'hydA.trimal_gt-0.8.nex.con.tre'
+bootstrap=$path'RAxML_bootstrap.hydA'
+out=$path'hydA.trimal_gt-0.8.nex.mb+raxml.tre'
 
 $placer $tree $bootstrap $out
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,9 +3,9 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-# os.chdir('/Users/kika/ownCloud/archamoebae/trees/ACS/ver5/')
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABs/endocytic/ver5/')
-files = [x for x in os.listdir() if x.endswith('.CD.mafft.aln')]
+os.chdir('/Users/kika/ownCloud/archamoebae/trees/hydA/ver7/')
+# os.chdir('/Users/kika/ownCloud/membrane-trafficking/trees/RABs/endocytic/ver5/')
+files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 
 for file in files:
 	print(file)
