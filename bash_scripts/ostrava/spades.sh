df034d12cdb234bb18c97edb9272afb0bc4940cf
kikinocka <kika.zahonova@gmail.com>
2022-09-02 12:02:17 +0200

    bla

--- bash_scripts/ostrava/spades.sh
+++ bash_scripts/ostrava/spades.sh
@@ -5,10 +5,10 @@
 #PBS -m ae
 #PBS -j oe
 
-work_dir='/mnt/data/kika/blastocrithidia/o_modryi/spades_karect/'
-read_dir='/mnt/data/kika/blastocrithidia/o_modryi/reads/'
-fwd=$read_dir'karect_modryi_trimmed_75_1.fq'
-rev=$read_dir'karect_modryi_trimmed_75_2.fq'
+work_dir='/mnt/data/kika/blastocrithidia/o_volfi/spades_karect/'
+read_dir='/mnt/data/kika/blastocrithidia/o_volfi/reads/'
+fwd=$read_dir'karect_CC37A_trimmed_75_1.fq'
+rev=$read_dir'karect_CC37A_trimmed_75_2.fq'
 
 cd $work_dir
 spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 -o $work_dir
