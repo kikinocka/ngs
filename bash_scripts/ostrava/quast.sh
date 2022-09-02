6008d26d17b9356d4bdd24234d6ecb40eac03eca
kikinocka <kika.zahonova@gmail.com>
2022-09-02 17:06:34 +0200

    bla

---
+++ bash_scripts/ostrava/quast.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -N quast
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=02:00:00
+#PBS -m ae
+#PBS -j oe
+
+
+assembly='/mnt/data/kika/blastocrithidia/b_frustrata/spades_karect/scaffolds.fasta'
+out='/mnt/data/kika/blastocrithidia/b_frustrata/quast/'
+
+quast.py -o $out -t 20 $assembly
