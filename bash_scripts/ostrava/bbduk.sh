ba0b17251ff5db7ae0ca37004ceea5e777f5ccce
kikinocka <kika.zahonova@gmail.com>
2022-09-06 09:15:33 +0200

    bla

--- bash_scripts/ostrava/bbduk.sh
+++ bash_scripts/ostrava/bbduk.sh
@@ -6,17 +6,17 @@
 #PBS -j oe
 
 
-raw_dir='/mnt/data/kika/blastocrithidia/o_modryi-OLD/genome/reads/'
-fwd=$raw_dir'Jaculum_1.fastq.gz'
-rev=$raw_dir'Jaculum_2.fastq.gz'
+raw_dir='/mnt/data/kika/blastocrithidia/b_triatomae-OLD/genome/reads/'
+fwd=$raw_dir'Blastoc_triat_1.fastq.gz'
+rev=$raw_dir'Blastoc_triat_2.fastq.gz'
 
 adapt='/home/users/kika/bbmap/resources/adapters.fa'
 
 
-cd '/mnt/data/kika/blastocrithidia/o_modryi/reads/'
-trimmed_fwd='modryi_trimmed_75_1.fq.gz'
-trimmed_rev='modryi_trimmed_75_2.fq.gz'
-report='modryi_bbduk.txt'
+cd '/mnt/data/kika/blastocrithidia/b_triatomae/reads/'
+trimmed_fwd='triat_trimmed_1.fq.gz'
+trimmed_rev='triat_trimmed_2.fq.gz'
+report='triat_bbduk.txt'
 len=75
 
 #illumina pair-end reads
@@ -24,4 +24,5 @@ len=75
 	in1=$fwd in2=$rev \
 	out1=$trimmed_fwd out2=$trimmed_rev \
 	ref=$adapt \
-	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 minlen=$len tpe tbo t=10 2> $report
+	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=10 2> $report
+	# minlen=$len 
