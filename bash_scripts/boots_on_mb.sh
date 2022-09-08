03cffd4d5eef63d6d7a5c15fb39435893ed1aee0
kikinocka <kika.zahonova@gmail.com>
2022-09-08 09:45:47 +0200

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -2,12 +2,12 @@
 
 placer='/Users/kika/scripts/py_scripts/boots_on_mb.py'
 
-cd '/Users/kika/ownCloud/archamoebae/trees/hydA/ver7/combined/'
+cd '/Users/kika/ownCloud/archamoebae/trees/PFO/ver4/combined/'
 # cd '/Users/kika/ownCloud/membrane-trafficking/trees/RABs/endocytic/ver4/combined/'
 # cd '/Users/kika/ownCloud/anaeramoeba/trees/TBCs/ver6/combined/'
 
-tree=$path'hydA.trimal_gt-0.8.nex.con.tre'
-bootstrap=$path'RAxML_bootstrap.hydA'
-out=$path'hydA.trimal_gt-0.8.nex.mb+raxml.tre'
+tree=$path'PFO.trimal_gt-0.8.nex.con_renamed.tre'
+bootstrap=$path'RAxML_bootstrap.PFO'
+out=$path'PFO.trimal_gt-0.8.nex.mb+raxml.tre'
 
 $placer $tree $bootstrap $out
--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/PFO/ver4/MrBayes/')
-trees = [x for x in os.listdir() if x.endswith('.tre')]
+os.chdir('/Users/kika/ownCloud/archamoebae/trees/PFO/ver4/combined/')
+trees = [x for x in os.listdir() if x.endswith('+raxml.tre')]
 
 #file in format Acc. number \t name of organism \n
-names = open('/Users/kika/ownCloud/archamoebae/trees/PFO/ver4/pfo_codes_acc.txt')
+names = open('/Users/kika/ownCloud/archamoebae/trees/PFO/ver4/pfo_codes_names.txt')
 
 name_dict = {}
 for name in names:
