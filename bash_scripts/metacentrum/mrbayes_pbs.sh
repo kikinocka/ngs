983a715401ca7dc2cccebae56967186880dd6e41
kikinocka <kika.zahonova@gmail.com>
2022-09-11 17:54:09 +0200

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N mrbayes
 #PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
-#PBS -l walltime=48:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,17 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/trafficking/RABs/endocytic/ver5/MrBayes/'
+data='/storage/brno3-cerit/home/kika/trafficking/wdr/ver7/MrBayes/'
 
 #copy files to scratch
-cp $data'endocytic.CD.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'wdr.CD.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 #proteins
-aln='endocytic.CD.trimal_gt-0.8.nex'
+aln='wdr.CD.trimal_gt-0.8.nex'
 
 mpirun -n $PBS_NUM_PPN mb-mpi $aln
 
