a4f1cb62f09a19d83f698dfd0edbb24d8937114c
kikinocka <kika.zahonova@gmail.com>
2022-09-19 17:35:54 +0200

    bla

--- bash_scripts/metacentrum/mrbayes_many_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N mrbayes-many
 #PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
-#PBS -l walltime=336:00:00
+#PBS -l walltime=168:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/trafficking/ARFs/ver9/MrBayes3'
+data='/storage/brno3-cerit/home/kika/trafficking/wdr/ver8/MrBayes'
 
 #copy files to scratch
 cp $data'/'*.nex $SCRATCHDIR
