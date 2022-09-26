cbaa294bc25e4837ca3e637a80b5f98b75115d88
kikinocka <kika.zahonova@gmail.com>
2022-09-26 18:49:18 +0200

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N mrbayes
 #PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
-#PBS -l walltime=24:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,17 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/trafficking/ARFs/sar1-sarB/ver2/MrBayes/'
+data='/storage/brno3-cerit/home/kika/trafficking/diplonemids_all/ArfGAPs/MrBayes/'
 
 #copy files to scratch
-cp $data'sar1-sarB.CD.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'arfgaps.CD.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 #proteins
-aln='sar1-sarB.CD.trimal_gt-0.8.nex'
+aln='arfgaps.CD.trimal_gt-0.8.nex'
 
 mpirun -n $PBS_NUM_PPN mb-mpi $aln
 
