c83fa81532b9b56d0433bce3e7a98e5dcd391adf
kikinocka <kika.zahonova@gmail.com>
2022-09-27 08:59:50 +0200

    bla

--- bash_scripts/ostrava/karect.sh
+++ bash_scripts/ostrava/karect.sh
@@ -20,18 +20,18 @@ log_eval='reads/karect_eval/Btr.karect_eval.txt'
 # karect -correct -threads=10 -matchtype=hamming -celltype=diploid -inputfile=$fwd -inputfile=$rev 2> $log
 
 karect -align -threads=10 -matchtype=hamming \
-      -inputfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_1.fq' \
-      -inputfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_2.fq' \
-      -refgenomefile='/mnt/data/kika/blastocrithidia/b_triatomae/spades_75_karect/scaffolds.fasta' \
-      -alignfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr_aln.txt' \
-      2> '/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr.karect_aln.txt'
+      -inputfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_1.fq \
+      -inputfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_2.fq \
+      -refgenomefile=/mnt/data/kika/blastocrithidia/b_triatomae/spades_75_karect/scaffolds.fasta \
+      -alignfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr_aln.txt \
+      2> /mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr.karect_aln.txt
 
 karect -eval -threads=10 -matchtype=hamming \
-      -inputfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_1.fq' \
-      -inputfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_2.fq' \
-      -resultfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_triat_trimmed_75_1.fq' \
-      -resultfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_triat_trimmed_75_2.fq' \
-      -refgenomefile='/mnt/data/kika/blastocrithidia/b_triatomae/spades_75_karect/scaffolds.fasta' \
-      -alignfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr_aln.txt' \
-      -evalfile='/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr_eval.txt' \ 
-      2> '/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr.karect_eval.txt'
+      -inputfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_1.fq \
+      -inputfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/triat_trimmed_75_2.fq \
+      -resultfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_triat_trimmed_75_1.fq \
+      -resultfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_triat_trimmed_75_2.fq \
+      -refgenomefile=/mnt/data/kika/blastocrithidia/b_triatomae/spades_75_karect/scaffolds.fasta \
+      -alignfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr_aln.txt \
+      -evalfile=/mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr_eval.txt \ 
+      2> /mnt/data/kika/blastocrithidia/b_triatomae/reads/karect_eval/Btr.karect_eval.txt
