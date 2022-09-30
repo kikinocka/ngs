6e22c610d141872e1bda8668c9acbd873022795a
kikinocka <kika.zahonova@gmail.com>
2022-09-30 16:23:48 +0200

    bla

--- bash_scripts/ostrava/bbduk.sh
+++ bash_scripts/ostrava/bbduk.sh
@@ -6,17 +6,17 @@
 #PBS -j oe
 
 
-raw_dir='/mnt/data/aalbanaz/Blastocrithidia_Obscuromonas/raw_reads/Blastocrithidia_frustrata/'
-fwd=$raw_dir'4FEM_1.fastq.gz'
-rev=$raw_dir'4FEM_2.fastq.gz'
+raw_dir='/mnt/data/aalbanaz/Blastocrithidia_Obscuromonas/raw_reads/Blastocrithidia_sp_HR05/'
+fwd=$raw_dir'HR-05_1.fastq.gz'
+rev=$raw_dir'HR-05_2.fastq.gz'
 
 adapt='/home/users/kika/bbmap/resources/adapters.fa'
 
 
-cd '/mnt/data/kika/blastocrithidia/b_frustrata/reads/'
-trimmed_fwd='4FEM_trimmed_1.fq.gz'
-trimmed_rev='4FEM_trimmed_2.fq.gz'
-report='bfru_bbduk.txt'
+cd '/mnt/data/kika/blastocrithidia/b_spHR05/reads/'
+trimmed_fwd='HR05_trimmed_1.fq.gz'
+trimmed_rev='HR05_trimmed_2.fq.gz'
+report='bsp_bbduk.txt'
 len=75
 
 #illumina pair-end reads
