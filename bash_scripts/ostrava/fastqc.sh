811204f63e89086f111da4bc5b2a0b0c73162941
kikinocka <kika.zahonova@gmail.com>
2022-09-30 16:47:46 +0200

    bla

--- bash_scripts/ostrava/fastqc.sh
+++ bash_scripts/ostrava/fastqc.sh
@@ -5,10 +5,10 @@
 #PBS -m ae
 #PBS -j oe
 
-cd '/mnt/data/kika/blastocrithidia/o_oborniki/reads/'
-fwd='M09_trimmed_1.fq.gz'
-rev='M09_trimmed_2.fq.gz'
-report='oobo_trimmed.fastqc.txt'
+cd '/mnt/data/kika/blastocrithidia/o_volfi/reads/'
+fwd='CC37A_trimmed_1.fq.gz'
+rev='CC37A_trimmed_2.fq.gz'
+report='ovol_trimmed.fastqc.txt'
 
 # fastqc -o ./fastqc -t 5 *.fq 2> $report
 fastqc -o ./fastqc -t 5 $fwd $rev 2> $report
