6a3ceb2eaca44833b1ce78e3d27993536b3b4b90
kikinocka <kika.zahonova@gmail.com>
2022-10-10 15:08:51 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,8 +6,8 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_COPII/trees/hyp24/')
-files = [x for x in os.listdir() if x.endswith('eugl.fa')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_COPII/trees/hyp27/')
+files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
 	print(file)
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,8 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/above99/metamonads/')
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/RquA/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_COPII/trees/hyp24/')
 files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 
 for file in files:
