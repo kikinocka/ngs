a0f94113a44cccbcf2e67afc321143aecd21e5eb
kikinocka <kika.zahonova@gmail.com>
2022-10-10 09:17:33 +0200

    bla

--- bash_scripts/ostrava/quast.sh
+++ bash_scripts/ostrava/quast.sh
@@ -6,7 +6,7 @@
 #PBS -j oe
 
 
-assembly='/mnt/data/kika/blastocrithidia/b_spHR05/spades_75_careful/scaffolds.fasta'
-out='/mnt/data/kika/blastocrithidia/b_spHR05/spades_75_careful/quast/scaffolds/'
+assembly='/mnt/data/kika/blastocrithidia/b_spHR05/spades_75_karect//scaffolds.fasta'
+out='/mnt/data/kika/blastocrithidia/b_spHR05/spades_75_karect/quast/scaffolds/'
 
 quast.py -o $out -t 20 $assembly
