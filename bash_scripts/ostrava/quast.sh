931f47c596db4f04b2e12f4b068799ca7679b131
kikinocka <kika.zahonova@gmail.com>
2022-10-11 16:58:30 +0200

    bla

--- bash_scripts/ostrava/quast.sh
+++ bash_scripts/ostrava/quast.sh
@@ -6,7 +6,7 @@
 #PBS -j oe
 
 
-assembly='/mnt/data/kika/blastocrithidia/o_oborniki/spades_75_careful/contigs.fasta'
-out='/mnt/data/kika/blastocrithidia/o_oborniki/spades_75_careful/quast/contigs/'
+assembly='/mnt/data/kika/blastocrithidia/o_oborniki/spades_all_carefull/contigs.fasta'
+out='/mnt/data/kika/blastocrithidia/o_oborniki/spades_all_carefull/quast/contigs/'
 
 quast.py -o $out -t 20 $assembly
