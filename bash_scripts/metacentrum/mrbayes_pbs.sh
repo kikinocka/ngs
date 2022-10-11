dafa1691ea302b6e86beec5bbff4408e13edb243
kikinocka <kika.zahonova@gmail.com>
2022-10-11 21:01:15 +0200

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -10,17 +10,17 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/trafficking/diplonemids_all/ArfGAPs/MrBayes'
+data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes/'
 
 #copy files to scratch
-cp $data'/'* $SCRATCHDIR
+cp $data'hydA.CD.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 #proteins
-aln='arfgaps.CD.trimal_gt-0.8.nex'
+aln='hydA.CD.trimal_gt-0.8.nex'
 
 mpirun -n $PBS_NUM_PPN mb-mpi $aln
 
