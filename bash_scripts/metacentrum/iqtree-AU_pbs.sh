01dfef06a7edf40a119e58ec3875c3fca9acab5c
kikinocka <kika.zahonova@gmail.com>
2022-10-12 16:50:45 +0200

    bla

--- bash_scripts/metacentrum/iqtree-AU_pbs.sh
+++ bash_scripts/metacentrum/iqtree-AU_pbs.sh
@@ -25,11 +25,11 @@ constr1='sec13.constr1'
 constr2='sec13.constr2'
 
 # iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN --test-weight --test-au --test 10000 -n 100 -s $aln --trees $trees
-iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -s $aln --prefix $pref.unconstr
-iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -s $aln -g $constr1 --prefix $pref.constr1
-iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -s $aln -g $constr2 --prefix $pref.constr2
+iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --prefix $pref.unconstr
+iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr1 --prefix $pref.constr1
+iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr2 --prefix $pref.constr2
 cat *constr*.treefile > $pref.trees
-iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -s $aln -trees $pref.trees --test-weight --test-au --test 10000 -n 0
+iqtree2 -m LG4X -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -trees $pref.trees --test-weight --test-au --test 10000 -n 0
 
 #copy files back
 # rm $aln
