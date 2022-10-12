9ba9704962b1ef8283752f828c3365834d2c46ae
kikinocka <kika.zahonova@gmail.com>
2022-10-12 16:17:16 +0200

    bla

--- bash_scripts/ostrava/spades.sh
+++ bash_scripts/ostrava/spades.sh
@@ -5,11 +5,11 @@
 #PBS -m ae
 #PBS -j oe
 
-work_dir='/mnt/data/kika/blastocrithidia/o_volfi/spades_75_careful/'
-read_dir='/mnt/data/kika/blastocrithidia/o_volfi/reads/'
-fwd=$read_dir'CC37A_trimmed_75_1.fq'
-rev=$read_dir'CC37A_trimmed_75_2.fq'
+work_dir='/mnt/data/kika/blastocrithidia/o_eliasi/spades_75_karect/'
+read_dir='/mnt/data/kika/blastocrithidia/o_eliasi/reads/'
+fwd=$read_dir'karect_PNG74_trimmed_75_1.fq'
+rev=$read_dir'karect_PNG74_trimmed_75_2.fq'
 
 cd $work_dir
-spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 --careful -o $work_dir
-# spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 -o $work_dir
+# spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 --careful -o $work_dir
+spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 -o $work_dir
