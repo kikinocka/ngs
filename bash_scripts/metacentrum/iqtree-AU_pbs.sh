f23b983269b059673c23bce89105600a80b970a0
kikinocka <kika.zahonova@gmail.com>
2022-10-24 15:10:24 +0200

    bla

--- bash_scripts/metacentrum/iqtree-AU_pbs.sh
+++ bash_scripts/metacentrum/iqtree-AU_pbs.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-2.2.0
 
-datadir='/storage/brno3-cerit/home/kika/trafficking/diplonemids_COPII/sec13_AU/ver8'
+datadir='/storage/brno3-cerit/home/kika/trafficking/diplonemids_COPII/sec13_AU/ver9'
 
 #copy files to scratch
 cp $datadir'/'* $SCRATCHDIR
@@ -25,10 +25,10 @@ constr1='sec13.constr1'
 constr2='sec13.constr2'
 ufb=1000
 
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr1 --prefix $pref.constr1
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr2 --prefix $pref.constr2
-cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
+# iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
+# iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr1 --prefix $pref.constr1
+# iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr2 --prefix $pref.constr2
+# cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
 iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
 
 #copy files back
