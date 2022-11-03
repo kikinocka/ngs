455af03dc9e41722e5b6dca45241389054ace0f6
kikinocka <kika.zahonova@gmail.com>
2022-11-03 15:45:51 +0100

    bla

--- bash_scripts/ostrava/merqury.sh
+++ bash_scripts/ostrava/merqury.sh
@@ -11,20 +11,22 @@ read_dir='/mnt/data/kika/blastocrithidia/b_frustrata/reads/'
 contigs=$work_dir'spades_75_karect/contigs.fasta'
 fwd=$read_dir'karect_4FEM_trimmed_75_1.fq'
 rev=$read_dir'karect_4FEM_trimmed_75_2.fq'
-out='Bfru_scaff.kat.out'
 base='Bfru_scaff.'
 
 
 cd $work_dir'merqury/'
-cp $fwd $rev .
 
-# #get the right k size
-# gsize=38237037
-# $merqury'best_k.sh' $gsize
-
-k=17.5764
+#get the right kmer size
+touch 'best_k.out'
+gsize=38237037
+$merqury'best_k.sh' $gsize > 'best_k.out'
+#getting the kmer decimal number from output
+kmer_tmp="tail -1 best_k.out"
+#rounding the kmer number to integer
+kmer="$($kmer_tmp | awk '{print int($1+0.5)}')" 
 
 #build meryl dbs
-for each karect$i.fq ; do
-    meryl k=$k count output karect$i.meryl karect$i.fq 2> $base'meryl.log'
-done
+meryl k=$kmer count output ${fwd%.fq}.meryl $fwd 2> $base.meryl_dbs.out
+meryl k=$kmer count output ${rev%.fq}.meryl $rev 2>> $base.meryl_dbs.out
+
+
