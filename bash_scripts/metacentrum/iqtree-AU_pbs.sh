da1836bf5cffcf31c8cf52c0f08f6bd6e9b54242
kikinocka <kika.zahonova@gmail.com>
2022-11-07 18:56:32 +0100

    bla

--- bash_scripts/metacentrum/iqtree-AU_pbs.sh
+++ bash_scripts/metacentrum/iqtree-AU_pbs.sh
@@ -13,6 +13,7 @@ module add iqtree-2.2.0
 datadir='/storage/brno3-cerit/home/kika/diplonema/cardiolipin/CLD1/ver5_AU'
 
 #copy files to scratch
+# cp $datadir'/'* $SCRATCHDIR
 cp $datadir'/'* $SCRATCHDIR
 
 
@@ -20,17 +21,16 @@ cp $datadir'/'* $SCRATCHDIR
 cd $SCRATCHDIR
 pref='cld'
 aln='cld.trimal_gt-0.8.aln'
-# trees='sec13_topologies.trees'
 constr1='cld.constr1'
 # constr2='sec13.constr2'
 ufb=1000
 
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
+# iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
 iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr1 --prefix $pref.constr1
 # iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr2 --prefix $pref.constr2
 cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
 iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
 
 #copy files back
-rm $aln $constr
+# rm $aln $constr
 cp * $datadir
