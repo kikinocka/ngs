633b68e2bbb6f1f4fbe995d045763ed95641e120
kikinocka <kika.zahonova@gmail.com>
2022-11-07 15:45:51 +0100

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,13 +3,13 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_COPII/trees/hyp24/ver2/')
+os.chdir('/Users/kika/ownCloud/diplonema/cardiolipin/CLD1/ver2/')
 files = [x for x in os.listdir() if x.endswith('mafft.aln')]
 
 for file in files:
 	print(file)
-	file_name = file.split('.')[0] + '.' + file.split('.')[1]
-	# file_name = file.split('.')[0]
+	# file_name = file.split('.')[0] + '.' + file.split('.')[1]
+	file_name = file.split('.')[0]
 	aut = 'automated1'
 	gt = 0.8 #fraction of sequences with a gap allowed
 	cons = 50 #minimum percentage of positions in the original alignment to conserve
