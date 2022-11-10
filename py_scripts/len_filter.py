38e7b394764fc5c28572b1761c778807b5ac7430
kikinocka <kika.zahonova@gmail.com>
2022-11-10 19:10:31 +0100

    bla

--- py_scripts/len_filter.py
+++ py_scripts/len_filter.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 os.chdir('/mnt/data/kika/blastocrithidia/b_spHR05/scaff_gap/')
 genome = SeqIO.parse('Braa.platanus_rnd2_scaffold.fa', 'fasta')
 
-with open('Braa.platanus_rnd2_scaffold.l500.fa', 'w') as out:
+with open('Braa.platanus_rnd2_scaffold.l500_test.fa', 'w') as out:
 	for seq in genome:
 		if len(seq) >= 500:
 			out.write('>{}\n{}\n'.format(seq.description, seq.seq))
