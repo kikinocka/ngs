00931891317846c129a9072540a0e5d3c69ce53b
kikinocka <kika.zahonova@gmail.com>
2022-11-11 16:13:07 +0100

    bla

--- bash_scripts/metacentrum/blast_many_pbs.sh
+++ bash_scripts/metacentrum/blast_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N blastn
+#PBS -N blastn-many
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=5gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=196:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,44 +10,37 @@ cat $PBS_NODEFILE
 #add module
 module add blast-plus/blast-plus-2.12.0-gcc-8.3.0-ohlv7t4
 
-datadir='/storage/brno3-cerit/home/kika/tRNAs-kinetoplastids/'
+datadir='/storage/brno3-cerit/home/kika/blasto_comparative/blobtools'
 
 #copy files to scratch
-# cp $datadir'/'*.fa $SCRATCHDIR
-cp $datadir'TB_Trp_cca.fa' $SCRATCHDIR
-cp $datadir'read_DB/'* $SCRATCHDIR
+cp $datadir'/'*.fa $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
 
-query='TB_Trp_cca.fa'
-db='Tbrucei-cyto.fa'
-out='TB_Trp_cca.reads_blast.tsv'
+db='/storage/projects/BlastDB/nt'
+program=blastn
+task=megablast
+eval=1e-20
 max_seqs=1
+max_hsps=1
 
-# blastn -query $query -db $db -out $out \
-# 	-outfmt "6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send" \
-# 	-max_target_seqs $max_seqs \
-# 	-num_threads $PBS_NUM_PPN
-
-blastn -query $query -db $db -out $out \
-	-outfmt "6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send" \
-	-num_threads $PBS_NUM_PPN
-
-# program=blastn
-# db='/storage/projects/BlastDB/nt'
-# outfmt=5
-# word=11
-# evalue=1e-03
-
-# for query in *.fa; do
-# 	echo $query
-# 	out=${query%.fa}'.blastn.xml'
-# 	$program -query $query -db $db -out $out -outfmt $outfmt -word_size $word -evalue $evalue -num_threads $PBS_NUM_PPN
-# 	echo ***BLAST done***
-# done
+for query in *.fa; do
+	echo $query
+	out=${query%.fa}'.nt_1e-20.megablast'
+	$program -task $task \
+		-query $query \
+		-db $db \
+		-out $out \
+		-outfmt "6 qseqid staxids bitscore std" \
+		# 'qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore' = equivalent to 'std'
+		-num_threads $PBS_NUM_PPN \
+		-evalue $eval \
+		-max_target_seqs $max_seqs \
+		-max_hsps $max_hsps
+	echo ***BLAST done***
+done
 
 #copy files back
-# rm *fa
-rm $query $db
+rm *.fa
 cp * $datadir
