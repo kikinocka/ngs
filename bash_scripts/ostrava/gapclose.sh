bd25d44e653313f3c688571236b3f3348a0cdff9
kikinocka <kika.zahonova@gmail.com>
2022-11-11 15:03:40 +0100

    bla

--- bash_scripts/ostrava/gapclose.sh
+++ bash_scripts/ostrava/gapclose.sh
@@ -5,12 +5,12 @@
 #PBS -l nodes=1:ppn=20
 #PBS -l walltime=02:00:00
 
-work_dir='/mnt/data/kika/blastocrithidia/o_oborniki/scaff_gap/'
+work_dir='/mnt/data/kika/blastocrithidia/o_volfi/scaff_gap/'
 
 cd $work_dir
-scaffolds='Oobo.platanus_rnd2_scaffold.l500.fa'
-config='Oobo.rnd2.config.file'
-out='Oobo.platanus_rnd2_scaffold.l500.gapcloser.fa'
-log='Oobo.platanus_rnd2_scaffold.l500.gapcloser.log'
+scaffolds='Ovol.platanus_rnd1_scaffold.l500.fa'
+config='Ovol.rnd1.config.file'
+out='Ovol.platanus_rnd1_scaffold.l500.gapcloser.fa'
+log='Ovol.platanus_rnd1_scaffold.l500.gapcloser.log'
 
 GapCloser -b $config -a $scaffolds -o $out -t 20 2> $log
