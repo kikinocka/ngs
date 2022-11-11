63ebe42510056e7a625fc2a6052a8339f4f0fc72
kikinocka <kika.zahonova@gmail.com>
2022-11-11 12:02:56 +0100

    bla

--- bash_scripts/ostrava/platanus.sh
+++ bash_scripts/ostrava/platanus.sh
@@ -13,8 +13,8 @@ read_dir='/mnt/data/kika/blastocrithidia/o_modryi/reads/'
 fwd=$read_dir'karect_modryi_trimmed_75_1.fq'
 rev=$read_dir'karect_modryi_trimmed_75_2.fq'
 contigs=$work_dir'spades_75_karect/contigs.fatsta'
-out='Oeli.platanus_rnd1'
-report='Oeli.platanus_rnd1.log'
+out='Omod.platanus_rnd1'
+report='Omod.platanus_rnd1.log'
 
 $platanus scaffold -o $out -c $contigs -IP1 $fwd $rev -t 40 2> $report
 
