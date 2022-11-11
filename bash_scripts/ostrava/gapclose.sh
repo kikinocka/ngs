a2253aec22c9e9315f244c463b04e15e2af6213e
kikinocka <kika.zahonova@gmail.com>
2022-11-11 12:15:28 +0100

    bla

--- bash_scripts/ostrava/gapclose.sh
+++ bash_scripts/ostrava/gapclose.sh
@@ -5,12 +5,12 @@
 #PBS -l nodes=1:ppn=20
 #PBS -l walltime=02:00:00
 
-work_dir='/mnt/data/kika/blastocrithidia/o_eliasi/scaff_gap/'
+work_dir='/mnt/data/kika/blastocrithidia/o_modryi/scaff_gap/'
 
 cd $work_dir
-scaffolds='Oeli.platanus_rnd2_scaffold.l500.fa'
-config='Oeli.rnd2.config.file'
-out='Oeli.platanus_rnd2_scaffold.l500.gapcloser.fa'
-log='Oeli.platanus_rnd2_scaffold.l500.gapcloser.log'
+scaffolds='Omod.platanus_rnd1_scaffold.l500.fa'
+config='Omod.rnd1.config.file'
+out='Omod.platanus_rnd1_scaffold.l500.gapcloser.fa'
+log='Omod.platanus_rnd1_scaffold.l500.gapcloser.log'
 
 GapCloser -b $config -a $scaffolds -o $out -t 20 2> $log
--- py_scripts/len_filter.py
+++ py_scripts/len_filter.py
@@ -2,10 +2,10 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/mnt/data/kika/blastocrithidia/o_eliasi/scaff_gap/')
-genome = SeqIO.parse('Oeli.platanus_rnd2_scaffold.fa', 'fasta')
+os.chdir('/mnt/data/kika/blastocrithidia/o_modryi/scaff_gap/')
+genome = SeqIO.parse('Omod.platanus_rnd1_scaffold.fa', 'fasta')
 
-with open('Oeli.platanus_rnd2_scaffold.l500.fa', 'w') as out:
+with open('Omod.platanus_rnd1_scaffold.l500.fa', 'w') as out:
 	for seq in genome:
 		if len(seq) >= 500:
 			out.write('>{}\n{}\n'.format(seq.description, seq.seq))
