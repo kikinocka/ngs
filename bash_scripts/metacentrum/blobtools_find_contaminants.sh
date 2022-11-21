ab89cf5e63f40a23d8bf85a85c1d652383bd2a6d
kikinocka <kika.zahonova@gmail.com>
2022-11-21 17:32:03 +0100

    bla

--- bash_scripts/metacentrum/blobtools_find_contaminants.sh
+++ bash_scripts/metacentrum/blobtools_find_contaminants.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd '/storage/brno3-cerit/home/kika/blasto_comparative/blobtools/'
+cd '/storage/brno3-cerit/home/kika/blasto_comparative/'
 
-blob_dir='reports/'
+blob_dir='blobtools/reports/'
 blast_dir='blasts/'
 base='Bfru'
 table=$blob_dir$base'_tables/phylum.'$base'.blobDB.table.txt'
@@ -11,5 +11,5 @@ blast=$blast_dir$base'.platanus_rnd2_scaffold.l500.gapcloser.nt_1e-20.megablast'
 # mkdir $blob_dir$base'_contaminants'
 
 # grep -v "^##" $table | grep -v -e Euglenozoa -e no-hit > $blob_dir$base'_contaminants/'$base'_contaminants.tsv'
-# grep -v "#" $blob_dir$base'_contaminants/'$base'_contaminants.tsv' | cut -f1 | sed -e 's/^/grep "/g' -e 's/$/" $blast/g' > $blob_dir$base'_contaminants/'$base'_get_cont_from_blast.sh'
+grep -v "#" $blob_dir$base'_contaminants/'$base'_contaminants.tsv' | cut -f1 | sed -e 's/^/grep "/g' -e 's/$/" $blast/g' > $blob_dir$base'_contaminants/'$base'_get_cont_from_blast.sh'
 sh $blob_dir$base'_contaminants/'$base'_get_cont_from_blast.sh' > $blob_dir$base'_contaminants/'$base'_contaminants_blast.tsv'
