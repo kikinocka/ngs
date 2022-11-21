8a3f1470bc604fac6f40ba5d241411040408a355
kikinocka <kika.zahonova@gmail.com>
2022-11-21 17:10:06 +0100

    bla

---
+++ bash_scripts/metacentrum/blobtools_find_contaminants.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd '/storage/brno3-cerit/home/kika/blasto_comparative/blobtools/'
+
+blob_dir='reports/'
+blast_dir='blasts/'
+base='Bfru'
+table=$blob_dir$base'_tables/phylum.'$base'.blobDB.table.txt'
+blast=$blast_dir$base'.platanus_rnd2_scaffold.l500.gapcloser.nt_1e-20.megablast'
+
+mkdir $blob_dir$base'_contaminants'
+
+grep -v "^##" $table | grep -v -e Euglenozoa -e no-hit > $blob_dir$base'_contaminants/'$base'_contaminants.tsv'
+grep -v "#" $blob_dir$base'_contaminants/'$base'_contaminants.tsv' | cut -f1 | \ 
+	sed -e 's/^/grep "/g' -e 's/$/" $blast/g' > $blob_dir$base'_contaminants/'$base'get_cont_from_blast.sh'
