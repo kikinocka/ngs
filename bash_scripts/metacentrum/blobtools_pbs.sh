b390f81b8285bdd5aede8fb5c94a6bd180ec3546
kikinocka <kika.zahonova@gmail.com>
2022-11-21 11:34:55 +0100

    bla

--- bash_scripts/metacentrum/blobtools_pbs.sh
+++ bash_scripts/metacentrum/blobtools_pbs.sh
@@ -29,13 +29,13 @@ blobtools view -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_tables/phyl
 echo 'Running BlobTools View - Super Kingdom'
 blobtools view -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_tables/superkingdom' --rank 'superkingdom'
 echo 'Running BlobTools Plot PNG - Phylum'
-blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images' --rank 'phylum' 
+blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images/' --rank 'phylum' 
 echo 'Running BlobTools Plot PNG - Super Kingdom'
-blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images' --rank 'superkingdom' 
+blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images/' --rank 'superkingdom' 
 echo 'Running BlobTools Plot SVG - Phylum'
-blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images' --rank 'phylum' --format 'svg'
+blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images/' --rank 'phylum' --format 'svg'
 echo 'Running BlobTools Plot SVG - Super Kingdom'
-blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images' --rank 'superkingdom' --format 'svg'
+blobtools plot -i $blob_dir$base'.blobDB.json' --out $blob_dir$base'_images/' --rank 'superkingdom' --format 'svg'
 
 # blobtools taxify -f $dmnd -m $taxid -s 1 -t 2
 # blobtools create -i $transcriptome -s $sam -t $taxified -o $base
