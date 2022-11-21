0151f378acac190568fb9134b1ccad8477766e22
kikinocka <kika.zahonova@gmail.com>
2022-11-21 09:31:03 +0100

    bla

--- bash_scripts/metacentrum/blobtools_pbs.sh
+++ bash_scripts/metacentrum/blobtools_pbs.sh
@@ -10,44 +10,45 @@ cat $PBS_NODEFILE
 #add module
 module add blobtools-1.0
 
-assembly_dir='/storage/brno3-cerit/home/kika/sags/reassembly/'
-blast_dir='/storage/brno3-cerit/home/kika/sags/reassembly/blast/'
-mapping_dir='/storage/brno3-cerit/home/kika/sags/reassembly/mapping/bwa/'
+work_dir='/storage/brno3-cerit/home/kika/blasto_comparative/blobtools/'
+assembly_dir=$work_dir'assemblies/'
+blast_dir=$work_dir'blasts/'
+mapping_dir=$work_dir'mapping/'
+blob_dir=$work_dir'report/'
 ncbi='/storage/brno3-cerit/home/kika/ncbi/'
-blob_dir='/storage/brno3-cerit/home/kika/sags/reassembly/reports/blobtools/'
 
 #copy files to scratch
-cp $assembly_dir'EU1718_contigs.fa' $SCRATCHDIR
-cp $blast_dir'EU1718.fa_vs_nt_1e-10.megablast' $SCRATCHDIR
-cp $mapping_dir'EU1718_bwa_mapped_all.sorted.bam' $SCRATCHDIR
+cp $assembly_dir'Bfru.platanus_rnd2_scaffold.l500.gapcloser.fa' $SCRATCHDIR
+cp $blast_dir'Bfru.platanus_rnd2_scaffold.l500.gapcloser.nt_1e-20.megablast' $SCRATCHDIR
+cp $mapping_dir'Bfru.bw2_sorted.bam' $SCRATCHDIR
 cp $ncbi'nodes.dmp' $ncbi'names.dmp' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-mkdir tables
-mkdir images
+base='Bfru'
+mkdir $base'tables'
+mkdir $base'images'
 
-genome='EU1718_contigs.fa'
-blast='EU1718.fa_vs_nt_1e-10.megablast'
-bam='EU1718_bwa_mapped_all.sorted.bam'
+genome=$base'.platanus_rnd2_scaffold.l500.gapcloser.fa'
+blast=$base'.platanus_rnd2_scaffold.l500.gapcloser.nt_1e-20.megablast'
+bam=$base'.bw2_sorted.bam'
 tax_nodes='nodes.dmp'
 tax_names='names.dmp'
-base='EU1718_blobtools'
 
 echo 'Running BlobTools create'
 blobtools create -i $genome --nodes $tax_nodes --names $tax_names -t $blast -b $bam -o $base
 echo 'Running BlobTools View - Phylum'
-blobtools view -i $base'.blobDB.json' --out tables/phylum --rank 'phylum'
+blobtools view -i $base'.blobDB.json' --out $base'tables/phylum' --rank 'phylum'
 echo 'Running BlobTools View - Super Kingdom'
-blobtools view -i $base'.blobDB.json' --out tables/superkingdom --rank 'superkingdom'
+blobtools view -i $base'.blobDB.json' --out $base'tables/superkingdom' --rank 'superkingdom'
 echo 'Running BlobTools Plot PNG - Phylum'
-blobtools plot -i $base'.blobDB.json' --out images/. --rank 'phylum' 
+blobtools plot -i $base'.blobDB.json' --out $base'images'/. --rank 'phylum' 
 echo 'Running BlobTools Plot PNG - Super Kingdom'
-blobtools plot -i $base'.blobDB.json' --out images/. --rank 'superkingdom' 
+blobtools plot -i $base'.blobDB.json' --out $base'images'/. --rank 'superkingdom' 
 echo 'Running BlobTools Plot SVG - Phylum'
-blobtools plot -i $base'.blobDB.json' --out images/. --rank 'phylum' --format 'svg'
+blobtools plot -i $base'.blobDB.json' --out $base'images'/. --rank 'phylum' --format 'svg'
 echo 'Running BlobTools Plot SVG - Super Kingdom'
-blobtools plot -i $base'.blobDB.json' --out images/. --rank 'superkingdom' --format 'svg'
+blobtools plot -i $base'.blobDB.json' --out $base'images'/. --rank 'superkingdom' --format 'svg'
 
 # blobtools taxify -f $dmnd -m $taxid -s 1 -t 2
 # blobtools create -i $transcriptome -s $sam -t $taxified -o $base
