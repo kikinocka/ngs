e010627daea65e3086b2fd711fd10a7cebb4f7f3
kikinocka <kika.zahonova@gmail.com>
2022-11-22 09:15:42 +0100

    bla

--- bash_scripts/metacentrum/blobtools_find_contaminants.sh
+++ bash_scripts/metacentrum/blobtools_find_contaminants.sh
@@ -11,5 +11,5 @@ blast=$blast_dir$base'.platanus_rnd2_scaffold.l500.gapcloser.nt_1e-20.megablast'
 # mkdir $report_dir$base'_contaminants'
 
 # grep -v "^##" $table | grep -v -e Euglenozoa -e no-hit > $report_dir$base'_contaminants/'$base'_contaminants.tsv'
-grep -v "#" $report_dir$base'_contaminants/'$base'_contaminants.tsv' | cut -f1 | sed -e 's/^/grep "/g' -i -e 's/$/" '$blast'/g' > $report_dir$base'_contaminants/'$base'_get_cont_from_blast.sh'
+grep -v "#" $report_dir$base'_contaminants/'$base'_contaminants.tsv' | cut -f1 | sed -e 's/^/grep "/g' -e 's/$/" '$blast'/g' > $report_dir$base'_contaminants/'$base'_get_cont_from_blast.sh'
 # sh $report_dir$base'_contaminants/'$base'_get_cont_from_blast.sh' > $report_dir$base'_contaminants/'$base'_contaminants_blast.tsv'
