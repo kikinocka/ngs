556c440e34369d47f74934d7e97d9b36e5f63945
kikinocka <kika.zahonova@gmail.com>
2022-11-24 15:05:57 +0100

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N mrbayes
 #PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
-#PBS -l walltime=96:00:00
+#PBS -l walltime=168:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,14 +13,14 @@ module add mrbayes-3.2.7a
 data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes/'
 
 #copy files to scratch
-cp $data'hydA.CD.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'miro.CD.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 #proteins
-aln='hydA.CD.trimal_gt-0.8.nex'
+aln='miro.CD.trimal_gt-0.8.nex'
 
 mpirun -n $PBS_NUM_PPN mb-mpi $aln
 
