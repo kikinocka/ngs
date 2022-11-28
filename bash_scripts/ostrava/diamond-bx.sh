5322e0de16e9264b340e6490da6ca2430da393ae
kikinocka <kika.zahonova@gmail.com>
2022-11-28 10:56:49 +0000

    bla

---
+++ bash_scripts/ostrava/diamond-bx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -d .
+#PBS -v PATH
+#PBS -N diamond-bx
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=100:00:00
+
+cd '/mnt/data/kika/blastocrithidia/possible_cont/'
+
+db='/mnt/data/diamond_nr/prot.accession2taxid.FULL'
+program=blastx
+eval=1e-10
+max_seqs=1
+max_hsps=1
+
+for query in *.fa; do
+	echo $query
+	out=${query%.fa}'.nr_'$eval'.diamond_'$program
+	diamond $program \
+		--sensitive \
+		-q $query \
+		-d $db \
+		-o $out \
+		-p 20 \
+		-f "6 qseqid staxids bitscore std" \
+		--evalue $eval \
+		--max-target-seqs $max_seqs \
+		-max_hsps $max_hsps
+	echo ***Diamond done***
+done
--- py_scripts/get_seqs.py
+++ py_scripts/get_seqs.py
@@ -10,15 +10,15 @@ nd = {}
 for line in names:
 	nd[line.split('\t')[0]] = line.strip().split('\t')[1]
 
-with open('Trp-tRNAs_renamed.fa', 'w') as out:
+with open('Gln-tRNAs_renamed.fa', 'w') as out:
 	for file in files:
 		print(file)
 		fname = '{}_{}'.format(file.split('_')[0], file.split('_')[1])
 		c = 0
 		if fname in nd.keys():
 			for seq in SeqIO.parse(file, 'fasta'):
-				if 'Trp' in seq.name:
+				if 'Gln' in seq.name:
 					c += 1
-					out.write('>{}__{}__{}\n{}\n'.format(nd[fname], fname, c, seq.seq))
+					out.write('>{}__{}__{}__{}\n{}\n'.format(nd[fname], fname, seq.name.split('(')[1].split(')')[0], c, seq.seq))
 		else:
 			print('{} not in names'.format(fname))
