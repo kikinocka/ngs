6e7339c518ad5e0ba1b9962fd4a2b4bbe1335071
kikinocka <kika.zahonova@gmail.com>
2022-11-29 14:56:02 +0000

    bla

--- bash_scripts/metacentrum/iqtree-ASR_pbs.sh
+++ bash_scripts/metacentrum/iqtree-ASR_pbs.sh
@@ -14,17 +14,20 @@ data_dir='/storage/brno3-cerit/home/kika/trafficking/AP1S'
 
 #copy files to scratch
 cp $data_dir'/'*.aln $SCRATCHDIR
+cp $data_dir'/'*.constr1 $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
+constr1='AP1S.constr1'
 for f in *.aln ; do
 	guide=guide_${f%.aln}
 	guide_tree=$guide'.treefile'
 	bb=1000
 	nm=5000
 	# iqtree -m GTR+G -nt AUTO -ntmax $PBS_NUM_PPN -b $bb -quiet -s ${f}
-	iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -nm $nm -quiet -s ${f} -asr
+	# iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -nm $nm -quiet -s ${f} -asr
+	iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -nm $nm -quiet -s ${f} -g $constr1 -asr
 	# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
 	# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -nm $nm -quiet -s ${f} -ft $guide_tree #-wsr
 done
