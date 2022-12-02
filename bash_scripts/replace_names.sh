762871d1700c379bcace8c2bf3b6c3713702166e
kikinocka <kika.zahonova@gmail.com>
2022-12-02 15:49:44 +0100

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -3,10 +3,10 @@
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
 # cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_COPII/trees/hyp27/ver2/'
-cd '/Users/kika/ownCloud/archamoebae/trees/rho/ver7_muscle/'
+cd '/Users/kika/ownCloud/archamoebae/trees/ACS/ver5/'
 
-for aln in *.mafft.aln ; do
-	out=${aln%.mafft.aln}.CD.mafft.aln
-	table=${aln%.mafft.aln}.table
+for aln in *.aln ; do
+	out=${aln%.aln}.CD.aln
+	table=${aln%.aln}.table
 	perl $replacer -f $aln $out $table
 done
