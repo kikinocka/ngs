fe2957474e131a9340dcba81d0fbbc118099001b
kikinocka <kika.zahonova@gmail.com>
2022-12-06 14:14:13 +0100

    bla

--- py_scripts/retrieve_seqs.py
+++ py_scripts/retrieve_seqs.py
@@ -3,8 +3,8 @@ import os
 from Bio import SeqIO
 
 os.chdir('/mnt/data/kika/blastocrithidia/contaminations/')
-files = [x for x in os.listdir() if x.endswith('Oobo_contaminants.acc')]
-database = '/mnt/data/kika/blastocrithidia/o_oborniki/scaff_gap/Oobo.platanus_rnd2_scaffold.l500.gapcloser.fa'
+files = [x for x in os.listdir() if x.endswith('Ovol_contaminants.acc')]
+database = '/mnt/data/kika/blastocrithidia/o_volfi/scaff_gap/Ovol.platanus_rnd2_scaffold.l500.gapcloser.fa'
 
 for accessions in files: 
 	print(accessions)
