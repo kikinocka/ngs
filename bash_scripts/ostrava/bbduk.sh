ba0cc9090925aa8c3145f057afd471369527fdb3
kikinocka <kika.zahonova@gmail.com>
2022-12-13 15:55:24 +0100

    bla

--- bash_scripts/ostrava/bbduk.sh
+++ bash_scripts/ostrava/bbduk.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 #PBS -d .
 #PBS -v PATH
-#PBS -N karect
+#PBS -N bbduk
 #PBS -l nodes=1:ppn=10
 #PBS -l walltime=02:00:00
 
-
-raw_dir='/mnt/data/aalbanaz/Blastocrithidia_Obscuromonas/raw_reads/Obscuromonas_eliasi/'
-fwd=$raw_dir'PNG74_1.fastq.gz'
-rev=$raw_dir'PNG74_2.fastq.gz'
-
 adapt='/home/users/kika/bbmap/resources/adapters.fa'
 
-
-cd '/mnt/data/kika/blastocrithidia/o_eliasi/reads/'
-trimmed_fwd='PNG74_trimmed_1.fq.gz'
-trimmed_rev='PNG74_trimmed_2.fq.gz'
-report='oeli_bbduk.txt'
-len=75
+cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_triatomae/reads/'
+fwd=$raw_dir'Blastoc_triat_1.fastq.gz'
+rev=$raw_dir'Blastoc_triat_2.fastq.gz'
+trimmed_fwd='triat_trimmed_1.fq.gz'
+trimmed_rev='triat_trimmed_2.fq.gz'
+report='btri_bbduk.txt'
+len=50
 
 #illumina pair-end reads
 /home/users/kika/bbmap/bbduk.sh overwrite=true \
 	in1=$fwd in2=$rev \
 	out1=$trimmed_fwd out2=$trimmed_rev \
 	ref=$adapt \
+	minlen=$len \
 	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=10 2> $report
-	# minlen=$len 
