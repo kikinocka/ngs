8de37bce589b534b4a580d204978af53776ea85f
kikinocka <kika.zahonova@gmail.com>
2022-12-13 16:04:33 +0100

    bla

--- bash_scripts/ostrava/bbduk.sh
+++ bash_scripts/ostrava/bbduk.sh
@@ -5,18 +5,19 @@
 #PBS -l nodes=1:ppn=10
 #PBS -l walltime=02:00:00
 
+bbduk='/home/users/kika/bbmap/bbduk.sh'
 adapt='/home/users/kika/bbmap/resources/adapters.fa'
 
-cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_triatomae/reads/'
-fwd='Blastoc_triat_1.fastq.gz'
-rev='Blastoc_triat_2.fastq.gz'
-trimmed_fwd='triat_trimmed_1.fq.gz'
-trimmed_rev='triat_trimmed_2.fq.gz'
-report='btri_bbduk.txt'
+cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_frustrata/reads/'
+fwd='4FEM_1.fastq.gz'
+rev='4FEM_2.fastq.gz'
+trimmed_fwd='bfru_trimmed_1.fq.gz'
+trimmed_rev='bfru_trimmed_2.fq.gz'
+report='bfru_bbduk.txt'
 len=50
 
 #illumina pair-end reads
-/home/users/kika/bbmap/bbduk.sh overwrite=true \
+$bbduk overwrite=true \
 	in1=$fwd in2=$rev \
 	out1=$trimmed_fwd out2=$trimmed_rev \
 	ref=$adapt \
