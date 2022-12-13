3f90c296b93b31a98a3c85f2f1337475f7845bac
kikinocka <kika.zahonova@gmail.com>
2022-12-13 21:32:52 +0100

    bla

--- bash_scripts/ostrava/fastqc.sh
+++ bash_scripts/ostrava/fastqc.sh
@@ -6,10 +6,10 @@
 #PBS -l walltime=02:00:00
 
 
-cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_triatomae/reads/'
+cd '/mnt/data/kika/blastocrithidia/transcriptomes/o_eliasi/reads/'
 # fwd='4FEM_trimmed_75_1.fq'
 # rev='4FEM_trimmed_75_2.fq'
-report='btri_trimmed.fastqc.txt'
+report='oeli_trimmed.fastqc.txt'
 
 fastqc -o ./fastqc -t 5 *.gz 2> $report
 fastqc -o ./fastqc -t 5 $fwd $rev 2> $report
