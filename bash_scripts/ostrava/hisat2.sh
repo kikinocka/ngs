00b34184b9b06f2ac923ef6a4083bf37545ff8a2
kikinocka <kika.zahonova@gmail.com>
2022-12-14 10:19:26 +0100

    bla

--- bash_scripts/ostrava/hisat2.sh
+++ bash_scripts/ostrava/hisat2.sh
@@ -6,12 +6,12 @@
 #PBS -l walltime=50:00:00
 
 
-cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_spHR05/'
+cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_triatomae/'
 
-genome='/mnt/data/kika/blastocrithidia/genomes/final_assemblies/Braa_genome_final.fa'
-fw='reads/braa_trimmed_1.fq.gz'
-rv='reads/braa_trimmed_1.fq.gz'
-index='braa_ht2'
+genome='/mnt/data/kika/blastocrithidia/genomes/final_assemblies/Btri_genome_final.fa'
+fw='reads/btri_trimmed_1.fq.gz'
+rv='reads/btri_trimmed_1.fq.gz'
+index='btri_ht2'
 unmapped_unpaired=$index'_unmapped_unpaired.fq.gz'
 unmapped_paired=$index'_unmapped_paired.fq.gz'
 sam=$index'.sam'
@@ -27,3 +27,4 @@ samtools view -bS $sam > $bam -@ 20
 samtools sort -o $sorted -@ 20 $bam 
 samtools index $sorted
 
+python3 /home/users/kika/scripts/py_scripts/slackbot.py OSTRAVA server: hisat2 done
