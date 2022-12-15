49de8e66ad7627a1fce2e816900f125da1841928
kikinocka <kika.zahonova@gmail.com>
2022-12-15 14:38:27 +0100

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,8 +2,7 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-# cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_COPII/trees/hyp27/ver2/'
-cd '/Users/kika/ownCloud/archamoebae/trees/AS/ver2/'
+cd '/Users/kika/ownCloud/kinetoplastids/IDHs/tree/ver2/'
 
 for aln in *.aln ; do
 	out=${aln%.aln}.CD.aln
