223ca8c0d115f60dad6d54f45692b43f66863e36
kikinocka <kika.zahonova@gmail.com>
2022-12-16 21:39:20 +0100

    bla

--- bash_scripts/leshy/raxml.sh
+++ bash_scripts/leshy/raxml.sh
@@ -2,8 +2,8 @@
 
 cd '/mnt/mokosz/home/kika/archam_trees/raxml/'
 
-aln='acs.CD.trimal_gt-0.8.aln'
-out=${aln%.trimal_gt-0.8.aln}
+aln='aat.CD.trimal_gt-0.8.CD.aln'
+out=${aln%.trimal_gt-0.8.CD.aln}
 
 #proteins
 raxmlHPC-PTHREADS-AVX  -m PROTGAMMALG4XF -f a -T 10 -x 123 -N 100 -p 12345 -s $aln -n $out
--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,7 +2,7 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/kinetoplastids/IDHs/tree/ver2/'
+cd '/Users/kika/ownCloud/archamoebae/trees/AAT/ver6/'
 
 for aln in *.aln ; do
 	out=${aln%.aln}.CD.aln
