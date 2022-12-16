a657cf8a0b90c23574dffd01b1ec05e9e154340d
kikinocka <kika.zahonova@gmail.com>
2022-12-16 10:52:38 +0100

    bla

--- bash_scripts/leshy/targetp.sh
+++ bash_scripts/leshy/targetp.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-cd '/mnt/mokosz/home/kika/cresol-NAD/'
+cd '/mnt/mokosz/home/kika/workdir/'
 
 plant='pl'
 non_plant='non-pl'
 
 for file in *; do
 	echo $file
-	targetp -fasta $file -org $non_plant -format short
+	# targetp -fasta $file -org $non_plant -format short
+	targetp -fasta $file -org $plant -format short
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py TargetP done
