a1158c1f6eccb1f9f110cccdb38fdbb1c7f39951
kikinocka <kika.zahonova@gmail.com>
2022-12-19 16:35:35 +0100

    bla

--- bash_scripts/ostrava/codetta.sh
+++ bash_scripts/ostrava/codetta.sh
@@ -9,9 +9,7 @@ cd '/home/users/kika/blastocrithidia/final_assemblies/'
 
 codetta_dir='/home/users/kika/codetta/'
 codetta=$codetta_dir'codetta.py'
-profiles=$codetta_dir'resources/Pfam-A_enone.hmm'
-genome='/home/users/kika/blastocrithidia/final_assemblies/Braa_genome_final_masked.fa'
 
-python3 $codetta -p $profiles $genome
+python3 $codetta $genome
 
 python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: codetta done
