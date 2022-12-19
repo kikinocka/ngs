ac56dcddf7f8acc9d514bc90406241e3eee2b1ec
kikinocka <kika.zahonova@gmail.com>
2022-12-19 16:50:16 +0100

    bla

---
+++ bash_scripts/codetta.sh
@@ -0,0 +1,7 @@
+cd '/home/users/kika/blastocrithidia/codetta/'
+
+genome='/Users/kika/ownCloud/blasto_comparative/genomes/Braa_genome_final_masked.fa'
+codetta_dir='/home/users/kika/codetta/'
+codetta=$codetta_dir'codetta.py'
+
+python3 $codetta $genome
