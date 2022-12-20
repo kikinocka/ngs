802d9295fe8822b54344771944fa6eba30ef4c0a
kikinocka <kika.zahonova@gmail.com>
2022-12-20 14:40:34 +0100

    bla

--- bash_scripts/ostrava/stringtie.sh
+++ bash_scripts/ostrava/stringtie.sh
@@ -9,14 +9,18 @@
 cd '/mnt/data/kika/blastocrithidia/'
 
 stringtie='/home/users/kika/stringtie/stringtie'
-bam='transcriptomes/b_frustrata/hisat2/bfru_ht2_sorted.bam'
-gtf='transcriptomes/b_frustrata/stringtie/Bfru_ST.gtf'
-out='transcriptomes/b_frustrata/stringtie/Bfru_ST.fa'
-genome='genomes/final_assemblies/Bfru_genome_final_masked.fa'
+bam='transcriptomes/b_triatomae/hisat2/btri_ht2_sorted.bam'
+gtf='transcriptomes/b_triatomae/stringtie/Btri_ST.gtf'
+gff='transcriptomes/b_triatomae/stringtie/Btri_ST.gff'
+gff_transcript='transcriptomes/b_triatomae/stringtie/Btri_ST_transcripts.gff'
+out='transcriptomes/b_triatomae/stringtie/Btri_ST.fa'
+genome='genomes/final_assemblies/Btri_genome_final_masked.fa'
 
 $stringtie -v -o $gtf $bam -m 50 -p 30
 #-m 	minimum assembled transcript length (default: 200)
 
-bedtools getfasta -fi $genome -bed $gff -fo $out -name
+gffread $gtf -o $gff
+grep 'transcript' $gff > $gff_transcripts
+bedtools getfasta -fi $genome -bed $gff_transcripts -fo $out -name
 
 python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: StringTie done
