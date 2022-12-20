c7a09308aea7d204165c5d82ac09fc8cf5e6ea10
kikinocka <kika.zahonova@gmail.com>
2022-12-20 09:55:52 +0100

    bla

--- bash_scripts/codetta.sh
+++ bash_scripts/codetta.sh
@@ -2,7 +2,7 @@
 
 cd '/Users/kika/ownCloud/blasto_comparative/codetta/'
 
-genome='/Users/kika/ownCloud/blasto_comparative/genomes/Braa_genome_final_masked.fa'
+genome='/Users/kika/ownCloud/blasto_comparative/genomes/Bfru_genome_final_masked.fa'
 codetta_dir='/Users/kika/programs/codetta/'
 codetta=$codetta_dir'codetta.py'
 
