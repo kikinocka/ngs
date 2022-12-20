280163d6a768bbd151072506283afa0b1f24a417
kikinocka <kika.zahonova@gmail.com>
2022-12-20 16:52:07 +0100

    bla

--- bash_scripts/ostrava/stringtie.sh
+++ bash_scripts/ostrava/stringtie.sh
@@ -9,12 +9,12 @@
 cd '/mnt/data/kika/blastocrithidia/'
 
 stringtie='/home/users/kika/stringtie/stringtie'
-bam='transcriptomes/o_modryi/hisat2/omod_ht2_sorted.bam'
-gtf='transcriptomes/o_modryi/stringtie/Omod_ST.gtf'
-gff='transcriptomes/o_modryi/stringtie/Omod_ST.gff'
-gff_transcripts='transcriptomes/o_modryi/stringtie/Omod_ST_transcripts.gff'
-out='transcriptomes/o_modryi/stringtie/Omod_ST.fa'
-genome='genomes/final_assemblies/Omod_genome_final_masked.fa'
+bam='transcriptomes/o_oborniki/hisat2/oobo_ht2_sorted.bam'
+gtf='transcriptomes/o_oborniki/stringtie/Oobo_ST.gtf'
+gff='transcriptomes/o_oborniki/stringtie/Oobo_ST.gff'
+gff_transcripts='transcriptomes/o_oborniki/stringtie/Oobo_ST_transcripts.gff'
+out='transcriptomes/o_oborniki/stringtie/Oobo_ST.fa'
+genome='genomes/final_assemblies/Oobo_genome_final_masked.fa'
 
 $stringtie -v -o $gtf $bam -m 50 -p 30
 #-m 	minimum assembled transcript length (default: 200)
