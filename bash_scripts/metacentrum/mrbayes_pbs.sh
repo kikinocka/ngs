6bb147a1a065e3de32888b79f4adcb0ecea16f03
kikinocka <kika.zahonova@gmail.com>
2022-12-21 09:54:59 +0100

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -10,10 +10,10 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/kinetoplastids/IDH/'
+data='/storage/brno3-cerit/home/kika/kinetoplastids/IDH'
 
 #copy files to scratch
-cp $data'IDHs.linsi.automated1.CD.nex' $SCRATCHDIR
+cp $data'/'* $SCRATCHDIR
 
 
 #compute on scratch
