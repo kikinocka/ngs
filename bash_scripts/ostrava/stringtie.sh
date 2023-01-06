fcab47cdb8d5eb072e10c17f9c8512e47cdd26aa
kikinocka <kika.zahonova@gmail.com>
2023-01-06 16:08:11 +0100

    bla

--- bash_scripts/ostrava/stringtie.sh
+++ bash_scripts/ostrava/stringtie.sh
@@ -9,12 +9,12 @@
 cd '/mnt/data/kika/blastocrithidia/'
 
 stringtie='/home/users/kika/stringtie/stringtie'
-bam='transcriptomes/o_volfi/hisat2/ovol_ht2_sorted.bam'
-gtf='transcriptomes/o_volfi/stringtie/Ovol_ST.gtf'
-gff='transcriptomes/o_volfi/stringtie/Ovol_ST.gff'
-gff_transcripts='transcriptomes/o_volfi/stringtie/Ovol_ST_transcripts.gff'
-out='transcriptomes/o_volfi/stringtie/Ovol_ST.fa'
-genome='genomes/final_assemblies/Ovol_genome_final_masked.fa'
+bam='transcriptomes/b_spHR05/hisat2/final_corrected2/braa_cor2_ht2_sorted.bam'
+gtf='transcriptomes/b_spHR05/stringtie/Braa_ST.gtf'
+gff='transcriptomes/b_spHR05/stringtie/Braa_ST.gff'
+gff_transcripts='transcriptomes/b_spHR05/stringtie/Braa_ST_transcripts.gff'
+out='transcriptomes/b_spHR05/stringtie/Braa_ST.fa'
+genome='genomes/final_assemblies/Braa_genome_final_corrected2_masked.fa'
 
 $stringtie -v -o $gtf $bam -m 50 -p 30
 #-m 	minimum assembled transcript length (default: 200)
