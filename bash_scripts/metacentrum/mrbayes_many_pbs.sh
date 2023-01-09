76c5018857c8f7e14f103ddd51d8ff20aad83748
kikinocka <kika.zahonova@gmail.com>
2023-01-09 14:51:55 +0100

    bla

--- bash_scripts/metacentrum/mrbayes_many_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_many_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N mrbayes-many
 #PBS -l select=1:ncpus=4:mem=100gb:scratch_local=1gb:os=debian11
-#PBS -l walltime=96:00:00
+#PBS -l walltime=168:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,10 +10,10 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes'
+data='/storage/brno3-cerit/home/kika/kinetoplastids/IDH/subtrees'
 
 #copy files to scratch
-cp $data'/aat.CD.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'/'* $SCRATCHDIR
 
 
 #compute on scratch
--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,9 +2,9 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/archamoebae/trees/AK/ver7/'
+cd '/Users/kika/ownCloud/kinetoplastids/IDH/tree/subtrees/'
 
-for aln in *.aln ; do
+for aln in *automated1.aln ; do
 	out=${aln%.aln}.CD.aln
 	table=${aln%.aln}.table
 	perl $replacer -f $aln $out $table
