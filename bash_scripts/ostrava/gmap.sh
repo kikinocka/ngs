9ee804832c25a6d2065f72394861213d1cd7e6a1
kikinocka <kika.zahonova@gmail.com>
2023-01-10 11:17:00 +0100

    bla

---
+++ bash_scripts/ostrava/gmap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -d .
+#PBS -v PATH
+#PBS -N gmap
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=100:00:00
+
+
+cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_spHR05/trinity/'
+
+genome='/mnt/data/kika/blastocrithidia/genomes/final_assemblies/Braa_genome_final_corrected2_masked.fa'
+species='braa'
+
+gmap_build -D . -d $species'_gmap' $genome
+gmap -D . -d $species'_gmap' -B 4 -t 20 -f gff3_match_cdna Trinity.fasta > Trinity_gmap.gff3
+gmap -D . -d $species'_gmap' -B 4 -t 20 -f samse Trinity.fasta > Trinity_gmap.sam
+samtools sort -o Trinity_gmap.bam -@ 20 Trinity_gmap.sam
+samtools index -b Trinity_gmap.bam
