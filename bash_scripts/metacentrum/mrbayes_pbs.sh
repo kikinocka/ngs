1d6da97c41b1754d6b97fb583d1dfe54524b6f2e
kikinocka <kika.zahonova@gmail.com>
2023-01-11 14:45:58 +0100

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -10,10 +10,10 @@ cat $PBS_NODEFILE
 #add module
 module add mrbayes-3.2.7a
 
-data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes'
+data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes/'
 
 #copy files to scratch
-cp $data'/ak.CD.'* $SCRATCHDIR
+cp $data'ak.CD.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
