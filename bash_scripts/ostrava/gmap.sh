afe2460603c21147b08393d2a5cf283dd7384599
kikinocka <kika.zahonova@gmail.com>
2023-01-12 10:05:00 +0100

    bla

--- bash_scripts/ostrava/gmap.sh
+++ bash_scripts/ostrava/gmap.sh
@@ -6,10 +6,10 @@
 #PBS -l walltime=100:00:00
 
 
-cd '/mnt/data/kika/blastocrithidia/transcriptomes/b_spHR05/trinity/'
+cd '/mnt/data/kika/blastocrithidia/transcriptomes/o_volfi/trinity/'
 
-genome='/mnt/data/kika/blastocrithidia/genomes/final_assemblies/Braa_genome_final_corrected2_masked.fa'
-species='braa'
+genome='/mnt/data/kika/blastocrithidia/genomes/final_assemblies/Ovol_genome_final_masked.fa'
+species='ovol'
 
 gmap_build -D . -d $species'_gmap' $genome
 gmap -D . -d $species'_gmap' -B 4 -t 20 -f gff3_match_cdna Trinity.fasta > Trinity_gmap.gff3
