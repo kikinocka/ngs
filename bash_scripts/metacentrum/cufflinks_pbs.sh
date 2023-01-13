6b6d43b6ed2dfe1c213313eae7b5be1692cf8569
kikinocka <kika.zahonova@gmail.com>
2023-01-13 15:24:59 +0100

    bla

--- bash_scripts/metacentrum/cufflinks_pbs.sh
+++ bash_scripts/metacentrum/cufflinks_pbs.sh
@@ -9,25 +9,25 @@ cat $PBS_NODEFILE
 
 #add modules
 module add cufflinks-2.2.1
-# module add bedtools-2.26.0
 
 gffread='/storage/brno3-cerit/home/kika/miniconda3/pkgs/gffread-0.12.7-h9a82719_0/bin/gffread'
-bam_dir='/storage/brno3-cerit/home/kika/blasto_comparative/hisat2/oeli'
-out_dir='/storage/brno3-cerit/home/kika/blasto_comparative/cufflinks/oeli/'
+bam_dir='/storage/brno3-cerit/home/kika/blasto_comparative/hisat2/oobo'
+out_dir='/storage/brno3-cerit/home/kika/blasto_comparative/cufflinks/oobo/'
 gen_dir='/storage/brno3-cerit/home/kika/blasto_comparative/final_genomes/'
+annotscript='/storage/brno2/home/kika/scripts/others/getAnnoFasta.pl'
 
 #copy files to scratch
 cp $bam_dir'/'*_sorted.bam $SCRATCHDIR
-# cp $gen_dir'Omod_genome_final_masked.fa' $SCRATCHDIR
+cp $gen_dir'Oobo_genome_final_masked.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-species='Oeli'
-# genome=$species'_genome_final_masked.fa'
+species='Oobo'
 cufflinks -p $PBS_NUM_PPN -o . *_sorted.bam
 $gffread transcripts.gtf -o $species'_cufflinks.gff'
-# bedtools getfasta -fi $genome -bed $species'_transcripts_cufflinks.gff' -fo $species'_transcripts_cufflinks.fa'
+perl $annotscript --seqfile=$species'_genome_final_masked.fa' --protein=off --codingseq=on $species'_cufflinks.gff'
+awk 'BEGIN{FS=" "}{if(!/>/){print toupper($0)}else{print $1}}' $species'_cufflinks.mrna' > $species'_cufflinks.fa'
 
 #copy files back
 rm *_sorted.bam $genome
