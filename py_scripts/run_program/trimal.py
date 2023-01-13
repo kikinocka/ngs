fa1615c2bc9c5cde0d891f0303f283853b69b234
kikinocka <kika.zahonova@gmail.com>
2023-01-13 16:13:48 +0100

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/archamoebae/trees/GCS-L/ver7/')
+os.chdir('/Users/kika/ownCloud/archamoebae/trees/GCS-L/ver8/')
 files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 # files = [x for x in os.listdir() if x.endswith('.einsi.aln')]
 
