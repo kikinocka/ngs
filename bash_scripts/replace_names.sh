2cdbec440ac6085541d9498c4b5383cea59ac162
kikinocka <kika.zahonova@gmail.com>
2023-01-13 16:17:16 +0100

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,9 +2,9 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/kinetoplastids/IDH/tree/subtrees/'
+cd '/Users/kika/ownCloud/archamoebae/trees/GCS-L/ver8/'
 
-for aln in *automated1.aln ; do
+for aln in *.aln ; do
 	out=${aln%.aln}.CD.aln
 	table=${aln%.aln}.table
 	perl $replacer -f $aln $out $table
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -4,7 +4,7 @@ import os
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
 os.chdir('/Users/kika/ownCloud/archamoebae/trees/GCS-L/ver8/')
-files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
+files = [x for x in os.listdir() if x.endswith('.mafft.CD.aln')]
 # files = [x for x in os.listdir() if x.endswith('.einsi.aln')]
 
 for file in files:
