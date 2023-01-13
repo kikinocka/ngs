d42b421f5efb0656416bac9e8a274d78423e8b60
kikinocka <kika.zahonova@gmail.com>
2023-01-13 16:20:55 +0100

    bla

--- bash_scripts/leshy/mrbayes_veles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd '/mnt/mokosz/home/kika/archam_trees/'
-
-aln='nifS.trimal_gt-0.8.nex'
-mpirun -np 4 mb -i $aln
-
-python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MrBayes done
