3d8902f75d4f1bc745b83b519f6ba0c74d64f068
kikinocka <kika.zahonova@gmail.com>
2023-01-18 11:33:18 +0100

    bla

--- py_scripts/split_seqs_files.py
+++ py_scripts/split_seqs_files.py
@@ -3,8 +3,8 @@ import os
 from Bio import SeqIO
 
 
-os.chdir('/Users/kika/ownCloud/proteromonas/peroxisome/amoebae_stramenopiles/queries/')
+os.chdir('/Users/kika/ownCloud/proteromonas/peroxisome/peroxins/')
 
-for seq in SeqIO.parse('sequence.fasta', 'fasta'):
+for seq in SeqIO.parse('pram_peroxins.fa', 'fasta'):
 	with open('{}.fa'.format(seq.name), 'w') as result:
 		result.write('>{}\n{}\n'.format(seq.description, seq.seq))
