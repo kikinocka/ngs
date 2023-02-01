4c2fadb46f515d6e67e2960263f6f3f9c8d58bab
kikinocka <kika.zahonova@gmail.com>
2023-02-01 13:50:41 +0100

    bla

--- bash_scripts/metacentrum/mrbayes_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_pbs.sh
@@ -13,13 +13,13 @@ module add mrbayes-3.2.7a
 data='/storage/brno3-cerit/home/kika/archamoebae/mrbayes/'
 
 #copy files to scratch
-cp $data'ak.CD.trimal_gt-0.8.nex' $SCRATCHDIR
+cp $data'transporters.CD.trimal_gt-0.8.nex' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='ak.CD.trimal_gt-0.8.nex'
+aln='transporters.CD.trimal_gt-0.8.nex'
 
 mpirun -n $PBS_NUM_PPN mb-mpi $aln
 
