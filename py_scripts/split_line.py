9c47d2f18ce7e6e013dac04382ed86483fc0e0c0
kikinocka <kika.zahonova@gmail.com>
2023-02-03 13:36:18 +0100

    bla

--- py_scripts/split_line.py
+++ py_scripts/split_line.py
@@ -6,7 +6,7 @@ gff = open('Omod_genome_final_masked.all.gff')
 
 with open('Omod_genome_final_masked.all_upd.gff', 'w') as out:
 	for line in gff:
-		if '_AED' not it line:
+		if '_AED' not in line:
 			out.write(line)
 		else:
 			aed = float(line.split('\t')[-1].split(';')[3].split('=')[1])
