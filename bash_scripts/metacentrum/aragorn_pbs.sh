dbf1f762c0f0932e80b3805aea606ed876fde9d9
kikinocka <kika.zahonova@gmail.com>
2023-02-09 17:57:01 +0100

    bla

--- bash_scripts/metacentrum/aragorn_pbs.sh
+++ bash_scripts/metacentrum/aragorn_pbs.sh
@@ -8,26 +8,26 @@
 cat $PBS_NODEFILE
 
 aragorn='/storage/brno3-cerit/home/kika/miniconda3/pkgs/aragorn-1.2.38-h779adbc_4/bin/aragorn'
-data_dir='/storage/brno3-cerit/home/kika/p57/genome'
+data_dir='/storage/brno3-cerit/home/kika/gln-tRNA'
 
 #copy files to scratch
-cp $data_dir'/'*.fa $SCRATCHDIR
+cp $data_dir'/'*.fna $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-for genome in *.fa ; do
+for genome in *.fna ; do
 	#no secondary structures
-	fout=${genome%.fa}.aragorn.fa
+	fout=${genome%.fna}.aragorn.fa
 	$aragorn -t -fo -o $fout $genome
 	
 	#with secondary structures
-	sout=${genome%.fa}.aragorn_structures.txt
+	sout=${genome%.fna}.aragorn_structures.txt
 	$aragorn -t -o $sout $genome
 done
 
 
 #copy files back
-rm *masked.fa
+rm *.fna
 cp * $data_dir
--- bash_scripts/tRNAscan.sh
+++ bash_scripts/tRNAscan.sh
@@ -2,14 +2,14 @@
 
 tRNAscan='/Users/kika/miniconda3/bin/tRNAscan-SE'
 
-cd '/Users/kika/ownCloud/blastocrithidia/genome_assembly/'
+cd '/Users/kika/data/opisthokonta/'
 
-for genome in p57_polished.fa ; do
+for genome in *genomic.fna ; do
 	echo $genome
 
-	table=${genome%.fa}.tRNAscan_table.tsv
-	seq=${genome%.fa}.tRNAscan.fa
-	structures=${genome%.fa}.tRNAscan_structures.txt
+	table=${genome%.fna}.tRNAscan_table.tsv
+	seq=${genome%.fna}.tRNAscan.fa
+	structures=${genome%.fna}.tRNAscan_structures.txt
 
-	$tRNAscan --thread 7 -o $table -a $seq -f $structures ${genome}
+	$tRNAscan --thread 6 -o $table -a $seq -f $structures ${genome}
 done
