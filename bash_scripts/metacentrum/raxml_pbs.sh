513f4a17608151a962d49c9a3503cbe22657e79d
kikinocka <kika.zahonova@gmail.com>
2023-02-09 09:27:58 +0100

    bla

--- bash_scripts/metacentrum/raxml_pbs.sh
+++ bash_scripts/metacentrum/raxml_pbs.sh
@@ -22,7 +22,7 @@ cd $SCRATCHDIR
 
 #proteins
 aln='transporters.CD.trimal_gt-0.8.aln'
-out=${aln%.trimal_gt-0.8.aln}_new
+out=${aln%.trimal_gt-0.8.aln}
 
 raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N 100 -p 12345 -s $aln -n $out
 
