48a05a4ba227ba21d77052be0ce1caa2f4bf672d
kikinocka <kika.zahonova@gmail.com>
2023-02-10 17:01:26 +0100

    bla

--- bash_scripts/metacentrum/interproscan_pbs.sh
+++ bash_scripts/metacentrum/interproscan_pbs.sh
@@ -11,7 +11,7 @@ cat $PBS_NODEFILE
 #add module
 module add interproscan-5.55-88.0
 
-datadir='/storage/brno3-cerit/home/kika/pelomyxa/predicted_proteins_transdecoder/'
+datadir='/storage/brno3-cerit/home/kika/p57/predicted_proteins/'
 
 #copy files to scratch
 cp $datadir'bnon_nohit.fa' $SCRATCHDIR
@@ -19,7 +19,7 @@ cp $datadir'bnon_nohit.fa' $SCRATCHDIR
 #compute on scratch
 cd $SCRATCHDIR
 input='bnon_nohit.fa'
-out='bnon_nohit.interpro.out'
+out='bnon_nohit.interpro'
 
 interproscan.sh -dp -f TSV,GFF3 -T $SCRATCHDIR -cpu $PBS_NUM_PPN -i $input -b $out -appl Pfam -goterms --pathways -iprlookup
 # -appl PRINTS,Pfam,Hamap,ProSitePatterns,ProSiteProfiles,Panther 
