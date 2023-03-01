9055379df2d46d21254c2cb1afbbbf256a526f81
kikinocka <kika.zahonova@gmail.com>
2023-03-01 17:23:34 +0100

    bla

--- bash_scripts/split_fasta.sh
+++ bash_scripts/split_fasta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 workdir='/Users/kika/ownCloud/blasto_comparative/genomes/'
-seqs=$workdir'Oeli_genome_final_masked.fa'
+seqs=$workdir'Ovol_genome_final_masked.fa'
 
 cd $workdir
 awk 'BEGIN {n_seq=0;} /^>/ {if(n_seq%3000==0){file=sprintf("myseq%d.fa",n_seq);} print >> file; n_seq++; next;} \
