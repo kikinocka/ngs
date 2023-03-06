5d0c0f6c6f5cbfb64ff014159a0a4970035d4f52
kikinocka <kika.zahonova@gmail.com>
2023-03-06 17:02:20 +0100

    bla

--- bash_scripts/metacentrum/bedtools_genomecov_pbs.sh
+++ bash_scripts/metacentrum/bedtools_genomecov_pbs.sh
@@ -12,20 +12,20 @@ source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module load bedtools
 
 assembly_dir='/storage/brno3-cerit/home/kika/blasto_comparative/final_genomes/'
-mapping_dir='/storage/brno3-cerit/home/kika/blasto_comparative/hisat2/bfru/'
+mapping_dir='/storage/brno3-cerit/home/kika/blasto_comparative/hisat2/braa/'
 
 
 #copy files to scratch
-cp $assembly_dir'Bfru_genome_final_masked.fa' $SCRATCHDIR
-cp $mapping_dir'bfru_ht2_sorted.bam' $SCRATCHDIR
+cp $assembly_dir'Braa_genome_final_corrected2_masked.fa' $SCRATCHDIR
+cp $mapping_dir'braa_ht2_sorted.bam' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-genome='Bfru_genome_final_masked.fa'
-bamfile='bfru_ht2_sorted.bam'
-out='bfru_gencov.tsv'
+genome='Braa_genome_final_corrected2_masked.fa'
+bamfile='braa_ht2_sorted.bam'
+out='braa_gencov.tsv'
 
 bedtools genomecov -bga -split -ibam $bamfile -g $genome > $out
 
