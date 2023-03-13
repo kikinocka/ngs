89896ede3b8dd4a06ea92e45f3967169117a547d
kikinocka <kika.zahonova@gmail.com>
2023-03-13 11:30:01 +0100

    bla

--- bash_scripts/metacentrum/mrbayes_many_pbs.sh
+++ bash_scripts/metacentrum/mrbayes_many_pbs.sh
@@ -13,7 +13,7 @@ module add mrbayes-3.2.7a
 data='/storage/brno3-cerit/home/kika/trafficking/diplonemids_all/all_coats/ver5/mrbayes'
 
 #copy files to scratch
-cp $data'/'*.nex $SCRATCHDIR
+cp $data'/'* $SCRATCHDIR
 
 
 #compute on scratch
--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/Dsl1/rint1/RAxML/')
-trees = [x for x in os.listdir() if x.endswith('RAxML_bipartitions.rint1.CD')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver5/MrBayes/')
+trees = [x for x in os.listdir() if x.endswith('con.tre')]
 
 #file in format Acc. number \t name of organism \n
-names = open('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/Dsl1/rint1/rint1_codes_names.txt')
+names = open('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver5/large-beta_codes_acc.txt')
 
 name_dict = {}
 for name in names:
