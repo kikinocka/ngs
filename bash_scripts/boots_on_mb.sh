6b525ddf3f57f707c082732665d8afd58d11a54a
kikinocka <kika.zahonova@gmail.com>
2023-03-23 11:01:16 +0100

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -2,10 +2,10 @@
 
 placer='/Users/kika/scripts/py_scripts/boots_on_mb.py'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/Dsl1/zw10-dsl1/combined/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/retromer-retriever/vps26-DSCR3/ver3/combined/'
 
-tree=$path'zw10.CD.trimal_gt-0.8.nex.con.tre'
-bootstrap=$path'RAxML_bootstrap.zw10.CD'
-out=$path'zw10-dsl1.CD.mb+raxml.tre'
+tree=$path'vps26.CD.trimal_gt-0.8.nex.con.tre'
+bootstrap=$path'RAxML_bootstrap.vps26.CD'
+out=$path'vps26.CD.mb+raxml.tre'
 
 $placer $tree $bootstrap $out
