98b041ea63e19d8a118222edfb13f02b1a6dd565
kikinocka <kika.zahonova@gmail.com>
2023-05-15 16:45:44 +0200

    bla

--- bash_scripts/get_sra_fastq.sh
+++ bash_scripts/get_sra_fastq.sh
@@ -3,11 +3,10 @@
 # raw_dir='/home/kika/'
 # fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip -O $raw_dir SRR2048652
 
-module add conda/modules
-conda activate sra-tools-3.0.3
+module add sratools-2.3.2
 
-cd '/storage/brno3-cerit/home/kika/amoebophrya/'
-wget https://sra-pub-run-odp.s3.amazonaws.com/sra/SRR1610334/SRR1610334
+cd '/storage/brno3-cerit/home/kika/amoebophrya'
+# wget https://sra-pub-run-odp.s3.amazonaws.com/sra/SRR1610334/SRR1610334
 echo 'SRA SRR1610334 downloaded'
 fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files --gzip SRR1610334
 echo 'SRA SRR1610334 split to pair-end files'
