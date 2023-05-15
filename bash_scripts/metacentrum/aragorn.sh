716de61f4b041a77e51d7a6eb2aae3bd92ea3271
kikinocka <kika.zahonova@gmail.com>
2023-05-15 20:47:17 +0200

    bla

--- bash_scripts/metacentrum/aragorn.sh
+++ bash_scripts/metacentrum/aragorn.sh
@@ -8,26 +8,26 @@
 cat $PBS_NODEFILE
 
 aragorn='/storage/brno3-cerit/home/kika/miniconda3/pkgs/aragorn-1.2.38-h779adbc_4/bin/aragorn'
-data_dir='/storage/brno3-cerit/home/kika/gln-tRNA'
+data_dir='/storage/brno3-cerit/home/kika/amoebophrya/'
 
 #copy files to scratch
-cp $data_dir'/'*.fna $SCRATCHDIR
+cp $data_dir'spades/scaffolds.fasta' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-for genome in *.fna ; do
+for genome in *.fasta ; do
 	#no secondary structures
-	fout=${genome%.fna}.aragorn.fa
+	fout=${genome%.fasta}.aragorn.fa
 	$aragorn -t -fo -o $fout $genome
 	
 	#with secondary structures
-	sout=${genome%.fna}.aragorn_structures.txt
+	sout=${genome%.fasta}.aragorn_structures.txt
 	$aragorn -t -o $sout $genome
 done
 
 
 #copy files back
-rm *.fna
+rm *.fasta
 cp * $data_dir
