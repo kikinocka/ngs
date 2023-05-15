07e1ce67c4aa0aecfd07bb6edc9ce19b8a96e5e7
kikinocka <kika.zahonova@gmail.com>
2023-05-15 15:42:05 +0200

    bla

--- bash_scripts/metacentrum/interproscan.sh
+++ bash_scripts/metacentrum/interproscan.sh
@@ -2,7 +2,7 @@
 #PBS -N InterProScan
 #PBS -q default
 #PBS -l select=1:ncpus=10:mem=5gb:scratch_local=50gb
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,11 +11,10 @@ cat $PBS_NODEFILE
 #add module
 module add interproscan-5.55-88.0
 
-datadir='/storage/brno3-cerit/home/kika/blasto_comparative/proteins_obscuro'
+datadir='/storage/brno3-cerit/home/kika/blasto_comparative/proteins_blasto'
 
 #copy files to scratch
-# cp $datadir'/'*.faa $SCRATCHDIR
-cp $datadir'/Ovol_companion.l30.faa' $SCRATCHDIR
+cp $datadir'/'*final.faa $SCRATCHDIR
 
 
 #compute on scratch
@@ -24,8 +23,9 @@ cd $SCRATCHDIR
 for fasta in *.faa; do
 	echo $fasta
 	out=${fasta%.faa}.interpro
-	interproscan.sh -dp -f TSV,GFF3 -T $SCRATCHDIR -cpu $PBS_NUM_PPN -i $fasta -b $out -appl Pfam -goterms --pathways -iprlookup
-	# -appl PRINTS,Pfam,Hamap,ProSitePatterns,ProSiteProfiles,Panther 
+	interproscan.sh -dp -f TSV -T $SCRATCHDIR -cpu $PBS_NUM_PPN -i $fasta -b $out -appl Pfam -goterms --pathways -iprlookup
+	#-f TSV,GFF3
+	#-appl PRINTS,Pfam,Hamap,ProSitePatterns,ProSiteProfiles,Panther 
 done
 
 #copy files back
