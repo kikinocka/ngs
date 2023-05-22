1c4f0aacdcfadaf2bc9b7c83e660fcf47ce56b83
kikinocka <kika.zahonova@gmail.com>
2023-05-22 09:36:15 +0200

    bla

--- bash_scripts/metacentrum/blast_many3.sh
+++ bash_scripts/metacentrum/blast_many3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N blast-many
+#PBS -N blast-many4
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=30gb
 #PBS -l walltime=196:00:00
 #PBS -m ae
@@ -16,7 +16,7 @@ db_dir='/storage/projects/BlastDB/'
 
 
 #copy files to scratch
-cp $datadir'/check_cont2.fa' $SCRATCHDIR
+cp $datadir'/check_cont4.fa' $SCRATCHDIR
 cp $db_dir'nt'* $SCRATCHDIR
 
 #run on scratch
@@ -33,7 +33,7 @@ max_hsps=1
 for query in *.fa; do
 	echo $query
 	# out=${query%.fa}'.nr_'$eval'.'$program
-	out='check_cont2.fwd_ncbi-nt.tsv'
+	out='check_cont4.fwd_ncbi-nt.tsv'
 	$program -task $task \
 		-query $query \
 		-db $db \
--- bash_scripts/split_fasta.sh
+++ bash_scripts/split_fasta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-workdir='/Users/kika/ownCloud/blasto_comparative/genomes/'
-seqs=$workdir'Ovol_genome_final_masked.fa'
+workdir='/Users/kika/ownCloud/oil_sands/amplicons/18S-V4-2018/filtration/'
+seqs=$workdir'check_cont2.fa'
 
 cd $workdir
-awk 'BEGIN {n_seq=0;} /^>/ {if(n_seq%3000==0){file=sprintf("myseq%d.fa",n_seq);} print >> file; n_seq++; next;} \
+awk 'BEGIN {n_seq=0;} /^>/ {if(n_seq%100000==0){file=sprintf("myseq%d.fa",n_seq);} print >> file; n_seq++; next;} \
 { print >> file; }' < $seqs
