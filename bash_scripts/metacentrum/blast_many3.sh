cef450f1889034e8e8b65ccd62b573e05828d7f0
kikinocka <kika.zahonova@gmail.com>
2023-05-22 13:32:46 +0200

    bla

--- bash_scripts/metacentrum/blast_many3.sh
+++ bash_scripts/metacentrum/blast_many3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N blast-many4
-#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=30gb
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=50gb
 #PBS -l walltime=196:00:00
 #PBS -m ae
 #PBS -j oe
