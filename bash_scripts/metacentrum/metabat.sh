fb98c3f3a6decbb7c64a5b65958c97fbc75cdc62
kikinocka <kika.zahonova@gmail.com>
2023-06-02 09:30:25 +0200

    bla

--- bash_scripts/metacentrum/metabat.sh
+++ bash_scripts/metacentrum/metabat.sh
@@ -23,9 +23,10 @@ genome='GCA_001499635.1_Condy_MAC_genomic.fna'
 bamfile='condy_sorted.bam'
 depth='condy_depth.txt'
 min_contig=1500
+min_samples=3
 
 jgi_summarize_bam_contig_depths --outputDepth $depth $bamfile
-metabat1 -t $PBS_NUM_PPN -m $min_contig -i $genome -a $depth -o $SCRATCHDIR
+metabat1 -t $PBS_NUM_PPN -m $min_contig -i --minSamples $min_samples $genome -a $depth -o $SCRATCHDIR
 
 #copy files back
 rm $genome $bamfile
