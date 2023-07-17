cca5a3120d16352f5d3d34057c207b344bda12f5
kikinocka <kika.zahonova@gmail.com>
2023-07-17 21:10:11 +0200

    bla

--- bash_scripts/metacentrum/pynast.sh
+++ bash_scripts/metacentrum/pynast.sh
@@ -8,25 +8,28 @@
 cat $PBS_NODEFILE
 
 #add module
-module add qiime-1.9.1
+# module add qiime-1.9.1
+source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
+module load qiime
 
-data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonada/'
+
+data_dir='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/euglenozoa/'
 
 #copy files to scratch
-cp $data_dir'arb-silva.de_2021-09-16_id1053505.upd.aln' $SCRATCHDIR
-cp $data_dir'metamonads_eukref-otus.unambiguous.fa' $SCRATCHDIR
+cp $data_dir'euglenozoa.mafft.aln' $SCRATCHDIR
+cp $data_dir'V9.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-existing='arb-silva.de_2021-09-16_id1053505.upd.aln'
-add='metamonads_eukref-otus.unambiguous.fa'
-aln='metamonads_V9.pynast.aln'
-log='metamonads_V9.pynast.log'
-length=50
+existing='euglenozoa.mafft.aln'
+add='V9.fa'
+aln='euglenozoa_V9.pynast.aln'
+log='euglenozoa_V9.pynast.log'
+# length=50
 
-pynast -a $aln -g $log -l $length -i $add -t $existing
-
+pynast -a $aln -g $log -i $add -t $existing
+# -l $length 
 
 #copy files back
 rm $existing $add
