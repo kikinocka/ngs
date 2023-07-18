e3bf42c3d823f8aeca8808a82470a1b6fcd3f670
kikinocka <kika.zahonova@gmail.com>
2023-07-18 18:15:55 +0200

    bla

--- bash_scripts/metacentrum/raxml.sh
+++ bash_scripts/metacentrum/raxml.sh
@@ -11,29 +11,29 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
-data='/storage/brno3-cerit/home/kika/archamoebae/raxml/ver4/'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/heterolobosea/ref_tree/'
 
 #copy files to scratch
-cp $data'transporters.CD.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data'heterolobosea.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-#proteins
-aln='transporters.CD.trimal_gt-0.8.aln'
-out=${aln%.trimal_gt-0.8.aln}
-
-raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N 100 -p 12345 -s $aln -n $out
-
-
-# #18S
-# aln='metamonads-AFR1.trimal_gt-0.8.aln'
+# #proteins
+# aln='transporters.CD.trimal_gt-0.8.aln'
 # out=${aln%.trimal_gt-0.8.aln}
 
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
+# raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N 100 -p 12345 -s $aln -n $out
+
+
+#18S
+aln='heterolobosea.trimal_gt-0.25_cons-50.aln'
+out=${aln%.trimal_gt-0.25_cons-50.aln}
+
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
 
 
 #copy files back
