a4f4f39107cea1cdb4d2c436013810b9b664504e
kikinocka <kika.zahonova@gmail.com>
2023-07-19 10:30:29 +0200

    bla

---
+++ py_scripts/down_extract_rename.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import pandas as pd
+
+
+os.chdir('/mnt/mokosz/home/kika/allDB/bacteria/rest/')
+table = pd.read_excel('metamonada.xlsx', sheet_name='bacteria')
+errors = open('errors.txt')
+
+for index, row in table.iterrows():
+	fname = row[4]
+	link = row[8]
+	unzipped = link.split('/')[-1].split('.gz')[0]
+	print(fname)
+	# print(link)
+	# print(unzipped)
+
+	try:
+		os.system('wget {}'.format(link))
+		os.system('gzip -d {}.gz'.format(unzipped))
+		os.system('mv {} {}'.format(unzipped, fname))
+	except:
+		errors.write('{}\n'.format(fname))
