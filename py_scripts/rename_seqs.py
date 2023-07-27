2a203d4937f2e0f0f32d07f439a0d7542e0321ca
kikinocka <kika.zahonova@gmail.com>
2023-07-27 20:10:10 +0200

    bla

--- py_scripts/rename_seqs.py
+++ py_scripts/rename_seqs.py
@@ -19,12 +19,14 @@ for index, row in table.iterrows():
 
 for file in proteins:
 	print(file)
-	with open('renamed/errors.txt', 'w') as errors:
+	with open('renamed/errors.txt', 'w') as errors, open('renamed/seq_dict.tsv', 'a') as seqdict:
 		c = 0
+		seqdict.write('original ID\treplaced ID\n')
 		if file in prot_dict.keys():
 			with open('renamed/{}'.format(file), 'w') as out:
 				for seq in SeqIO.parse(file, 'fasta'):
 					c += 1
-					out.write('>{}_{} {}\n{}\n'.format(prot_dict[file], c, seq.description, seq.seq))
+					out.write('>{}_{}\n{}\n'.format(prot_dict[file], c, seq.seq))
+					seqdict.write('{}\t{}_{}\n'.format(seq.description, prot_dict[file], c))
 		else:
 			errors.write('{}: Not found\n'.format(file))
