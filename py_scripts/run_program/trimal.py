bac4d363f7ad592dd3945cd57b5d5ff4f31710d7
kikinocka <kika.zahonova@gmail.com>
2023-07-31 11:36:20 +0200

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,8 +3,8 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/all_euks/')
-files = [x for x in os.listdir() if x.endswith('.mafft_add.aln')]
+os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/metamonads/')
+files = [x for x in os.listdir() if x.endswith('V9.mafft.aln')]
 # files = [x for x in os.listdir() if x.endswith('.einsi.aln')]
 
 for file in files:
