522e1fb6f55b095eaf550bc43b431812a115192d
kikinocka <kika.zahonova@gmail.com>
2023-07-31 15:54:06 +0200

    bla

--- bash_scripts/metacentrum/raxml-epa.sh
+++ bash_scripts/metacentrum/raxml-epa.sh
@@ -13,19 +13,19 @@ source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
 
-data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/heterolobosea/'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/metamonads/'
 
 #copy files to scratch
-cp $data'placement/heterolobosea_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
-cp $data'ref_tree/RAxML_bipartitions.heterolobosea3' $SCRATCHDIR
+cp $data'placement/metamonads_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'ref_tree/RAxML_bipartitions.metamonads3' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='heterolobosea_V9.trimal_gt-0.25_cons-50.aln'
-tree='RAxML_bipartitions.heterolobosea3'
-out='EPARUN_heterolobosea'
+aln='metamonads_V9.trimal_gt-0.25_cons-50.aln'
+tree='RAxML_bipartitions.metamonads3'
+out='EPARUN_metamonads'
 
 raxmlHPC-PTHREADS -f v -G 0.2 -m GTRCAT -n $out -s $aln -t $tree -T $PBS_NUM_PPN
 
