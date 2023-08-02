b500d5fe56bf8dad491c0ad213517c1cf28f2a57
kikinocka <kika.zahonova@gmail.com>
2023-08-02 17:29:08 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -28,10 +28,10 @@ bb=1000
 nm=5000
 
 # iqtree2 -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-iqtree2 -m LG+C20+G4 -T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree
+# iqtree2 -m LG+C20+G4 -T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree
 
-# iqtree2 -m TEST -madd C10,C20,C30,C40,C50,C60,LG4M,LG4X,LG+F+G,LG+C10+F+G,LG+C20+F+G,LG+C30+F+G,LG+C40+F+G,LG+C60+F+G \
-# 	-B $bb --nmax $nm -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln
+iqtree2 -m TEST -madd C10,C20,C30,C40,C50,C60,LG4M,LG4X,LG+F+G,LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C60+G \
+	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
 
 # iqtree -m TEST -bb $bb -nm $nm -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
 # iqtree -m GTR+G -bb $bb -nm $nm -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln
