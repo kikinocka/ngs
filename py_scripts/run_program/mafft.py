10f7f94c1fc06aeb26b125ef3e1d9af21842d553
kikinocka <kika.zahonova@gmail.com>
2023-08-03 10:27:21 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/blastocrithidia/genes/amastins/')
+os.chdir('/Users/kika/ownCloud/blastocrithidia/genes/amastins/ver2/')
 files = [x for x in os.listdir() if x.endswith('amastins.fa')]
 
 for file in files:
