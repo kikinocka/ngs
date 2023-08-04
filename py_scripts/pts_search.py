4fb5759ab9eb7f493e481c0226e944e061f696b1
kikinocka <kika.zahonova@gmail.com>
2023-08-04 08:55:28 +0200

    bla

--- py_scripts/pts_search.py
+++ py_scripts/pts_search.py
@@ -3,8 +3,8 @@ import os
 import re
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/Euglena_gracilis/compartments_markers/')
-proteins = SeqIO.parse('EG_peroxy.fa', 'fasta')
+os.chdir('/Users/kika/ownCloud/blastocrithidia/predicted_proteins/')
+proteins = SeqIO.parse('bnon_proteins_annotated.fa', 'fasta')
 
 #kinetoplastids - from Fred
 pts1 = r'(S|A|G|C|N|P)(R|H|K|S|N|Q)(L|I|V|F|A|M|Y)'
@@ -23,7 +23,7 @@ mito = r'^M(R|H|K|F|L)\w{0,1}(R|H|K|S|T)\w{1,10}(S|T|R|K)'
 # pts1 = r'(S|A|C)(K|R|H|Q)(L|M)'
 # pts2 = r'^\w{1,21}R(L|I|V|Q)\w{2}(L|I|V|Q|H)(L|S|G|A)\w{1}(H|Q)(L|A)'
 
-with open('EG_peroxy.possibly_peroxisomal_kineto-Fred.fa', 'w') as out:
+with open('bnon_proteins_annotated.possibly_peroxisomal.Fred.fa', 'w') as out:
 	for protein in proteins:
 		if str(protein.seq)[-1:] == '*':
 			protein.seq = str(protein.seq)[:-1]
@@ -32,9 +32,9 @@ with open('EG_peroxy.possibly_peroxisomal_kineto-Fred.fa', 'w') as out:
 		elif re.search(pts2, str(protein.seq)):
 			match = re.search(pts2, str(protein.seq))
 			out.write('>{} @PTS2:{}\n{}\n'.format(protein.description, match.group(), protein.seq))
-		elif re.search(mito, str(protein.seq)):
-			match = re.search(mito, str(protein.seq))
-			out.write('>{} @mito:{}\n{}\n'.format(protein.description, match.group(), protein.seq))
+		# elif re.search(mito, str(protein.seq)):
+		# 	match = re.search(mito, str(protein.seq))
+		# 	out.write('>{} @mito:{}\n{}\n'.format(protein.description, match.group(), protein.seq))
 		else:
 			print(protein.description, '_____no PTS or mito signal')
 
