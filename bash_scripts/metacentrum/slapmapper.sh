8627b08a010756acf987b3bfa268ebfbe00f0b00
kikinocka <kika.zahonova@gmail.com>
2023-08-06 10:13:56 +0200

    bla

--- bash_scripts/metacentrum/slapmapper.sh
+++ bash_scripts/metacentrum/slapmapper.sh
@@ -15,13 +15,15 @@ module load trimmomatic
 slap_mapper='/storage/brno2/home/kika/tools/SLaPMapper/SLaPMapper.pl'
 genome_dir='/storage/brno3-cerit/home/kika/blasto_comparative/final_genomes/'
 read_dir='/storage/brno3-cerit/home/kika/blasto_comparative/frustrata/transcriptome_reads/'
-datadir='/storage/brno3-cerit/home/kika/blasto_comparative/slapmapper/bfru/'
+datadir='/storage/brno3-cerit/home/kika/blasto_comparative/slapmapper/kmers/bfru/'
 
 
 #copy files to scratch
 cp $genome_dir'Bfru_genome_final_masked.fa' $SCRATCHDIR
 cp $read_dir'bfru_trimmed_1.fq.gz' $SCRATCHDIR
 cp $read_dir'bfru_trimmed_2.fq.gz' $SCRATCHDIR
+cp $datadir'bfru_kmers.txt' $SCRATCHDIR
+
 
 #run on scratch
 cd $SCRATCHDIR
@@ -30,12 +32,18 @@ touch 'bfru_empty.gff'
 genome='Bfru_genome_final_masked.fa'
 fwd='bfru_trimmed_1.fq.gz'
 rev='bfru_trimmed_1.fq.gz'
+kmers='bfru_kmers.txt'
 gff='bfru_empty.gff'
-SL='AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG' #blastocrithidia
+# SL='AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG' #blastocrithidia
 min_length='6'
 
-$slap_mapper -g $genome -l $fwd -r $rev -a $gff -i $SL -s $min_length
 
+while read line; do
+	echo $line
+	$slap_mapper -g $genome -l $fwd -r $rev -a $gff -i $line -s $min_length
+done < $kmers
+
+# $slap_mapper -g $genome -l $fwd -r $rev -a $gff -i $SL -s $min_length
 
 #copy files back
 rm *.fa *fq.gz
