b49a5605ed36a4f664c4ba26621a4f4437ced20d
kikinocka <kika.zahonova@gmail.com>
2023-08-06 10:28:12 +0200

    bla

--- bash_scripts/metacentrum/slapmapper.sh
+++ bash_scripts/metacentrum/slapmapper.sh
@@ -14,26 +14,26 @@ module load trimmomatic
 
 slap_mapper='/storage/brno2/home/kika/tools/SLaPMapper/SLaPMapper.pl'
 genome_dir='/storage/brno3-cerit/home/kika/blasto_comparative/final_genomes/'
-read_dir='/storage/brno3-cerit/home/kika/blasto_comparative/sp_HR-05/transcriptome_reads/'
-datadir='/storage/brno3-cerit/home/kika/blasto_comparative/slapmapper/kmers/braa/'
+read_dir='/storage/brno3-cerit/home/kika/blasto_comparative/triatomae/transcriptome_reads/'
+datadir='/storage/brno3-cerit/home/kika/blasto_comparative/slapmapper/kmers/btri/'
 
 
 #copy files to scratch
-cp $genome_dir'Braa_genome_final_corrected2_masked.fa' $SCRATCHDIR
-cp $read_dir'braa_trimmed_1.fq.gz' $SCRATCHDIR
-cp $read_dir'braa_trimmed_2.fq.gz' $SCRATCHDIR
-cp $datadir'braa_kmers.txt' $SCRATCHDIR
+cp $genome_dir'Btri_genome_final_masked.fa' $SCRATCHDIR
+cp $read_dir'btri_trimmed_1.fq.gz' $SCRATCHDIR
+cp $read_dir'btri_trimmed_2.fq.gz' $SCRATCHDIR
+cp $datadir'btri_kmers.txt' $SCRATCHDIR
 
 
 #run on scratch
 cd $SCRATCHDIR
-touch 'braa_empty.gff'
+touch 'btri_empty.gff'
 
-genome='Braa_genome_final_corrected2_masked.fa'
-fwd='braa_trimmed_1.fq.gz'
-rev='braa_trimmed_1.fq.gz'
-kmers='braa_kmers.txt'
-gff='braa_empty.gff'
+genome='Btri_genome_final_masked.fa'
+fwd='btri_trimmed_1.fq.gz'
+rev='btri_trimmed_1.fq.gz'
+kmers='btri_kmers.txt'
+gff='btri_empty.gff'
 # SL='AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG' #blastocrithidia
 min_length='6'
 
