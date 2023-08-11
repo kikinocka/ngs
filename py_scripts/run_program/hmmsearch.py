7d9e670755d9680621f36f178d6c9a4b2421cb1e
kikinocka <kika.zahonova@gmail.com>
2023-08-11 11:49:32 +0200

    bla

--- py_scripts/run_program/hmmsearch.py
+++ py_scripts/run_program/hmmsearch.py
@@ -24,17 +24,17 @@ dbs = [x for x in os.listdir('/mnt/mokosz/home/kika/allDB/renamed/') if x.endswi
 os.chdir('/mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/')
 for db in dbs:
 	print(db)
-	orgn = db.split('.faa')[0]
-	for file in files:
-		print(file)
-		name = file.split('.hmm')[0]
+	# orgn = db.split('.faa')[0]
+	# for file in files:
+	# 	print(file)
+	# 	name = file.split('.hmm')[0]
 
-		if os.path.isdir('{}'.format(name)) == True:
-			pass
-		else:
-			os.mkdir('{}'.format(name))
+	# 	if os.path.isdir('{}'.format(name)) == True:
+	# 		pass
+	# 	else:
+	# 		os.mkdir('{}'.format(name))
 
-		subprocess.call('cd {}'.format(name), shell=True)
-		out = orgn + '.' + name + '.hmmsearch.tsv'
-		subprocess.call('hmmsearch --tblout {} --cpu 10 {} {}'.format(out, file, db), shell=True)
-		subprocess.call('cd ..', shell=True)
+	# 	subprocess.call('cd {}'.format(name), shell=True)
+	# 	out = orgn + '.' + name + '.hmmsearch.tsv'
+	# 	subprocess.call('hmmsearch --tblout {} --cpu 10 {} {}'.format(out, file, db), shell=True)
+	# 	subprocess.call('cd ..', shell=True)
