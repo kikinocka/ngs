a42ccf68c927d111fe5fb158270fb99145588d7c
kikinocka <kika.zahonova@gmail.com>
2023-08-11 12:08:11 +0200

    bla

--- bash_scripts/leshy/hmmsearch.sh
+++ bash_scripts/leshy/hmmsearch.sh
@@ -3,13 +3,20 @@
 cd '/mnt/mokosz/home/kika/mito_import_HMMs/'
 
 db='/mnt/mokosz/home/kika/eukprot_amoebozoa/amoebozoa.fa'
-orgn='eukprot_amoebozoa'
+# orgn='eukprot_amoebozoa'
 
-for profile in *.hmm ; do
-	echo $profile
-	output=$orgn'_'${profile%.hmm}.hmm_search.table
-	threads=10
-	hmmsearch --tblout $output --cpu $threads $profile $db
+for db in /mnt/mokosz/home/kika/allDB/renamed/*.faa ; do
+	echo $db
+	for profile in *.hmm ; do
+		echo $profile
+		folder='${profile%.hmm}'
+		mkdir $folder
+		cd folder
+		output=${db%.faa}'.'${profile%.hmm}.hmmsearch.table
+		threads=10
+		hmmsearch --tblout $output --cpu $threads $profile $db
+		cd ..
+	done
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py HMMsearch done
