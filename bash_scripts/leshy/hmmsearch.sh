58dfa4fe42e50023fd3bc33a2509c23e757b63f1
kikinocka <kika.zahonova@gmail.com>
2023-08-11 13:07:25 +0200

    bla

--- bash_scripts/leshy/hmmsearch.sh
+++ bash_scripts/leshy/hmmsearch.sh
@@ -9,15 +9,16 @@ threads=10
 for profile in /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/*.hmm ; do
 	echo $profile
 	folder=`echo $profile | cut -d / -f 8`
-	folder=${profile%.hmm}
+	folder=${folder%.hmm}
 	echo $folder
 	for  db in /mnt/mokosz/home/kika/allDB/renamed/*.faa; do
 		echo $db
 		# mkdir /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
 		# cd /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
 		output=`echo $db | cut -d / -f 8`
+		output=${output%.faa}
 		echo $output
-		output=$output'.'${profile%.hmm}.hmmsearch.tsv
+		output=$output'.'${folder}.hmmsearch.tsv
 		echo $output
 		# output=${db%.faa}'.'${profile%.hmm}.hmmsearch.tsv
 		# output=$orgn'_'${profile%.hmm}.hmmsearch.tsv
