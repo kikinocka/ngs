ea24f4210161ceaf545c83ba631ef81783c3b468
kikinocka <kika.zahonova@gmail.com>
2023-08-11 12:42:01 +0200

    bla

--- bash_scripts/leshy/hmmsearch.sh
+++ bash_scripts/leshy/hmmsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/'
+cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/'
 
 db='/mnt/mokosz/home/kika/eukprot_amoebozoa/amoebozoa.fa'
 # orgn='eukprot_amoebozoa'
@@ -9,12 +9,14 @@ for db in /mnt/mokosz/home/kika/allDB/renamed/*.faa ; do
 	echo $db
 	for profile in /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/*.hmm ; do
 		echo $profile
+		folder=$profile | cut -d / -f8
 		folder=${profile%.hmm}
-		mkdir /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
-		cd /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
-		output=$db | cut -d / -f8
-		output=$output'.'${profile%.hmm}.hmmsearch.tsv
-		echo $output
+		echo $folder
+		# mkdir /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
+		# cd /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
+		# output=$db | cut -d / -f8
+		# output=$output'.'${profile%.hmm}.hmmsearch.tsv
+		# echo $output
 		#output=${db%.faa}'.'${profile%.hmm}.hmmsearch.tsv
 		# output=$orgn'_'${profile%.hmm}.hmmsearch.tsv
 		# threads=10
