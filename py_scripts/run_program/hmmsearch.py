f7dee9bf9594c916571c73246bc53a1d4c473909
kikinocka <kika.zahonova@gmail.com>
2023-08-11 11:37:54 +0200

    bla

--- py_scripts/run_program/hmmsearch.py
+++ py_scripts/run_program/hmmsearch.py
@@ -2,18 +2,36 @@
 import os
 import subprocess
 
-hmmsearch = '/Users/kika/miniconda3/bin/hmmsearch'
+# hmmsearch = '/Users/kika/miniconda3/bin/hmmsearch'
 
-os.chdir('/Users/kika/ownCloud/blastocrithidia/genes/amastins/')
+os.chdir('/mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/')
 files = [x for x in os.listdir() if x.endswith('.hmm')]
 
-db = '/Users/kika/ownCloud/blastocrithidia/genome_assembly/p57_polished_translated.fa'
-orgn = 'bnon_gen'
+# db = '/Users/kika/ownCloud/blastocrithidia/genome_assembly/p57_polished_translated.fa'
+# orgn = 'bnon_gen'
 
-for file in files:
-	print(file)
-	name = file.split('.hmm')[0]
-	out = orgn + '_' + name + '.hmmsearch.out'
-	subprocess.call('{} -o {} --cpu 7 {} {}'.format(hmmsearch, out, file, db), shell=True)
-# -o
-# --tblout
+# for file in files:
+# 	print(file)
+# 	name = file.split('.hmm')[0]
+# 	out = orgn + '_' + name + '.hmmsearch.out'
+# 	subprocess.call('{} -o {} --cpu 7 {} {}'.format(hmmsearch, out, file, db), shell=True)
+# # -o
+# # --tblout
+
+
+dbs = [x for x in os.listdir('/mnt/mokosz/home/kika/allDB/renamed/') if x.endswith('.faa')]
+
+os.chdir('/mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/')
+for db in dbs:
+	print(db)
+	orgn = db.split('.faa')[0]
+	for file in files:
+		print(file)
+		if os.path.isdir('{}'.format(file.split('.hmm')[0])) == True:
+			pass
+		else:
+			os.mkdir('{}'.format(file.split('.hmm')[0]))
+			
+		name = file.split('.hmm')[0]
+		out = orgn + '.' + name + '.hmmsearch.tsv'
+		subprocess.call('hmmsearch --tblout {} --cpu 10 {} {}'.format(out, file, db), shell=True)
