c489c95698a08a678d819b5ac6e0b7b5d1ef272c
kikinocka <kika.zahonova@gmail.com>
2023-08-11 11:55:02 +0200

    bla

--- py_scripts/run_program/hmmsearch.py
+++ py_scripts/run_program/hmmsearch.py
@@ -34,8 +34,9 @@ for db in dbs:
 		else:
 			os.mkdir('{}'.format(name))
 
-		subprocess.call('cd {}'.format(name), shell=True)
-		subprocess.call('pwd'.format(name), shell=True)
+		os.chdir('/mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/{}'.format(name))
+		# subprocess.call('cd {}'.format(name), shell=True)
+		subprocess.call('pwd', shell=True)
 	# 	out = orgn + '.' + name + '.hmmsearch.tsv'
 	# 	subprocess.call('hmmsearch --tblout {} --cpu 10 {} {}'.format(out, file, db), shell=True)
-		subprocess.call('cd ..', shell=True)
+		# subprocess.call('cd ..', shell=True)
