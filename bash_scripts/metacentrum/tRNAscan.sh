9773dbada63a9d83b02b5bd3923d8b0bdb00b7b5
kikinocka <kika.zahonova@gmail.com>
2023-08-16 09:34:30 +0200

    bla

--- bash_scripts/metacentrum/tRNAscan.sh
+++ bash_scripts/metacentrum/tRNAscan.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 #PBS -N tRNAscan
 #PBS -l select=1:ncpus=10:mem=1gb:scratch_local=5gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 tRNAscan='/storage/brno3-cerit/home/kika/miniconda3/bin/tRNAscan-SE'
-data_dir='/storage/brno3-cerit/home/kika/p57/mtDNA/'
+data_dir='/storage/brno3-cerit/home/kika/p57/mtDNA'
 
 #copy files to scratch
-cp $data_dir'Bnon_mtDNA.fa' $SCRATCHDIR
+cp $data_dir'/'*.fna $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-for genome in *.fa ; do
+for genome in *.fna ; do
 	echo $genome
 
-	table=${genome%.fa}.tRNAscan_table.tsv
-	seq=${genome%.fa}.tRNAscan.fa
-	structures=${genome%.fa}.tRNAscan_structures.txt
+	table=${genome%.fna}.tRNAscan_table.tsv
+	seq=${genome%.fna}.tRNAscan.fa
+	structures=${genome%.fna}.tRNAscan_structures.txt
 
-	$tRNAscan --thread $PBS_NUM_PPN -O -o $table -a $seq -f $structures ${genome}
+	$tRNAscan --thread $PBS_NUM_PPN -o $table -a $seq -f $structures ${genome}
 done
 
 
