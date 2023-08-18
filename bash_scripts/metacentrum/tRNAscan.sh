595f9dbdbf3891ecd170a6571b1e4d7f0a22b814
kikinocka <kika.zahonova@gmail.com>
2023-08-18 10:37:14 +0200

    bla

--- bash_scripts/metacentrum/tRNAscan.sh
+++ bash_scripts/metacentrum/tRNAscan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N tRNAscan
 #PBS -l select=1:ncpus=10:mem=1gb:scratch_local=5gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,23 +11,23 @@ tRNAscan='/storage/brno3-cerit/home/kika/miniconda3/bin/tRNAscan-SE'
 data_dir='/storage/brno3-cerit/home/kika/p57/mtDNA'
 
 #copy files to scratch
-cp $data_dir'/'*.fna $SCRATCHDIR
+cp $data_dir'/'*.fasta $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-for genome in *.fna ; do
+for genome in *.fasta ; do
 	echo $genome
 
-	table=${genome%.fna}.tRNAscan_table.tsv
-	seq=${genome%.fna}.tRNAscan.fa
-	structures=${genome%.fna}.tRNAscan_structures.txt
+	table=${genome%.fasta}.tRNAscan_table.tsv
+	seq=${genome%.fasta}.tRNAscan.fa
+	structures=${genome%.fasta}.tRNAscan_structures.txt
 
-	$tRNAscan --thread $PBS_NUM_PPN -o $table -a $seq -f $structures ${genome}
+	$tRNAscan --thread $PBS_NUM_PPN -O -o $table -a $seq -f $structures ${genome}
 done
 
 
 #copy files back
-rm *.fna
+rm *.fasta
 cp * $data_dir
