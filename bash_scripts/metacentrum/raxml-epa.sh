b5615bcbf346e10df55629528e0d77cbef98dab0
kikinocka <kika.zahonova@gmail.com>
2023-08-21 10:08:55 +0200

    bla

--- bash_scripts/metacentrum/raxml-epa.sh
+++ bash_scripts/metacentrum/raxml-epa.sh
@@ -13,18 +13,18 @@ source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
 
-data='/storage/brno3-cerit/home/kika/oil_sands/Lane26_18S_V9/euglenozoa/'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/ciliates/'
 
 #copy files to scratch
-cp $data'placement/euglenozoa_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
-cp $data'ref_tree/RAxML_bipartitions.euglenozoa3' $SCRATCHDIR
+cp $data'placement/ciliates_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'ref_tree/RAxML_bipartitions.ciliates3' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='euglenozoa_V9.trimal_gt-0.25_cons-50.aln'
-tree='RAxML_bipartitions.euglenozoa3'
+aln='ciliates_V9.trimal_gt-0.25_cons-50.aln'
+tree='RAxML_bipartitions.ciliates3'
 out='EPARUN_euglenozoa'
 
 raxmlHPC-PTHREADS -f v -G 0.2 -m GTRCAT -n $out -s $aln -t $tree -T $PBS_NUM_PPN
