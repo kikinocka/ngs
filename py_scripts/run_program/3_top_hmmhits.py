a78271f988c8d20b5f9f07f38dfb7850eb7f8e82
kikinocka <kika.zahonova@gmail.com>
2023-08-25 09:27:42 +0200

    bla

---
+++ py_scripts/run_program/3_top_hmmhits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+from Bio import SeqIO
+
+os.chdir('/Users/kika/ownCloud/metamonada/OGs/')
+files = [x for x in os.listdir() if x.endswith('hmm_hits.fa')]
+
+eval_dict = {}
+for file in files:
+	print(file)
+	for seq in SeqIO.parse(file, 'fasta'):
+		orgn = seq.name.split('_')[0]
+		eval = float(seq.description.split(' ')[1].split(':')[1])
+		
+		if orgn not in eval_dict:
+			eval_dict[orgn]	= [[eval, seq.description, seq.seq]]
+		else:
+			eval_dict[orgn].append([eval, seq.description, seq.seq])
+
+	with open('{}.reduced.fa'.format(file.split('.fa')[0]), 'w') as out:
+		for key, value in eval_dict.items():
+			if len(value) > 3:
+				value = value[0:3]
+			for v in value:
+				out.write('>{}\n{}\n'.format(v[1], v[2]))
