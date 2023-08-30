e7ffceb5d803bbb8a45550cdebfb965b8af16b8f
kikinocka <kika.zahonova@gmail.com>
2023-08-30 16:03:39 +0200

    bla

--- bash_scripts/metacentrum/gappa_heattree.sh
+++ bash_scripts/metacentrum/gappa_heattree.sh
@@ -8,7 +8,7 @@
 cat $PBS_NODEFILE
 
 gappa='/storage/brno3-cerit/home/kika/miniconda3/bin/gappa'
-data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/ciliates/placement'
+data='/storage/brno3-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/apusomonads/placement'
 
 #copy files to scratch
 cp $data'/'*.jplace $SCRATCHDIR
