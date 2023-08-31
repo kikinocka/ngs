0524b2a57fd50b0189ffa262a4f7f9b34f9102e0
kikinocka <kika.zahonova@gmail.com>
2023-08-31 15:46:40 +0200

    bla

---
+++ bash_scripts/leshy/muscle_many.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#name prefetch_wrapper
+
+HOMEDIR='/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_fasta'
+ALNDIR='/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_muscle'
+
+#cd $HOMEDIR
+
+while read -r sample
+do
+	if [ ! -e $ALNDIR/${sample/fa/muscle.aln} ]; then
+		touch $ALNDIR/${sample/fa/muscle.aln}
+	    muscle5.1 -super5 $HOMEDIR/$sample -output $ALNDIR/${sample/fa/muscle.aln} -threads 10
+    fi
+done < 'large.txt' #last item must end with a newline!
+
+python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py muscle5 done
+
