a2b845e9768f8178f8e8dbacda9b09550908f73c
kikinocka <kika.zahonova@gmail.com>
2023-09-01 17:06:04 +0200

    bla

--- bash_scripts/metacentrum/trinity.sh
+++ bash_scripts/metacentrum/trinity.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #PBS -N Trinity
 #PBS -l select=1:ncpus=30:mem=100gb:scratch_local=50gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
-module add trinity-2.11.0
+# module add trinity-2.11.0
+source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
+module load trinity
 
-read_dir='/storage/brno3-cerit/home/kika/blasto_comparative/modryi/transcriptome_reads/'
-out_dir='/storage/brno3-cerit/home/kika/blasto_comparative/trinity/'
+read_dir='/storage/brno3-cerit/home/kika/UGA_decoding/nyctotherus/reads'
+out_dir='/storage/brno3-cerit/home/kika/UGA_decoding/nyctotherus/trinity/'
 
 #copy files to scratch
 cp $read_dir'/'*trimmed_*.fq.gz $SCRATCHDIR
@@ -20,8 +22,8 @@ cp $read_dir'/'*trimmed_*.fq.gz $SCRATCHDIR
 #compute on scratch
 cd $SCRATCHDIR
 
-fw='omod_trimmed_1.fq.gz'
-rv='omod_trimmed_2.fq.gz'
+fw='all_trimmed_1.fq.gz'
+rv='all_trimmed_2.fq.gz'
 
 Trinity --seqType fq --left $fw --right $rv --max_memory 100G --CPU $PBS_NUM_PPN
 
