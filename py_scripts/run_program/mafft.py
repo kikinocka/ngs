84b6ec8b149b27512122a35aa6bd4827ba02fce9
kikinocka <kika.zahonova@gmail.com>
2023-09-07 15:12:18 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/trees/metazoa_mesozoa/')
+os.chdir('/Users/kika/ownCloud/kinetoplastids/IDH/alignment/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 
 for file in files:
