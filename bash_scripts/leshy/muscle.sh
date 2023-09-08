a2e5b91c1a6d803f5e6413fb13883e5c5c1043d5
kikinocka <kika.zahonova@gmail.com>
2023-09-08 20:47:39 +0200

    bla

--- bash_scripts/leshy/muscle.sh
+++ bash_scripts/leshy/muscle.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-cd '/mnt/mokosz/home/kika/workdir/'
+cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_fasta/'
 
-for fasta in *.fa; do
+for fasta in q2004590.og_hmm.fa; do
 	echo $fasta
 	aln=${fasta%.fa}.muscle.aln
 	log=${fasta%.fa}.muscle.log
 
-	#short alns
-	muscle -threads 10 -align $fasta -output $aln 2> $log
+	# #short alns
+	# muscle -threads 15 -align $fasta -output $aln 2> $log
 	
-	# #long alns
-	# muscle -threads 15 -super5 $fasta -output $aln 2> $log
+	#long alns
+	muscle -threads 15 -super5 $fasta -output $aln 2> $log
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py muscle5 done
