15055c47c6003f8fa981bcf6d18058d930fd4cc4
kikinocka <kika.zahonova@gmail.com>
2023-09-08 23:14:10 +0200

    bla

--- bash_scripts/leshy/trimal.sh
+++ bash_scripts/leshy/trimal.sh
@@ -3,7 +3,7 @@
 #align denovo
 cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_muscle/'
 
-for a in *.muscle.aln ; do
+for f in *.muscle.aln ; do
 	echo $f
 	out=${f%.muscle.aln}.trimal_gt-0.8.aln
 	trimal -in $f -out $out -gt 0.8 -fasta
