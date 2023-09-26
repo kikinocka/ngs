99c8eb1c64f11b6eb201a394ba73fe5de382b395
kikinocka <kika.zahonova@gmail.com>
2023-09-26 11:12:56 +0200

    bla

--- bash_scripts/remove_isoforms.sh
+++ bash_scripts/remove_isoforms.sh
@@ -4,6 +4,7 @@ if_removal='/storage/brno2/home/kika/scripts/py_scripts/remove_isoforms.py'
 
 cd '/storage/brno3-cerit/home/kika/archamoebae/prot_assemblies_filtration-20220127/'
 
-infile='mab.trinity.NRfilt.faa'
-
-python3 $if_removal $infile
+for infile in *.NRfilt.faa; do
+	echo $infile
+	python3 $if_removal $infile
+done
