b385d8cd5369a9cf8c593d8061118501ad01b829
kikinocka <kika.zahonova@gmail.com>
2023-10-03 15:25:38 +0200

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -2,10 +2,10 @@
 
 placer='/Users/kika/scripts/py_scripts/boots_on_mb.py'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ver10/combined/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver6/combined/'
 
-tree=$path'arfs.CD.trimal_gt-0.8.nex.con.tre'
-bootstrap=$path'RAxML_bootstrap.arfs.CD'
-out=$path'arfs.CD.mb+raxml.tre'
+tree=$path'propeller-solenoid.trimal_gt-0.8.nex.con.tre'
+bootstrap=$path'RAxML_bootstrap.propeller-solenoid'
+out=$path'propeller-solenoid.mb+raxml.tre'
 
 $placer $tree $bootstrap $out
