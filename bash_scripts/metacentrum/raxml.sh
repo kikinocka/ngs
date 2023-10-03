e8290d1db54a455117e5afcd62fe41f39bf533af
kikinocka <kika.zahonova@gmail.com>
2023-10-03 13:59:18 +0200

    bla

--- bash_scripts/metacentrum/raxml.sh
+++ bash_scripts/metacentrum/raxml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml
 #PBS -l select=1:ncpus=20:mem=2gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,17 +11,17 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
-data='/storage/brno3-cerit/home/kika/trafficking/diplonemids_all/SNARE/RAxML/'
+data='/storage/brno3-cerit/home/kika/trafficking/diplonemids_all/RABs/ver14/RAxML/'
 
 #copy files to scratch
-cp $data'r.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data'rabs.trimal_gt-0.8.aln' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 #proteins
-aln='r.trimal_gt-0.8.aln'
+aln='rabs.trimal_gt-0.8.aln'
 out=${aln%.trimal_gt-0.8.aln}
 
 raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N autoMRE_IGN -p 12345 -s $aln -n $out
