041517f76839b6683a7a37bcb1634628ee25ffa5
kikinocka <kika.zahonova@gmail.com>
2023-10-04 11:24:43 +0200

    bla

--- bash_scripts/leshy/trimal.sh
+++ bash_scripts/leshy/trimal.sh
@@ -6,11 +6,14 @@ cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_muscle/'
 for f in *.muscle.aln ; do
 	echo $f
 	
-	out=${f%.muscle.aln}.trimal_at1.aln
-	trimal -in $f -out $out -automated1 -fasta
+	# out=${f%.muscle.aln}.trimal_at1.aln
+	# trimal -in $f -out $out -automated1 -fasta
 	
 	# out=${f%.muscle.aln}.trimal_gt-0.8.aln
 	# trimal -in $f -out $out -gt 0.8 -fasta
+
+	out=${f%.muscle.aln}.trimal_gt-0.5_cons-50.aln
+	trimal -in $f -out $out -gt 0.5 -cons 50 -fasta
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py trimAl done
