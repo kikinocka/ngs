808ceb24fdd6590265f3c4b849caf3bcd0a35b4f
kikinocka <kika.zahonova@gmail.com>
2023-10-12 10:53:43 +0200

    bla

--- bash_scripts/leshy/iqtree_pmsf.sh
+++ bash_scripts/leshy/iqtree_pmsf.sh
@@ -21,6 +21,7 @@ ALNDIR='/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_iqtree'
 
 while read -r sample
 do
+	echo $sample
 	iqtree2 -m LG+G4 -T AUTO --threads-max 10 --quiet --safe -s $HOMEDIR/$sample --prefix $ALNDIR/guide_${sample%.aln}
 	iqtree2 -m LG+C20+G4 -T 10 -B 1000 --nmax 5000 --quiet --safe -s $HOMEDIR/$sample --tree-freq $ALNDIR/guide_${sample%.aln}.treefile --prefix $ALNDIR/${sample%.aln}
 done < $HOMEDIR'/at1_to_iqt.txt' #last item must end with a newline!
