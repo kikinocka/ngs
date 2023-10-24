b8e8972fa16fb3e59dc32790e20f7e9e183acbac
kikinocka <kika.zahonova@gmail.com>
2023-10-24 13:48:37 +0200

    bla

--- bash_scripts/filter_aln.sh
+++ bash_scripts/filter_aln.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filt_script='/Users/kika/scripts/py_scripts/filter_alignment.py'
+filt_script='/mnt/mokosz/home/kika/scripts/py_scripts/filter_alignment.py'
 
 cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_trimal_automated1/'
 threshold=50
