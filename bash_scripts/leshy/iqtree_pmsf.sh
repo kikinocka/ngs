e9bf7ef9c313a38d404abfa8a81bed288c4c22b7
kikinocka <kika.zahonova@gmail.com>
2023-10-25 10:06:50 +0200

    bla

--- bash_scripts/leshy/iqtree_pmsf.sh
+++ bash_scripts/leshy/iqtree_pmsf.sh
@@ -22,8 +22,10 @@ ALNDIR='/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_iqtree'
 while read -r sample
 do
 	echo $sample
-	iqtree -m LG+G -nt AUTO -ntmax 10 -quiet -safe -s $HOMEDIR/$sample -pre $ALNDIR/guide_${sample%.aln}
-	iqtree -m LG+C20+G -nt AUTO -ntmax 10 -bb 1000 -nm 5000 -quiet -safe -s $HOMEDIR/$sample -ft $ALNDIR/guide_${sample%.aln}.treefile -pre $ALNDIR/${sample%.aln}
+	# iqtree -m LG+G -nt AUTO -ntmax 10 -quiet -safe -s $HOMEDIR/$sample -pre $ALNDIR/guide_${sample%.aln}
+	# iqtree -m LG+C20+G -nt AUTO -ntmax 10 -bb 1000 -nm 5000 -quiet -safe -s $HOMEDIR/$sample -ft $ALNDIR/guide_${sample%.aln}.treefile -pre $ALNDIR/${sample%.aln}
+
+	iqtree -m LG+C20+G -nt AUTO -ntmax 10 -bb 1000 -nm 5000 -quiet -safe -s $HOMEDIR/$sample -pre $ALNDIR/${sample%.aln}
 
 	# iqtree2 -m LG+G4 -T AUTO --threads-max 10 --quiet --safe -s $HOMEDIR/$sample --prefix $ALNDIR/guide_${sample%.aln}
 	# iqtree2 -m LG+C20+G4 -T 10 -B 1000 --nmax 5000 --quiet --safe -s $HOMEDIR/$sample --tree-freq $ALNDIR/guide_${sample%.aln}.treefile --prefix $ALNDIR/${sample%.aln}
