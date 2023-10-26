15977c7d74a6f4549c0794b9c9d0b01b27372f0e
kikinocka <kika.zahonova@gmail.com>
2023-10-26 10:48:00 +0200

    bla

---
+++ bash_scripts/metacentrum/muscle.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -N muscle
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l walltime=24:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
+module load muscle
+
+datadir='/storage/brno3-cerit/home/kika/blasto_comparative/orthofinder/all_species/'
+
+#copy files to scratch
+cp $datadir'OGs_fasta/'*.fa $SCRATCHDIR
+
+#compute on scratch
+cd $SCRATCHDIR
+
+for fasta in *.fa; do
+	echo $fasta
+	aln=${fasta%.fa}.muscle.aln
+	log=${fasta%.fa}.muscle.log
+
+	# #short alns
+	# muscle -threads $PBS_NUM_PPN -align $fasta -output $aln 2> $log
+	
+	#long alns
+	muscle -threads $PBS_NUM_PPN -super5 $fasta -output $aln 2> $log
+done
+
+#copy files back
+rm *.fa
+cp * $datadir'OGs_muscle'
