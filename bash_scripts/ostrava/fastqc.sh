65a3aeba76cee14e41cec9d5f47e3b522115252f
kikinocka <kika.zahonova@gmail.com>
2023-10-26 11:46:45 +0200

    bla

--- bash_scripts/ostrava/fastqc.sh
+++ bash_scripts/ostrava/fastqc.sh
@@ -6,12 +6,12 @@
 #PBS -l walltime=02:00:00
 
 
-cd '/mnt/data/kika/blastocrithidia/transcriptomes/o_volfi/reads/'
+cd '/home/users/kika/bnon_pfr_ko/reads/'
 # fwd='4FEM_trimmed_75_1.fq'
 # rev='4FEM_trimmed_75_2.fq'
-report='ovol_trimmed.fastqc.txt'
+report='bnon_PF16_KO.fastqc.txt'
 
 fastqc -o ./fastqc -t 5 *.gz 2> $report
-fastqc -o ./fastqc -t 5 $fwd $rev 2> $report
+# fastqc -o ./fastqc -t 5 $fwd $rev 2> $report
 
 python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: FASTQC done
