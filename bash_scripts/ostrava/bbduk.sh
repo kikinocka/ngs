bf494561b3d03ecc7df1e7ed2108652210067708
kikinocka <kika.zahonova@gmail.com>
2023-10-26 11:20:31 +0200

    bla

--- bash_scripts/ostrava/bbduk.sh
+++ bash_scripts/ostrava/bbduk.sh
@@ -8,20 +8,20 @@
 bbduk='/home/users/kika/bbmap/bbduk.sh'
 adapt='/home/users/kika/bbmap/resources/adapters.fa'
 
-cd '/mnt/data/kika/blastocrithidia/transcriptomes/o_volfi/reads/'
-fwd='CC37A_1.fastq.gz'
-rev='CC37A_2.fastq.gz'
-trimmed_fwd='ovol_trimmed_1.fq.gz'
-trimmed_rev='ovol_trimmed_2.fq.gz'
+cd '/home/users/kika/bnon_pfr_ko/reads/'
+fwd='P57_PF16_1.fastq.gz'
+rev='P57_PF16_2.fastq.gz'
+trimmed_fwd='bnon_PF16_KO_trimmed_1.fq.gz'
+trimmed_rev='bnon_PF16_KO_trimmed_2.fq.gz'
 report='ovol_bbduk.txt'
-len=50
+# len=50
 
 #illumina pair-end reads
 $bbduk overwrite=true \
 	in1=$fwd in2=$rev \
 	out1=$trimmed_fwd out2=$trimmed_rev \
 	ref=$adapt \
-	minlen=$len \
 	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=10 2> $report
+# minlen=$len \
 
 python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: bbduk done
