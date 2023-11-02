41e38bcea233809a702002370c98c8053e7bd436
kikinocka <kika.zahonova@gmail.com>
2023-11-02 09:33:17 +0100

    bla

--- bash_scripts/metacentrum/iqtree_many.sh
+++ bash_scripts/metacentrum/iqtree_many.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-many
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=02:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,10 +10,21 @@ cat $PBS_NODEFILE
 #add module
 module add iqtree-1.6.12
 
-data_dir='/storage/brno3-cerit/home/kika/sumk/tspoon'
+data_dir='/storage/brno3-cerit/home/kika/metamonads/'
 
 #copy files to scratch
-cp $data_dir'/'*.aln $SCRATCHDIR
+# cp $data_dir'/'*.aln $SCRATCHDIR
+cp 'q2011222.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2012767.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2019047.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2011452.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2012772.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2019130.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2011616.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2012784.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2019144.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+cp 'q2011627.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
+
 
 #compute on scratch
 cd $SCRATCHDIR
@@ -28,8 +39,12 @@ for f in *.aln ; do
 	# iqtree -m TEST -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -nm $nm -quiet -s ${f}
 	# iqtree -m GTR+G -nt AUTO -ntmax $PBS_NUM_PPN -b $bb -quiet -s ${f}
 
-	iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
-	iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
+	# iqtree -m LG+F+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s ${f} -pre $guide
+	# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
+
+
+	iqtree -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -bb 1000 -nm 5000 -quiet -safe -s ${f} -pre ${f%.aln}
+
 done
 
 #copy files back
