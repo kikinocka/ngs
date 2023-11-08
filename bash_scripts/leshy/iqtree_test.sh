1f752767ab6906823b8633a16b9098c7a12d80c7
kikinocka <kika.zahonova@gmail.com>
2023-11-08 11:10:58 +0100

    bla

--- bash_scripts/leshy/iqtree_test.sh
+++ bash_scripts/leshy/iqtree_test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cd '/mnt/mokosz/home/kika/workdir/'
+cd '/mnt/mokosz/home/kika/metamonads_ancestral/OGs+HMMhits_trimal_gt-0.8/'
 
-for aln in *.aln ; do
+for aln in q2001042.og_hmm.trimal_gt-0.8.filtered-50.aln ; do
 	echo $aln
 	guide=guide_${aln%.aln}
 	guide_tree=$guide'.treefile'
 	bb=1000
 	nm=5000
-	iqtree -m TEST -nt AUTO -ntmax 10 -bb $bb -nm $nm -quiet -s ${aln}
+	# iqtree -m TEST -nt AUTO -ntmax 10 -bb $bb -nm $nm -quiet -s ${aln}
+	iqtree -m LG+C20+G -nt AUTO -ntmax 10 -bb 1000 -nm 5000 -quiet -safe -s ${aln} -pre ${aln%.aln}
+
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py IQTREE TEST done
