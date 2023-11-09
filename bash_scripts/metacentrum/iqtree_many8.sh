e553f99f3863d701a09a56833a8ead47c2adfaa8
kikinocka <kika.zahonova@gmail.com>
2023-11-09 14:02:51 +0100

    bla

--- bash_scripts/metacentrum/iqtree_many8.sh
+++ bash_scripts/metacentrum/iqtree_many8.sh
@@ -14,12 +14,6 @@ data_dir='/storage/brno3-cerit/home/kika/metamonads/'
 
 #copy files to scratch
 # cp $data_dir'/'*.aln $SCRATCHDIR
-cp $data_dir'q2007978.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
-cp $data_dir'q2007994.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
-cp $data_dir'q2007999.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
-cp $data_dir'q2008231.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
-cp $data_dir'q2008240.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
-cp $data_dir'q2008242.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
 cp $data_dir'q2008526.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
 cp $data_dir'q2008566.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
 cp $data_dir'q2009028.og_hmm.trimal_gt-0.8.filtered-50.aln' $SCRATCHDIR
@@ -43,7 +37,7 @@ for f in *.aln ; do
 	# iqtree -m LG+C20+F+G -nt AUTO -ntmax $PBS_NUM_PPN -bb $bb -quiet -s ${f} -ft $guide_tree #-wsr
 
 
-	iqtree -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -bb 1000 -nm 5000 -quiet -safe -s ${f} -pre ${f%.aln}
+	iqtree -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -bb 1000 -nm 10000 -quiet -safe -s ${f} -pre ${f%.aln}
 
 done
 
