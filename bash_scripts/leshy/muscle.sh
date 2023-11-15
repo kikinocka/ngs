395aa8c5607e85db015cb59b6cfeeae26966f5d9
kikinocka <kika.zahonova@gmail.com>
2023-11-15 16:10:55 +0100

    bla

--- bash_scripts/leshy/muscle.sh
+++ bash_scripts/leshy/muscle.sh
@@ -11,7 +11,7 @@ for fasta in *.fa; do
 	# muscle -threads 15 -align $fasta -output $aln 2> $log
 	
 	#long alns
-	muscle -threads 15 -super5 $fasta -output $aln 2> $log
+	muscle -threads 10 -super5 $fasta -output $aln 2> $log
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py muscle5 done
