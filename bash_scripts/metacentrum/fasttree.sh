885beab69a80145fd4c370fd26e7c108e1c39a56
kikinocka <kika.zahonova@gmail.com>
2023-11-16 14:59:57 +0100

    bla

---
+++ bash_scripts/metacentrum/fasttree.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#PBS -N FastTree
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l walltime=168:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+#add module
+source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
+module load fasttree
+
+
+data_dir='/storage/brno3-cerit/home/kika/metamonads/fasttree'
+
+#copy files to scratch
+cp $data_dir'/'*.aln $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
+for aln in *.aln ; do
+	echo $aln
+	out=${aln%.aln}.fasttree.treefile
+	fasttree $aln > $out
+done
+
+#copy files back
+rm *.aln
+cp * $data_dir
