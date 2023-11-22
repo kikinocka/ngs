4354dffd46eaf30918fe9e6a659f6ce041b7311a
kikinocka <kika.zahonova@gmail.com>
2023-11-22 16:44:58 +0100

    bla

--- py_scripts/retrieve_seqs.py
+++ py_scripts/retrieve_seqs.py
@@ -3,8 +3,8 @@ import os
 from Bio import SeqIO
 
 os.chdir('/mnt/mokosz/home/kika/metamonads_ancestral/metamonads_assemblies/')
-files = [x for x in os.listdir() if x.endswith('ssal.acc')]
-database = '/mnt/mokosz/home/kika/metamonads_ancestral/metamonads_assemblies/Spironucleus.faa'
+files = [x for x in os.listdir() if x.endswith('gmur.acc')]
+database = '/mnt/mokosz/home/kika/allDB/eukaryotes/Giardia_muris.faa'
 
 for accessions in files: 
 	print(accessions)
