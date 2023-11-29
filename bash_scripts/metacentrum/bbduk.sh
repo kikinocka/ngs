67c472104ba5c89761a0dbd2d24268cc67316593
kikinocka <kika.zahonova@gmail.com>
2023-11-29 12:36:14 +0100

    bla

--- bash_scripts/metacentrum/bbduk.sh
+++ bash_scripts/metacentrum/bbduk.sh
@@ -11,47 +11,47 @@ source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module load bbmap
 
 
-raw_dir='/storage/brno3-cerit/home/kika/UGA_decoding/peritromus/reads/'
+raw_dir='/storage/brno3-cerit/home/kika/ciliates/condylostoma/transcriptome/reads/'
 # trim_dir='/storage/brno3-cerit/home/kika/archamoebae/rhizomastix_libera/trimmed_reads/'
 
 #copy data to scratch
 cp '/storage/brno2/home/kika/tools/bbmap/resources/adapters.fa' $SCRATCHDIR
-cp $raw_dir'CRR626409_f1.fq.gz' $SCRATCHDIR
-cp $raw_dir'CRR626409_r2.fq.gz' $SCRATCHDIR
+cp $raw_dir'all_RNA.fq.gz' $SCRATCHDIR
+# cp $raw_dir'CRR626409_r2.fq.gz' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
 adapt='adapters.fa'
-name='pkah'
-fw='CRR626409_f1.fq.gz'
-rv='CRR626409_r2.fq.gz'
-trimmed_fw=$name'_trimmed_1.fq.gz'
-trimmed_rv=$name'_trimmed_2.fq.gz'
-# single='all_reads.fq.gz'
-# trimmed='all_reads_trimmed.fq.gz'
+name='cmag'
+# fw='CRR626409_f1.fq.gz'
+# rv='CRR626409_r2.fq.gz'
+# trimmed_fw=$name'_trimmed_1.fq.gz'
+# trimmed_rv=$name'_trimmed_2.fq.gz'
+single='all_RNA.fq.gz'
+trimmed='all_RNA_trimmed.fq.gz'
 report=$name'_bbduk_report.txt'
 
-#illumina pair-end reads
-bbduk.sh overwrite=true \
-	in1=$fw in2=$rv \
-	out1=$trimmed_fw out2=$trimmed_rv \
-	ref=$adapt \
-	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
-
-# #illumina single reads
+# #illumina pair-end reads
 # bbduk.sh overwrite=true \
-# 	in=$single \
-# 	out=$trimmed \
+# 	in1=$fw in2=$rv \
+# 	out1=$trimmed_fw out2=$trimmed_rv \
 # 	ref=$adapt \
-# 	ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN qtrim=rl trimq=20 2> $report
-# 	# qtrim=rl trimq=20 ktrim=r t=$PBS_NUM_PPN 2> $report
+# 	qtrim=rl trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN 2> $report
+
+#illumina single reads
+bbduk.sh overwrite=true \
+	in=$single \
+	out=$trimmed \
+	ref=$adapt \
+	ktrim=r k=22 mink=11 hdist=2 tpe tbo t=$PBS_NUM_PPN qtrim=rl trimq=20 2> $report
+	# qtrim=rl trimq=20 ktrim=r t=$PBS_NUM_PPN 2> $report
 
 # #454 reads
 # bbduk.sh in=$fw out=$trimmed_fw ref=$adapt k=23 ktrim=r mink=11 edist=1 qtrim=rl trimq=20 t=$PBS_NUM_PPN 2> $report
 
 #copy files back
-rm $fw $rv $adapt
-# rm $single $adapt
+# rm $fw $rv $adapt
+rm $single $adapt
 cp -r * $raw_dir
--- py_scripts/codon_usage.py
+++ py_scripts/codon_usage.py
@@ -3,8 +3,8 @@ import os
 from Bio import SeqIO
 from collections import OrderedDict
 
-os.chdir('/Users/kika/ownCloud/blasto_comparative/proteins/')
-files = [x for x in os.listdir() if x.endswith('OK_l30.fna')]
+os.chdir('/Users/kika/ownCloud/blastocrithidia/luciferase_vector/')
+files = [x for x in os.listdir() if x.endswith('.fa')]
 
 codons = OrderedDict([
 		('GCG', 0), ('GCA', 0), ('GCT', 0), ('GCC', 0), ('TGT', 0), ('TGC', 0), ('GAT', 0), ('GAC', 0), ('GAA', 0), 
@@ -29,7 +29,7 @@ def count_codons(sequence):
 
 for file in files:
 	print(file)
-	table = file.split('.fna')[0] + '.codons.tsv'
+	table = file.split('.fa')[0] + '.codons.tsv'
 	# table = file.split('_')[0] + '_' + file.split('_')[1] + '.codons.tsv'
 	# table = file.split('.')[0] + '.' + file.split('.')[1] + '.codons.tsv'
 	with open(table, 'w') as result:
