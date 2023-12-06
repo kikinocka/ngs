56d9d0fffa2847d3c647c023a693d6ea832aded5
kikinocka <kika.zahonova@gmail.com>
2023-12-06 09:42:06 +0100

    bla

--- bash_scripts/metacentrum/raxml.sh
+++ bash_scripts/metacentrum/raxml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml
 #PBS -l select=1:ncpus=20:mem=2gb:scratch_local=1gb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,29 +11,29 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
-data='/storage/brno3-cerit/home/kika/trafficking/diplonemids_all/RABs/ver14/RAxML/'
+data='/storage/brno3-cerit/home/kika/blasto_comparative/18S/'
 
 #copy files to scratch
-cp $data'rabs.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data'18S_dataset4trim.aln' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-#proteins
-aln='rabs.trimal_gt-0.8.aln'
-out=${aln%.trimal_gt-0.8.aln}
+# #proteins
+# aln='rabs.trimal_gt-0.8.aln'
+# out=${aln%.trimal_gt-0.8.aln}
 
-raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N autoMRE_IGN -p 12345 -s $aln -n $out
+# raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N autoMRE_IGN -p 12345 -s $aln -n $out
 
 
-# #18S
-# aln='mesozoa.trimal_gt-0.25_cons-50.aln'
-# out=${aln%.trimal_gt-0.25_cons-50.aln}
+#18S
+aln='18S_dataset4trim.aln'
+out=${aln%_dataset4trim.aln}
 
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
 
 
 #copy files back
