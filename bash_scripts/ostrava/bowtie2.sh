c1ebd90d4800ae53e4769eed6d9e1016ad3e5f63
kikinocka <kika.zahonova@gmail.com>
2024-02-05 10:47:37 +0100

    bla

--- bash_scripts/ostrava/bowtie2.sh
+++ bash_scripts/ostrava/bowtie2.sh
@@ -5,12 +5,12 @@
 #PBS -l nodes=1:ppn=80
 #PBS -l walltime=600:00:00
 
-cd '/home/users/kika/bnon_pfr_ko/'
+cd '/home/users/kika/bnon_KOs/catalase/'
 
-ref='p57_polished.fa'
-p1_1='reads/bnon_PF16_KO.trimmed_1.fq.gz'
-p1_2='reads/bnon_PF16_KO.trimmed_2.fq.gz'
-base_name='bw2_mapping/bnon_PF16_KO.bw2'
+ref='/home/users/kika/bnon_KOs/p57_polished.fa'
+p1_1='reads/cat_KO.trimmed_1.fq.gz'
+p1_2='reads/cat_KO.trimmed_2.fq.gz'
+base_name='bw2_mapping/cat_KO.bw2'
 samfile=$base_name'.sam'
 mapped=$base_name'_mapped.fq.gz'
 unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
@@ -39,7 +39,7 @@ bowtie2 --very-sensitive -p 50 \
 
 
 # samtools view -bS -F 4 $samfile > $bamfile -@ 50 #writes only mapped reads to bamfile
-samtools view -bS -@ 20 $samfile > $bamfile
+samtools view -bS -@ 50 $samfile > $bamfile
 samtools sort -o $sorted -@ 50 $bamfile 
 samtools index -b $sorted
 
