846930741a012d1d964f77606daa621b95e411fd
kikinocka <kika.zahonova@gmail.com>
2024-02-06 09:44:50 +0100

    bla

--- bash_scripts/metacentrum/blast.sh
+++ bash_scripts/metacentrum/blast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N blast
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=3gb
-#PBS -l walltime=196:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,19 +11,19 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module load blast
 
-datadir='/storage/brno3-cerit/home/kika/oil_sands/V4-sediment/'
+datadir='/storage/brno12-cerit/home/kika/oil_sands/metagenomes/'
 db_dir='/storage/projects/BlastDB/'
 
 
 #copy files to scratch
-cp $datadir'check_cont.fa' $SCRATCHDIR
+cp $datadir'metagenomes.fa' $SCRATCHDIR
 cp $db_dir'nt'* $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
 
-query='check_cont.fa'
-out='check_cont.fwd_ncbi-nt.tsv'
+query='metagenomes.fa'
+out='metagenomes.nt_ncbi-nt.tsv'
 db='nt'
 program=blastn
 task=blastn
@@ -35,7 +35,7 @@ $program -task $task \
 	-query $query \
 	-db $db \
 	-out $out \
-	-outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore ppos" \
+	-outfmt '6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send' \
 	-num_threads $PBS_NUM_PPN \
 	-evalue $eval \
 	-max_target_seqs $max_seqs \
