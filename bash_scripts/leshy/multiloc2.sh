4735185d644b77e5d40a0797a8b1da0e1809ee31
kikinocka <kika.zahonova@gmail.com>
2024-02-14 18:15:46 +0100

    bla

--- bash_scripts/leshy/multiloc2.sh
+++ bash_scripts/leshy/multiloc2.sh
@@ -11,14 +11,14 @@ f=fungal
 for file in *.fa; do
 	echo $file
 	
-	# out=${file%.*}'.ML2_fungal.txt'
-	# python2 $multiloc -fasta=$file -origin=$f -predictor=LowRes -result=$out -output=simple
+	out=${file%.*}'.ML2_fungal.txt'
+	python2 $multiloc -fasta=$file -origin=$f -predictor=LowRes -result=$out -output=simple
 	
-	# out=${file%.*}'.ML2_animal.txt'
-	# python2 $multiloc -fasta=$file -origin=$a -predictor=LowRes -result=$out -output=simple
+	out=${file%.*}'.ML2_animal.txt'
+	python2 $multiloc -fasta=$file -origin=$a -predictor=LowRes -result=$out -output=simple
 
-	out=${file%.*}'.ML2_plant.txt'
-	python2 $multiloc -fasta=$file -origin=$p -predictor=LowRes -result=$out -output=simple
+	# out=${file%.*}'.ML2_plant.txt'
+	# python2 $multiloc -fasta=$file -origin=$p -predictor=LowRes -result=$out -output=simple
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MultiLoc2 done
--- py_scripts/pts_search.py
+++ py_scripts/pts_search.py
@@ -3,8 +3,8 @@ import os
 import re
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/blastocrithidia/genes/metabolism/')
-proteins = SeqIO.parse('refs.fa', 'fasta')
+os.chdir('/Users/kika/ownCloud/blastocrithidia/genes/')
+proteins = SeqIO.parse('candidates.fa', 'fasta')
 
 #kinetoplastids - from Fred
 pts1 = r'(S|A|G|C|N|P)(R|H|K|S|N|Q)(L|I|V|F|A|M|Y)'
@@ -23,7 +23,7 @@ mito = r'^M(R|H|K|F|L)\w{0,1}(R|H|K|S|T)\w{1,10}(S|T|R|K)'
 # pts1 = r'(S|A|C)(K|R|H|Q)(L|M)'
 # pts2 = r'^\w{1,21}R(L|I|V|Q)\w{2}(L|I|V|Q|H)(L|S|G|A)\w{1}(H|Q)(L|A)'
 
-with open('refs.possibly_peroxisomal.Fred.fa', 'w') as out:
+with open('candidates.possibly_peroxisomal.Fred.fa', 'w') as out:
 	for protein in proteins:
 		if str(protein.seq)[-1:] == '*':
 			protein.seq = str(protein.seq)[:-1]
