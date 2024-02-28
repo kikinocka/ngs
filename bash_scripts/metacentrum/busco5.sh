b8d179a0d3fadf20c7ecd150bc59ffabcc32ee0b
kikinocka <kika.zahonova@gmail.com>
2024-02-28 21:13:25 +0100

    bla

--- bash_scripts/metacentrum/busco5.sh
+++ bash_scripts/metacentrum/busco5.sh
@@ -18,7 +18,7 @@ conda activate busco
 assembly_dir='/storage/brno12-cerit/home/kika/blasto_comparative/proteins_FINAL'
 
 #copy files to scratch
-cp $assembly_dir'/O'*.faa $SCRATCHDIR
+cp $assembly_dir'/'*.faa $SCRATCHDIR
 
 
 #compute on scratch
@@ -31,14 +31,14 @@ for fasta in *.faa; do
 	mode='proteins'
 	
 	lineage='eukaryota_odb10'
-	base=${fasta%.faa}
+	base=${fasta%.faa}_$lineage
 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	cp $base'/short_summary.specific.'$lineage'.'$base'.txt' BUSCO_summaries
+	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
 
 	lineage='euglenozoa_odb10'
-	base=${fasta%.faa}
+	base=${fasta%.faa}_$lineage
 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	cp $base'/short_summary.specific.'$lineage'.'$base'.txt' BUSCO_summaries
+	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
 done
 
 generate_plot.py -wd BUSCO_summaries
--- py_scripts/retrieve_seqs.py
+++ py_scripts/retrieve_seqs.py
@@ -2,27 +2,28 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/blasto_comparative/proteins/CDS_problematic/')
-files = [x for x in os.listdir() if x.startswith('omod_problematic')]
-database = '/Users/kika/ownCloud/blasto_comparative/proteins/Omod_proteins_annotated.fa'
+os.chdir('/Users/kika/ownCloud/blasto_comparative/proteins/FINAL/')
+files = [x for x in os.listdir() if x.startswith('oeli')]
+database = '/Users/kika/ownCloud/blasto_comparative/proteins/Oeli_companion.CDS_cdseq_all.fna'
 
 for accessions in files: 
 	print(accessions)
 	fname = accessions.split('.acc')[0]
 	retrieve = set()
 
-	with open('{}_proteins_annotated.CDS_OK_l30.faa'.format(fname), 'w') as out:
+	with open('{}_proteins_annotated.CDS_OK.fna'.format(fname), 'w') as out:
 		db = SeqIO.parse(database, 'fasta')
 		# print(db)
 		for line in open(accessions):
 			retrieve.add(line[:-1])
 		# print(retrieve)
 		for seq in db:
-			if seq.name in retrieve:
+			if seq.name.split('.')[0] in retrieve:
 				# print(seq.name)
-				# out.write('>{}\n{}\n'.format(seq.description, seq.seq))
-				pass
-			else:
-				# pass
 				out.write('>{}\n{}\n'.format(seq.description, seq.seq))
+				# pass
+			else:
+				# print(seq.name)
+				pass
+				# out.write('>{}\n{}\n'.format(seq.description, seq.seq))
 
