c5f0cb0a070ef495b6b58e2bfd65ef8c91e6ea34
kikinocka <kika.zahonova@gmail.com>
2024-02-29 19:27:30 +0100

    bla

--- bash_scripts/metacentrum/raxml-epa.sh
+++ bash_scripts/metacentrum/raxml-epa.sh
@@ -13,19 +13,19 @@ source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
 
-data='/storage/brno12-cerit/home/kika/oil_sands/Lane26_18S_V9/euglenozoa/ver2/'
+data='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/dinoflagellates/'
 
 #copy files to scratch
-cp $data'placement/euglenozoa_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
-cp $data'ref_tree/RAxML_bipartitions.euglenozoa3' $SCRATCHDIR
+cp $data'placement/dinoflagellates_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'ref_tree/RAxML_bipartitions.dinoflagellates3' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='euglenozoa_V9.trimal_gt-0.25_cons-50.aln'
-tree='RAxML_bipartitions.euglenozoa3'
-out='EPARUN_euglenozoa'
+aln='dinoflagellates_V9.trimal_gt-0.25_cons-50.aln'
+tree='RAxML_bipartitions.dinoflagellates3'
+out='EPARUN_dinoflagellates'
 
 raxmlHPC-PTHREADS -f v -G 0.2 -m GTRCAT -n $out -s $aln -t $tree -T $PBS_NUM_PPN
 
