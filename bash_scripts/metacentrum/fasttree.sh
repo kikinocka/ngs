d7f1d5f4594055abf68ba24ff5303b80825e66fd
kikinocka <kika.zahonova@gmail.com>
2024-03-07 20:52:32 +0100

    bla

--- bash_scripts/metacentrum/fasttree.sh
+++ bash_scripts/metacentrum/fasttree.sh
@@ -15,7 +15,7 @@ module load fasttree
 data_dir='/storage/brno12-cerit/home/kika/metamonads/fasttree/not_filtered'
 
 #copy files to scratch
-cp $data_dir'/q2002142'*.aln $SCRATCHDIR
+cp $data_dir'/q2002260'*.aln $SCRATCHDIR
 
 
 #compute on scratch
