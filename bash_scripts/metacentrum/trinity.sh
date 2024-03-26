677ac4517401b3afe3eb0a5f6c8174c4a50298d5
kikinocka <kika.zahonova@gmail.com>
2024-03-26 20:44:55 +0100

    bla

--- bash_scripts/metacentrum/trinity.sh
+++ bash_scripts/metacentrum/trinity.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 #PBS -N Trinity
 #PBS -l select=1:ncpus=30:mem=100gb:scratch_local=50gb
-#PBS -l walltime=48:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
-module add trinity-2.11.0
+# module add trinity-2.11.0
+source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
+module load trinity
 
-read_dir='/storage/brno3-cerit/home/kika/UGA_decoding/nyctotherus/reads'
-out_dir='/storage/brno3-cerit/home/kika/UGA_decoding/nyctotherus/trinity/'
+
+workdir='/storage/brno12-cerit/home/kika/schizosaccharomyces_japonicus/'
 
 #copy files to scratch
-cp $read_dir'/'*trimmed_*.fq.gz $SCRATCHDIR
+cp $workdir'reads/NG-A0875_Donna_1_trimmed_'*.fq.gz $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-fw='all_trimmed_1.fq.gz'
-rv='all_trimmed_2.fq.gz'
+fw='NG-A0875_Donna_1_trimmed_1.fq.gz'
+rv='NG-A0875_Donna_1_trimmed_2.fq.gz'
+out='donna1_trinity'
 
-Trinity --seqType fq --left $fw --right $rv --max_memory 100G --CPU $PBS_NUM_PPN
+Trinity --seqType fq --left $fw --right $rv --output $out --max_memory 100G --CPU $PBS_NUM_PPN
 
 
 #copy files back
 rm *trimmed_*fq.gz
-cp -r * $out_dir
+cp -r * $workdir
