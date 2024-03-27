18f8d13a1d30d90405dbd0c3fba61f6051dfdcd5
kikinocka <kika.zahonova@gmail.com>
2024-03-27 18:14:09 +0100

    bla

--- bash_scripts/metacentrum/trinity.sh
+++ bash_scripts/metacentrum/trinity.sh
@@ -8,9 +8,9 @@
 cat $PBS_NODEFILE
 
 #add module
-# module add trinity-2.11.0
-source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
-module load trinity
+module add trinity-2.11.0
+# source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
+# module load trinity
 
 
 workdir='/storage/brno12-cerit/home/kika/schizosaccharomyces_japonicus/'
@@ -26,7 +26,7 @@ fw='NG-A0875_Donna_1_trimmed_1.fq.gz'
 rv='NG-A0875_Donna_1_trimmed_2.fq.gz'
 out='donna1_trinity'
 
-Trinity --seqType fq --left $fw --right $rv --output $out --JM 100G --CPU $PBS_NUM_PPN
+Trinity --seqType fq --left $fw --right $rv --output $out --max_memory 100G --CPU $PBS_NUM_PPN
 
 
 #copy files back
--- bash_scripts/ostrava/trinity.sh
+++ bash_scripts/ostrava/trinity.sh
@@ -2,7 +2,7 @@
 #PBS -d .
 #PBS -v PATH
 #PBS -N Trinity
-#PBS -l nodes=1:ppn=20
+#PBS -l nodes=1:ppn=30
 #PBS -l walltime=100:00:00
 
 
@@ -15,5 +15,12 @@ fw=$read_dir'NG-A0875_Donna_1_trimmed_1.fq.gz'
 rv=$read_dir'NG-A0875_Donna_1_trimmed_2.fq.gz'
 
 
+eval "$(/home/users/kika/miniconda3/bin/conda shell.bash hook)"
+conda activate trinity
+
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --CPU 30
 
+conda deactivate
+
+
+python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: Trinity done
