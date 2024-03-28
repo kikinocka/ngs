ffe7b59baa747a6f978b861b58ff6c073ce3ee8f
kikinocka <kika.zahonova@gmail.com>
2024-03-28 09:35:48 +0100

    bla

---
+++ bash_scripts/ostrava/hmmsearch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -d .
+#PBS -v PATH
+#PBS -N Trinity
+#PBS -l nodes=1:ppn=15
+#PBS -l walltime=100:00:00
+
+cd '/home/users/kika/schizosaccharomyces_japonicus/hmms'
+threads=15
+eval=1e-10
+
+#several profiles and one database
+db='/home/users/kika/schizosaccharomyces_japonicus/donna1.Trinity_translated.fa'
+orgn='donna1'
+
+for profile in *.hmm ; do
+	out=$orgn'_'${profile%.trimal_gt-0.8.aln.hmm}.hmmsearch.tsv
+	hmmsearch --tblout $out --cpu $threads -E $eval $profile $db
+done
+
+
+python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: hmmsearch done
