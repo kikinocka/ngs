93982a2477e3130395f76273b1d7694c055e0c22
kikinocka <kika.zahonova@gmail.com>
2024-03-28 15:06:58 +0100

    bla

--- py_scripts/translation.py
+++ py_scripts/translation.py
@@ -3,9 +3,9 @@ import os
 from Bio import SeqIO
 
 os.chdir('/home/users/kika/schizosaccharomyces_japonicus/')
-infile = SeqIO.parse('donna6_trinity/donna6.Trinity.fasta', 'fasta')
-output = open('donna6.Trinity_translated.fa', 'w')
-error = open('donna6.errors.fa', 'w')
+infile = SeqIO.parse('donna7_trinity/donna7.Trinity.fasta', 'fasta')
+output = open('donna7.Trinity_translated.fa', 'w')
+error = open('donna7.errors.fa', 'w')
 
 gencode = {
     'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
