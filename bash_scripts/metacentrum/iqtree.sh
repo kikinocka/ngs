6c2406bb382ad5045cd127b3d65ab102e504cd32
kikinocka <kika.zahonova@gmail.com>
2024-04-02 16:52:31 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -12,7 +12,7 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module load iqtree
 
-datadir='/storage/brno12-cerit/home/kika/kinetoplastids/kinesins/kin2/ver6/'
+datadir='/storage/brno12-cerit/home/kika/kinetoplastids/kinesins/kin2/ver7/'
 
 #copy files to scratch
 # cp $datadir'/'*.aln $SCRATCHDIR
