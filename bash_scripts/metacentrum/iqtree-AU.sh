18d8f7889bd2eca4a62e89d9d58d5d41bc6dc95a
kikinocka <kika.zahonova@gmail.com>
2024-04-03 14:01:43 +0200

    bla

--- bash_scripts/metacentrum/iqtree-AU.sh
+++ bash_scripts/metacentrum/iqtree-AU.sh
@@ -8,29 +8,32 @@
 cat $PBS_NODEFILE
 
 #add module
-module add iqtree-2.2.0
+module load iqtree
 
-datadir='/storage/brno3-cerit/home/kika/diplonema/cardiolipin/CLD1/ver5_AU'
+datadir='/storage/brno12-cerit/home/kika/kinetoplastids/kinesins/kin2/ver6/'
 
 #copy files to scratch
-# cp $datadir'/'* $SCRATCHDIR
-cp $datadir'/'* $SCRATCHDIR
+cp $datadir'kinesins.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $datadir'kinesins.trimal_gt-0.8.aln.ufboot' $SCRATCHDIR
+cp $datadir'kinesins.trimal_gt-0.8.constr1' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
-pref='cld'
-aln='cld.trimal_gt-0.8.aln'
-constr1='cld.constr1'
+pref='kinesins.trimal_gt-0.8'
+aln='kinesins.trimal_gt-0.8.aln'
+boottrees='kinesins.trimal_gt-0.8.aln.ufboot'
+constr1='kinesins.trimal_gt-0.8.constr1'
 # constr2='sec13.constr2'
 ufb=1000
 
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr1 --prefix $pref.constr1
-# iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln -g $constr2 --prefix $pref.constr2
-cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
-iqtree2 -m LG+C20+G -nt AUTO -ntmax $PBS_NUM_PPN -quiet -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
+# iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
+iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr1 --prefix $pref.constr1
+# iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr2 --prefix $pref.constr2
+# cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
+cat $boottrees $pref.constr*.treefile > $pref.trees
+iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
 
 #copy files back
-# rm $aln $constr
+rm $aln $constr1 $boottrees
 cp * $datadir
