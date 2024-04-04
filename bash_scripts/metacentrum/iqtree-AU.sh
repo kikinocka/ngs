a139bc6243bac1ffb3f52a54f0c1916792cd872e
kikinocka <kika.zahonova@gmail.com>
2024-04-04 10:08:49 +0200

    bla

--- bash_scripts/metacentrum/iqtree-AU.sh
+++ bash_scripts/metacentrum/iqtree-AU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-AU
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -15,25 +15,38 @@ datadir='/storage/brno12-cerit/home/kika/kinetoplastids/kinesins/kin2/ver6/'
 #copy files to scratch
 cp $datadir'kinesins.trimal_gt-0.8.aln' $SCRATCHDIR
 cp $datadir'kinesins.trimal_gt-0.8.aln.ufboot' $SCRATCHDIR
-cp $datadir'kinesins.trimal_gt-0.8.constr1' $SCRATCHDIR
+cp $datadir'au_test/kinesins.constr1' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
-pref='kinesins.trimal_gt-0.8'
-aln='kinesins.trimal_gt-0.8.aln'
-boottrees='kinesins.trimal_gt-0.8.aln.ufboot'
-constr1='kinesins.trimal_gt-0.8.constr1'
+
+# #calculate UFB trees and then perform AU test
+# pref='sec13'
+# aln='sec13.trimal_gt-0.8.aln'
+# constr1='sec13.constr1'
 # constr2='sec13.constr2'
+# ufb=1000
+
+# iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
+# iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr1 --prefix $pref.constr1
+# iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr2 --prefix $pref.constr2
+# cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
+# iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
+
+
+#already having UFB trees; perform only AU test
+pref='kinesins'
+aln='kinesins.trimal_gt-0.8.aln'
+ufb_trees='kinesins.trimal_gt-0.8.aln.ufboot'
+constr1='kinesins.constr1'
 ufb=1000
 
-# iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $pref.unconstr -B $ufb --boot-trees
-iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr1 --prefix $pref.constr1
-# iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr2 --prefix $pref.constr2
-# cat $pref.unconstr.ufboot $pref.constr*.treefile > $pref.trees
-cat $boottrees $pref.constr*.treefile > $pref.trees
-iqtree2 -m LG+C20+G -T 15 --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
+iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr1 --prefix $pref.constr1
+cat $ufb_trees $pref.constr1.treefile > $pref.trees
+iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
+
 
 #copy files back
-rm $aln $constr1 $boottrees
-cp * $datadir'au_test'
+rm $aln $constr1 $ufb_trees
+cp * $datadir'au_test/'
