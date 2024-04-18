0dfad2b30af1f133389a8e8431b29b02aacc2478
kikinocka <kika.zahonova@gmail.com>
2024-04-18 18:41:24 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,8 +6,9 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ArfGEFs/')
-files = [x for x in os.listdir() if x.endswith('.fa')]
+os.chdir('/Users/kika/ownCloud/blasto_comparative/genes/aaRS/')
+# files = [x for x in os.listdir() if x.endswith('.fa')]
+files = [x for x in os.listdir() if x.startswith('kineto')]
 
 for file in files:
 	print(file)
