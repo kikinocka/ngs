fcdbe2dea20d2018c6844ed554c3afa32876400d
kikinocka <kika.zahonova@gmail.com>
2024-04-19 16:59:52 +0200

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,9 +2,9 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/Dsl1/rint1/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ph-arf/'
 
-for aln in *.aln ; do
+for aln in *mafft.aln ; do
 	out=${aln%.mafft.aln}.CD.mafft.aln
 	table=${aln%.aln}.table
 	perl $replacer -f $aln $out $table
