ada20ee86f89d30361aa7200becf7fb79366e3e6
kikinocka <kika.zahonova@gmail.com>
2024-04-19 09:54:42 +0200

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,8 +3,8 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ArfGEFs/')
-files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ph-arf/')
+files = [x for x in os.listdir() if x.endswith('reduced.mafft.aln')]
 # files = [x for x in os.listdir() if x.endswith('.einsi.aln')]
 
 for file in files:
--- py_scripts/treereducer.py
+++ py_scripts/treereducer.py
@@ -2,22 +2,22 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/kinetoplastids/kinesins/kinesins/kin2/')
-inacc = open('kinesins.trimal_gt-0.8.marked.treefile')
-infasta = SeqIO.parse('/Users/kika/ownCloud/kinetoplastids/kinesins/kinesins/kin_all_tree/ver2/kinesins.fa', 'fasta')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ph-arf/')
+inacc = open('arfs.mb+raxml_renamed.tre')
+infasta = SeqIO.parse('arfs.mafft.aln', 'fasta')
 
 omitted = []
 for line in inacc:
 	if 'color=' in line:
-		print(line.split('[')[0].replace('\'', '').replace('\t', ''))
-	omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
+		# print(line.split('[')[0].replace('\'', '').replace('\t', ''))
+		omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
 
 c = 0
-with open('kinesins.fa', 'w') as result:
+with open('arfs_reduced.mafft.aln', 'w') as result:
 	for seq in infasta:
 		if seq.description in omitted:
 			c += 1
-			print('{} in omitted list'.format(seq.name))
+			print('{} in omitted list'.format(seq.description))
 		else:
 			result.write('>{}\n{}\n'.format(seq.description, seq.seq))
 print(c)
