4b436bfe07c35000606923d4ce907eb0357066b7
kikinocka <kika.zahonova@gmail.com>
2024-04-25 11:18:22 +0200

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,9 +2,9 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ph-arf/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver8/'
 
-for aln in *mafft.aln ; do
+for aln in *.aln ; do
 	out=${aln%.mafft.aln}.CD.mafft.aln
 	table=${aln%.aln}.table
 	perl $replacer -f $aln $out $table
