cb74969bf32c0cdbf9c3ed928be56866be95b752
kikinocka <kika.zahonova@gmail.com>
2024-05-10 13:40:23 +0200

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -5,60 +5,60 @@ import subprocess
 mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
-# #align de-novo
-# os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver7/')
-# files = [x for x in os.listdir() if x.endswith('.fa')]
-# # files = [x for x in os.listdir() if x.startswith('kineto')]
+#align de-novo
+os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/trees/ichthyosporea//')
+files = [x for x in os.listdir() if x.endswith('.fa')]
+# files = [x for x in os.listdir() if x.startswith('kineto')]
 
-# for file in files:
-# 	print(file)
-# 	#L-INS-i (proteins with one alignable domain)
-# 	out = '{}.mafft.aln'.format(file.split('.fa')[0])
-# 	log = '{}.mafft.log'.format(file.split('.fa')[0])
-# 	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
-# 		mafft, file, out, log), shell=True)
+for file in files:
+	print(file)
+	#L-INS-i (proteins with one alignable domain)
+	out = '{}.mafft.aln'.format(file.split('.fa')[0])
+	log = '{}.mafft.log'.format(file.split('.fa')[0])
+	subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
+		mafft, file, out, log), shell=True)
 
-# 	# #E-INS-i (proteins with several functional domains)
-# 	# out = '{}.einsi.aln'.format(file.split('.fa')[0])
-# 	# log = '{}.einsi.log'.format(file.split('.fa')[0])
-# 	# subprocess.call('{} --thread 7 --genafpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
-# 	# 	mafft, file, out, log), shell=True)
+	# #E-INS-i (proteins with several functional domains)
+	# out = '{}.einsi.aln'.format(file.split('.fa')[0])
+	# log = '{}.einsi.log'.format(file.split('.fa')[0])
+	# subprocess.call('{} --thread 7 --genafpair --maxiterate 1000 --inputorder {} > {} 2> {}'.format(
+	# 	mafft, file, out, log), shell=True)
 
-# 	# #tRNAs
-# 	# out = '{}.mafft.aln'.format(file.split('.fa')[0])
-# 	# log = '{}.mafft.log'.format(file.split('.fa')[0])
-# 	# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --op 10 --ep 0 --inputorder {} > {} 2> {}'.format(
-# 	# 	mafft, file, out, log), shell=True)
+	# #tRNAs
+	# out = '{}.mafft.aln'.format(file.split('.fa')[0])
+	# log = '{}.mafft.log'.format(file.split('.fa')[0])
+	# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --op 10 --ep 0 --inputorder {} > {} 2> {}'.format(
+	# 	mafft, file, out, log), shell=True)
 	
-# 	# subprocess.call('{} --auto --inputorder {} > {} 2> {}'.format(mafft, file, out, log), shell=True)
+	# subprocess.call('{} --auto --inputorder {} > {} 2> {}'.format(mafft, file, out, log), shell=True)
 
 
 # #add to aligned sequences
-# os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/')
-# existing = 'medium_nodiplo_noeugl.mafft.aln'
-# add = 'ver7/diplo_AP5M.fa'
-# out = 'ver8/medium.mafft.aln'
-# log = 'ver8/medium.mafft.log'
-# subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
-# # subprocess.call('{} --addfragments {} --thread 7 --inputorder --keeplength {} > {} 2> {}'.format(mafft, add, existing, out, log), 
-# # 	shell=True)
+# os.chdir('/Users/kika/ownCloud/SL_Euglenozoa/V9/trees/ichthyosporea/')
+# existing = 'Ichthyosporea_alignDeapSea.aln'
+# add = 'V9.fa'
+# out = 'ichthyosporea_V9.mafft.aln'
+# log = 'ichthyosporea_V9.mafft.log'
+# # subprocess.call('{} --add {} --thread 7 --inputorder {} > {} 2> {}'.format(mafft, add, existing, out, log), shell=True)
+# subprocess.call('{} --addfragments {} --thread 7 --inputorder --keeplength {} > {} 2> {}'.format(mafft, add, existing, out, log), 
+# 	shell=True)
 	
 
-#merge alignments
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver8/')
-aln1 = '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver5/medium.mafft.aln'
-aln2 = 'diplo_AP5M.mafft.aln'
-# fasta = 'arfs.fa'
-input = 'medium.mafft.in'
-table = 'medium.mafft.table'
-out = 'medium.mafft.aln'
-log = 'medium.mafft.log'
-# subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
-subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
-print('Alignments concatenated\n\n')
-subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
-# subprocess.call('ruby {} {} {} {} {} {} {} > {}'.format(maketable, aln1, aln2, aln3, aln4, aln5, aln6, table), shell=True)
-print('Table prepared\n\n')
-# subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
-print('Alignments merged')
+# #merge alignments
+# os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver8/')
+# aln1 = '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/all_adaptors/ver5/medium.mafft.aln'
+# aln2 = 'diplo_AP5M.mafft.aln'
+# # fasta = 'arfs.fa'
+# input = 'medium.mafft.in'
+# table = 'medium.mafft.table'
+# out = 'medium.mafft.aln'
+# log = 'medium.mafft.log'
+# # subprocess.call('cat {} {} > {}'.format(aln1, fasta, input), shell=True)
+# subprocess.call('cat {} {} > {}'.format(aln1, aln2, input), shell=True)
+# print('Alignments concatenated\n\n')
+# subprocess.call('ruby {} {} > {}'.format(maketable, aln1, table), shell=True)
+# # subprocess.call('ruby {} {} {} {} {} {} {} > {}'.format(maketable, aln1, aln2, aln3, aln4, aln5, aln6, table), shell=True)
+# print('Table prepared\n\n')
+# # subprocess.call('{} --thread 7 --localpair --maxiterate 1000 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# subprocess.call('{} --thread 7 --merge {} {} > {} 2> {}'.format(mafft, table, input, out, log), shell=True)
+# print('Alignments merged')
--- py_scripts/treereducer.py
+++ py_scripts/treereducer.py
@@ -2,22 +2,45 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ph-arf/')
-inacc = open('arfs.mb+raxml_renamed.tre')
-infasta = SeqIO.parse('arfs.mafft.aln', 'fasta')
+os.chdir('/Users/kika/ownCloud/metamonada/ancestral_OGs/fasttree/test/')
+# inacc = open('arfs.mb+raxml_renamed.tre')
+# infasta = SeqIO.parse('arfs.mafft.aln', 'fasta')
+inacc_files = [x for x in os.listdir() if x.endswith('.treefile')]
+infasta_files = [x for x in os.listdir() if x.endswith('.fa')]
+
+# omitted = []
+# for line in inacc:
+# 	if 'color=' in line:
+# 		# print(line.split('[')[0].replace('\'', '').replace('\t', ''))
+# 		omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
+
+# c = 0
+# with open('arfs_reduced.mafft.aln', 'w') as result:
+# 	for seq in infasta:
+# 		if seq.description in omitted:
+# 			c += 1
+# 			print('{} in omitted list'.format(seq.description))
+# 		else:
+# 			result.write('>{}\n{}\n'.format(seq.description, seq.seq))
+# print(c)
+
 
 omitted = []
-for line in inacc:
-	if 'color=' in line:
-		# print(line.split('[')[0].replace('\'', '').replace('\t', ''))
-		omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
+for inacc in inacc_files:
+	fname = inacc.split('.')[0]
+	print(inacc)
+	# print(fname)
+	for line in open(inacc):
+		if 'color=#ff0000' in line:
+			# print(line.split('[')[0].replace('\'', '').replace('\t', ''))
+			omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
+	# print(omitted)
 
-c = 0
-with open('arfs_reduced.mafft.aln', 'w') as result:
-	for seq in infasta:
-		if seq.description in omitted:
-			c += 1
-			print('{} in omitted list'.format(seq.description))
-		else:
-			result.write('>{}\n{}\n'.format(seq.description, seq.seq))
-print(c)
+	with open('{}.og_hmm.final.fa'.format(fname), 'w') as result:
+		for infasta in infasta_files:
+			if fname in infasta:
+				for seq in SeqIO.parse(infasta, 'fasta'):
+					if seq.description in omitted:
+						pass
+					else:
+						result.write('>{}\n{}\n'.format(seq.description, seq.seq))
