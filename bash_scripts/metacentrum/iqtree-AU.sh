b22bba286627372cca62784b2830fc1c08a2d670
kikinocka <kika.zahonova@gmail.com>
2024-05-16 11:45:50 +0200

    bla

--- bash_scripts/metacentrum/iqtree-AU.sh
+++ bash_scripts/metacentrum/iqtree-AU.sh
@@ -16,7 +16,7 @@ datadir='/storage/brno12-cerit/home/kika/trafficking/diplonemids_all/ARFs/ph-arf
 cp $datadir'iqtree/arfs_reduced.trimal_gt-0.8.aln' $SCRATCHDIR
 cp $datadir'iqtree/arfs_reduced.trimal_gt-0.8.aln.treefile' $SCRATCHDIR
 cp $datadir'iqtree/arfs_reduced.trimal_gt-0.8.aln.ufboot' $SCRATCHDIR
-cp $datadir'au_test/arfs_reduced.constr1' $SCRATCHDIR
+cp $datadir'au_test/arfs_reduced.constr'* $SCRATCHDIR
 
 
 #compute on scratch
@@ -42,13 +42,15 @@ aln='arfs_reduced.trimal_gt-0.8.aln'
 ufb_trees='arfs_reduced.trimal_gt-0.8.aln.ufboot'
 ml_tree='arfs_reduced.trimal_gt-0.8.aln.treefile'
 constr1='arfs_reduced.constr1'
+constr2='arfs_reduced.constr2'
 ufb=1000
 
 iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr1 --prefix $pref.constr1
-cat $ml_tree $pref.constr1.treefile $ufb_trees > $pref.trees
+iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln -g $constr1 --prefix $pref.constr2
+cat $ml_tree $pref.constr*.treefile $ufb_trees > $pref.trees
 iqtree2 -m LG+C20+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --trees $pref.trees --test-weight --test-au --test 10000 -n 0
 
 
 #copy files back
-rm $aln $constr1 $ufb_trees
+rm $aln $constr* $ufb_trees
 cp * $datadir'au_test/'
