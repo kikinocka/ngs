7be9c6c5c652c0e3ba8cab2f727f90efb6660859
kikinocka <kika.zahonova@gmail.com>
2024-05-30 11:35:33 +0200

    bla

--- bash_scripts/metacentrum/raxml_many.sh
+++ bash_scripts/metacentrum/raxml_many.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml-many
 #PBS -l select=1:ncpus=20:mem=1gb:scratch_local=1gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,7 +11,7 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
-data='/storage/brno12-cerit/home/kika/trafficking/diplonemids_all/ARFs/ph-arf/ver3/'
+data='/storage/brno12-cerit/home/kika/trafficking/diplonemids_all/ARFs/ph-arf/ver4/'
 
 #copy files to scratch
 cp $data'/'*.aln $SCRATCHDIR
@@ -37,4 +37,4 @@ done
 
 #copy files back
 rm *.aln
-cp -R * $data
+cp -R * $data'raxml/'
