2b6d751f3c67f51254616e00566ec7a2f4386bd9
kikinocka <kika.zahonova@gmail.com>
2024-06-10 11:35:24 +0200

    bla

--- bash_scripts/metacentrum/raxml.sh
+++ bash_scripts/metacentrum/raxml.sh
@@ -33,7 +33,6 @@ out=${aln%.trimal_gt-0.25_cons-50.aln}
 
 raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
 # raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
-# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N autoMRE_IGN -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
 raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
 
 
--- bash_scripts/metacentrum/raxml2.sh
+++ bash_scripts/metacentrum/raxml2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml2
 #PBS -l select=1:ncpus=20:mem=10gb:scratch_local=1gb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=168:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -32,7 +32,7 @@ aln='apus+brev.trimal_gt-0.25_cons-50.aln'
 out=${aln%.trimal_gt-0.25_cons-50.aln}
 
 raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N autoMRE_IGN -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
 raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
 
 
