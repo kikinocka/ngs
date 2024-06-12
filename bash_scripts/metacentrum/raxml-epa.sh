a3f4c53d0dd2fdec48d560c9bffe361cb787373a
kikinocka <kika.zahonova@gmail.com>
2024-06-12 12:13:28 +0300

    bla

--- bash_scripts/metacentrum/raxml-epa.sh
+++ bash_scripts/metacentrum/raxml-epa.sh
@@ -13,19 +13,19 @@ source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
 
-data='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/ichthyosporea/'
+data='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/apus+brev/'
 
 #copy files to scratch
-cp $data'placement/ichthyosporea_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
-cp $data'ref_tree/RAxML_bipartitions.ichthyosporea3' $SCRATCHDIR
+cp $data'placement/apus+brev_V9.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'ref_tree/RAxML_bipartitions.apus+brev3' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-aln='ichthyosporea_V9.trimal_gt-0.25_cons-50.aln'
-tree='RAxML_bipartitions.ichthyosporea3'
-out='EPARUN_ichthyosporea'
+aln='apus+brev_V9.trimal_gt-0.25_cons-50.aln'
+tree='RAxML_bipartitions.apus+brev3'
+out='EPARUN_apus+brev'
 
 raxmlHPC-PTHREADS -f v -G 0.2 -m GTRCAT -n $out -s $aln -t $tree -T $PBS_NUM_PPN
 
