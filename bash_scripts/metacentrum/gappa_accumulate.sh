1dca02230d9e214a26ebaa9c0b0aec2641335586
kikinocka <kika.zahonova@gmail.com>
2024-06-14 16:17:02 +0300

    bla

--- bash_scripts/metacentrum/gappa_accumulate.sh
+++ bash_scripts/metacentrum/gappa_accumulate.sh
@@ -8,16 +8,16 @@
 cat $PBS_NODEFILE
 
 gappa='/storage/brno3-cerit/home/kika/miniconda3/bin/gappa'
-data='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/apicomplexans/placement/'
+data='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/myxozoa/placement/'
 
 #copy files to scratch
-cp $data'RAxML_portableTree.EPARUN_apicomplexans.jplace' $SCRATCHDIR
+cp $data'RAxML_portableTree.EPARUN_myxozoa.jplace' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-jplace='RAxML_portableTree.EPARUN_apicomplexans.jplace'
+jplace='RAxML_portableTree.EPARUN_myxozoa.jplace'
 prefix=${jplace%.jplace}.
 log=${jplace%.jplace}.accumulated.log
 threshold=0.8
