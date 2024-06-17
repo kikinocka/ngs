8efe5e9d6acbffc33b560aaaa1c8b37f9f561f9d
kikinocka <kika.zahonova@gmail.com>
2024-06-17 14:56:46 +0300

    bla

--- bash_scripts/metacentrum/iqtree_many19.sh
+++ bash_scripts/metacentrum/iqtree_many19.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-many19
 #PBS -l select=1:ncpus=15:mem=10gb:scratch_local=10gb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=336:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,16 +13,16 @@ module load iqtree-2.2.0
 data_dir='/storage/brno12-cerit/home/kika/metamonads/iqtree/'
 
 #copy files to scratch
-# cp $data_dir'q2001826.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001829.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001831.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001832.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001833.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001839.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001843.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001853.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001876.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $data_dir'q2001880.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002144.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002151.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002158.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002159.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002167.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002174.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002175.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002176.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002181.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2002194.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
 
 
 #compute on scratch
