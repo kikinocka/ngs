d75e6a7629e579b82addc7c2964bb84678447f0b
kikinocka <kika.zahonova@gmail.com>
2024-06-18 10:29:25 +0300

    bla

--- bash_scripts/metacentrum/iqtree_many20.sh
+++ bash_scripts/metacentrum/iqtree_many20.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-many20
 #PBS -l select=1:ncpus=15:mem=10gb:scratch_local=10gb
-#PBS -l walltime=336:00:00
+#PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,16 +13,16 @@ module load iqtree-2.2.0
 data_dir='/storage/brno12-cerit/home/kika/metamonads/iqtree/'
 
 #copy files to scratch
-cp $data_dir'q2001961.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001962.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001968.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001972.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001974.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001979.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001980.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2001985.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001961.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001962.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001968.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001972.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001974.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001979.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001980.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2001985.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
 cp $data_dir'q2002000.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2002004.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+# cp $data_dir'q2002004.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
 
 
 #compute on scratch
@@ -32,7 +32,7 @@ for f in *.aln ; do
 	echo ${f}
 	bb=1000
 	nm=5000
-	iqtree2 -m LG+C20+G4 -T 15 -B $bb --nmax $nm --quiet --safe -s ${f} --boot-trees
+	iqtree2 -m LG+C20+G4 -T 15 -B $bb --nmax $nm --quiet --safe -s ${f} --boot-trees --keep-ident
 done
 
 #copy files back
