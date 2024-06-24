f79e7e4dc4d85bd6176e7a17af83347d00564390
kikinocka <kika.zahonova@gmail.com>
2024-06-24 17:18:18 +0200

    bla

--- py_scripts/motif_search.py
+++ py_scripts/motif_search.py
@@ -3,19 +3,19 @@ import os
 import re
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/tbCAPs/')
-proteins = SeqIO.parse('diplo_TbCAP80.fa', 'fasta')
+os.chdir('/Users/kika/ownCloud/mycobacterium_smegmatis/')
+proteins = SeqIO.parse('GCF_013349145.1_ASM1334914v1_protein.faa', 'fasta')
 
 # #beta-barrel
-# motif = r'[KRHSTNQ]\w{1}G[AC]\w{1}[LIVFMWY]\w{1}[LIVFMWY]'
+motif = r'[KRHSTNQ]\w{1}G[AC]\w{1}[LIVFMWY]\w{1}[LIVFMWY]'
 
 #clathrin-binding
 # motif = r'L[LI][DEN][LF][DE]')
 # motif = r'L[V/I/L/F/W/Y/M][N/Q/S/T][V/I/L/F/W/Y/M][DE]')
 # motif = r'PWDLW'
-motif = r'LLDLL'
+# motif = r'LLDLL'
 
-with open('diplo_TbCAP80.clathrin-binding.fa', 'w') as out:
+with open('GCF_013349145.beta-barrel.fa', 'w') as out:
 	for protein in proteins:
 		if re.search(motif, str(protein.seq)):
 			match = re.search(motif, str(protein.seq))
