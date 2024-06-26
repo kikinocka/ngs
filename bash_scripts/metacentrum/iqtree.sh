165788dd6d6720308370324db4edfda0918b4870
kikinocka <kika.zahonova@gmail.com>
2024-06-26 18:01:05 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree-2.2.0
 
-datadir='/storage/brno12-cerit/home/kika/trafficking/mantamonas/rabs/ver2/'
+datadir='/storage/brno12-cerit/home/kika/trafficking/mantamonas/rabs/ver3/'
 
 #copy files to scratch
 cp $datadir'rabs.trimal_gt-0.8.aln' $SCRATCHDIR
