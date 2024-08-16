713fe854dfa21af5730864509c2eec3a3c9c2dde
kikinocka <kika.zahonova@gmail.com>
2024-08-16 10:23:52 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -8,14 +8,12 @@
 cat $PBS_NODEFILE
 
 #add module
-# module load iqtree-2.2.0
-module load iqtree
+module load iqtree-2.2.0
 
-datadir='/storage/brno12-cerit/home/kika/kinetoplastids/AOX/ver5/iqtree'
+datadir='/storage/brno12-cerit/home/kika/kinetoplastids/AOX/ver5/iqtree/'
 
 #copy files to scratch
-cp $datadir'/'* $SCRATCHDIR
-# cp $datadir'/'* $SCRATCHDIR
+cp $datadir'aox.CD.trimal_gt-0.8.aln' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
@@ -25,11 +23,9 @@ guide_tree=$guide'.treefile'
 bb=1000
 nm=5000
 
-# iqtree -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-iqtree -m LG+C20+G4 -T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 
-# iqtree2 -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-# iqtree2 -m LG+C20+G4 -T 15 --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
+iqtree2 -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+iqtree2 -m LG+C20+G4 -T 15 --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 
 # iqtree2 -m TEST -madd C10,C20,C30,C40,C50,C60,LG4M,LG4X,LG+F+G,LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C60+G \
 # 	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
