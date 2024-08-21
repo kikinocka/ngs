14c9104042d2b10664ff97f1787ade7ad4cf1753
kikinocka <kika.zahonova@gmail.com>
2024-08-21 10:42:31 +0200

    bla

--- bash_scripts/leshy/mafft.sh
+++ bash_scripts/leshy/mafft.sh
@@ -7,8 +7,8 @@ for f in *.fa ; do
 	echo $f
 	aln=${f%.fa}.mafft.aln
 	log=${f%.fa}.mafft.log
-	mafft --thread 15 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
-	# mafft --thread 15 --auto --inputorder ${f} > ${aln} 2> ${log}
+	# mafft --thread 15 --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
+	mafft --thread 15 --auto --inputorder ${f} > ${aln} 2> ${log}
 done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py MAFFT done
