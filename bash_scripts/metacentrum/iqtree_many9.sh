034dd940eee627efd38ec996db4dde386470df15
kikinocka <kika.zahonova@gmail.com>
2024-08-23 14:14:47 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many9.sh
+++ bash_scripts/metacentrum/iqtree_many9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-many9
 #PBS -l select=1:ncpus=15:mem=10gb:scratch_local=10gb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -13,7 +13,7 @@ module load iqtree-2.2.0
 data_dir='/storage/brno12-cerit/home/kika/metamonads/iqtree/'
 
 #copy files to scratch
-cp $data_dir'q2002972.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'q2003048.og_hmm.final2.trimal_at1.aln' $SCRATCHDIR
 
 
 #compute on scratch
