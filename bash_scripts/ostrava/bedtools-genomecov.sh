7d25410aa9432fc27ea5ceffb7a566d45918b9df
kikinocka <kika.zahonova@gmail.com>
2024-09-16 12:10:31 +0200

    bla

--- bash_scripts/ostrava/bedtools-genomecov.sh
+++ bash_scripts/ostrava/bedtools-genomecov.sh
@@ -12,5 +12,7 @@ genome='/home/users/kika/bnon_KOs/p57_polished.fa'
 bamfile='cat_KO.bw2_sorted.bam'
 out='cat_KO.genomecov.tsv'
 
-bedtools genomecov -d -split -ibam $bamfile -g $genome > $out
-# bedtools genomecov -bga -split -ibam $bamfile -g $genome > $out
+# bedtools genomecov -d -split -ibam $bamfile -g $genome > $out
+bedtools genomecov -bga -split -ibam $bamfile -g $genome > $out
+
+python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: bedtools genome coverage done
