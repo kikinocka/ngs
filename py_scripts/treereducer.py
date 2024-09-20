ee7a039c491637f4c89bfff7974b14c0e22b689e
kikinocka <kika.zahonova@gmail.com>
2024-09-20 12:20:11 +0100

    bla

--- py_scripts/treereducer.py
+++ py_scripts/treereducer.py
@@ -2,60 +2,60 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/metamonada/ancestral_OGs/')
-inacc = open('fasttree/q2002972.og_hmm.trimal_gt-0.8.filtered-50.fasttree.treefile')
-infasta = SeqIO.parse('red/q2002972.og_hmm.fa', 'fasta')
-# inacc_files = [x for x in os.listdir() if x.endswith('.treefile')]
-# infasta_files = [x for x in os.listdir() if x.endswith('.og_hmm.fa')]
-# inaln_files = [x for x in os.listdir() if x.endswith('.aln')]
+os.chdir('/mnt/mokosz/home/kika/workdir/')
+# inacc = open('fasttree/q2002972.og_hmm.trimal_gt-0.8.filtered-50.fasttree.treefile')
+# infasta = SeqIO.parse('red/q2002972.og_hmm.fa', 'fasta')
+inacc_files = [x for x in os.listdir() if x.endswith('.treefile')]
+inaln_files = [x for x in os.listdir() if x.endswith('.aln')]
+infasta_files = [x for x in os.listdir() if x.endswith('.og_hmm.fa')]
 
-omitted = []
-for line in inacc:
-	if 'color=#ff0000' in line:
-		# print(line.split('[')[0].replace('\'', '').replace('\t', '').replace('eval_', 'eval-'))
-		omitted.append(line.split('[')[0].replace('\'', '').replace('\t', '').replace('eval_', 'eval-'))
+# omitted = []
+# for line in inacc:
+# 	if 'color=#ff0000' in line:
+# 		# print(line.split('[')[0].replace('\'', '').replace('\t', '').replace('eval_', 'eval-'))
+# 		omitted.append(line.split('[')[0].replace('\'', '').replace('\t', '').replace('eval_', 'eval-'))
 
-c = 0
-with open('red/q2002972.og_hmm.final.fa', 'w') as result:
-	for seq in infasta:
-		if seq.description in omitted:
-			c += 1
-			print('{} in omitted list'.format(seq.description))
-		else:
-			result.write('>{}\n{}\n'.format(seq.description, seq.seq))
-print(c)
+# c = 0
+# with open('red/q2002972.og_hmm.final.fa', 'w') as result:
+# 	for seq in infasta:
+# 		if seq.description in omitted:
+# 			c += 1
+# 			print('{} in omitted list'.format(seq.description))
+# 		else:
+# 			result.write('>{}\n{}\n'.format(seq.description, seq.seq))
+# print(c)
 
 
-# for inacc in inacc_files:
-# 	omitted = []
-# 	accessions = []
-# 	fname = inacc.split('.')[0]
-# 	print(inacc)
-# 	# print(fname)
-# 	for line in open(inacc):
-# 		if 'color=#ff0000' in line:
-# 			# print(line.split('[')[0].replace('\'', '').replace('\t', ''))
-# 			omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
-# 	# print(len(omitted))
+for inacc in inacc_files:
+	omitted = []
+	accessions = []
+	fname = inacc.split('.')[0]
+	print(inacc)
+	# print(fname)
+	for line in open(inacc):
+		if 'color=#ff0000' in line:
+			# print(line.split('[')[0].replace('\'', '').replace('\t', ''))
+			omitted.append(line.split('[')[0].replace('\'', '').replace('\t', ''))
+	# print(len(omitted))
 
-# 	for inaln in inaln_files:
-# 		if fname in inaln:
-# 			for seq in SeqIO.parse(inaln, 'fasta'):
-# 				if seq.description in omitted:
-# 					pass
-# 				else:
-# 					accessions.append(seq.description)
-# 	# print(len(accessions))
+	for inaln in inaln_files:
+		if fname in inaln:
+			for seq in SeqIO.parse(inaln, 'fasta'):
+				if seq.description in omitted:
+					pass
+				else:
+					accessions.append(seq.description)
+	# print(len(accessions))
 
-# 	with open('{}.og_hmm.final.fa'.format(fname), 'w') as result:
-# 		for infasta in infasta_files:
-# 			# print(infasta)
-# 			if fname in infasta:
-# 				# print(fname)
-# 				for seq in SeqIO.parse(infasta, 'fasta'):
-# 					if seq.description in accessions:
-# 						result.write('>{}\n{}\n'.format(seq.description, seq.seq))
-# 					else:
-# 						pass
-# 						# print(seq.description)
+	with open('{}.og_hmm.final.fa'.format(fname), 'w') as result:
+		for infasta in infasta_files:
+			# print(infasta)
+			if fname in infasta:
+				# print(fname)
+				for seq in SeqIO.parse(infasta, 'fasta'):
+					if seq.description in accessions:
+						result.write('>{}\n{}\n'.format(seq.description, seq.seq))
+					else:
+						pass
+						# print(seq.description)
 
