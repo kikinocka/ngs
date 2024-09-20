d790105efbf67b4cdd16e566745e4ae2a7898cc3
kikinocka <kika.zahonova@gmail.com>
2024-09-20 09:41:04 +0100

    bla

--- bash_scripts/metacentrum/raxml.sh
+++ bash_scripts/metacentrum/raxml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N raxml
 #PBS -l select=1:ncpus=20:mem=10gb:scratch_local=1gb
-#PBS -l walltime=336:00:00
+#PBS -l walltime=96:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,29 +11,29 @@ cat $PBS_NODEFILE
 source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module add raxml/8.2.12-gcc-10.2.1-nu7c3k5
 
-data='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/apicomplexans/ref_tree/'
+data='/storage/brno12-cerit/home/kika/trafficking/mantamonas/arfs/raxml/'
 
 #copy files to scratch
-cp $data'apicomplexans.trimal_gt-0.25_cons-50.aln' $SCRATCHDIR
+cp $data'arfs.CD.trimal_gt-0.8.aln' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-# #proteins
-# aln='rabs.trimal_gt-0.8.aln'
-# out=${aln%.trimal_gt-0.8.aln}
+#proteins
+aln='arfs.CD.trimal_gt-0.8.aln'
+out=${aln%.trimal_gt-0.8.aln}
 
-# raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N autoMRE_IGN -p 12345 -s $aln -n $out
+raxmlHPC-PTHREADS -m PROTGAMMALG4XF -f a -T $PBS_NUM_PPN -x 123 -N autoMRE_IGN -p 12345 -s $aln -n $out
 
 
-#18S
-aln='apicomplexans.trimal_gt-0.25_cons-50.aln'
-out=${aln%.trimal_gt-0.25_cons-50.aln}
+# #18S
+# aln='apicomplexans.trimal_gt-0.25_cons-50.aln'
+# out=${aln%.trimal_gt-0.25_cons-50.aln}
 
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
-raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
+# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -N 3 -s $aln -n $out\1 -T $PBS_NUM_PPN
+# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -b 12345 -N 100 -f d -s $aln -n $out\2 -T $PBS_NUM_PPN
+# raxmlHPC-PTHREADS -m GTRCAT -p 12345 -f b -t RAxML_bestTree.$out\1 -z RAxML_bootstrap.$out\2 -n $out\3 -T $PBS_NUM_PPN
 
 
 #copy files back
--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,10 +2,10 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/kinetoplastids/AOX/tree/ver5/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/ARFs/iqtree/'
 
 for aln in *0.8.aln ; do
 	out=${aln%.trimal_gt-0.8.aln}.CD.trimal_gt-0.8.aln
-	table=${aln%.aln}.table
+	table=${aln%.aln}.codes_acc.txt
 	perl $replacer -f $aln $out $table
 done
