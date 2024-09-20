0557ed13793d656eca797ed1ad21aa941b3bc914
kikinocka <kika.zahonova@gmail.com>
2024-09-20 10:56:33 +0100

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,10 +2,10 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/ver3/iqtree/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/endocytic/ver3/iqtree/'
 
 for aln in *0.8.aln ; do
 	out=${aln%.trimal_gt-0.8.aln}.CD.trimal_gt-0.8.aln
-	table=${aln%.aln}.codes_acc.txt
+	table=${aln%.trimal_gt-0.8.aln}.codes_acc.txt
 	perl $replacer -f $aln $out $table
 done
