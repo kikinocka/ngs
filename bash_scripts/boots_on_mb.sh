86f7ec55801d696d593359bc28ea0ba5cd6f53e9
kikinocka <kika.zahonova@gmail.com>
2024-09-23 09:10:54 +0100

    bla

--- bash_scripts/boots_on_mb.sh
+++ bash_scripts/boots_on_mb.sh
@@ -2,10 +2,10 @@
 
 placer='/Users/kika/scripts/py_scripts/boots_on_mb.py'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/trees/ARFs/ph-arf/ver4/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/endocytic/ver3/'
 
-tree=$path'MrBayes/arfs_reduced.CD.trimal_gt-0.8.nex.con.tre'
-bootstrap=$path'RAxML/RAxML_bootstrap.arfs_reduced.CD'
-out=$path'combined/arfs_reduced.CD.mb+raxml.tre'
+tree=$path'MrBayes/endocytic.CD.trimal_gt-0.8.nex.con.tre'
+bootstrap=$path'RAxML/RAxML_bootstrap.endocytic.CD'
+out=$path'combined/endocytic.CD.mb+raxml.tre'
 
 $placer $tree $bootstrap $out
--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/ver3/raxml/')
-trees = [x for x in os.listdir() if x.endswith('RAxML_bipartitions.rabs.CD')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/endocytic/ver3/combined/')
+trees = [x for x in os.listdir() if x.endswith('.tre')]
 
 #file in format Acc. number \t name of organism \n
-names = open('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/ver3/rabs.codes_names.txt')
+names = open('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/RABs/endocytic/ver3/endocytic.codes_names.txt')
 
 name_dict = {}
 for name in names:
