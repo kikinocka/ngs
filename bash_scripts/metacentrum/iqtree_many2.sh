a14c6dc69aa9919d91c291837c02c764de866d0b
kikinocka <kika.zahonova@gmail.com>
2024-10-07 17:47:41 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many2.sh
+++ bash_scripts/metacentrum/iqtree_many2.sh
@@ -14,8 +14,6 @@ data_dir='/storage/brno12-cerit/home/kika/metamonads/iqtree/'
 
 #copy files to scratch
 cp $data_dir'q2000493.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2000853.og_hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'q2000947.og_hmm.final.trimal_at1.aln' $SCRATCHDIR
 
 
 #compute on scratch
@@ -24,7 +22,7 @@ cd $SCRATCHDIR
 for f in *.aln ; do
 	echo ${f}
 	bb=1000
-	nm=15000
+	nm=20000
 	iqtree2 -m LG+C20+G4 -T 15 -B $bb --nmax $nm --quiet --safe -s ${f} --boot-trees
 done
 
