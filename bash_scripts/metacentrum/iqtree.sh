369196b8cc369e12302d812a008203a3781d599b
kikinocka <kika.zahonova@gmail.com>
2024-11-19 11:50:27 +0100

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree-2.2.0
 
-datadir='/storage/brno12-cerit/home/kika/trafficking/diplonemids_ESCRTs/escrt0/backbone/ver3/'
+datadir='/storage/brno12-cerit/home/kika/trafficking/diplonemids_ESCRTs/escrt0/backbone/ver4/'
 
 #copy files to scratch
 cp $datadir'amorphea.mafft.trimal_gt-0.8.aln' $SCRATCHDIR
