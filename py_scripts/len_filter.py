4826888a088d04598fbbf1de9f4057f9aa427c4b
kikinocka <kika.zahonova@gmail.com>
2024-11-28 15:38:29 +0100

    bla

--- py_scripts/len_filter.py
+++ py_scripts/len_filter.py
@@ -2,10 +2,10 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/oil_sands/metagenomes/')
-genome = SeqIO.parse('20210222_BML-P3B/2-spades/P3B_scaffolds.fasta', 'fasta')
+os.chdir('/Users/kika/ownCloud/blasto_comparative/transcriptomes/')
+genome = SeqIO.parse('Ovol_cufflinks.fa', 'fasta')
 
-with open('ncbi_submission/P3B_NCBI.l200.fa', 'w') as out:
+with open('ncbi_submission/Ovol_cufflinks.l200.fa', 'w') as out:
 	for seq in genome:
 		if len(seq) >= 200:
 			out.write('>{}\n{}\n'.format(seq.description, seq.seq))
--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_ESCRTs/ESCRT-0/tree/backbone/ver8/')
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_ESCRTs/ESCRT-0/tree/backbone/ver9/')
 files = [x for x in os.listdir() if x.endswith('mafft.aln')]
 # files = [x for x in os.listdir() if x.endswith('.einsi.aln')]
 
