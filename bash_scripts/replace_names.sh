7e062c46e989a7564ce8d12c6a0f4b9efa829a10
kikinocka <kika.zahonova@gmail.com>
2024-11-28 19:26:38 +0100

    bla

--- bash_scripts/replace_names.sh
+++ bash_scripts/replace_names.sh
@@ -2,7 +2,7 @@
 
 replacer='/Users/kika/ownCloud/lab_documents/Joel/lael_scripts/name_replace/name_replace.pl'
 
-cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/ARFs/ver3/iqtree/'
+cd '/Users/kika/ownCloud/membrane-trafficking/diplonemids_ESCRTs/ESCRT-0/tree/backbone/ver10/'
 
 for aln in *0.8.aln ; do
 	out=${aln%.trimal_gt-0.8.aln}.CD.trimal_gt-0.8.aln
