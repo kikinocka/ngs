a6ae90600681549738248598c7a330a0c2980565
kikinocka <kika.zahonova@gmail.com>
2024-11-28 11:27:18 +0100

    bla

--- bash_scripts/ostrava/qualimap.sh
+++ bash_scripts/ostrava/qualimap.sh
@@ -6,13 +6,13 @@
 #PBS -l walltime=100:00:00
 
 
-name='Bfru'
-bam='/mnt/data/kika/blastocrithidia/genomes/final_bw2/'$name'_bw2_sorted.bam'
+name='Braa'
+bam='/mnt/data/kika/blastocrithidia/genomes/final_bw2/'$name'_cor2_bw2_sorted.bam'
 outdir='/mnt/data/kika/blastocrithidia/genomes/final_assemblies/qualimap/'
 
 qualimap bamqc -bam $bam -nt 20 -outdir $outdir -outformat pdf
 
-mv 'genome_results.txt' $name'_genome_results.txt'
-mv 'report.pdf' $name'_report.pdf'
+mv $outdir'genome_results.txt' $outdir$name'_genome_results.txt'
+mv $outdir'report.pdf' $outdir$name'_report.pdf'
 
 python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: qualimap done
