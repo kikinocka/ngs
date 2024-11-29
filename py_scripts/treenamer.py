ff01435a12bf12878a64f5b28e4b79223640a53a
kikinocka <kika.zahonova@gmail.com>
2024-11-29 11:13:25 +0100

    bla

--- py_scripts/treenamer.py
+++ py_scripts/treenamer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import os
 
-os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/ARFs/ver2/combined/')
-trees = [x for x in os.listdir() if x.endswith('.tre')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/diplonemids_ESCRTs/ESCRT-0/tree/backbone/ver10/iqtree_bs/')
+trees = [x for x in os.listdir() if x.endswith('aln.treefile')]
 
 #file in format Acc. number \t name of organism \n
-names = open('/Users/kika/ownCloud/membrane-trafficking/diplonemids_all/mantamonas/ARFs/ver2/arfs.codes_names.txt')
+names = open('/Users/kika/ownCloud/membrane-trafficking/diplonemids_ESCRTs/ESCRT-0/tree/backbone/ver10/amorphea.codes_names.txt')
 
 name_dict = {}
 for name in names:
