12e1d1403a2223afc0c3b3ac5ec8bcdc98815cff
kikinocka <kika.zahonova@gmail.com>
2025-02-17 11:50:36 +0100

    bla

--- bash_scripts/leshy/hmmsearch.sh
+++ bash_scripts/leshy/hmmsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd '/mnt/mokosz/home/kika/allDB/renamed/'
+cd '/mnt/mokosz/home/kika/metamonads/MRO_proteins/metamonads_assemblies/'
 threads=15
 eval=1e-10
 
@@ -13,40 +13,40 @@ eval=1e-10
 # 	hmmsearch --tblout $out --cpu $threads -E $eval $profile $db
 # done
 
-#one profile and several database
-workdir='/mnt/mokosz/home/kika/workdir/'
-profile=$workdir'archaea_eRF1.hmm'
+# #one profile and several database
+# workdir='/mnt/mokosz/home/kika/workdir/'
+# profile=$workdir'archaea_eRF1.hmm'
 
-for db in *.faa ; do
-	orgn=${db%.faa}
-	out=$orgn'.eRF1_hmmsearch.tsv'
-	hmmsearch --tblout $out --cpu $threads -E $eval $profile $db
-done
-
-mv *tsv $workdir
-
-# #several profiles and several databases
-# for profile in /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/*.hmm ; do
-# 	echo $profile
-# 	folder=`echo $profile | cut -d / -f 8`
-# 	folder=${folder%.hmm}
-# 	# echo $folder
-# 	for db in /mnt/mokosz/home/kika/allDB/renamed/*.fa ; do
-# 		echo $db
-# 		# mkdir /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
-# 		cd /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/hmmsearch/$folder
-# 		output=`echo $db | cut -d / -f 8`
-# 		output=${output%.fa}
-# 		# echo $output
-# 		output=$output'.'${folder}.hmmsearch.tsv
-# 		# echo $output
-# 		# output=${db%.faa}'.'${profile%.hmm}.hmmsearch.tsv
-# 		# output=$orgn'_'${profile%.hmm}.hmmsearch.tsv
-# 		hmmsearch --tblout $output --cpu $threads -E $eval $profile $db
-# 		# cd /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/
-# 		# sleep 5
-# 		echo
-# 	done
+# for db in *.faa ; do
+# 	orgn=${db%.faa}
+# 	out=$orgn'.eRF1_hmmsearch.tsv'
+# 	hmmsearch --tblout $out --cpu $threads -E $eval $profile $db
 # done
+
+# mv *tsv $workdir
+
+#several profiles and several databases
+for profile in /mnt/mokosz/home/kika/metamonads/MRO_proteins/MRO_hmm/*.hmm ; do
+	echo $profile
+	folder=`echo $profile | cut -d / -f 8`
+	folder=${folder%.hmm}
+	# echo $folder
+	for db in /mnt/mokosz/home/kika/metamonads/MRO_proteins/metamonads_assemblies/*.fa ; do
+		echo $db
+		mkdir /mnt/mokosz/home/kika/metamonads/MRO_proteins/hmmsearch_metamonads/$folder
+		cd /mnt/mokosz/home/kika/metamonads/MRO_proteins/hmmsearch_metamonads/$folder
+		output=`echo $db | cut -d / -f 8`
+		output=${output%.fa}
+		# echo $output
+		output=$output'.'${folder}.hmmsearch.tsv
+		echo $output
+		# output=${db%.faa}'.'${profile%.hmm}.hmmsearch.tsv
+		# output=$orgn'_'${profile%.hmm}.hmmsearch.tsv
+		# hmmsearch --tblout $output --cpu $threads -E $eval $profile $db
+		# cd /mnt/mokosz/home/kika/metamonads_ancestral/OGs_hmm/
+		# sleep 5
+		echo
+	done
+done
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py HMMsearch done
--- py_scripts/rename_seqs.py
+++ py_scripts/rename_seqs.py
@@ -3,49 +3,32 @@ import os
 import pandas as pd
 from Bio import SeqIO
 
-os.chdir('/Users/kika/ownCloud/metamonada/MRO_proteins/trees/fasta/from_zoli/')
-# table = pd.read_excel('database.xlsx', sheet_name='eukaryotes')
-proteins = [x for x in os.listdir() if x.endswith('.fasta')]
+os.chdir('/Users/kika/ownCloud/metamonada/assemblies/renamed/')
+table = pd.read_excel('database.xlsx', sheet_name='eukaryotes')
+proteins = [x for x in os.listdir() if x.endswith('.faa')]
 
-orgns = {'excADUpa' : 'E-me-Fo-Apal', 'excBLATn' : 'E-me-Pr-Bnau', 'excCARPm' : 'E-me-Fo-Cmem', 'excCHIca' : 'E-me-Fo-Ccau', 
-	'excCHIcu' : 'E-me-Fo-Ccus', 'excDYSNb' : 'E-me-Fo-Dbre', 'excERGcy' : 'E-me-Fo-Ecyp', 'Giardia' : 'E-me-Fo-Gint', 
-	'excHISTm' : 'E-me-Pa-Hmel', 'excIOTAs' : 'E-me-Fo-Ispi', 'excKIPFb' : 'E-me-Fo-Kbia', 'excMONOe' : 'E-me-Pr-Mexi', 
-	'excPTRIp' : 'E-me-Pr-Ppyr', 'excSPIRs' : 'E-me-Fo-Ssal', 'Streblo' : 'E-me-Pr-Sstr', 'excTREPs' : 'E-me-Fo-TPC1', 
-	'TVAG' : 'E-me-Pa-Tvag', 'excTRIMm' : 'E-me-Pr-Tmar', 'extTTRIf' : 'E-me-Pa-Tfoe', 'R.caviae' : 'E-me-Fo-Rcav', 
-	'R.dobelli' : 'E-me-Fo-Rdob'}
+prot_dict = {}
+for index, row in table.iterrows():
+	if row[4] == '-':
+		pass
+	else:
+		if row[0] == 'Metamonada':
+			prot_dict[row[4]] = row[6]
+		else:
+			pass
+# print(prot_dict)
 
+with open('errors.txt', 'w') as errors, open('metamonads_in_OGs.seq_dict.tsv', 'w') as seqdict:
+	seqdict.write('original ID\treplaced ID\n')
+	for file in proteins:
+			c = 0
+			print(file)
+			if file in prot_dict.keys():
+				with open('{}_renamed.faa'.format(file.split('.')[0]), 'w') as out:
+					for seq in SeqIO.parse(file, 'fasta'):
+						c += 1
+						out.write('>{}_{}\n{}\n'.format(prot_dict[file], c, seq.seq.replace('*', '')))
+						seqdict.write('{}\t{}_{}\n'.format(seq.description, prot_dict[file], c))
+			else:
+				errors.write('{}: Not found\n'.format(file))
 
-# prot_dict = {}
-# for index, row in table.iterrows():
-# 	if row[4] == '-':
-# 		pass
-# 	else:
-# 		prot_dict[row[4]] = row[6]
-
-c = 0
-# with open('errors.txt', 'w') as errors, open('OGs.seq_dict.tsv', 'w') as seqdict:
-for file in proteins:
-	print(file)
-	# seqdict.write('original ID\treplaced ID\n')
-	with open('{}_renamed.fa'.format(file.split('.')[0]), 'w') as out:
-		for key in orgns.keys():
-			for seq in SeqIO.parse(file, 'fasta'):
-				if key in seq.description:
-					c += 1
-					# print(key)
-					# print(seq.description)
-					new_acc = '_ '.join(seq.name.split('_')[1:])
-					out.write('>{}_{}\n{}\n'.format(orgns[key], new_acc, seq.seq.replace('*','')))
-					# seqdict.write('{}\t{}_{}\n'.format(seq.description, orgns[key], c))
-
-
-
-
-		# if file in prot_dict.keys():
-# 			with open('renamed/{}'.format(file), 'w') as out:
-# 				for seq in SeqIO.parse(file, 'fasta'):
-# 					c += 1
-# 					out.write('>{}_{}\n{}\n'.format(prot_dict[file], c, seq.seq))
-# 					seqdict.write('{}\t{}_{}\n'.format(seq.description, prot_dict[file], c))
-# 		else:
-# 			errors.write('{}: Not found\n'.format(file))
