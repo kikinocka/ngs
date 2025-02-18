7f9ba92a6c4f49f7add24020fd22c5fa6f873af3
kikinocka <kika.zahonova@gmail.com>
2025-02-18 17:17:21 +0100

    bla

--- py_scripts/run_program/hmmparse.py
+++ py_scripts/run_program/hmmparse.py
@@ -2,9 +2,9 @@
 import os
 from Bio import SeqIO
 
-os.chdir('/mnt/mokosz/home/kika/metamonads/MRO_proteins/1-hmmsearch_metamonads/')
+os.chdir('/storage/brno12-cerit/home/kika/kinetoplastids/angomonas/EAPs/hmmsearch/')
 files = [x for x in os.listdir() if x.endswith('hmmsearch.tsv')]
-db = SeqIO.parse('/mnt/mokosz/home/kika/metamonads/MRO_proteins/metamonads_assemblies/all_metamonads.faa', 'fasta')
+db = SeqIO.parse('/storage/brno12-cerit/home/kika/databases/all.faa', 'fasta')
 
 seq_d = {}
 for seq in db:
