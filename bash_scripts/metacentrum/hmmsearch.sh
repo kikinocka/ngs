6a94cdf0efd7c9bd4e3076cf1b13fe0b1a923b2f
kikinocka <kika.zahonova@gmail.com>
2025-02-18 15:28:54 +0100

    bla

--- bash_scripts/metacentrum/hmmsearch.sh
+++ bash_scripts/metacentrum/hmmsearch.sh
@@ -21,26 +21,21 @@ cp $db_dir'/'*.faa $SCRATCHDIR
 #compute on scratch
 cd $SCRATCHDIR
 
-mkdir hmms databases
-mv *hmm hmms
-mv *faa databases
-
 eval=1e-10
-for profile in hmms/*.hmm ; do
+for profile in *.hmm ; do
 	echo $profile
-	for db in databases/*.faa ; do
+	for db in *.faa ; do
 		echo $db
-		orgn=`echo ${db%.faa} | cut -d / -f 2`
-		prot=`echo ${profile%.hmm} | cut -d / -f 2`
+		orgn=${db%.faa}
+		prot=${profile%.hmm}
 		output=$orgn'.'$prot'.hmmsearch.tsv'
 		echo $output
 		hmmsearch --tblout $output --cpu $PBS_NUM_PPN -E $eval $profile $db
-		cd hmms/
 		# sleep 5
 		echo
 	done
 done
 
 #copy files back
-rm -r hmms databases
+# rm -r hmms databases
 cp *tsv $hmm_dir
