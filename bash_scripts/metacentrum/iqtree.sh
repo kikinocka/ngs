d3def0770a6f39a0c98bd28b4ff654f84ecd9878
kikinocka <kika.zahonova@gmail.com>
2025-02-28 14:32:50 +0100

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -8,7 +8,7 @@
 cat $PBS_NODEFILE
 
 #add module
-module load iqtree-2.2.0
+module load iqtree
 
 datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/'
 
@@ -26,10 +26,10 @@ nm=5000
 
 
 # iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-# iqtree -m LG+C60+G -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
+iqtree -m LG+C60+G -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 
-iqtree2 -m TEST -madd C10,C20,C30,C40,C50,C60,LG4M,LG4X,LG+F+G,LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C60+G \
-	-T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
+# iqtree -m TEST -madd C10,C20,C30,C40,C50,C60,LG4M,LG4X,LG+F+G,LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C60+G \
+# 	-T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 
 
 #copy files back
