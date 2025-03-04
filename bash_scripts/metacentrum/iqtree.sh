b8d69b2c466b1f2339a7665d587476a887890827
kikinocka <kika.zahonova@gmail.com>
2025-03-04 15:25:55 +0100

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -14,6 +14,7 @@ datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/ver2/'
 
 #copy files to scratch
 cp $datadir'CHC_opisthokonta.man_trim.renamed.aln' $SCRATCHDIR
+cp $datadir'guide'* $SCRATCHDIR
 cp $datadir'spp_constr.tre' $SCRATCHDIR
 
 #compute on scratch
@@ -26,7 +27,7 @@ bb=1000
 nm=5000
 
 
-iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+# iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
 iqtree -m LG+C60+G -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree -g $constr --boot-trees
 
 # iqtree -m TEST -madd C10,C20,C30,C40,C50,C60,LG4M,LG4X,LG+F+G,LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C60+G \
