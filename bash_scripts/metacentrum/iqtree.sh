b93bc1be979e7703535d653f45a1504fa4f81203
kikinocka <kika.zahonova@gmail.com>
2025-03-04 15:04:34 +0100

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -10,15 +10,15 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree
 
-datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/contrained/'
+datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/ver2/'
 
 #copy files to scratch
-cp $datadir'CHC_opisthokonta.man_trimmed.aln' $SCRATCHDIR
+cp $datadir'CHC_opisthokonta.man_trim.renamed.aln' $SCRATCHDIR
 cp $datadir'spp_constr.tre' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
-aln='CHC_opisthokonta.man_trimmed.aln'
+aln='CHC_opisthokonta.man_trim.renamed.aln'
 guide='guide_CHC_opist'
 guide_tree=$guide'.treefile'
 constr='spp_constr.tre'
