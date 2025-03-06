0507ca5ab6c115afdf3e40bb2a60c24c9a011898
kikinocka <kika.zahonova@gmail.com>
2025-03-06 15:46:58 +0100

    bla

--- bash_scripts/filter_aln.sh
+++ bash_scripts/filter_aln.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filt_script='/Users/kika/scripts/py_scripts/filter_alignment.py'
+filt_script='/mnt/mokosz/home/kika/scripts/py_scripts/filter_alignment.py'
 
 cd '/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_trimal_gt-0.8/'
 threshold=50
