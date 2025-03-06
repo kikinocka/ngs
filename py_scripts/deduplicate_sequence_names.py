ef935a2b100afc50366ce1f5e0cdd74b251a3739
kikinocka <kika.zahonova@gmail.com>
2025-03-06 20:23:48 +0100

    bla

--- py_scripts/deduplicate_sequence_names.py
+++ py_scripts/deduplicate_sequence_names.py
@@ -4,10 +4,11 @@ from Bio import SeqIO
 from collections import OrderedDict
 from collections import defaultdict
 
-os.chdir('/Users/kika/ownCloud/kinetoplastids/angomonas/EAPs/hmmsearch/rnd2/')
-infile = SeqIO.parse('CAD2216821.fa', 'fasta')
-out_fasta = open('CAD2216821_deduplicated.fa', 'w')
-out_names = open('CAD2216821_dupl-names.txt', 'w')
+os.chdir('/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_fasta/')
+files = [x for x in os.listdir() if x.endswith('.fa')]
+# infile = SeqIO.parse('CAD2216821.fa', 'fasta')
+# out_fasta = open('CAD2216821_deduplicated.fa', 'w')
+# out_names = open('CAD2216821_dupl-names.txt', 'w')
 
 # names = []
 # with open('RABs_deduplicated.fa', 'w') as out:	
@@ -25,19 +26,23 @@ out_names = open('CAD2216821_dupl-names.txt', 'w')
 # 			out.write('>{}\n{}\n'.format(new_desc[:50], seq.seq))
 # 			print('{} changed to {}'.format(seq.name, new_name))
 
-multiplications = defaultdict(list)
-seq_dict = OrderedDict()
-for sequence in infile:
-	multiplications[sequence.name].append(sequence.description)
-	if sequence.name not in seq_dict:
-		seq_dict[sequence.name] = [sequence.description, sequence.seq]
+for infile in files:
+	print(file)
+	with open('{}_deduplicated.fa'.format(file.split('.fa')[0]), 'w') as out_fasta, \
+		 open('{}_dupl-names.txt'.format(file.split('.fa')[0]), 'w') as out_names:
+		multiplications = defaultdict(list)
+		seq_dict = OrderedDict()
+		for sequence in infile:
+			multiplications[sequence.name].append(sequence.description)
+			if sequence.name not in seq_dict:
+				seq_dict[sequence.name] = [sequence.description, sequence.seq]
 
-for key, value in seq_dict.items():
-	out_fasta.write('>{}\n{}\n'.format(value[0], value[1]))
+		for key, value in seq_dict.items():
+			out_fasta.write('>{}\n{}\n'.format(value[0], value[1]))
 
-for key, value in multiplications.items():
-    if len(value) > 1:
-        out_names.write('{}\n'.format(str(value)))
+		for key, value in multiplications.items():
+    		if len(value) > 1:
+        		out_names.write('{}\n'.format(str(value)))
 
 out_fasta.close()
 out_names.close()
