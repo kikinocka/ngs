20a6e968e46322621a644f904666320c84b81b5f
kikinocka <kika.zahonova@gmail.com>
2025-03-06 15:55:32 +0100

    bla

--- bash_scripts/metacentrum/fasttree.sh
+++ bash_scripts/metacentrum/fasttree.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #PBS -N FastTree
 #PBS -l select=1:ncpus=1:mem=20gb:scratch_local=1gb
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
-source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module load fasttree
 
 
-data_dir='/storage/brno12-cerit/home/kika/metamonads/fasttree/not_filtered/'
+data_dir='/storage/brno12-cerit/home/kika/metamonads/MRO_proteins/fasttree'
 
 #copy files to scratch
-cp $data_dir'q2010722.og_hmm.trimal_at1.aln' $SCRATCHDIR
+cp $data_dir'/'*.aln $SCRATCHDIR
 
 
 #compute on scratch
