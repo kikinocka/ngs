92048c3d5c462ed06fbe42b7be52ffcfd8a2809f
kikinocka <kika.zahonova@gmail.com>
2025-03-06 14:52:36 +0100

    bla

--- py_scripts/alignio.py
+++ py_scripts/alignio.py
@@ -2,9 +2,9 @@
 import os
 from Bio import AlignIO
 
-os.chdir('/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_trimal_automated1/')
+os.chdir('/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_trimal_gt-0.8/')
 files = [x for x in os.listdir() if x.endswith('.aln')]
-out = 'trimal-at1_len.tsv'
+out = 'trimal_gt-0.8_len.tsv'
 
 # #number of sequences
 # print(len(alignment))
