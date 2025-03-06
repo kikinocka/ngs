bf78b049b5267b9595f428334d070f37247efb5c
kikinocka <kika.zahonova@gmail.com>
2025-03-06 15:48:49 +0100

    bla

--- bash_scripts/filter_aln.sh
+++ bash_scripts/filter_aln.sh
@@ -2,7 +2,7 @@
 
 filt_script='/mnt/mokosz/home/kika/scripts/py_scripts/filter_alignment.py'
 
-cd '/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_trimal_gt-0.8/'
+cd '/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_trimal_automated1/'
 threshold=50
 
 for fasta in *trimal*.aln ; do
