8f75f94767998be1dc8ec5131867497001f6e16e
kikinocka <kika.zahonova@gmail.com>
2025-03-06 15:45:21 +0100

    bla

--- bash_scripts/filter_aln.sh
+++ bash_scripts/filter_aln.sh
@@ -6,5 +6,6 @@ cd '/mnt/mokosz/home/kika/metamonads/MRO_proteins/3-MRO+HMMhits_trimal_gt-0.8/'
 threshold=50
 
 for fasta in *trimal*.aln ; do
+	echo $fasta
 	$filt_script -f $fasta -t $threshold
 done
