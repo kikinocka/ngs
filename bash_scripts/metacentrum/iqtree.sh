08e50f7e11a2822d0d0e01a972b80ead3165c4ec
kikinocka <kika.zahonova@gmail.com>
2025-03-28 10:05:39 +0100

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -29,7 +29,7 @@ nm=5000
 # iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
 # iqtree -m LG+C60+G -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree -g $constr --boot-trees
 
-iqtree -m MFP --set C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
+iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
 	-T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln -g $constr --boot-trees
 # --tree-freq $guide_tree 
 
