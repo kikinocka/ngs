8c7ab5fe69f370cec80998934ae74cc02a975f2c
kikinocka <kika.zahonova@gmail.com>
2025-03-31 15:52:52 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree
 
-datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/ver4/'
+datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/ver5/'
 
 #copy files to scratch
 cp $datadir'CHC_opisthokonta.man_trim.CD.aln' $SCRATCHDIR
@@ -26,12 +26,12 @@ bb=1000
 nm=5000
 
 
-iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-iqtree -m LG+C40+G+F+I+R5 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree -g $constr --boot-trees
+# iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+# iqtree -m LG+C40+G+F+I+R5 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree -g $constr --boot-trees
 
-# iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
-# 	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
+iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
+	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
 
 #copy files back
 rm $aln $constr
-cp * $datadir'tree_MFP/'
+cp * $datadir'model_testing/'
