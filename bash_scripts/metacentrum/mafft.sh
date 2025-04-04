102d7a0798774dfe41cb72a66c5e2bdebfe87ec1
kikinocka <kika.zahonova@gmail.com>
2025-04-04 16:59:53 +0200

    bla

--- bash_scripts/metacentrum/mafft.sh
+++ bash_scripts/metacentrum/mafft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N mafft
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module load mafft
 
-data_dir='/storage/brno12-cerit/home/kika/kinetoplastids/tbKIN/kin2/ver11'
+data_dir='/storage/brno12-cerit/home/kika/kinetoplastids/tbKIN/kin2/ver12'
 
 #copy files to scratch
 cp $data_dir'/'*'.fa' $SCRATCHDIR
