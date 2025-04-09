b7eb28e805c011e6c5ecf08b434f1d432680695f
kikinocka <kika.zahonova@gmail.com>
2025-04-09 08:58:04 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many3.sh
+++ bash_scripts/metacentrum/iqtree_many3.sh
@@ -13,16 +13,13 @@ module load iqtree-2.2.0
 data_dir='/storage/brno12-cerit/home/kika/metamonads/MRO_proteins/iqtree/'
 
 #copy files to scratch
-cp $data_dir'GiOR1.mro+hmm.final.trimal_at1.aln' $SCRATCHDIR
-cp $data_dir'Grx5.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'HCP.mro+hmm.final.trimal_at1.aln' $SCRATCHDIR
-cp $data_dir'Hsp70.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'Hsp90.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'HydE.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'HydF.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'HydG.mro+hmm.final.trimal_at1.aln' $SCRATCHDIR
-cp $data_dir'Ind.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
-cp $data_dir'IPP.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'Pam18.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'Cpn10.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'Cpn60.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'NifU.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'Fra.mro+hmm.final.trimal_at1.aln' $SCRATCHDIR
+cp $data_dir'Arh1.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
+cp $data_dir'Fdx.mro+hmm.final.trimal_at1.aln' $SCRATCHDIR
 
 
 #compute on scratch
