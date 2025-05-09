d80e4eb5d56d652469269888312e4d4ee100d4d7
kikinocka <kika.zahonova@gmail.com>
2025-05-09 16:14:26 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -10,24 +10,25 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree
 
-datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/ver5/'
+datadir='/storage/brno12-cerit/home/kika/diplonema/IDH/'
 
 #copy files to scratch
-cp $datadir'CHC_opisthokonta.man_trim.CD.aln' $SCRATCHDIR
-cp $datadir'spp_constr.tre' $SCRATCHDIR
+cp $datadir'idh.trimal_automated1.aln' $SCRATCHDIR
+# cp $datadir'spp_constr.tre' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 aln='CHC_opisthokonta.man_trim.CD.aln'
 guide='guide_CHC_opist'
 guide_tree=$guide'.treefile'
-constr='spp_constr.tre'
+# constr='spp_constr.tre'
 bb=1000
 nm=5000
 
 
 iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-iqtree -m LG+C40+G+F+R5 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree -g $constr --boot-trees
+iqtree -m LG+C20+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
+# -g $constr 
 
 # iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
 # 	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
