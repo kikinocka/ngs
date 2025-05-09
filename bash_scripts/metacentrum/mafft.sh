c9f7314c2de501348096c83e1f4d4b127cefd957
kikinocka <kika.zahonova@gmail.com>
2025-05-09 15:39:16 +0200

    bla

--- bash_scripts/metacentrum/mafft.sh
+++ bash_scripts/metacentrum/mafft.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module load mafft
 
-data_dir='/storage/brno12-cerit/home/kika/kinetoplastids/GP63'
+data_dir='/storage/brno12-cerit/home/kika/diplonema/IDH'
 
 #copy files to scratch
 cp $data_dir'/'*'.fa' $SCRATCHDIR
