1ebed5fb15c66ec12c093c83ce075d8acd495a19
kikinocka <kika.zahonova@gmail.com>
2025-05-13 15:39:41 +0200

    bla

--- bash_scripts/metacentrum/iqtree2.sh
+++ bash_scripts/metacentrum/iqtree2.sh
@@ -27,11 +27,11 @@ nm=5000
 
 
 iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-# iqtree -m LG+C20+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
+iqtree -m LG+C20+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 # -g $constr 
 
-iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
-	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
+# iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
+# 	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
 
 #copy files back
 rm $aln #$constr
