66228d6c89b1c45ed1cdaaaa2787831b7c88ba0c
kikinocka <kika.zahonova@gmail.com>
2025-05-15 11:18:01 +0200

    bla

--- bash_scripts/metacentrum/iqtree2.sh
+++ bash_scripts/metacentrum/iqtree2.sh
@@ -14,21 +14,21 @@ datadir='/storage/brno12-cerit/home/kika/trafficking/clathrin/CLC/ver2/'
 
 #copy files to scratch
 cp $datadir'CLC_opisthokonta.man_trim.aln' $SCRATCHDIR
-cp $datadir'spp_constr.tre' $SCRATCHDIR
+# cp $datadir'spp_constr.tre' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 aln='CLC_opisthokonta.man_trim.aln'
 guide='guide_CLC_opist'
 guide_tree=$guide'.treefile'
-constr='spp_constr.tre'
+# constr='spp_constr.tre'
 bb=1000
 nm=5000
 
 
-iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
-iqtree -m LG4M+F+R4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --tree-freq $guide_tree -g $constr --boot-trees
-# 
+# iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+iqtree -m LG4M+F+R4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln -g $constr --boot-trees
+# --tree-freq $guide_tree 
 
 # iqtree -m MFP --mset C20,C40,C60,LG4M,LG4X,LG+F+G,LG+C20+G,LG+C40+G,LG+C60+G,LG+PMSF+G \
 # 	-T AUTO --threads-max $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln
