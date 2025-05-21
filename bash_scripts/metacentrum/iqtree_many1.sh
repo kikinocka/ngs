4d9822df63d746785fb4e664e5c3b5d2bffae8d7
kikinocka <kika.zahonova@gmail.com>
2025-05-21 10:17:30 +0200

    bla

--- bash_scripts/metacentrum/iqtree_many1.sh
+++ bash_scripts/metacentrum/iqtree_many1.sh
@@ -13,8 +13,6 @@ module load iqtree-2.2.0
 data_dir='/storage/brno12-cerit/home/kika/metamonads/MRO_proteins/iqtree/'
 
 #copy files to scratch
-cp $data_dir'GCS-H.mro+hmm.final.trimal_at1.aln' $SCRATCHDIR
-cp $data_dir'GCS-L.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
 cp $data_dir'GCS-P.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
 cp $data_dir'GCS-T.mro+hmm.final.trimal_gt-0.8.aln' $SCRATCHDIR
 
