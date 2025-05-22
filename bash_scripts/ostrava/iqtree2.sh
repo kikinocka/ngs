03282eeb5e7a8e1fb86486043a106f0564840476
kikinocka <kika.zahonova@gmail.com>
2025-05-22 13:46:42 +0200

    bla

--- bash_scripts/ostrava/iqtree2.sh
+++ bash_scripts/ostrava/iqtree2.sh
@@ -15,7 +15,7 @@ bb=1000
 nm=5000
 
 
-# iqtree -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s ${f} --prefix $guide
-iqtree -m LG+C60+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s ${f} --boot-trees --tree-freq $guide_tree
+# iqtree -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+iqtree -m LG+C60+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --boot-trees --tree-freq $guide_tree
 
 python /home/users/kika/scripts/py_scripts/slackbot.py OSU: IQT2 done
