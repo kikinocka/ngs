5875c9cb283c6dd213400866b31e9e4ee95bcf92
kikinocka <kika.zahonova@gmail.com>
2025-06-16 17:18:03 +0200

    bla

--- bash_scripts/ostrava/blast.sh
+++ bash_scripts/ostrava/blast.sh
@@ -18,7 +18,7 @@ evalue=1e-05
 max_seqs=1
 max_hsps=1
 
-for file in *.blast_hits.fa ; do 
+for file in CAD2221027.2.hmm_hits.fa ; do 
 	for query in $file ; do
 		echo $query
 		# out=${query%.fa}'.nr_'$evalue'.'$program'.tsv'
