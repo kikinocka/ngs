847cb29649ba84e1238e0a1d6c67058620ca562f
kikinocka <kika.zahonova@gmail.com>
2025-06-26 13:47:17 +0200

    bla

--- bash_scripts/scp.sh
+++ bash_scripts/scp.sh
@@ -2,14 +2,15 @@
 
 server='amoebae'
 scp_dir='/home/kikaz/amoebae_jpp/resources/local_db_files'
+data_dir='/Users/kika/data/eukprot_v3/'
 
-scp EP00015_* $server:$scp_dir
-scp EP00727_* $server:$scp_dir
-scp EP00005_* $server:$scp_dir
-scp EP00006_* $server:$scp_dir
-scp EP00023_* $server:$scp_dir
-scp EP01124_* $server:$scp_dir
-scp EP00029_* $server:$scp_dir
-scp EP01126_* $server:$scp_dir
-scp EP00027_* $server:$scp_dir
-scp EP00002_* $server:$scp_dir
+scp $data_dir'EP00114_'* $server:$scp_dir
+scp $data_dir'EP00118_'* $server:$scp_dir
+scp $data_dir'EP00119_'* $server:$scp_dir
+scp $data_dir'EP00059_'* $server:$scp_dir
+scp $data_dir'EP00067_'* $server:$scp_dir
+scp $data_dir'EP00074_'* $server:$scp_dir
+scp $data_dir'EP00076_'* $server:$scp_dir
+scp $data_dir'EP00040_'* $server:$scp_dir
+scp $data_dir'EP00042_'* $server:$scp_dir
+scp $data_dir'EP00044_'* $server:$scp_dir
