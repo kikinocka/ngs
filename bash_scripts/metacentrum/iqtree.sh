42360dc729d9dfc7c27193dcc9ebdf1d1c278fe3
kikinocka <kika.zahonova@gmail.com>
2025-07-21 16:32:22 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -10,7 +10,7 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree
 
-datadir='/storage/brno12-cerit/home/kika/kinetoplastids/gp63/ver3'
+datadir='/storage/brno12-cerit/home/kika/kinetoplastids/gp63/ver3/'
 
 #copy files to scratch
 cp $datadir'gp63.trimal_at1.aln' $SCRATCHDIR
