ba96e4c6f57fc478ba59e4eacdb40dda8f231af8
kikinocka <kika.zahonova@gmail.com>
2025-07-23 12:08:01 +0200

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -14,7 +14,7 @@ datadir='/storage/brno12-cerit/home/kika/kinetoplastids/gp63/ver4/'
 
 #copy files to scratch
 cp $datadir'gp63.bgme.aln' $SCRATCHDIR
-cp $datadir'guide'* $SCRATCHDIR
+# cp $datadir'guide'* $SCRATCHDIR
 # cp $datadir'spp_constr.tre' $SCRATCHDIR
 
 #compute on scratch
@@ -27,7 +27,7 @@ bb=1000
 nm=5000
 
 
-# iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+iqtree -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
 iqtree -m LG+C60+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --alrt $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 # -g $constr 
 
