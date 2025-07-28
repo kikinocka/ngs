3441977604efaa9442f9f33a3079ef7cbce7e3b3
kikinocka <kika.zahonova@gmail.com>
2025-07-28 21:10:04 +0200

    bla

--- bash_scripts/ostrava/iqtree2.sh
+++ bash_scripts/ostrava/iqtree2.sh
@@ -17,6 +17,7 @@ nm=5000
 
 # iqtree -m LG+G4 -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
 # iqtree -m LG+C60+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --quiet --safe -s $aln --boot-trees --tree-freq $guide_tree
-iqtree2 -s combinedArfs_Carpedi_Arf6_removed.phy -m LG+C60+R10 -safe -nt AUTO -keep_empty_seq -te unrootedtree_Carpedi_Arf6_removed.nwk
+iqtree2 -s combinedArfsRoot__Carpedi_Arf6_removed.phy -nt AUTO -m LG+C60+R10 -keep_empty_seq -te combinedArfsRoot_Carpedi_Arf6_removed.phy.treefile -asr
+
 
 python /home/users/kika/scripts/py_scripts/slackbot.py OSU: IQT2 done
