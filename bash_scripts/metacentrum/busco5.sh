012eda0b1a3c5420bf37e2f8c1ef66853c8d1bf1
kikinocka <kika.zahonova@gmail.com>
2025-09-11 21:40:43 +0200

    bla

--- bash_scripts/metacentrum/busco5.sh
+++ bash_scripts/metacentrum/busco5.sh
@@ -18,34 +18,36 @@ conda activate busco
 assembly_dir='/storage/brno12-cerit/home/kika/Egr_2024'
 
 #copy files to scratch
-cp $assembly_dir'/'*.fsa_nt $SCRATCHDIR
+cp -r $assembly_dir'/BUSCO_summaries_eug' $SCRATCHDIR
+cp -r $assembly_dir'/BUSCO_summaries_euk' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-# mkdir BUSCO_summaries
+# # mkdir BUSCO_summaries
 
-for fasta in *.fsa_nt; do
-	echo $fasta
-	# mode='proteins'
-	# mode='genome'
-	mode='transcriptome'
+# for fasta in *.fsa_nt; do
+# 	echo $fasta
+# 	# mode='proteins'
+# 	# mode='genome'
+# 	mode='transcriptome'
 	
-	lineage='eukaryota_odb10'
-	base=${fasta%.fa}_$lineage
-	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
+# 	lineage='eukaryota_odb10'
+# 	base=${fasta%.fa}_$lineage
+# 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
+# 	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
 
-	lineage='euglenozoa_odb10'
-	base=${fasta%.fa}_$lineage
-	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
-done
+# 	lineage='euglenozoa_odb10'
+# 	base=${fasta%.fa}_$lineage
+# 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
+# 	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
+# done
 
-# generate_plot.py -wd BUSCO_summaries
+generate_plot.py -wd BUSCO_summaries_eug
+generate_plot.py -wd BUSCO_summaries_euk
 
 
 #copy files back
-rm *.fsa_nt
+# rm *.fsa_nt
 cp -r * $assembly_dir
