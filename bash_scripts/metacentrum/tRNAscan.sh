218f0fa9db1a9d4c097053c66570c0e86a3dcff5
kikinocka <kika.zahonova@gmail.com>
2025-09-30 09:36:13 +0200

    bla

--- bash_scripts/metacentrum/tRNAscan.sh
+++ bash_scripts/metacentrum/tRNAscan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N tRNAscan
 #PBS -l select=1:ncpus=10:mem=1gb:scratch_local=5gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -11,8 +11,15 @@ tRNAscan='/storage/brno12-cerit/home/kika/miniconda3/bin/tRNAscan-SE'
 data_dir='/storage/brno12-cerit/home/kika/tRNA-aaRS/'
 
 #copy files to scratch
-cp $data_dir'/'*_genome.fna $SCRATCHDIR
-
+# cp $data_dir'/'*_genome.fna $SCRATCHDIR
+cp $data_dir'Bsty_genome.fna' $SCRATCHDIR
+cp $data_dir'Clep_genome.fna' $SCRATCHDIR
+cp $data_dir'Dhan_genome.fna' $SCRATCHDIR
+cp $data_dir'Hsap_genome.fna' $SCRATCHDIR
+cp $data_dir'Pmar_genome.fna' $SCRATCHDIR
+cp $data_dir'Ptan_genome.fna' $SCRATCHDIR
+cp $data_dir'Scer_genome.fna' $SCRATCHDIR
+cp $data_dir'Tmin_genome.fna' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
