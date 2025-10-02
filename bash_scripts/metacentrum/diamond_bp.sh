3baefebf251cdb9b735fce11e3d66d72e2d96fd6
kikinocka <kika.zahonova@gmail.com>
2025-10-02 16:32:09 +0200

    bla

--- bash_scripts/metacentrum/diamond_bp.sh
+++ bash_scripts/metacentrum/diamond_bp.sh
@@ -10,27 +10,30 @@ cat $PBS_NODEFILE
 #add module
 module add diamond
 
-db='/storage/brno3-cerit/home/kika/dmnd/refseq.dmnd'
+db_dir='/storage/projects-du-praha/BlastDB/'
 data_dir='/storage/brno12-cerit/home/kika/Egr_2025/'
 taxify='/storage/brno2/home/kika/scripts/py_scripts/taxify_DMND_nr_gz.py'
 
 
 #copy files to scratch
 cp $data_dir'HBDM01.transdecoder_clstr99.rep_seq.fasta' $SCRATCHDIR
+cp $db_dir'nr' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
 eval=1e-10
 max_seqs=1
+db='nr'
+
 
 for query in *.fasta ; do
 	echo $query
-	out=${query%.fa}.dmnd.out
-	diamond blastp \
-		-q $query \
-		-d $db \
-		-o $out \
+	out=${query%.rep_seq.fasta}.dmnd.out
+	mamba run -n busco-6.0.0 diamond blastp \
+		--query $query \
+		--db $db \
+		--out $out \
 		--outfmt 6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send \
 		--threads $PBS_NUM_PPN \
 		--evalue $eval \
