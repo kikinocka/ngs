35f2799e7097535a09cd3b993dee2656253d75bb
kikinocka <kika.zahonova@gmail.com>
2025-10-06 12:28:49 +0200

    bla

--- bash_scripts/leshy/blast_many.sh
+++ bash_scripts/leshy/blast_many.sh
@@ -2,8 +2,8 @@
 
 cd '/mnt/mokosz/home/kika/workdir/forn-bask/'
 
-db='Dysnectes_brevis.faa'
-query='fwd_hits.fa'
+# db='Dysnectes_brevis.faa'
+query='Gint.fa'
 program=blastp
 task=blastp
 # outfmt=5
@@ -12,16 +12,16 @@ max_seqs=1
 max_hsps=1
 cpu=8
 
-#one query, one database
-$program -task $task \
-	-query $query \
-	-db $db \
-	-out fwd_hits.rev_blast.tsv \
-	-num_threads $cpu \
-	-outfmt '6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send' \
-	-max_target_seqs $max_seqs \
-	-max_hsps $max_hsps
-	# -evalue $eval \
+# #one query, one database
+# $program -task $task \
+# 	-query $query \
+# 	-db $db \
+# 	-out fwd_hits.rev_blast.tsv \
+# 	-num_threads $cpu \
+# 	-outfmt '6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send' \
+# 	-max_target_seqs $max_seqs \
+# 	-max_hsps $max_hsps
+# 	# -evalue $eval \
 
 
 # #many queries, one database
@@ -38,17 +38,17 @@ $program -task $task \
 # 		# -max_hsps $max_hsps
 # done
 
-# #one query, many databases
-# for db in *faa ; do
-# 	echo $db
-# 	$program -task $task \
-# 		-query $query \
-# 		-db $db \
-# 		-out ${db%.faa}.CAD2221027.fwd_blast.tsv \
-# 		-num_threads $cpu \
-# 		-evalue $eval \
-# 		-outfmt '6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send' 
-# done
+#one query, many databases
+for db in *faa ; do
+	echo $db
+	$program -task $task \
+		-query $query \
+		-db $db \
+		-out ${db%.faa}.gint_CLC.fwd_blast.tsv \
+		-num_threads $cpu \		
+		-outfmt '6 qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send' 
+done
+#-evalue $eval \
 
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py BLAST done
