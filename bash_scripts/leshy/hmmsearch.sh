46cf59925f0bd7f738fb539821382a79067b1a2c
kikinocka <kika.zahonova@gmail.com>
2025-10-08 17:39:41 +0200

    bla

--- bash_scripts/leshy/hmmsearch.sh
+++ bash_scripts/leshy/hmmsearch.sh
@@ -5,8 +5,8 @@ threads=15
 eval=1e-05
 
 #one profile and one database
-db='Dysnectes_brevis.faa'
-profile='noDbre_CLC.hmm'
+db='Giardia_AWB.faa'
+profile='noGiar_CLC.hmm'
 out=${db%.faa}'.'${profile%.hmm}.hmmsearch.tsv
 hmmsearch --tblout $out --cpu $threads -E $eval $profile $db
 
