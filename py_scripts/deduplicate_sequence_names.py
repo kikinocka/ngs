086d4bf09a36ac046129ae4d0d4bf30faed36c54
kikinocka <kika.zahonova@gmail.com>
2025-10-19 17:21:25 +0200

    bla

--- py_scripts/deduplicate_sequence_names.py
+++ py_scripts/deduplicate_sequence_names.py
@@ -4,13 +4,13 @@ from Bio import SeqIO
 from collections import OrderedDict
 from collections import defaultdict
 
-os.chdir('/Users/kika/Downloads/')
-files = [x for x in os.listdir() if x.endswith('.faa')]
+os.chdir('/Users/kika/ownCloud/metamonada/alphaprot_contribution/MRO_proteins/')
+files = [x for x in os.listdir() if x.endswith('.aln')]
 
 for infile in files:
 	print(infile)
-	with open('{}_deduplicated.fa'.format(infile.split('.faa')[0]), 'w') as out_fasta, \
-		 open('{}_dupl-names.txt'.format(infile.split('.faa')[0]), 'w') as out_names:
+	with open('{}_deduplicated.fa'.format(infile.split('.aln')[0]), 'w') as out_fasta, \
+		 open('{}_dupl-names.txt'.format(infile.split('.aln')[0]), 'w') as out_names:
 		multiplications = defaultdict(list)
 		seq_dict = OrderedDict()
 		for sequence in SeqIO.parse(infile, 'fasta'):
