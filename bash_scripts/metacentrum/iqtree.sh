c7f9361414ee12641477a82e6ef17c5436336ccc
kikinocka <kika.zahonova@gmail.com>
2025-11-11 13:20:14 +0400

    bla

--- bash_scripts/metacentrum/iqtree.sh
+++ bash_scripts/metacentrum/iqtree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N IQT
-#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l select=1:ncpus=20:mem=30gb:scratch_local=1gb
 #PBS -l walltime=48:00:00
 #PBS -m ae
 #PBS -j oe
@@ -14,7 +14,7 @@ datadir='/storage/brno12-cerit/home/kika/membrane-trafficking/clathrin/tree/'
 
 #copy files to scratch
 cp $datadir'CHC.trimal_gt-0.8.aln' $SCRATCHDIR
-# cp $datadir'guide'* $SCRATCHDIR
+cp $datadir'guide'* $SCRATCHDIR
 # cp $datadir'spp_constr.tre' $SCRATCHDIR
 
 #compute on scratch
@@ -27,7 +27,7 @@ bb=1000
 nm=5000
 
 
-iqtree3-mpi -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
+# iqtree3-mpi -m LG+G -T AUTO --threads-max $PBS_NUM_PPN --quiet --safe -s $aln --prefix $guide
 iqtree3-mpi -m LG+C60+G4 -T $PBS_NUM_PPN -B $bb --nmax $nm --alrt $nm --quiet --safe -s $aln --tree-freq $guide_tree --boot-trees
 # -g $constr 
 
