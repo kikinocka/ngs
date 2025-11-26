f08879c9b0ec4c2752a9c640213aab291dbabfb7
kikinocka <kika.zahonova@gmail.com>
2025-11-26 17:07:05 +0100

    bla

--- bash_scripts/ostrava/codetta.sh
+++ bash_scripts/ostrava/codetta.sh
@@ -6,5 +6,8 @@ cd '/home/users/kika/strigomonadinae/'
 # genome='p57_polished.fa'
 
 for genome in *.fa ; do
+	echo $genome
 	python3 $codetta $genome
 done
+
+python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: Codetta done
