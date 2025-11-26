1db0be084c1e1aae9613d7e7a9d84c5dd5252657
kikinocka <kika.zahonova@gmail.com>
2025-11-26 17:06:31 +0100

    bla

--- bash_scripts/ostrava/codetta.sh
+++ bash_scripts/ostrava/codetta.sh
@@ -2,7 +2,9 @@
 
 codetta='/home/users/kika/codetta/codetta.py'
 
-cd '/mnt/data/kika/blastocrithidia/genomes/final_assemblies/'
-genome='p57_polished.fa'
+cd '/home/users/kika/strigomonadinae/'
+# genome='p57_polished.fa'
 
-python3 $codetta $genome
+for genome in *.fa ; do
+	python3 $codetta $genome
+done
