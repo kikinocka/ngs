1c92eba793eacf8c89470b8c37d302939c2ec44e
kikinocka <kika.zahonova@gmail.com>
2025-12-11 15:19:57 +0100

    bla

--- bash_scripts/leshy/hifiadapterfilt.sh
+++ bash_scripts/leshy/hifiadapterfilt.sh
@@ -5,7 +5,7 @@ cd '/mnt/mokosz/home/kika/egracilis/PacBio/hifi_reads/'
 hififilt='hifiadapterfilt.sh'
 name='m21121_251126_103750'
 
-$hififilt -p $name -t 10 2> $name.hififilt.log
+$hififilt -p $name -t 10 2> $name.filt.log
 
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py HiFiAdapterFilt done
