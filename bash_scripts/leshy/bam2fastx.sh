192c2bfd5d241c39bb7d4f407f0a1240bf74ef48
kikinocka <kika.zahonova@gmail.com>
2025-12-11 11:15:55 +0100

    bla

--- bash_scripts/leshy/bam2fastx.sh
+++ bash_scripts/leshy/bam2fastx.sh
@@ -7,10 +7,10 @@ bam2fasta='/mnt/mokosz/home/kika/miniconda3/bin/bam2fasta'
 bamfile='m21121_251126_103750.hifi_reads.bam'
 out_prefix='EG_hifi'
 
-#generates <out_prefix>.fasta.gz
-$bam2fasta --num-threads 20 -o $out_prefix $bamfile
+# #generates <out_prefix>.fasta.gz
+# $bam2fasta --num-threads 20 -o $out_prefix $bamfile
 
-# #generates <out_prefix>.fastq.gz
-# $bam2fastq --num-threads 20 -o $out_prefix $bamfile
+#generates <out_prefix>.fastq.gz
+$bam2fastq --num-threads 20 -o $out_prefix $bamfile
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py bam2fasta done
