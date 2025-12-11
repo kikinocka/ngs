4cd4ceaaf3102e13b46585bdf8ea2362a2f9a554
kikinocka <kika.zahonova@gmail.com>
2025-12-11 11:17:34 +0100

    bla

--- bash_scripts/leshy/bam2fastx.sh
+++ bash_scripts/leshy/bam2fastx.sh
@@ -2,7 +2,7 @@
 
 cd '/mnt/mokosz/home/kika/egracilis/PacBio/hifi_reads/'
 
-# bam2fasta='/mnt/mokosz/home/kika/miniconda3/bin/bam2fasta'
+bam2fasta='/mnt/mokosz/home/kika/miniconda3/bin/bam2fasta'
 bam2fastq='/mnt/mokosz/home/kika/miniconda3/bin/bam2fastq'
 bamfile='m21121_251126_103750.hifi_reads.bam'
 out_prefix='EG_hifi'
