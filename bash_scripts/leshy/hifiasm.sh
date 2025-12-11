fd03c5deaccb5915488b9dc7639b8e41b6c5abbc
kikinocka <kika.zahonova@gmail.com>
2025-12-11 12:59:33 +0100

    bla

--- bash_scripts/leshy/hifiasm.sh
+++ bash_scripts/leshy/hifiasm.sh
@@ -5,7 +5,7 @@ cd '/mnt/mokosz/home/kika/egracilis/PacBio/'
 reads='hifi_reads/EG_hifi.fastq.gz'
 name='EG_hifi.asm'
 
-hiafiasm -o $name -t 30 $reads 2> $name.log
+hifiasm -o $name -t 30 $reads 2> $name.log
 awk '/^S/{print ">"$2;print $3}' $name.bp.p_ctg.gfa > $name.p_ctg.fa
 
-python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py hiafiasm done
+python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py hifiasm done
