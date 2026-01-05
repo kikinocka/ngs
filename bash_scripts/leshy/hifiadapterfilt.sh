61452517bd833ce07010ac75a6d43c6acae528d0
kikinocka <kika.zahonova@gmail.com>
2026-01-05 10:47:55 +0100

    bla

--- bash_scripts/leshy/hifiadapterfilt.sh
+++ bash_scripts/leshy/hifiadapterfilt.sh
@@ -2,12 +2,12 @@
 
 export PATH="/mnt/mokosz/home/kika/tools/HiFiAdapterFilt:/mnt/mokosz/home/kika/tools/HiFiAdapterFilt/DB:$PATH"
 
-cd '/mnt/mokosz/home/kika/egracilis/PacBio/hifi_reads/'
+cd '/mnt/mokosz/home/kika/egracilis/PacBio/reads/'
 
 hififilt='hifiadapterfilt.sh'
-name='m21121_251126_103750'
+prefix='euglena_m21121_251212_153655.hifi_reads.bc2002'
 
-$hififilt -p $name -t 10 2> $name.filt.log
+$hififilt -p $prefix -t 10 2> $prefix.filt.log
 
 
 python3 /mnt/mokosz/home/kika/scripts/py_scripts/slackbot.py HiFiAdapterFilt done
