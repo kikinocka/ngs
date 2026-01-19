c08d5acfe60fab0a86680962f9117e9b230d1229
kikinocka <kika.zahonova@gmail.com>
2026-01-19 13:45:36 +0100

    bla

--- bash_scripts/metacentrum/braker.sh
+++ bash_scripts/metacentrum/braker.sh
@@ -16,28 +16,29 @@ export AUGUSTUS_CONFIG_PATH=$SCRATCHDIR/augustus_configs
 
 
 genome_dir='/storage/brno12-cerit/home/kika/paratrimastix/'
-rna_dir='/storage/brno12-cerit/home/kika/paratrimastix/RNA_reads'
+# rna_dir='/storage/brno12-cerit/home/kika/paratrimastix/RNA_reads'
 prot_dir='/storage/brno12-cerit/home/kika/databases/'
 
 #copy files to scratch
 cp $genome_dir'PaPyr_JAPMOS01.fna' $SCRATCHDIR
-cp $rna_dir'/'* $SCRATCHDIR
+# cp $rna_dir'/'* $SCRATCHDIR
 cp $prot_dir'metamonads.faa' $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
 
 genome=PaPyr_JAPMOS01.fna
-rna_ids=SRR651098,SRR651041,SRR33713718_1,SRR33713718_2
+rna_ids=SRR651098,SRR651041,SRR33713718
 # bam=
 prot=metamonads.faa
 name='paratrimastix_pyriformis'
 
 singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif braker.pl \
 	--genome=$genome \
-	--rnaseq_sets_ids=$rna_ids --rnaseq_sets_dir=$SCRATCHDIR \
+	--rnaseq_sets_ids=$rna_ids \
 	--prot_seq=$prot \
 	--species=$name \
+	--workingdir=$SCRATCHDIR \
 	--threads $PBS_NUM_PPN \
 	--gff3 
 
@@ -47,5 +48,5 @@ singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif br
 
 
 #copy files back
-rm $genome *.fastq $prot
-cp * $genome_dir'braker'
+rm $genome $prot
+cp -r * $genome_dir'braker'
