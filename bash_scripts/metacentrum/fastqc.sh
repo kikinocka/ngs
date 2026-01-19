cc93ac5e86e280cb363367e081572cf3e59ab78f
kikinocka <kika.zahonova@gmail.com>
2026-01-19 16:48:13 +0100

    bla

--- bash_scripts/metacentrum/fastqc.sh
+++ bash_scripts/metacentrum/fastqc.sh
@@ -10,11 +10,11 @@ cat $PBS_NODEFILE
 #add modules
 module load fastqc
 
-read_dir='/storage/brno12-cerit/home/kika/paratrimastix/RNA_reads'
+read_dir='/storage/brno12-cerit/home/kika/paratrimastix/RNA_reads/'
 
 
 #copy data to scratch
-cp $read_dir'/'* $SCRATCHDIR
+cp $read_dir'SRR651098_trimmed.fq.gz' $SCRATCHDIR
 
 
 #compute on scratch
