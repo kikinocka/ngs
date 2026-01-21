b7d4818df7338867a102f0e9ad311172ebca469d
kikinocka <kika.zahonova@gmail.com>
2026-01-21 12:33:26 +0100

    bla

--- bash_scripts/metacentrum/filter_bam.sh
+++ bash_scripts/metacentrum/filter_bam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N filter_bam
-#PBS -l select=1:ncpus=1:mem=5gb:scratch_local=1gb
+#PBS -l select=1:ncpus=1:mem=20gb:scratch_local=10gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
