8d0c313d632588cfdce25528e1b7f6e00b5bc639
kikinocka <kika.zahonova@gmail.com>
2026-01-21 12:03:34 +0100

    bla

--- bash_scripts/metacentrum/filter_bam.sh
+++ bash_scripts/metacentrum/filter_bam.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+#PBS -N filter_bam
+#PBS -l select=1:ncpus=1:mem=5gb:scratch_local=1gb
+#PBS -l walltime=02:00:00
+#PBS -m ae
+#PBS -j oe
 
-cd '/storage/brno12-cerit/home/kika/paratrimastix/hisat2/illumina/'
+cat $PBS_NODEFILE
 
+#add module
 module add python36-modules-gcc
 
+
+data='/storage/brno12-cerit/home/kika/paratrimastix/hisat2/illumina/'
 script='/storage/brno12-cerit/home/kika/scripts/py_scripts/filter_bam.py'
+
+#copy files to scratch
+cp $data'PaPyr_ht2_sorted.bam' $SCRATCHDIR
+cp $script $SCRATCHDIR
+
+
+#compute on scratch
+cd $SCRATCHDIR
+
 bam='PaPyr_ht2_sorted.bam'
 
 python3 $script $bam
+
+
+#copy files back
+rm $bam
+cp -r * $data
