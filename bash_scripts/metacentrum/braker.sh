5865533bbbae24dcce7d631d741a428f78702ec5
kikinocka <kika.zahonova@gmail.com>
2026-01-23 09:06:31 +0100

    bla

--- bash_scripts/metacentrum/braker.sh
+++ bash_scripts/metacentrum/braker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N braker
-#PBS -l select=1:ncpus=20:mem=70gb:scratch_local=50gb
+#PBS -l select=1:ncpus=20:mem=90gb:scratch_local=50gb
 #PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
