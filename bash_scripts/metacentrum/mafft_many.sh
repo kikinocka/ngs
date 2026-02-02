2d0dfc38daca32c7a3e561fe78c7cc4425dbf5c8
kikinocka <kika.zahonova@gmail.com>
2026-02-02 15:40:48 +0100

    bla

--- bash_scripts/metacentrum/mafft_many.sh
+++ bash_scripts/metacentrum/mafft_many.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #PBS -N mafft-many
-#PBS -l select=1:ncpus=20:mem=100gb:scratch_local=1gb
-#PBS -l walltime=96:00:00
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
+#PBS -l walltime=04:00:00
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
 #add module
-# module add mafft-7.453
-source /cvmfs/software.metacentrum.cz/modulefiles/5.1.0/loadmodules
 module load mafft
 
-data_dir='/storage/brno12-cerit/home/kika/sl_euglenozoa/v9/V9_DeepSea/apicomplexans'
+data_dir='/storage/brno12-cerit/home/kika/membrane-trafficking/tset_haptophytes'
 
 #copy files to scratch
 cp $data_dir'/'*.fa $SCRATCHDIR
@@ -23,8 +21,8 @@ cd $SCRATCHDIR
 for f in *fa ; do
 	aln=${f%.fa}.mafft.aln
 	log=${f%.fa}.mafft.log
-	# mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
-	mafft --thread $PBS_NUM_PPN --auto --inputorder ${f} > ${aln} 2> ${log}
+	mafft --thread $PBS_NUM_PPN --localpair --maxiterate 1000 --inputorder ${f} > ${aln} 2> ${log}
+	# mafft --thread $PBS_NUM_PPN --auto --inputorder ${f} > ${aln} 2> ${log}
 done
 
 
