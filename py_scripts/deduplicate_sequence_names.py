99eedb0b99eecf7f819a26dbce53c785420c9cdb
kikinocka <kika.zahonova@gmail.com>
2026-02-03 10:08:29 +0100

    bla

--- py_scripts/deduplicate_sequence_names.py
+++ py_scripts/deduplicate_sequence_names.py
@@ -4,19 +4,19 @@ from Bio import SeqIO
 from collections import OrderedDict
 from collections import defaultdict
 
-os.chdir('/Users/kika/Downloads/')
-files = [x for x in os.listdir() if x.endswith('Chrsim136_1_FilteredModels1_proteins_2025-02-25.fasta')]
+os.chdir('/Users/kika/ownCloud/membrane-trafficking/TSET_haptophytes/ver1/')
+files = [x for x in os.listdir() if x.endswith('.aln')]
 
 for infile in files:
 	print(infile)
-	with open('{}_deduplicated.fa'.format(infile.split('.aln')[0]), 'w') as out_fasta, \
+	with open('{}_deduplicated.aln'.format(infile.split('.aln')[0]), 'w') as out_fasta, \
 		 open('{}_dupl-names.txt'.format(infile.split('.aln')[0]), 'w') as out_names:
 		multiplications = defaultdict(list)
 		seq_dict = OrderedDict()
 		for sequence in SeqIO.parse(infile, 'fasta'):
-			multiplications[sequence.name.split('__')[0]].append(sequence.description)
-			if sequence.name.split('__')[0] not in seq_dict:
-				seq_dict[sequence.name.split('__')[0]] = [sequence.description, sequence.seq]
+			multiplications[sequence.name].append(sequence.description)
+			if sequence.name not in seq_dict:
+				seq_dict[sequence.name] = [sequence.description, sequence.seq]
 
 		for key, value in seq_dict.items():
 			out_fasta.write('>{}\n{}\n'.format(value[0], value[1]))
