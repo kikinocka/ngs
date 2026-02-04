1098bf14a230e9f64e99ff72657fdbf69d3c602a
kikinocka <kika.zahonova@gmail.com>
2026-02-04 18:03:56 +0100

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/quinones/trees/UbiG/ver6/')
+os.chdir('/Users/kika/ownCloud/quinones/trees/UbiG/ver8/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 # files = [x for x in os.listdir() if x.startswith('CAD2221027.2')]
 
