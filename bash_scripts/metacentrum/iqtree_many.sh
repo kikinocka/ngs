f5f4e706a51b00903ed46633e8dfb227e25d4ba9
kikinocka <kika.zahonova@gmail.com>
2026-02-11 21:11:07 +0100

    bla

--- bash_scripts/metacentrum/iqtree_many.sh
+++ bash_scripts/metacentrum/iqtree_many.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N IQT-many
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=10gb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=168:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,10 +10,10 @@ cat $PBS_NODEFILE
 #add module
 module load iqtree
 
-data_dir='/storage/brno12-cerit/home/kika/membrane-trafficking/tset_haptophytes/'
+data_dir=' meta:/storage/brno12-cerit/home/kika/membrane-trafficking/tset_haptophytes/ver2'
 
 #copy files to scratch
-cp $data_dir'small.trimal_gt-0.7.aln' $SCRATCHDIR
+cp $data_dir'/'*aln $SCRATCHDIR
 
 
 #compute on scratch
