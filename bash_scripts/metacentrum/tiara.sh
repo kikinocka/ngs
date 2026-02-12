a8ce4d9bb7481b0e76a85bf20e699309b753f650
kikinocka <kika.zahonova@gmail.com>
2026-02-12 16:07:56 +0100

    bla

---
+++ bash_scripts/metacentrum/tiara.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#PBS -N IQT
+#PBS -l select=1:ncpus=20:mem=20gb:scratch_local=50gb
+#PBS -l walltime=04:00:00
+#PBS -m ae
+#PBS -j oe
+
+cat $PBS_NODEFILE
+
+conda activate tiara_env
+
+datadir='/storage/brno12-cerit/home/kika/cz-au_fire/'
+
+#copy files to scratch
+cp $datadir'contigs_fixlabel.fasta' $SCRATCHDIR
+
+#compute on scratch
+cd $SCRATCHDIR
+
+metagenome='contigs_fixlabel.fasta'
+out='tiara.out'
+
+tiara -i $metagenome -o $out -t $PBS_NUM_PPN --tf all --pr
+# -m MIN_LEN, --min_len MIN_LEN
+#                       Minimum length of a sequence. Sequences shorter than min_len are discarded. 
+#                               Default: 3000.
+# --first_stage_kmer FIRST_STAGE_KMER, --k1 FIRST_STAGE_KMER
+#                       k-mer length used in the first stage of classification. Default: 6.
+# --second_stage_kmer SECOND_STAGE_KMER, --k2 SECOND_STAGE_KMER
+#                       k-mer length used in the second stage of classification. Default: 7.
+# -p cutoff [cutoff ...], --prob_cutoff cutoff [cutoff ...]
+#                       Probability threshold needed for classification to a class. 
+#                               If two floats are provided, the first is used in a first stage, the second in the second stage
+#                               Default: [0.65, 0.65].
+# --to_fasta class [class ...], --tf class [class ...]
+#                       Write sequences to fasta files specified in the arguments to this option.
+#                               The arguments are: mit - mitochondria, pla - plastid, bac - bacteria, 
+#                               arc - archaea, euk - eukarya, unk - unknown, pro - prokarya, 
+#                               all - all classes present in input fasta (to separate fasta files).
+# -t THREADS, --threads THREADS
+#                       Number of threads used.
+# --probabilities, --pr
+#                       Whether to write probabilities of individual classes for each sequence to the output.
+# -v, --verbose         Whether to display some additional messages and progress bar during classification.
+# --gzip, --gz          Whether to gzip results or not.
+conda deactivate
+
+#copy files back
+rm $metagenome
+cp -r * $datadir
+
