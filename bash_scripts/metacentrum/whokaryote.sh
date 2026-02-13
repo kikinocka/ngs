7b80457e0adb06fd65c28710ca486eeaef632311
kikinocka <kika.zahonova@gmail.com>
2026-02-13 14:03:05 +0100

    bla

--- bash_scripts/metacentrum/whokaryote.sh
+++ bash_scripts/metacentrum/whokaryote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N whokaryote
-#PBS -l select=1:ncpus=20:mem=100gb:scratch_local=80gb
+#PBS -l select=1:ncpus=20:mem=100gb:scratch_local=100gb
 #PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
