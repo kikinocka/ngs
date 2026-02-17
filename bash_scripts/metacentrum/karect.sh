2041ea0d8f724366f486c303d6cd2bba922ffbf7
kikinocka <kika.zahonova@gmail.com>
2026-02-17 11:23:43 +0100

    bla

--- bash_scripts/metacentrum/karect.sh
+++ bash_scripts/metacentrum/karect.sh
@@ -14,19 +14,20 @@ karect='/storage/brno12-cerit/home/kika/tools/karect/karect'
 read_dir='/storage/brno12-cerit/home/kika/pkld/trimmed_all'
 
 #copy data to scratch
-cp $read_dir'/'*fq.gz $SCRATCHDIR
+cp $read_dir'/'*fq $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-gzip -d *fq.gz
+# gzip -d *fq.gz
 
 for file in *_1.fq ; do 
       name=${file%_*.fq}
-      fw=$name'_1.fq'
-      rv=$name'_2.fq'
+      fwd=$name'_1.fq'
+      rev=$name'_2.fq'
       report=$name'_karect_report.txt'
-
+      echo $name
+      
       $karect -correct -threads=$PBS_NUM_PPN -matchtype=hamming -celltype=diploid \
             -inputfile=$fwd -inputfile=$rev 2> $report
 done
