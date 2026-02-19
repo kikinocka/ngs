ead7a2d4f8377ed0a5bbb3c3f821b3180703abd7
kikinocka <kika.zahonova@gmail.com>
2026-02-19 11:28:44 +0100

    bla

--- bash_scripts/metacentrum/metaeuk.sh
+++ bash_scripts/metacentrum/metaeuk.sh
@@ -10,17 +10,16 @@ cat $PBS_NODEFILE
 data_dir='/storage/brno12-cerit/home/kika/cz-au_fire/'
 metaeuk='/storage/brno3-cerit/home/kika/miniconda3/bin/metaeuk'
 db_dir='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/'
-# database='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/MMETSP_uniclust50_MERC'
 
 #copy files to scratch
-cp $data_dir'tiara/eukarya_contigs_fixlabel.fasta' $SCRATCHDIR
-cp $db_dir'MMETSP_uniclust50_MERC_profiles' $SCRATCHDIR
+cp $data_dir'whokaryote_out/eukaryotes.fasta' $SCRATCHDIR
+cp $db_dir'MMETSP_uniclust50_MERC' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-database='MMETSP_uniclust50_MERC_profiles'
-contigs='eukarya_contigs_fixlabel.fasta'
+database='MMETSP_uniclust50_MERC'
+contigs='eukaryotes.fasta'
 out='eukarya_metaeuk'
 
 $metaeuk easy-predict $contigs $database $out $SCRATCHDIR --threads $PBS_NUM_PPN
