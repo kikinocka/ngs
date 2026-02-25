2b9a525ae1462ce3b019f1deea41e2c1fbe90870
kikinocka <kika.zahonova@gmail.com>
2026-02-25 10:18:31 +0100

    bla

--- bash_scripts/metacentrum/metaeuk.sh
+++ bash_scripts/metacentrum/metaeuk.sh
@@ -9,16 +9,16 @@ cat $PBS_NODEFILE
 
 data_dir='/storage/brno12-cerit/home/kika/cz-au_fire/'
 metaeuk='/storage/brno3-cerit/home/kika/miniconda3/bin/metaeuk'
-db_dir='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC/'
+db_dir='/storage/brno3-cerit/home/kika/databases/MMETSP_uniclust50_MERC_OLD/'
 
 #copy files to scratch
 cp $data_dir'whokaryote_out/eukaryotes.fasta' $SCRATCHDIR
-cp $db_dir'MMETSP_uniclust50_MERC' $SCRATCHDIR
+cp $db_dir'MMETSP_uniclust50_MERC_profiles' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-database='MMETSP_uniclust50_MERC'
+database='MMETSP_uniclust50_MERC_profiles'
 contigs='eukaryotes.fasta'
 out='eukarya_metaeuk'
 
