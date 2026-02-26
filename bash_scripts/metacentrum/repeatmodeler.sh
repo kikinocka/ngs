42397d878e848dae01ff877b4ed0aa94c3527a2f
kikinocka <kika.zahonova@gmail.com>
2026-02-26 10:39:04 +0100

    bla

--- bash_scripts/metacentrum/repeatmodeler.sh
+++ bash_scripts/metacentrum/repeatmodeler.sh
@@ -19,18 +19,18 @@ genome='flye_assembly.pilon.remove_contaminants.260210.fasta'
 name=${genome%.pilon.remove_contaminants.260210.fasta}
 
 #Generate the RepeatModeler sequence database
-singularity run /cvmfs/singularity.metacentrum.cz/TE-Tools/TE-tools-1.4.sif BuildDatabase \
+singularity run -B $SCRATCHDIR /cvmfs/singularity.metacentrum.cz/TE-Tools/TE-tools-1.4.sif BuildDatabase \
 	-name $name $genome
 #Run RepeatModeler
-singularity run /cvmfs/singularity.metacentrum.cz/TE-Tools/TE-tools-1.4.sif RepeatModeler \
+singularity run -B $SCRATCHDIR /cvmfs/singularity.metacentrum.cz/TE-Tools/TE-tools-1.4.sif RepeatModeler \
 	-database $name -threads $PBS_NUM_PPN -LTRStruct
 #How many consensus sequences have been predicted?
 grep -c '>' $name'-families.fa'
 #How is the distribution of repeats by types?
 grep '>' $name'-families.fa' | sed -r 's/.+#//' | sed -r 's/\s+.+//' | sort | uniq -c > $name'.repeat_types.txt'
 #Run RepeatMasker - USE SOFT MASKING (-xsmall)
-singularity run /cvmfs/singularity.metacentrum.cz/TE-Tools/TE-tools-1.4.sif RepeatMasker \
-	-lib $name'-families.fa' -xsmall -pa 7 $genome
+singularity run -B $SCRATCHDIR /cvmfs/singularity.metacentrum.cz/TE-Tools/TE-tools-1.4.sif RepeatMasker \
+	-lib $name'-families.fa' -xsmall -pa $PBS_NUM_PPN $genome
 #Transform the .out file into a .gff file
 perl -ne 'BEGIN{ $l=0; print "##gff-version 3\n"} chomp($_); $l++; if ($l > 3) { $_ =~ s/^\s+//; @a=split(/\s+/, $_); $strand = $a[8]; if ($strand eq "C") { $strand = "-"; } @feats=("ID=RepeatMaskerHitId".$a[14], "Name=".$a[9], "Note=Family:".$a[9]."|Superfamily:".$a[10]."|ConsensusPercSNP:".$a[1]."|ConsensusPercDel:". $a[2]."|ConsensusPercIns:".$a[3]."|ConsensusHit:\"".$a[9]." ".$a[11]." ".$a[12]." ".$a[13]."\""); $featp=join(";", @feats); print "$a[4]\tRepeatMasker\tmatch\t$a[5]\t$a[6]\t$a[0]\t$strand\t$featp\n"} ' $genome'.out' > $name'.repeats_by_repeatmasker.gff'
 #Count the different types of repeats
