0eedd235742e561c41c3f4ea69bad9a3136b839b
kikinocka <kika.zahonova@gmail.com>
2026-02-27 11:19:27 +0100

    bla

--- bash_scripts/metacentrum/eukan.sh
+++ bash_scripts/metacentrum/eukan.sh
@@ -12,7 +12,7 @@ read_dir='/storage/brno12-cerit/home/kika/paratrimastix/RNA_reads'
 
 #copy files to scratch
 cp $genome_dir'flye_assembly.pilon.remove_contaminants.260210.fasta' $SCRATCHDIR
-cp $read_dir'/'*fq.gz $SCRATCHDIR
+cp $read_dir'/SRR33713718'*fq.gz $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
@@ -21,16 +21,17 @@ cd $SCRATCHDIR
 genome='flye_assembly.pilon.remove_contaminants.260210.fasta'
 fwd='SRR33713718_trimmed_1.fq.gz'
 rev='SRR33713718_trimmed_2.fq.gz'
-sg1='SRR651041_trimmed.fq.gz'
-sg2='SRR651098_trimmed.fq.gz'
+# sg1='SRR651041_trimmed.fq.gz'
+# sg2='SRR651098_trimmed.fq.gz'
 max_intron=10000
 
 singularity exec /storage/brno12-cerit/home/kika/tools/eukan.sif /bin/bash /opt/eukan/transcriptome_assembly.sh \
-	-l $fwd -r $rev -s $sg1,$sg2 \
+	-l $fwd -r $rev \
 	-g $genome \
 	-M $max_intron \
 	-A -T -P \
 	-n $PBS_NUM_PPN
+#-s $sg1,$sg2 
 
 #copy files back
 rm $genome $fwd $rev $sg1 $sg2
