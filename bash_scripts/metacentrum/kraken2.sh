68799f2661a15e746246e9e2642ff2f2b19b6f8b
kikinocka <kika.zahonova@gmail.com>
2026-02-27 11:53:02 +0100

    bla

--- bash_scripts/metacentrum/kraken2.sh
+++ bash_scripts/metacentrum/kraken2.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 #PBS -N kraken
-#PBS -l select=1:ncpus=20:mem=350gb:scratch_local=10gb
+#PBS -l select=1:ncpus=20:mem=900gb:scratch_local=10gb
 #PBS -l walltime=02:00:00
+#PBS -l cluster=turin
 #PBS -m ae
 #PBS -j oe
 
 cat $PBS_NODEFILE
 
-module add kraken2-1.2
+module load kraken2
 module add krona-2.8
 
-kraken2DB='/storage/brno3-cerit/home/kika/databases/kraken2DB-eukprot'
-datadir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
+kraken2DB='/storage/projects-du-praha/Bio_databases/kraken2/kraken2_nt_20240530'
+datadir='/storage/brno12-cerit/home/kika/pkld/'
 
 #copy files to scratch
-cp $datadir'1-reads/P1B_trimmed_1.fq.gz' $SCRATCHDIR
-cp $datadir'1-reads/P1B_trimmed_2.fq.gz' $SCRATCHDIR
+cp $datadir'trimmed_all/karect_Ag83_trimmed_1.fq.gz' $SCRATCHDIR
+cp $datadir'trimmed_all/karect_Ag83_trimmed_2.fq.gz' $SCRATCHDIR
 # cp $datadir'2-spades/scaffolds.fasta' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-fwd='P1B_trimmed_1.fq.gz'
-rev='P1B_trimmed_2.fq.gz'
-classified='P1B.classified#.fq'
-unclassified='P1B.unclassified#.fq'
+fwd='karect_Ag83_trimmed_1.fq.gz'
+rev='karect_Ag83_trimmed_2.fq.gz'
+classified='Ag83.classified#.fq'
+unclassified='Ag83.unclassified#.fq'
 # assembly='scaffolds.fasta'
 # classified='P1B.classified.fa'
 # unclassified='P1B.unclassified.fa'
-out='P1B.kraken.out'
-report='P1B.kraken.report'
-krona='P1B.kraken.html'
+out='Ag83.kraken.out'
+report='Ag83.kraken.report'
+krona='Ag83.krona.html'
 
 #on reads
 kraken2 --db $kraken2DB --threads $PBS_NUM_PPN \
@@ -51,4 +52,4 @@ ImportTaxonomy.pl -m $PBS_NUM_PPN -t 5 $report -o $krona
 #copy files back
 rm $fwd $rev
 # rm $assembly
-cp -R * $datadir'4b-kraken2-eukprot_reads/'
+cp -R * $datadir'kraken2/'
