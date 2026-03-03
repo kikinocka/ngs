f2922161569ff84a30c516f1918fcaed0320246c
kikinocka <kika.zahonova@gmail.com>
2026-03-03 14:15:03 +0100

    bla

--- bash_scripts/ostrava/spades.sh
+++ bash_scripts/ostrava/spades.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#PBS -d .
-#PBS -v PATH
-#PBS -N spades
-#PBS -l nodes=1:ppn=80
-#PBS -l walltime=100:00:00
+#SBATCH --job-name=spades
+#SBATCH --output=spades.%j.out
+#SBATCH --error=spades.%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=24:00:00
+#SBATCH --export=ALL
 
-
-work_dir='/mnt/data/kika/blastocrithidia/o_eliasi/spades_all_careful/'
-read_dir='/mnt/data/kika/blastocrithidia/o_eliasi/reads/'
-fwd=$read_dir'PNG74_trimmed_1.fq.gz'
-rev=$read_dir'PNG74_trimmed_2.fq.gz'
+work_dir='/home/kika/pkld/spades/'
+read_dir='/home/kika/pkld/trimmed_all/'
+fwd=$read_dir'BHU814_trimmed_1.fq.gz'
+rev=$read_dir'BHU814_trimmed_2.fq.gz'
 
 cd $work_dir
-spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 --careful -o $work_dir
-# spades.py --pe1-1 $fwd --pe1-2 $rev -t 40 -o $work_dir
-
-python3 /home/users/kika/scripts/py_scripts/slackbot.py OSU: SPAdes done
+conda run -n assembly spades.py \
+	--pe1-1 $fwd --pe1-2 $rev -t 40 --careful -o $work_dir
