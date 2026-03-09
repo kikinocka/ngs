b77627edcf8b494b606ecfedcc49ed0aca7abc04
kikinocka <kika.zahonova@gmail.com>
2026-03-09 15:02:22 +0100

    bla

--- bash_scripts/metacentrum/hisat2.sh
+++ bash_scripts/metacentrum/hisat2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N hisat2
-#PBS -l select=1:ncpus=20:mem=70gb:scratch_local=50gb
+#PBS -l select=1:ncpus=20:mem=50gb:scratch_local=10gb
 #PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
@@ -11,23 +11,23 @@ cat $PBS_NODEFILE
 module load hisat2
 module load samtools
 
-genome_dir='/storage/brno12-cerit/home/kika/paratrimastix/repeatmodeler_flye/'
-read_dir='/storage/brno12-cerit/home/kika/paratrimastix/RNA_reads'
-outdir='/storage/brno12-cerit/home/kika/paratrimastix/hisat2/all_flye_masked/'
+genome_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/'
+read_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/RNA_reads'
+outdir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni//hisat2/'
 
 #copy files to scratch
-cp $genome_dir'flye_assembly.pilon.remove_contaminants.260210.fasta.masked' $SCRATCHDIR
-cp $read_dir'/'*trimmed* $SCRATCHDIR
+cp $genome_dir'Tboi_masked.fna' $SCRATCHDIR
+cp $read_dir'/'*trimmed50* $SCRATCHDIR
 # cp $outdir'/'* $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
 
-genome='flye_assembly.pilon.remove_contaminants.260210.fasta.masked'
-fw='SRR33713718_trimmed_1.fq.gz'
-rv='SRR33713718_trimmed_2.fq.gz'
-sg='SRR651041_trimmed.fq.gz,SRR651098_trimmed.fq.gz'
-index='PaPyr_ht2'
+genome='Tboi_masked.fna'
+fw='Tboi_trimmed50_1.fq.gz'
+rv='Tboi_trimmed50_1.fq.gz'
+# sg='SRR651041_trimmed.fq.gz,SRR651098_trimmed.fq.gz'
+index='Tboi_ht2'
 unmapped_unpaired=$index'_unmapped_unpaired.fq.gz'
 unmapped_paired=$index'_unmapped_paired.fq.gz'
 sam=$index'.sam'
@@ -41,10 +41,10 @@ hisat2 -p $PBS_NUM_PPN -x $index \
 	--dta --secondary \
 	-1 $fw \
 	-2 $rv \
-	-U $sg \
 	--un-gz $unmapped_unpaired \
 	--un-conc-gz $unmapped_paired \
 	-S $sam 2> $report
+#-U $sg \
 
 samtools view -bS $sam > $bam -@ $PBS_NUM_PPN
 samtools sort -o $sorted -@ $PBS_NUM_PPN $bam 
