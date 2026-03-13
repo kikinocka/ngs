adbebc4a8390117cdb9b8aab0b0caa3b32548db8
kikinocka <kika.zahonova@gmail.com>
2026-03-13 08:58:43 +0100

    bla

--- py_scripts/run_program/mafft.py
+++ py_scripts/run_program/mafft.py
@@ -6,7 +6,7 @@ mafft = '/Users/kika/miniconda3/bin/mafft'
 maketable = '/Users/kika/miniconda3/bin/makemergetable.rb'
 
 #align de-novo
-os.chdir('/Users/kika/ownCloud/Lmaj_re-virulence/LmjF.34.2380/')
+os.chdir('/Users/kika/ownCloud/euglenozoa_mito/import/')
 files = [x for x in os.listdir() if x.endswith('.fa')]
 # files = [x for x in os.listdir() if x.startswith('CAD2221027.2')]
 
