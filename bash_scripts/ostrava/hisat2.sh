2ca916ff61f682056277c24d53ffb44320613975
kikinocka <kika.zahonova@gmail.com>
2026-03-16 17:10:44 +0100

    bla

--- bash_scripts/ostrava/hisat2.sh
+++ bash_scripts/ostrava/hisat2.sh
@@ -10,30 +10,33 @@
 cd '/home/kika/tboissoni/'
 
 genome='Tboi_masked.fna'
-fw='Tboi_trimmed50_1.fq.gz'
-rv='Tboi_trimmed50_2.fq.gz'
+# fw='Tboi_trimmed50_1.fq.gz'
+# rv='Tboi_trimmed50_2.fq.gz'
 base_name='tboi_ht2'
-unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
-unmapped_paired=$base_name'_unmapped_paired.fq.gz'
-sam=$base_name'.sam'
-report=$base_name'_report.txt'
-bam=$base_name'_unsorted.bam'
+# unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
+# unmapped_paired=$base_name'_unmapped_paired.fq.gz'
+# sam=$base_name'.sam'
+# report=$base_name'_report.txt'
+# bam=$base_name'_unsorted.bam'
 sorted=$base_name'_sorted.bam'
+view_report=$base_name'.view.txt'
 
-hisat2-build -p 50 $genome $base_name
-hisat2 -p 50 \
-	--very-sensitive \
-	--dta --secondary \
-	-x $base_name \
-	-1 $fw \
-	-2 $rv \
-	--un-gz $unmapped_unpaired \
-	--un-conc-gz $unmapped_paired \
-	-S $sam 2> $report
-#--dta 			reports alignments tailored for transcript assemblers
-#--secondary	reports secondary alignments
+# hisat2-build -p 50 $genome $base_name
+# hisat2 -p 50 \
+# 	--very-sensitive \
+# 	--dta --secondary \
+# 	-x $base_name \
+# 	-1 $fw \
+# 	-2 $rv \
+# 	--un-gz $unmapped_unpaired \
+# 	--un-conc-gz $unmapped_paired \
+# 	-S $sam 2> $report
+# #--dta 			reports alignments tailored for transcript assemblers
+# #--secondary	reports secondary alignments
 
-# samtools view -bS -F 4 $sam > $bam -@ 20 #writes only mapped reads to bamfile
-samtools view -bS -@ 50 $sam > $bam
-samtools sort -o $sorted -@ 50 $bam 
-samtools index $sorted
+# # samtools view -bS -F 4 $sam > $bam -@ 20 #writes only mapped reads to bamfile
+# samtools view -bS -@ 50 $sam > $bam
+# samtools sort -o $sorted -@ 50 $bam 
+# samtools index $sorted
+
+samtools tview --reference $genome $sorted > $view_report
