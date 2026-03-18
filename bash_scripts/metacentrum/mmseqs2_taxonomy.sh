034b32f647ef0aa54a989c515b32200be4b39e6c
kikinocka <kika.zahonova@gmail.com>
2026-03-18 15:25:33 +0100

    bla

--- bash_scripts/metacentrum/mmseqs2_taxonomy.sh
+++ bash_scripts/metacentrum/mmseqs2_taxonomy.sh
@@ -8,31 +8,31 @@
 cat $PBS_NODEFILE
 
 mmseqs='/storage/brno3-cerit/home/kika/miniconda3/bin/mmseqs'
-db_dir='/storage/brno3-cerit/home/kika/databases/eukprotDB'
-data_dir='/storage/brno3-cerit/home/kika/oil_sands/metagenomes/P1B_1-05C_L001-ds.ec8b691bd68b44deb59919ca3da275ba/'
+db_dir='/storage/brno12-cerit/home/kika/databases/eukprot_v3_DB/'
+data_dir='/storage/brno12-cerit/home/kika/cz-au_fire/'
 
 #copy files to scratch
-cp $db_dir'/'* $SCRATCHDIR
-cp $data_dir'5-metaeuk/euk_P1B_metaeuk.fas' $SCRATCHDIR
+cp $db_dir'eukprot_v3_DB'* $SCRATCHDIR
+cp $data_dir'3-metaeuk/eukarya_metaeuk.fas' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-database='eukprotDB'
-query='euk_P1B_metaeuk.fas'
-queryDB='euk_P1B_metaeukDB'
-taxonomy='euk_P1B_metaeuk.tax'
-taxres=$taxonomy'.tsv'
-report=$taxonomy'.html'
+database='eukprot_v3_DB'
+query='eukarya_metaeuk.fas'
+queryDB='eukarya_metaeuk'
+taxonomy='eukarya_metaeuk.tax'
+table=$taxonomy'.tsv'
+krona=$taxonomy'.html'
 
 $mmseqs createdb $query $queryDB
 
 $mmseqs taxonomy $queryDB $database $taxonomy tmp --threads $PBS_NUM_PPN
-$mmseqs createtsv $queryDB $taxonomy $taxres --threads $PBS_NUM_PPN
-$mmseqs taxonomyreport $database $taxonomy $report --report-mode 1 --threads $PBS_NUM_PPN
+$mmseqs createtsv $queryDB $taxonomy $table --threads $PBS_NUM_PPN
+$mmseqs taxonomyreport $database $taxonomy $krona --report-mode 1 --threads $PBS_NUM_PPN
 #--report-mode INT   Taxonomy report mode 0: Kraken 1: Krona [0]
 
 #copy files back
-rm -r $query tmp eukprotDB*
-cp -r * $data_dir'6-mmseqs2'
+rm -r $query $database* tmp
+cp -r * $data_dir'4-mmseqs2'
