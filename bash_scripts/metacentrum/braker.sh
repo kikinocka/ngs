cfdf2f94a46a6b9100faf40552940a6af8c1d3f9
kikinocka <kika.zahonova@gmail.com>
2026-03-19 17:34:22 +0100

    bla

--- bash_scripts/metacentrum/braker.sh
+++ bash_scripts/metacentrum/braker.sh
@@ -16,27 +16,26 @@ export AUGUSTUS_CONFIG_PATH=$SCRATCHDIR/augustus_configs
 
 
 genome_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/'
-# map_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/hisat2/'
+map_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/hisat2/'
 # rna_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/RNA_reads/'
 prot_dir='/storage/brno12-cerit/home/kika/databases/'
 
 #copy files to scratch
 cp $genome_dir'Tboi_masked.fna' $SCRATCHDIR
-# cp $map_dir'Tboi_ht2_sorted.pass.bam' $SCRATCHDIR
-# cp $rna_dir'Tboi_trimmed50'* $SCRATCHDIR
-cp $prot_dir'Eukaryota_odb12.fa' $SCRATCHDIR
+cp $map_dir'Tboi_ht2_sorted.pass.bam' $SCRATCHDIR
+cp $prot_dir'kinetoplastids.faa' $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
 
 genome='Tboi_masked.fna'
-# bam='Tboi_ht2_sorted.pass.bam'
-# reads='Tboi_trimmed50_1,Tboi_trimmed50_2'
-prot='Eukaryota_odb12.fa'
+bam='Tboi_ht2_sorted.pass.bam'
+prot='kinetoplastids.faa'
 name='trypanosoma_boissoni'
 
 singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif braker.pl \
 	--genome=$genome \
+	--bam=$bam \
 	--prot_seq=$prot \
 	--species=$name \
 	--workingdir=$SCRATCHDIR \
@@ -49,6 +48,6 @@ singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif br
 
 
 #copy files back
-rm $genome $prot #$bam *fq.gz
+rm $genome $prot $bam #*fq.gz
 rm -r augustus_configs
-cp -r * $genome_dir'braker/'
+cp -r * $genome_dir'braker/braker_kinetoplastids_RNA/'
--- bash_scripts/ostrava/hisat2.sh
+++ bash_scripts/ostrava/hisat2.sh
@@ -4,39 +4,39 @@
 #SBATCH --error=hisat2.%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=50
-#SBATCH --time=100:00:00
+#SBATCH --time=02:00:00
 #SBATCH --export=ALL
 
 cd '/home/kika/tboissoni/'
 
 genome='Tboi_masked.fna'
-# fw='Tboi_trimmed50_1.fq.gz'
-# rv='Tboi_trimmed50_2.fq.gz'
+fw='Tboi_trimmed50_1.fq.gz'
+rv='Tboi_trimmed50_2.fq.gz'
 base_name='Tboi_ht2'
-# unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
-# unmapped_paired=$base_name'_unmapped_paired.fq.gz'
-# sam=$base_name'.sam'
-# report=$base_name'_report.txt'
-# bam=$base_name'_unsorted.bam'
+unmapped_unpaired=$base_name'_unmapped_unpaired.fq.gz'
+unmapped_paired=$base_name'_unmapped_paired.fq.gz'
+sam=$base_name'.sam'
+report=$base_name'_report.txt'
+bam=$base_name'_unsorted.bam'
 sorted=$base_name'_sorted.bam'
-view_report=$base_name'.view.txt'
 
-# hisat2-build -p 50 $genome $base_name
-# hisat2 -p 50 \
-# 	--very-sensitive \
-# 	--dta --secondary \
-# 	-x $base_name \
-# 	-1 $fw \
-# 	-2 $rv \
-# 	--un-gz $unmapped_unpaired \
-# 	--un-conc-gz $unmapped_paired \
-# 	-S $sam 2> $report
-# #--dta 			reports alignments tailored for transcript assemblers
-# #--secondary	reports secondary alignments
+hisat2-build -p 50 $genome $base_name
+hisat2 -p 50 \
+	--very-sensitive \
+	--dta --secondary \
+	-x $base_name \
+	-1 $fw \
+	-2 $rv \
+	--un-gz $unmapped_unpaired \
+	--un-conc-gz $unmapped_paired \
+	-S $sam 2> $report
+#--dta 			reports alignments tailored for transcript assemblers
+#--secondary	reports secondary alignments
 
-# # samtools view -bS -F 4 $sam > $bam -@ 20 #writes only mapped reads to bamfile
-# samtools view -bS -@ 50 $sam > $bam
-# samtools sort -o $sorted -@ 50 $bam 
-# samtools index $sorted
+# samtools view -bS -F 4 $sam > $bam -@ 20 #writes only mapped reads to bamfile
+samtools view -bS -@ 50 $sam > $bam
+samtools sort -o $sorted -@ 50 $bam 
+samtools index $sorted
 
-samtools tview --reference $genome $sorted > $view_report
+# #has to be run interactively
+# samtools tview --reference $genome $sorted
