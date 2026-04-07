703da99dc9429b81a683a5c8b9733ad696c59086
kikinocka <kika.zahonova@gmail.com>
2026-04-07 15:48:31 +0200

    bla

--- bash_scripts/metacentrum/busco5.sh
+++ bash_scripts/metacentrum/busco5.sh
@@ -15,38 +15,38 @@ conda activate busco
 # #available datasets
 # busco --list-datasets
 
-assembly_dir='/storage/brno12-cerit/home/kika/fish_tryps'
+assembly_dir='/storage/brno12-cerit/home/kika/blastocystis/braker/'
 
 #copy files to scratch
-cp $assembly_dir'/'*.faa $SCRATCHDIR
+cp $assembly_dir'braker.aa' $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
 
-# mkdir BUSCO_summaries
+mkdir BUSCO_summaries
 
-for fasta in *.faa; do
+for fasta in *.aa; do
 	echo $fasta
 	mode='proteins'
 	# mode='genome'
 	# mode='transcriptome'
 	
 	lineage='eukaryota_odb10'
-	base=${fasta%.fa}_$lineage
+	base=${fasta%.aa}_$lineage
 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	# cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
+	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
 
-	lineage='euglenozoa_odb10'
-	base=${fasta%.fa}_$lineage
+	lineage='stramenopiles_odb10'
+	base=${fasta%.aa}_$lineage
 	busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	# # cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
+	cp $base'/short_summary.specific.'$base'.txt' BUSCO_summaries
 done
 
-# generate_plot.py -wd BUSCO_summaries
+generate_plot.py -wd BUSCO_summaries
 mamba deactivate
 
 
 #copy files back
-rm *.faa
+rm *.aa
 cp -r * $assembly_dir && clean_scratch
