75a86604e04fc2b734be29799d0c1c7864acd41f
kikinocka <kika.zahonova@gmail.com>
2026-04-07 14:59:36 +0200

    bla

--- bash_scripts/metacentrum/mafft.sh
+++ bash_scripts/metacentrum/mafft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N mafft
 #PBS -l select=1:ncpus=20:mem=20gb:scratch_local=1gb
-#PBS -l walltime=04:00:00
+#PBS -l walltime=02:00:00
 #PBS -m ae
 #PBS -j oe
 
@@ -10,10 +10,10 @@ cat $PBS_NODEFILE
 #add module
 module load mafft
 
-data_dir='/storage/brno12-cerit/home/kika/euglenozoa_mito/'
+data_dir='/storage/brno12-cerit/home/kika/membrane-trafficking/tset_haptophytes/rhodophytes/ver2/'
 
 #copy files to scratch
-cp $data_dir'Pam17-PF08566.fa' $SCRATCHDIR
+cp $data_dir'large-beta.fa' $SCRATCHDIR
 
 #compute on scratch
 cd $SCRATCHDIR
