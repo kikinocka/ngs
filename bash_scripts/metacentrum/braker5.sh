c7545ebb5aee4df6073a12ee92ce8f6ea4a763ba
kikinocka <kika.zahonova@gmail.com>
2026-04-08 15:07:56 +0200

    bla

--- bash_scripts/metacentrum/braker5.sh
+++ bash_scripts/metacentrum/braker5.sh
@@ -15,23 +15,23 @@ cp -r $AUGUSTUS_CONFIG_PATH/* $SCRATCHDIR/augustus_configs/
 export AUGUSTUS_CONFIG_PATH=$SCRATCHDIR/augustus_configs
 
 
-genome_dir='/storage/brno12-cerit/home/kika/blastocystis/ASM2924171v1/'
+genome_dir='/storage/brno12-cerit/home/kika/blastocystis/BRZ_BR__eukbin11/'
 # map_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/hisat2/'
 # rna_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/RNA_reads/'
 prot_dir='/storage/brno12-cerit/home/kika/databases/'
 
 #copy files to scratch
-cp $genome_dir'GCA_029241715.1_ASM2924171v1_genomic.fna' $SCRATCHDIR
+cp $genome_dir'GCA_944391175.1_BRZ_BR_eukbin11_genomic.fna' $SCRATCHDIR
 # cp $map_dir'Tboi_ht2_sorted.pass.bam' $SCRATCHDIR
 cp $prot_dir'Stramenopiles.fa' $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
 
-genome='GCA_029241715.1_ASM2924171v1_genomic.fna'
+genome='GCA_944391175.1_BRZ_BR_eukbin11_genomic.fna'
 # bam='Tboi_ht2_sorted.pass.bam'
 prot='Stramenopiles.fa'
-name='blastocystis_CHMP1T05'
+name='blastocystis_eukbin11'
 
 singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif braker.pl \
 	--genome=$genome \
