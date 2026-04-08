cd4f9f98ff94ad86c5b4c3d2e0d480592db1bfde
kikinocka <kika.zahonova@gmail.com>
2026-04-08 13:48:09 +0200

    bla

--- bash_scripts/metacentrum/braker3.sh
+++ bash_scripts/metacentrum/braker3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N braker3
-#PBS -l select=1:ncpus=20:mem=50gb:scratch_local=50gb
+#PBS -l select=1:ncpus=20:mem=30gb:scratch_local=50gb
 #PBS -l walltime=24:00:00
 #PBS -m ae
 #PBS -j oe
@@ -15,23 +15,23 @@ cp -r $AUGUSTUS_CONFIG_PATH/* $SCRATCHDIR/augustus_configs/
 export AUGUSTUS_CONFIG_PATH=$SCRATCHDIR/augustus_configs
 
 
-genome_dir='/storage/brno12-cerit/home/kika/blastocystis/ASM2924109v1/'
+genome_dir='/storage/brno12-cerit/home/kika/blastocystis/ASM2924124v1/'
 # map_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/hisat2/'
 # rna_dir='/storage/brno12-cerit/home/kika/trypanosoma_boissoni/RNA_reads/'
 prot_dir='/storage/brno12-cerit/home/kika/databases/'
 
 #copy files to scratch
-cp $genome_dir'GCA_029241095.1_ASM2924109v1_genomic.fna' $SCRATCHDIR
+cp $genome_dir'GCA_029241245.1_ASM2924124v1_genomic.fna' $SCRATCHDIR
 # cp $map_dir'Tboi_ht2_sorted.pass.bam' $SCRATCHDIR
 cp $prot_dir'Stramenopiles.fa' $SCRATCHDIR
 
 #run on scratch
 cd $SCRATCHDIR
 
-genome='GCA_029241095.1_ASM2924109v1_genomic.fna'
+genome='GCA_029241245.1_ASM2924124v1_genomic.fna'
 # bam='Tboi_ht2_sorted.pass.bam'
 prot='Stramenopiles.fa'
-name='blastocystis_CHMP2T04'
+name='blastocystis_CHMP1T31'
 
 singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif braker.pl \
 	--genome=$genome \
@@ -49,4 +49,4 @@ singularity exec /cvmfs/singularity.metacentrum.cz/Braker/braker3-v.3.0.8.sif br
 #copy files back
 rm $genome $prot #$bam *fq.gz
 rm -r augustus_configs
-cp -r * $genome_dir'braker/'
+cp -r * $genome_dir'braker/' && clean_scratch
