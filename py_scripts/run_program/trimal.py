b5e11d33faf32afec428d3eb62a97228b47ba35b
kikinocka <kika.zahonova@gmail.com>
2026-04-16 15:22:12 +0200

    bla

--- py_scripts/run_program/trimal.py
+++ py_scripts/run_program/trimal.py
@@ -3,7 +3,7 @@ import os
 
 trimal = '/Users/kika/miniconda3/bin/trimal'
 
-os.chdir('/Users/kika/ownCloud/kinetoplastids/base_J/JBP2_SNF2_origin/tree/ver2/')
+os.chdir('/Users/kika/ownCloud/kinetoplastids/base_J/JBP2_SNF2_origin/tree/ver3/')
 files = [x for x in os.listdir() if x.endswith('.mafft.aln')]
 # files = [x for x in os.listdir() if x.endswith('.einsi.aln')]
 
