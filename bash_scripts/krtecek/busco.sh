c2bfff953fb922c5ee71ef828b71c364d2e7a411
kikinocka <kika.zahonova@gmail.com>
2026-05-04 18:26:53 +0200

    bla

--- bash_scripts/krtecek/busco.sh
+++ bash_scripts/krtecek/busco.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+#SBATCH --job-name=busco
+#SBATCH --output=busco.%j.out
+#SBATCH --error=busco.%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=02:00:00
+#SBATCH --export=ALL
+export OMP_NUM_THREADS=$CPU_count
+
 source activate BUSCO
 
-cd '/home/users/kika/blastocystis/'
+cd '/home/users/kika/trimastix/'
 mkdir BUSCO_summaries
 
-for fasta in *.faa; do
+for fasta in *.fasta; do
 	echo $fasta
 	mode='proteins'
 	# mode='genome'
 	# mode='transcriptome'
 	
-	# lineage='eukaryota_odb12'
-	# base=${fasta%.faa}_$lineage
-	# busco -i $fasta -l $lineage -o $base -m $mode -c $PBS_NUM_PPN
-	# cp $base'/short_summary.'*'.json' BUSCO_summaries
-
-	lineage='stramenopiles_odb12'
-	base=${fasta%.faa}_$lineage
-	busco -i $fasta -l $lineage -o $base -m $mode -c 5
+	lineage='eukaryota_odb12'
+	base=${fasta%.fasta}_$lineage
+	busco -i $fasta -l $lineage -o $base -m $mode -c $CPU_count
 	cp $base'/short_summary.'*'.json' BUSCO_summaries
+
+	# lineage='stramenopiles_odb12'
+	# base=${fasta%.fasta}_$lineage
+	# busco -i $fasta -l $lineage -o $base -m $mode -c $CPU_count
+	# cp $base'/short_summary.'*'.json' BUSCO_summaries
 done
 
 busco --plot BUSCO_summaries --plot_percentages
