4cdcc0413d77de398ff5b16c6aa98c12b22be6ee
kikinocka <kika.zahonova@gmail.com>
2026-05-04 18:32:37 +0200

    bla

--- bash_scripts/krtecek/busco.sh
+++ bash_scripts/krtecek/busco.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=busco
-#SBATCH --output=busco.%j.out
-#SBATCH --error=busco.%j.err
+#SBATCH --output=busco.out
+#SBATCH --error=busco.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --time=02:00:00
 #SBATCH --export=ALL
 export OMP_NUM_THREADS=$CPU_count
 
-source activate BUSCO
+# #activate in terminal, then run script, then deactivate
+# conda activate BUSCO
 
 cd '/home/users/kika/trimastix/'
 mkdir BUSCO_summaries
@@ -32,4 +33,4 @@ done
 
 busco --plot BUSCO_summaries --plot_percentages
 
-conda deactivate
+# conda deactivate
