eb20b573c235f48b1729f98b98ad97e0faa9f83d
kikinocka <kika.zahonova@gmail.com>
2026-05-14 14:50:11 +0200

    bla

--- bash_scripts/metacentrum/bbmap_rpkm.sh
+++ bash_scripts/metacentrum/bbmap_rpkm.sh
@@ -8,37 +8,35 @@
 cat $PBS_NODEFILE
 
 #add module
-module add bbmap-36.92
+module load bbmap
 
-assembly_dir='/storage/brno12-cerit/home/kika/p57/transcriptome/'
-read_dir='/storage/brno12-cerit/home/kika/p57/hisat2/'
+datadir='/storage/brno12-cerit/home/kika/kinetoplastids/AOX/transcriptomics/adean/'
 
 #copy files to scratch
-cp $assembly_dir'p57_GG_trinity.fasta' $SCRATCHDIR
-cp $read_dir'p57_trimmed_1.fq.gz' $SCRATCHDIR
-cp $read_dir'p57_trimmed_2.fq.gz' $SCRATCHDIR
+cp $datadir'TriTrypDB-68_AdeanaiCavalhoATCCPRA-265_AnnotatedCDSs.fasta' $SCRATCHDIR
+cp $datadir'wt/'*fq.gz $SCRATCHDIR
 
 
 #compute on scratch
 cd $SCRATCHDIR
-assembly='p57_GG_trinity.fasta'
-fw='p57_trimmed_1.fq.gz'
-rv='p57_trimmed_2.fq.gz'
+assembly='TriTrypDB-68_AdeanaiCavalhoATCCPRA-265_AnnotatedCDSs.fasta'
+# fw='p57_trimmed_1.fq.gz'
+# rv='p57_trimmed_2.fq.gz'
 # all='el_reads_new.fa'
 
-base=${assembly%fasta}ht2.
+base=adean.
 sam=$base'sam'
 rpkm=$base'rpkm_bbmap.tsv'
 report=$base'report_bbmap.txt'
 
-#separate read files
-bbmap.sh in=$fw in2=$rv out=$sam ref=$assembly rpkm=$rpkm threads=$PBS_NUM_PPN 2> $report
+# #separate read files
+# bbmap.sh ref=$assembly in=$fw in2=$rv out=$sam rpkm=$rpkm threads=$PBS_NUM_PPN 2> $report
 
-#one read file
-# bbmap.sh in=$all out=$sam ref=$assembly rpkm=$rpkm threads=$PBS_NUM_PPN 2> $report
+one read file
+bbmap.sh ref=$assembly in=*fq.gz out=$sam rpkm=$rpkm threads=$PBS_NUM_PPN 2> $report
 
 
 #copy files back
-rm $assembly $fw $rv
-# rm $assembly $all
-cp -r * $read_dir
+# rm $assembly $fw $rv
+rm $assembly *fq.gz
+cp -r * $datadir
