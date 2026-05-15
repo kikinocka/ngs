4d54b13984fdcda5ea7baa74a169076dff7bf4bb
kikinocka <kika.zahonova@gmail.com>
2026-05-15 13:14:02 +0200

    bla

--- bash_scripts/metacentrum/bbduk.sh
+++ bash_scripts/metacentrum/bbduk.sh
@@ -9,7 +9,7 @@ cat $PBS_NODEFILE
 
 module load bbmap
 
-read_dir='/storage/brno12-cerit/home/kika/kinetoplastids/AOX/transcriptomics/tbruc/cBF'
+read_dir='/storage/brno12-cerit/home/kika/kinetoplastids/AOX/transcriptomics/tbruc/slBF'
 
 #copy data to scratch
 cp $read_dir'/'*.fastq.gz $SCRATCHDIR
