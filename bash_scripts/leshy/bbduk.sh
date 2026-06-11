8b445707f5da4d443e7ee244118164f2623b1e7b
kikinocka <kika.zahonova@gmail.com>
2026-06-11 15:59:38 +0200

    bla

--- bash_scripts/leshy/bbduk.sh
+++ bash_scripts/leshy/bbduk.sh
@@ -3,7 +3,7 @@
 bbduk='/mnt/mokosz/home/kika/tools/bbmap/bbduk.sh'
 adapt='/mnt/mokosz/home/kika/tools/bbmap/resources/adapters.fa'
 
-cd '/mnt/mokosz/home/kika/mastigamoeba_abducta_CHOM1/reads/'
+cd '/mnt/mokosz/home/kika/egracilis/chinese/RNA_reads/'
 
 for file in *_1.fastq.gz ; do 
 	name=${file%_*.fastq.gz}
--- bash_scripts/quast.sh
+++ bash_scripts/quast.sh
@@ -2,8 +2,8 @@
 
 quast='/Users/kika/miniconda3/bin/quast.py'
 
-cd '/Users/kika/ownCloud/Euglena_gracilis/genome/hifi/'
-assembly='EG_hifi.asm_telomere.p_ctg.fa'
-output='quast/telomere/'
+cd '/Users/kika/Downloads/'
+assembly='GCA_039621445.1_ASM3962144v1_genomic.fna'
+output='quast/'
 
 python $quast --eukaryote -o $output -t 6 $assembly
